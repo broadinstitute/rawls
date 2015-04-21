8d7195296d6f8fbd8c955bf30afa415a07c702fa
Matt Sooknah <msooknah@broadinstitute.org>
2015-04-21 16:06:51 -0400

    Fixed that for me

--- docker/install.sh
+++ docker/install.sh
@@ -5,6 +5,6 @@ set -e
 RAWLS_DIR=$1
 cd $RAWLS_DIR
 sbt assembly
-SNOOP_JAR=$(find target | grep 'rawls.*\.jar')
-mv $RAWLS_DIR .
+RAWLS_JAR=$(find target | grep 'rawls.*\.jar')
+mv RAWLS_JAR .
 sbt clean
