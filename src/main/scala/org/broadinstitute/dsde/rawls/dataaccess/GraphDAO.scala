bfbe73f7ac8b951642c990883b9e293515a1f3f1
Matt Sooknah <msooknah@broadinstitute.org>
2015-05-15 16:27:58 -0400

    PR comments + attribute serialization

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphDAO.scala
@@ -7,7 +7,21 @@ import com.tinkerpop.pipes.PipeFunction
 import scala.collection.JavaConversions._
 
 trait GraphDAO {
-  val db: Graph
+
+  implicit def toPipeFunction[A, B](f: A => B) = new PipeFunction[A, B] {
+    override def compute(a: A): B = f(a)
+  }
+
+  def isReservedProperty(key: String) = key.startsWith("_")
+
+  def getSinglePipelineResult[T](pipeline: GremlinPipeline[_, T]): Option[T] = {
+    // Calling count() is destructive, so we first pop off the head, then check for more
+    val first = pipeline.headOption
+    if (pipeline.count() > 0) throw new IllegalStateException("Expected at most one result, but got multiple")
+    first
+  }
+
+  // named PipeFunctions
 
   def hasProperty[T](key: String, value: T) = new PipeFunction[Vertex, java.lang.Boolean] {
     override def compute(v: Vertex) = v.getProperty[T](key).equals(value)
@@ -18,33 +32,23 @@ trait GraphDAO {
     override def compute(v: Vertex) = props.map(p => v.getProperty(p._1).equals(p._2)).reduce(_&&_)
   }
 
-  implicit def toPipeFunction[A, B](f: A => B) = new PipeFunction[A, B] {
-    override def compute(a: A): B = f(a)
+  // named GremlinPipelines
+
+  def workspacePipeline(db: Graph, workspaceNamespace: String, workspaceName: String) = {
+    new GremlinPipeline(db).V("_clazz", "workspace").filter(hasProperties(Map("_namespace" -> workspaceNamespace, "_name" -> workspaceName)))
   }
 
-  def isReservedProperty(key: String) = key.startsWith("_")
-
-  def getWorkspaceVertex(namespace: String, name: String): Option[Vertex] = {
-    def workspaceFunc = hasProperties(Map("_namespace" -> namespace, "_name" -> name))
-    val verticesRemaining = new GremlinPipeline(db).V("_clazz", "workspace").filter(workspaceFunc)
-    // NB: since GremlinPipelines are like iterators, count() is destructive. So first we pop off the head,
-    // NB: then if anything is left over it's a duplicate.
-    val workspaceVertex = verticesRemaining.headOption
-    if (verticesRemaining.count() > 0) {
-      throw new IllegalStateException("Found more than one workspace at " + namespace + "::" + name)
-    }
-    workspaceVertex
+  def entityPipeline(db: Graph, workspaceNamespace: String, workspaceName: String, entityType: String, entityName: String) = {
+    workspacePipeline(db, workspaceNamespace, workspaceName).out(entityType).filter(hasProperty("_name", entityName))
   }
 
-  def getEntityVertex(workspaceNamespace: String, workspaceName: String, entityType: String, entityName: String): Option[Vertex] = {
-    def entityFunc = hasProperty("_name", entityName)
-    getWorkspaceVertex(workspaceNamespace, workspaceName).flatMap(w => {
-      val verticesRemaining = new GremlinPipeline(w).out(entityType).filter(entityFunc)
-      val entityVertex = verticesRemaining.headOption
-      if (verticesRemaining.count() > 0) {
-        throw new IllegalStateException("Found more than one entity with type " + entityType + " and name " + entityName)
-      }
-      entityVertex
-    })
+  // convenience getters
+
+  def getWorkspaceVertex(db: Graph, workspaceNamespace: String, workspaceName: String) = {
+    getSinglePipelineResult(workspacePipeline(db, workspaceNamespace, workspaceName))
+  }
+
+  def getEntityVertex(db: Graph, workspaceNamespace: String, workspaceName: String, entityType: String, entityName: String) = {
+    getSinglePipelineResult(entityPipeline(db, workspaceNamespace, workspaceName, entityType, entityName))
   }
 }
--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphWorkspaceDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphWorkspaceDAO.scala
@@ -1,40 +1,53 @@
 package org.broadinstitute.dsde.rawls.dataaccess
 
-import com.tinkerpop.blueprints.{Vertex, Direction, Graph}
+import java.util.Date
+
+import com.tinkerpop.blueprints.{Direction, Graph, Vertex}
 import com.tinkerpop.gremlin.java.GremlinPipeline
-import org.broadinstitute.dsde.rawls.model.{WorkspaceShort, Workspace}
+import org.broadinstitute.dsde.rawls.model.{Entity, Workspace, WorkspaceShort}
+import org.joda.time.DateTime
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
-class GraphWorkspaceDAO(backingDatabase: Graph) extends WorkspaceDAO with GraphDAO {
-  val db = backingDatabase
+class GraphWorkspaceDAO(db: Graph) extends WorkspaceDAO with GraphDAO {
+
+  implicit def toJodaTime(d: Date): DateTime = new DateTime(d)
+
+  private def toWorkspaceShort(v: Vertex) = {
+    WorkspaceShort(
+      v.getProperty("_namespace"),
+      v.getProperty("_name"),
+      v.getProperty[Date]("_createdDate"),
+      v.getProperty("_createdBy")
+    )
+  }
+
+  private def workspaceFromShort(ws: WorkspaceShort, entities: Map[String, Map[String, Entity]]) = {
+    Workspace(ws.namespace, ws.name, ws.createdDate, ws.createdBy, entities)
+  }
 
   def save(workspace: Workspace) = {
-    val workspaceVertex = getWorkspaceVertex(workspace.namespace, workspace.name).getOrElse({
-      val created = backingDatabase.addVertex(null)
+    val workspaceVertex = getWorkspaceVertex(db, workspace.namespace, workspace.name).getOrElse({
+      val created = db.addVertex(null)
       created.setProperty("_name", workspace.name)
       created.setProperty("_namespace", workspace.namespace)
       created.setProperty("_clazz", "workspace")
-      created.setProperty("_createdDate", workspace.createdDate)
+      created.setProperty("_createdDate", workspace.createdDate.toDate) // OrientDB only supports java.util.Date
       created.setProperty("_createdBy", workspace.createdBy)
       created
     })
 
-    val entityDAO = new GraphEntityDAO(backingDatabase)
+    val entityDAO = new GraphEntityDAO(db)
     workspace.entities.foreach(e => e._2.foreach(f => entityDAO.save(workspace.namespace, workspace.name, f._2)))
   }
 
   def load(namespace: String, name: String): Option[Workspace] = {
-    val workspaceVertex = getWorkspaceVertex(namespace, name)
+    val workspaceVertex = getWorkspaceVertex(db, namespace, name)
     // for now, assume that all edges coming out of workspace vertex are entity types.
-    val entityDAO = new GraphEntityDAO(backingDatabase)
-    workspaceVertex.map(v => Workspace(
-      v.getProperty("_namespace"),
-      v.getProperty("_name"),
-      v.getProperty("_createdDate"),
-      v.getProperty("_createdBy"),
-      // here we go...
+    val entityDAO = new GraphEntityDAO(db)
+    workspaceVertex.map(v => workspaceFromShort(
+      toWorkspaceShort(v),
       v.getEdges(Direction.OUT).map(
         entityTypeEdge =>
           entityTypeEdge.getLabel ->
@@ -47,22 +60,10 @@ class GraphWorkspaceDAO(backingDatabase: Graph) extends WorkspaceDAO with GraphD
   }
 
   def loadShort(namespace: String, name: String): Option[WorkspaceShort] = {
-    val workspaceVertex = getWorkspaceVertex(namespace, name)
-    workspaceVertex.map(v => WorkspaceShort(
-      v.getProperty("_namespace"),
-      v.getProperty("_name"),
-      v.getProperty("_createdDate"),
-      v.getProperty("_createdBy")
-    ))
+    getWorkspaceVertex(db, namespace, name).map(toWorkspaceShort(_))
   }
 
   def list(): Seq[WorkspaceShort] = {
-    def shortFunc(v: Vertex) = WorkspaceShort(
-      v.getProperty("_namespace"),
-      v.getProperty("_name"),
-      v.getProperty("_createdDate"),
-      v.getProperty("_createdBy")
-    )
-    new GremlinPipeline(backingDatabase).V("_clazz", "workspace").transform((v: Vertex) => shortFunc(v)).toList.asScala
+    new GremlinPipeline(db).V("_clazz", "workspace").transform((v: Vertex) => toWorkspaceShort(v)).toList.asScala
   }
 }
