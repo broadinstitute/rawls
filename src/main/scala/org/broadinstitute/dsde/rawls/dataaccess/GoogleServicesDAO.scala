29de7ae292c90e3a40954de3541ff06461fbfa10
Joel Thibault <thibault@broadinstitute.org>
2015-11-02 15:30:35 -0500

    import cleanup

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GoogleServicesDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GoogleServicesDAO.scala
@@ -1,13 +1,11 @@
 package org.broadinstitute.dsde.rawls.dataaccess
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException
-import com.google.api.services.admin.directory.model.Group
 import org.broadinstitute.dsde.rawls.model.WorkspaceAccessLevels._
 import org.broadinstitute.dsde.rawls.model.{ErrorReport, WorkspacePermissionsPair, UserInfo, WorkspaceACLUpdate, WorkspaceACL, WorkspaceName}
 import org.joda.time.DateTime
 import spray.http.StatusCodes
 import scala.concurrent.Future
-import scala.util.Try
 
 trait GoogleServicesDAO {
 
