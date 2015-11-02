a4140ccd49678c28ba8568c5d65a4a1d61d14704
Joel Thibault <thibault@broadinstitute.org>
2015-11-02 16:37:09 -0500

    deleteGroup and deleteWorkspaceAccessGroups - Move groupAccessLevelsAscending to WorkspaceAccessLevels

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/AuthDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/AuthDAO.scala
@@ -8,7 +8,15 @@ trait AuthDAO {
 
   def saveGroup(rawlsGroup: RawlsGroup, txn: RawlsTransaction): RawlsGroup
 
+  def deleteGroup(rawlsGroup: RawlsGroupRef, txn: RawlsTransaction)
+
   def createWorkspaceAccessGroups(workspaceName: WorkspaceName, userInfo: UserInfo, txn: RawlsTransaction): Map[WorkspaceAccessLevel, RawlsGroupRef]
+
+  def deleteWorkspaceAccessGroups(workspace: Workspace, txn: RawlsTransaction) = {
+    workspace.accessLevels foreach { case (_, group) =>
+      deleteGroup(group, txn)
+    }
+  }
 
   def getMaximumAccessLevel(userSubjectId: String, workspaceId: String, txn: RawlsTransaction): WorkspaceAccessLevel
 
