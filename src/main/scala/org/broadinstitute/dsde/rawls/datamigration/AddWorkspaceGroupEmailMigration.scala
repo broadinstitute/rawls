e547186d5d228c1b035a237af6554a076bfe31dd
Douglas Voet <dvoet@broadinstitute.org>
2016-01-21 23:25:55 -0500

    data migration infrastructure and workspace group email migration

---
+++ src/main/scala/org/broadinstitute/dsde/rawls/datamigration/AddWorkspaceGroupEmailMigration.scala
@@ -0,0 +1,28 @@
+package org.broadinstitute.dsde.rawls.datamigration
+
+import com.typesafe.config.Config
+import org.broadinstitute.dsde.rawls.dataaccess.{RawlsTransaction, GraphContainerDAO}
+import org.broadinstitute.dsde.rawls.model.RawlsGroupEmail
+import org.broadinstitute.dsde.rawls.model.WorkspaceAccessLevels.WorkspaceAccessLevel
+
+/**
+ * Populates missing rawls group email addresses for workspace access groups
+ */
+object AddWorkspaceGroupEmailMigration extends DataMigration {
+  override val migrationId: String = "AddWorkspaceGroupEmailMigration"
+
+  override def migrate(config: Config, containerDAO: GraphContainerDAO, txn: RawlsTransaction): Unit = {
+    val appsDomain = config.getString("gcs.appsDomain")
+    def toGroupId(bucketName: String, accessLevel: WorkspaceAccessLevel) = s"${bucketName}-${accessLevel.toString}@${appsDomain}"
+
+    val accessGroups = for (
+      workspace <- containerDAO.workspaceDAO.list(txn);
+      (accessLevel, accessGroupRef) <- workspace.accessLevels;
+      accessGroup <- containerDAO.authDAO.loadGroup(accessGroupRef, txn)
+      if (accessGroup.groupEmail == RawlsGroupEmail(""))
+    ) {
+      containerDAO.authDAO.saveGroup(accessGroup.copy(groupEmail = RawlsGroupEmail(toGroupId(workspace.bucketName, accessLevel))), txn)
+    }
+  }
+
+}
