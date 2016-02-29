2cc271464e94590a3217c5490d348c262482b097
Matt Bemis <mbemis@broadinstitute.org>
2016-02-29 11:06:32 -0500

    return NoAccess when user is not a part of required realm

--- src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphAuthDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/GraphAuthDAO.scala
@@ -74,12 +74,48 @@ class GraphAuthDAO extends AuthDAO with GraphDAO {
   }
 
   override def getMaximumAccessLevel(user: RawlsUserRef, workspaceId: String, txn: RawlsTransaction): WorkspaceAccessLevel = {
-    val workspaces = listWorkspaces(user, isTargetWorkspace(workspaceId), txn)
+    val workspaces = getWorkspaceAccessLevels(user, isTargetWorkspace(workspaceId), txn)
     workspaces.find(_.workspaceId == workspaceId).map(_.accessLevel).getOrElse(WorkspaceAccessLevels.NoAccess)
   }
 
   override def listWorkspaces(user: RawlsUserRef, txn: RawlsTransaction): Seq[WorkspacePermissionsPair] = {
     listWorkspaces(user, isVertexOfClass(VertexSchema.Workspace), txn)
+  }
+
+  private def getWorkspaceAccessLevels(user: RawlsUserRef, loopEmitFn: PipeFunction[Vertex, java.lang.Boolean], txn: RawlsTransaction): Seq[WorkspacePermissionsPair] = {
+    txn withGraph { db =>
+
+      //Returns a list of paths starting with a group the user belongs in and ending with the workspace's access map.
+      // user -> group -> ...some other groups... -> group -> workspace access map
+      //Multiple paths implies that the user belongs to multiple groups associated with the workspace.
+      val accessMapPaths =
+        userPipeline(db, user).as("vtx").in()
+          .loop(
+            "vtx", //start point of loop
+            or(isVertexOfClass(VertexSchema.Group), isVertexOfClass(VertexSchema.Map)), //loop condition: walk while vertex is on an acl path (group or a list which looks like map)
+            loopEmitFn)
+          .enablePath.path()
+
+      //The last 3 elements of each path are the group then the workspace access map then the workspace.
+      //The edge label between the group and the workspace access map corresponds to the access level for the group.
+      //It is possible for there to be more than 1 access level for a user for a workspace
+      //Group the paths by workspace then take the max access level for each workspace
+      val accessByWorkspace = accessMapPaths.toList.groupBy(path => loadObject[Workspace](path.last.asInstanceOf[Vertex]))
+      val accessLevels = accessByWorkspace map { case (workspace, paths) =>
+        paths.foldLeft(WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.NoAccess))({ (maxAccessLevel, path) =>
+          val reversePath = path.reverseIterator
+          reversePath.next() // remove workspace vertex
+          val accessMap = reversePath.next().asInstanceOf[Vertex]
+          val group = reversePath.next().asInstanceOf[Vertex]
+
+          val accessLabel = accessMap.getEdges(Direction.OUT).filter(_.getVertex(Direction.IN) == group).head.getLabel
+          val accessLevel = WorkspaceAccessLevels.withName(EdgeSchema.stripEdgeRelation(accessLabel))
+
+          WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.max(accessLevel, maxAccessLevel.accessLevel))
+        })
+      }
+      accessLevels.toSeq
+    }
   }
 
   private def listWorkspaces(user: RawlsUserRef, loopEmitFn: PipeFunction[Vertex, java.lang.Boolean], txn: RawlsTransaction): Seq[WorkspacePermissionsPair] = {
@@ -100,18 +136,29 @@ class GraphAuthDAO extends AuthDAO with GraphDAO {
       //The edge label between the group and the workspace access map corresponds to the access level for the group.
       //It is possible for there to be more than 1 access level for a user for a workspace
       //Group the paths by workspace then take the max access level for each workspace
-      val accessByWorkspace = accessMapPaths.toList.groupBy(path => loadObject[Workspace](path.last.asInstanceOf[Vertex]).workspaceId)
-      val accessLevels = accessByWorkspace map { case (workspaceId, paths) =>
-        paths.foldLeft(WorkspacePermissionsPair(workspaceId, WorkspaceAccessLevels.NoAccess))({ (maxAccessLevel, path) =>
+      val accessByWorkspace = accessMapPaths.toList.groupBy(path => loadObject[Workspace](path.last.asInstanceOf[Vertex]))
+      val accessLevels = accessByWorkspace map { case (workspace, paths) =>
+        paths.foldLeft(WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.NoAccess))({ (maxAccessLevel, path) =>
           val reversePath = path.reverseIterator
           reversePath.next() // remove workspace vertex
           val accessMap = reversePath.next().asInstanceOf[Vertex]
           val group = reversePath.next().asInstanceOf[Vertex]
 
-          val accessLabel = accessMap.getEdges(Direction.OUT).filter( _.getVertex(Direction.IN) == group ).head.getLabel
+          val accessLabel = accessMap.getEdges(Direction.OUT).filter(_.getVertex(Direction.IN) == group).head.getLabel
           val accessLevel = WorkspaceAccessLevels.withName(EdgeSchema.stripEdgeRelation(accessLabel))
 
-          WorkspacePermissionsPair(workspaceId, WorkspaceAccessLevels.max(accessLevel, maxAccessLevel.accessLevel))
+          //the logic works as follows: if the workspace has no realm, then we proceed as usual
+          //if the workspace has a realm, we check to see if the user is in that realm group. if they aren't return NoAccess
+          //if they are in the realm group, return their normal access level
+          workspace.realm match {
+            case None => WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.max(accessLevel, maxAccessLevel.accessLevel))
+            case Some(realmGroup) => {
+              loadGroupIfMember(realmGroup, user, txn) match {
+                case None => WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.NoAccess)
+                case Some(_) => WorkspacePermissionsPair(workspace.workspaceId, WorkspaceAccessLevels.max(accessLevel, maxAccessLevel.accessLevel))
+              }
+            }
+          }
         })
       }
       accessLevels.toSeq
