d0a907c063787a455ba72e07ed41dbb63c4c2f14
helgridly <hussein@broadinstitute.org>
2016-05-16 14:45:47 -0400

    wip

--- src/main/scala/org/broadinstitute/dsde/rawls/jobexec/WorkflowSubmissionActor.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/jobexec/WorkflowSubmissionActor.scala
@@ -8,10 +8,10 @@ import com.google.api.client.auth.oauth2.Credential
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.rawls.RawlsException
 import org.broadinstitute.dsde.rawls.dataaccess.{GoogleServicesDAO, SlickWorkspaceContext, ExecutionServiceDAO, SlickDataSource}
-import org.broadinstitute.dsde.rawls.dataaccess.slick.{ReadAction, WorkspaceRecord, DataAccess, WorkflowRecord}
+import org.broadinstitute.dsde.rawls.dataaccess.slick._
 import org.broadinstitute.dsde.rawls.jobexec.WorkflowSubmissionActor._
 import org.broadinstitute.dsde.rawls.model._
-import org.broadinstitute.dsde.rawls.util.FutureSupport
+import org.broadinstitute.dsde.rawls.util.{MethodWiths, FutureSupport}
 import slick.dbio.DBIOAction
 import spray.http.OAuth2BearerToken
 import spray.json._
@@ -100,13 +100,6 @@ trait WorkflowSubmission extends FutureSupport with LazyLogging with MethodWiths
     }
   }
 
-  //futurey utility function to take a bunch of workflow records and turn them into real grownup workflows
-  def loadWorkflows(wfRecs: Seq[WorkflowRecord]): Future[Seq[Option[Workflow]]] = {
-    datasource.inTransaction { dataAccess =>
-      DBIO.sequence( wfRecs.map(dataAccess.workflowQuery.loadWorkflow) )
-    }
-  }
-
   def buildWorkflowOpts(workspace: WorkspaceRecord, submissionId: UUID, user: RawlsUser, token: String, billingProject: RawlsBillingProject) = {
     ExecutionServiceWorkflowOptions(
       s"gs://${workspace.bucketName}/${submissionId}",
@@ -117,6 +110,14 @@ trait WorkflowSubmission extends FutureSupport with LazyLogging with MethodWiths
     )
   }
 
+  def getWdl(methodConfig: MethodConfiguration): ReadWriteAction[String] = {
+    withMethod(methodConfig.methodRepoMethod.methodNamespace, methodConfig.methodRepoMethod.methodName, methodConfig.methodRepoMethod.methodVersion, getUserInfo(credential)) { method =>
+      withWdl(method) { wdl =>
+        DBIO.successful(wdl)
+      }
+    }
+  }
+
   //submit the batch of workflows with the given ids
   def submitWorkflowBatch(workflowIds: Seq[Long]): WorkflowSubmissionMessage = {
     //pull out the records, but don't reify them into workflows yet as we need their submission ID
@@ -125,17 +126,16 @@ trait WorkflowSubmission extends FutureSupport with LazyLogging with MethodWiths
     }
 
     datasource.inTransaction { dataAccess =>
-
-      for {
+      val oop = for {
         //Load a bunch of things we'll need to reconstruct information:
         //The list of workflows in this submission
         wfRecs <- dataAccess.workflowQuery.findWorkflowByIds(workflowIds).result
         submissionId = wfRecs.head.submissionId
         assert(wfRecs.forall( _.submissionId == submissionId)) //sanity check they're all in the same submission
-        unpackedWfOpts <- loadWorkflows(wfRecs) //reify to real workflows
+        unpackedWfOpts <- DBIO.sequence( wfRecs.map(dataAccess.workflowQuery.loadWorkflow) ) //reify to real workflows
         assert(unpackedWfOpts.size == wfRecs.size) //there should be the right number of them
         assert(unpackedWfOpts.forall(_.isDefined)) //and they should all have been found
-        unpackedWfs <- for { wfOpt <- unpackedWfOpts; wf <- wfOpt } yield { wf }
+        unpackedWfs = for { wfOpt <- unpackedWfOpts; wf <- wfOpt } yield { wf }
 
         //The submission itself
         subRecs <- dataAccess.submissionQuery.findById(submissionId).result
@@ -155,38 +155,14 @@ trait WorkflowSubmission extends FutureSupport with LazyLogging with MethodWiths
         tokenOpt <- DBIO.from(googleServicesDAO.getToken(submitter))
         token <- tokenOpt
 
-        //The method configuration
+        //The wdl
         mcOpt <- dataAccess.methodConfigurationQuery.loadMethodConfigurationById(submissionRec.methodConfigurationId)
         methodConfig <- mcOpt
-
-        execSvcWorkflowOpts = buildWorkflowOpts(workspaceRec, submissionId, submitter, token, billingProject )
-
-
-
-
+        wdl <- getWdl(methodConfig)
       } yield {
 
-      }
+        val wfOpts = buildWorkflowOpts(workspaceRec, submissionId, submitter, token, billingProject)
 
-    }
-
-    //this terrifying monster constructs the workflow options and the list of reified workflows
-    val submitThingsFuture = for {
-      wfRecs <- wfRecsFuture //get the records
-      assert(wfRecs.forall( _.submissionId == wfRecs.head.submissionId)) //sanity check they're all in the same submission
-      execSvcWfOpts <- buildWorkflowOptions(wfRecs.head.submissionId) //build the workflow opts
-      unpackedWfs <- loadWorkflows(wfRecs) //reify to real workflows
-      assert(unpackedWfs.size == wfRecs.size) //there should be the right number of them
-      assert(unpackedWfs.forall(_.isDefined)) //and they should all have been found
-      toSubmit = unpackedWfs.map(_.get) //un-option them too
-    } yield {
-      (execSvcWfOpts, toSubmit)
-    }
-
-    //now we have a future of workflow options and the workflows themselves...
-    submitThingsFuture map {
-      case (wfOpts: ExecutionServiceWorkflowOptions, unpackedWfs: Seq[Workflow]) =>
-        //make the list of workflow inputs
         val wfInputsBatch = unpackedWfs map { wf =>
           val methodProps = wf.inputResolutions map {
             case svv: SubmissionValidationValue if svv.value.isDefined =>
@@ -195,54 +171,16 @@ trait WorkflowSubmission extends FutureSupport with LazyLogging with MethodWiths
           MethodConfigResolver.propertiesToWdlInputs(methodProps.toMap)
         }
         import ExecutionJsonSupport.ExecutionServiceWorkflowOptionsFormat
-        executionServiceDAO.submitWorkflows("WHERE'S THE WDL?", wfInputsBatch, Option(wfOpts.toJson.toString), getUserInfo(credential))
-
-        //TODO: if cromwell submission fails, set workflow to Failed rather than WorkflowFailure
-
-
-    }
-    LookForWorkflows
-  }
-
-  //Returns a tuple of some weird things we need later.
-  //these are: the user associated with the submission, the submission's workspace, and its billing project
-  def getSomeThings(submissionId: UUID): Future[(Option[RawlsUser], Option[WorkspaceRecord], RawlsBillingProject)] = {
-    datasource.inTransaction { dataAccess =>
-      for {
-        subRec <- dataAccess.submissionQuery.findById(submissionId).result
-        userOpt <- dataAccess.rawlsUserQuery.load(RawlsUserRef(RawlsUserSubjectId(subRec.head.submitterId)))
-        wsOpt <- dataAccess.workspaceQuery.findByIdQuery(subRec.head.workspaceId).result
-        workspace <- wsOpt
-        billingProject <- dataAccess.rawlsBillingProjectQuery.load(RawlsBillingProjectName(workspace.namespace))
-      } yield {
-        (userOpt, wsOpt.headOption, billingProject)
+        executionServiceDAO.submitWorkflows(wdl, wfInputsBatch, Option(wfOpts.toJson.toString), getUserInfo(credential))
+        //TODO: what now?
       }
+
+      oop
+
     }
-  }
 
-  def buildWorkflowOptions(submissionId: UUID, dataAccess: DataAccess): ReadAction[ExecutionServiceWorkflowOptions] = {
-    for {
-      subRec <- dataAccess.submissionQuery.findById(submissionId).result
-      userOpt <- dataAccess.rawlsUserQuery.load(RawlsUserRef(RawlsUserSubjectId(subRec.head.submitterId)))
-      wsOpt <- dataAccess.workspaceQuery.findByIdQuery(subRec.head.workspaceId).result
-
-      user <- userOpt
-      workspace <- wsOpt
-
-      bpOpt <- dataAccess.rawlsBillingProjectQuery.load(RawlsBillingProjectName(workspace.namespace))
-      billingProject <- bpOpt
-
-      tokenOpt <- DBIO.from(googleServicesDAO.getToken(user))
-      token <- tokenOpt
-    } yield {
-      ExecutionServiceWorkflowOptions(
-        s"gs://${workspace.bucketName}/${submissionId}",
-        workspace.namespace,
-        user.userEmail.value,
-        token,
-        billingProject.cromwellAuthBucketUrl
-      )
-    }
+    //TODO: if cromwell submission fails, set workflow to Failed rather than WorkflowFailure
+    LookForWorkflows
   }
 
   //FIXME: I lifted this from the bottom of SubmissionMonitorActor. Maybe it should be centralised?
