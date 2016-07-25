ce220b34741f05b5524fea6599a96039de79cb83
Joel Thibault <thibault@broadinstitute.org>
2016-07-25 16:27:04 -0400

    Move getUserInfo to UserInfo and note caveats

--- src/main/scala/org/broadinstitute/dsde/rawls/model/UserInfo.scala
+++ src/main/scala/org/broadinstitute/dsde/rawls/model/UserInfo.scala
@@ -1,8 +1,21 @@
 package org.broadinstitute.dsde.rawls.model
 
+import com.google.api.client.auth.oauth2.Credential
 import spray.http.OAuth2BearerToken
 
 /**
  * Created by dvoet on 7/21/15.
  */
 case class UserInfo(userEmail: String, accessToken: OAuth2BearerToken, accessTokenExpiresIn: Long, userSubjectId: String)
+
+object UserInfo {
+  // TODO: handle failure cases
+  def buildFromTokens(credential: Credential): UserInfo = {
+    val expiresInSeconds: Long = Option(credential.getExpiresInSeconds).map(_.toLong).getOrElse(0)
+    // TODO: alternate expiration test
+    if (expiresInSeconds <= 5*60) {
+      credential.refreshToken()
+    }
+    UserInfo("", OAuth2BearerToken(credential.getAccessToken), Option(credential.getExpiresInSeconds).map(_.toLong).getOrElse(0), "")
+  }
+}
