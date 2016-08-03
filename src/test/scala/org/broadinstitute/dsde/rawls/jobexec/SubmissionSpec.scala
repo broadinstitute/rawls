f0ca32fe354c9a101287b15450eb2c9d73447b5c
ansingh7115 <singh.an@husky.neu.edu>
2016-08-03 11:05:18 -0400

    GAWB-645 more changes

--- src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
@@ -254,7 +254,6 @@ class SubmissionSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpe
     val monitorActor = waitForSubmissionActor(newSubmissionReport.submissionId)
     assert(monitorActor != None) //not really necessary, failing to find the actor above will throw an exception and thus fail this test
 
-    assert(newSubmissionReport.notstarted.size == 0)
     assert(newSubmissionReport.workflows.size == 1)
 
     checkSubmissionStatus(workspaceService, newSubmissionReport.submissionId)
@@ -280,7 +279,6 @@ class SubmissionSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpe
       status
     }
 
-    assert( newSubmissionReport.notstarted.size == 0 )
     assert( newSubmissionReport.workflows.size == 6 )
 
     checkSubmissionStatus(workspaceService, newSubmissionReport.submissionId)
@@ -325,7 +323,6 @@ class SubmissionSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpe
       status
     }
 
-    assert( newSubmissionReport.notstarted.size == 3 )
     assert( newSubmissionReport.workflows.size == 0 )
 
     checkSubmissionStatus(workspaceService, newSubmissionReport.submissionId)
@@ -339,7 +336,6 @@ class SubmissionSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpe
       status
     }
 
-    assert( newSubmissionReport.notstarted.size == 1 )
     assert( newSubmissionReport.workflows.size == 2 )
 
     checkSubmissionStatus(workspaceService, newSubmissionReport.submissionId)
@@ -380,14 +376,13 @@ class SubmissionSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpe
     }
 
     // Only the workflow with the dodgy expression (sample.tumortype on a normal) should fail
-    assert(submissionStatusResponse.notstarted.size == 1)
-    assert(submissionStatusResponse.workflows.size == 2)
-
-    // the first fails to start for bad expression issues
-    assert(submissionStatusResponse.notstarted.head.errors.head.value == "Expected single value for workflow input, but evaluated result set was empty")
+    assert(submissionStatusResponse.workflows.size == 3)
 
     // the rest are in queued
     assert( submissionStatusResponse.workflows.forall(_.status == WorkflowStatuses.Queued) )
+    assert( submissionStatusResponse.workflows.forall(_.status == WorkflowStatuses.Failed) )
+    assert( submissionStatusResponse.workflows.forall(_.status == WorkflowStatuses.Submitted) )
+    assert( submissionStatusResponse.workflows.forall(_.status == WorkflowStatuses.Succeeded) )
   }
 
   "Submission validation requests" should "report a BadRequest for an unparseable entity expression" in withWorkspaceService { workspaceService =>
