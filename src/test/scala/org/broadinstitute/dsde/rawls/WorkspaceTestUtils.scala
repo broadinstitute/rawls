3d0dcda7dadcd928d4a519d400e87662860e268f
Greg Rushton <grushton@broadinstitute.org>
2016-11-10 12:36:21 -0500

    doc update

--- src/test/scala/org/broadinstitute/dsde/rawls/WorkspaceTestUtils.scala
+++ src/test/scala/org/broadinstitute/dsde/rawls/WorkspaceTestUtils.scala
@@ -16,12 +16,19 @@ trait WorkspaceTestUtils extends TestDriverComponentWithFlatSpecAndMatchers {
     }
   }
 
+  /**
+    * The following three methods are needed when comparing a workspace expected vs actual value in the test framework.
+    * In the case of a function that results in an update to the last modified date, the original expected value will
+    * be different from the actual value only in that field. We need this version of assertion to ignore that field
+    * and proceed in checking the equivalence of all other components of the two workspaces.
+    */
   def assertWorkspaceResult(expected: Workspace)(actual: Workspace) = {
     assertResult(expected) {
       actual.copy(lastModified = expected.lastModified)
     }
   }
 
+  // Option flavor of above
   def assertWorkspaceResult(expected: Option[Workspace])(actual: Option[Workspace]) = {
     expected match {
       case None => throw new TestFailedException(Some("Unable to parse expected value: " + expected.toString), None, 4)
@@ -32,6 +39,7 @@ trait WorkspaceTestUtils extends TestDriverComponentWithFlatSpecAndMatchers {
     }
   }
 
+  // Seq flavor of above
   def assertWorkspaceResult(expected: Seq[Workspace])(actual: Seq[Workspace]) = {
     expected zip actual foreach {
       case (e, a) =>
