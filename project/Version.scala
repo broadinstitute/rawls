4e207c30da88190324b18ae8d5a988296c8c857a
Hussein Elgridly <hussein@broadinstitute.org>
2016-12-01 14:46:25 -0500

    passing in git model hash from outside the container in jenkinsland

--- project/Version.scala
+++ project/Version.scala
@@ -5,7 +5,12 @@ object Version {
   val baseModelVersion = "0.1"
 
   def getVersionString(baseDir: File) = {
-    val lastModelCommit = (s"""git log -n 1 --pretty=format:%h ${baseDir.getAbsolutePath}""" !!).trim
+    def getLastModelCommitFromGit = { s"""git log -n 1 --pretty=format:%h ${baseDir.getAbsolutePath}""" !! }
+
+    //jenkins builds will pass the last model commit in as an env variable because we currently build rawls
+    //inside a docker container that doesn't know the code is a git repo.
+    //normal "developer" builds should be building from inside the rawls git repo so we can fall back to git if the env isn't specified.
+    val lastModelCommit = sys.env.getOrElse("GIT_MODEL_HASH", getLastModelCommitFromGit ).trim()
     val version = baseModelVersion + "-" + lastModelCommit
 
     // The project isSnapshot string passed in via command line settings, if desired.
