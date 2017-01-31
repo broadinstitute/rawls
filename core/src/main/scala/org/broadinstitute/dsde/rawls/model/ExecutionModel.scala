d65e142c97b000d8cba2f98f02f69f3f95d38a97
ansingh7115 <singh.an@husky.neu.edu>
2017-01-31 12:01:30 -0500

    GAWB-1139: Import statement magic

--- core/src/main/scala/org/broadinstitute/dsde/rawls/model/ExecutionModel.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/model/ExecutionModel.scala
@@ -7,10 +7,6 @@ import org.broadinstitute.dsde.rawls.model.ExecutionJsonSupport.OutputType
 import org.broadinstitute.dsde.rawls.model.SubmissionStatuses.SubmissionStatus
 import org.broadinstitute.dsde.rawls.model.UserModelJsonSupport.RawlsUserRefFormat
 import org.broadinstitute.dsde.rawls.model.WorkflowStatuses.WorkflowStatus
-import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport.AttributeNameFormat
-import UserModelJsonSupport.RawlsUserRefFormat
-
-import spray.json._
 import org.joda.time.DateTime
 import spray.json._
 
