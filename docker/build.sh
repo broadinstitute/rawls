510649ae06d2c40a4b022a9d0d78228451467ee0
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-02-27 12:02:14 -0500

    fix docker running check/mysql validation

--- docker/build.sh
+++ docker/build.sh
@@ -6,18 +6,23 @@
 set -eux
 
 # check if mysql is running
-mysql_running=$(docker inspect -f {{.State.Running}} mysql)
-if ! $mysql_running ; then
+CONTAINER=mysql
+RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER || echo "false")
+
+# mysql set-up
+if ! $RUNNING; then
+    # force remove mysql in case it is stopped
+    echo "attempting to remove old mysql container..."
+    docker rm -f $CONTAINER || echo "docker rm failed. nothing to rm."
+
     # start up mysql
-    docker pull mysql/mysql-server:5.7.15
-    docker run --name mysql -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:5.7.15
+    echo "starting up mysql container..."
+    docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:5.7.15
 
     # validate mysql
-    sudo yum -y install mysql
-    sleep 5
-    mysql -uroot -prawls-test --host=127.0.0.1 --port=3310 -e "SELECT VERSION();SELECT NOW()"
-    mysql -urawls-test -prawls-test --host=127.0.0.1 --port=3310 -e "SELECT VERSION();SELECT NOW()"
-    mysql -urawls-test -prawls-test --host=127.0.0.1 --port=3310 -e "SELECT VERSION();SELECT NOW()" testdb
+    echo "running mysql validation..."
+    docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
+    echo "mysql validation succeeded."
 fi
 
 # Get the last commit hash of the model directory and set it as an environment variable
