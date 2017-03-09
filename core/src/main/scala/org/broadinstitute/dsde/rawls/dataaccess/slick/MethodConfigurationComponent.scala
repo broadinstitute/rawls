d28ad6399e8b2737f8329e039072195c516bbdd4
Joel Thibault <thibault@broadinstitute.org>
2017-03-09 11:16:27 -0500

    Add comments to MC methods

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/MethodConfigurationComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/MethodConfigurationComponent.scala
@@ -191,10 +191,12 @@ trait MethodConfigurationComponent {
       }
     }
 
+    // includes "deleted" MCs
     def list(workspaceContext: SlickWorkspaceContext): ReadAction[Seq[MethodConfigurationShort]] = {
       findByWorkspace(workspaceContext.workspaceId).result.map(recs => recs.map(rec => unmarshalMethodConfigToShort(rec)))
     }
 
+    // standard listing: does not include "deleted" MCs
     def listActive(workspaceContext: SlickWorkspaceContext): ReadAction[Seq[MethodConfigurationShort]] = {
       findActiveByWorkspace(workspaceContext.workspaceId).result.map(recs => recs.map(rec => unmarshalMethodConfigToShort(rec)))
     }
