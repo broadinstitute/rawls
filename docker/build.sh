d6ece63d0e31082eeb9d555f2b0b24f0491d6cf1
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-03-20 15:17:19 -0400

    expand build script to use cmd line options

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 
-# Run tests and make jar
-# Uses docker.
+# Single source of truth for building Rawls.
+# @ Jackie Roberti
+#
+# Provide command line options to do one or several things:
+#   make : build rawls jar
+#   publish : run rawls/core/src/bin/publishSnapshot.sh to publish to Artifactory
+#   -d | --docker : provide arg either "build" or "push", to build and push docker image
+# Jenkins build job should run with all options, for example,
+#   ./docker/build.sh jar publish -d push
 
-set -eux
+set -ex
 
-bash ./docker/run-mysql.sh
+function make_jar()
+{
+    echo "building jar..."
+    bash ./docker/run-mysql.sh
 
-# Get the last commit hash of the model directory and set it as an environment variable
-GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h model)
+    # Get the last commit hash of the model directory and set it as an environment variable
+    GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h model)
 
-# make jar.  cache sbt dependencies.
-docker run --rm --link mysql:mysql -e GIT_MODEL_HASH=$GIT_MODEL_HASH -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
+    # make jar.  cache sbt dependencies.
+    docker run --rm --link mysql:mysql -e GIT_MODEL_HASH=$GIT_MODEL_HASH -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
+}
 
+function artifactory_push()
+{
+    # publish snapshot to artifactory
+    echo "Pushing to artifactory..."
+    docker run --rm -v $PWD:/$PROJECT -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 -w="/$PROJECT" -e ARTIFACTORY_USERNAME=$ARTIFACTORY_USERNAME -e ARTIFACTORY_PASSWORD=$ARTIFACTORY_PASSWORD broadinstitute/scala-baseimage:scala-2.11.8 /$PROJECT/core/src/bin/publishSnapshot.sh
+}
+
+function docker_cmd()
+{
+    if [ $DOCKER_CMD="build" ] || [ $DOCKER_CMD="push" ]; then
+        echo "building docker image..."
+        GIT_SHA=$(git rev-parse ${GIT_BRANCH})
+        echo GIT_SHA=$GIT_SHA > env.properties  # for jenkins jobs
+        docker build -t $REPO:${GIT_SHA:0:12} .
+
+        if [ $DOCKER_CMD="push" ]; then
+            echo "pushing docker image..."
+            docker push $REPO:${GIT_SHA:0:12}
+        fi
+    else
+        echo "Not a valid docker option!  Choose either build or push (which includes build)"
+    fi
+}
+
+# parse command line options
+DOCKER_CMD=
+while [ "$1" != "" ]; do
+    case $1 in
+        make) make_jar ;;
+        publish) artifactory_push ;;
+        -d | --docker) shift
+                       echo $1
+                       DOCKER_CMD=$1
+                       docker_cmd
+                       ;;
+    esac
+    shift
+done
