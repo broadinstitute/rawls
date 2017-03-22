b86104093777933c15ada4a79890f77a5dd43623
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-03-22 12:25:38 -0400

    mysql boot wait message

--- docker/sql_validate.sh
+++ docker/sql_validate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # validate mysql
+echo "sleeping for 60 seconds during mysql boot..."
 sleep 60
 mysql -uroot -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -urawls-test -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
