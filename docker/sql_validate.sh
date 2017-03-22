ac2857889d1dd9107a12c0eccffe3996254ed32f
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-03-22 11:11:42 -0400

    increase database start up wait time

--- docker/sql_validate.sh
+++ docker/sql_validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # validate mysql
-sleep 20
+sleep 60
 mysql -uroot -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -urawls-test -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -urawls-test -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" testdb
