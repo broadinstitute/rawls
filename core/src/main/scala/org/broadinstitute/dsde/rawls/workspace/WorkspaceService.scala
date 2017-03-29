effe0b9065105c94942c64e925d087f7099b5788
Hussein Elgridly <hussein@broadinstitute.org>
2017-03-29 14:33:23 -0400

    read committed on list workspaces query

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -35,6 +35,7 @@ import spray.json.DefaultJsonProtocol._
 import spray.json._
 import org.broadinstitute.dsde.rawls.model.WorkspaceACLJsonSupport.{WorkspaceACLFormat, WorkspaceCatalogFormat, WorkspaceCatalogUpdateResponseListFormat}
 import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport._
+import _root_.slick.jdbc.TransactionIsolation
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -365,7 +366,7 @@ class WorkspaceService(protected val userInfo: UserInfo, val dataSource: SlickDa
     }
 
   def listWorkspaces(): Future[PerRequestMessage] =
-    dataSource.inTransaction { dataAccess =>
+    dataSource.inTransaction ({ dataAccess =>
 
       val query = for {
         permissionsPairs <- listWorkspaces(RawlsUser(userInfo), dataAccess)
@@ -392,7 +393,7 @@ class WorkspaceService(protected val userInfo: UserInfo, val dataSource: SlickDa
       }
 
       results.map { responses => RequestComplete(StatusCodes.OK, responses) }
-    }
+    }, TransactionIsolation.ReadCommitted)
 
   def listWorkspaces(user: RawlsUser, dataAccess: DataAccess): ReadAction[Seq[WorkspacePermissionsPair]] = {
     val rawPairs = for {
