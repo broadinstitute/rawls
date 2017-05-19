4922647ead6f4d25485432edf41bd43f16f5c974
Rob Title <rtitle@broadinstitute.org>
2017-05-18 22:15:08 -0400

    Cleaned up HealthMonitorActor

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/HealthMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/HealthMonitor.scala
@@ -1,27 +1,35 @@
 package org.broadinstitute.dsde.rawls.monitor
 
+import java.util.concurrent.TimeoutException
+
 import akka.actor.{Actor, Props}
-import akka.pattern.pipe
+import akka.pattern.{after, pipe}
+import cats._
+import cats.implicits._
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.rawls.dataaccess.{GoogleServicesDAO, MethodRepoDAO, SlickDataSource, UserDirectoryDAO}
 import org.broadinstitute.dsde.rawls.google.GooglePubSubDAO
 import org.broadinstitute.dsde.rawls.model.Subsystems._
-import org.broadinstitute.dsde.rawls.model.{AgoraStatus, StatusCheckResponse, SubsystemStatus}
+import org.broadinstitute.dsde.rawls.model.{StatusCheckResponse, SubsystemStatus}
 import org.broadinstitute.dsde.rawls.monitor.HealthMonitor._
 
-import scala.collection.GenTraversableOnce
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.util.control.NonFatal
-
-import cats._
-import cats.implicits._
 
 /**
   * Created by rtitle on 5/17/17.
   */
 object HealthMonitor {
-  case object Check
+  // Actor API:
+
+  /** Triggers subsystem checking */
+  case object CheckAll
+
+  /** Stores status for a particular subsystem */
   case class Store(subsystem: Subsystem, status: SubsystemStatus)
+
+  /** Retrieves current status and sends back to caller */
   case object GetCurrentStatus
 
   def props(slickDataSource: SlickDataSource, googleServicesDAO: GoogleServicesDAO, googlePubSubDAO: GooglePubSubDAO, userDirectoryDAO: UserDirectoryDAO, methodRepoDAO: MethodRepoDAO,
@@ -32,114 +40,180 @@ object HealthMonitor {
 class HealthMonitor(val slickDataSource: SlickDataSource, val googleServicesDAO: GoogleServicesDAO, val googlePubSubDAO: GooglePubSubDAO, val userDirectoryDAO: UserDirectoryDAO, val methodRepoDAO: MethodRepoDAO,
                     val groupsToCheck: Seq[String], val bucketsToCheck: Seq[String], val topicsToCheck: Seq[String]) extends Actor with LazyLogging {
 
+  // Use the execution context for this actor's dispatcher for all asynchronous operations.
+  // We define a separate execution context (a fixed thread pool) for health checking to
+  // not interfere with user facing operations.
   import context.dispatcher
 
-  var data: Map[Subsystem, SubsystemStatus] = _
+  val AllSubsystems = List(Agora, Cromwell, Database, GoogleBilling, GoogleBuckets, GoogleGenomics, GoogleGroups, GooglePubSub, LDAP)
+  val FutureTimeout = 1 minute
+  val StaleThreshold = 15 minutes
+
+  /**
+    * Stores each subsystem status along with a timestamp.
+    * Initialized with unknown status.
+    */
+  var data: Map[Subsystem, (SubsystemStatus, Long)] = {
+    val now = System.currentTimeMillis
+    AllSubsystems.map(_ -> (unknownStatus, now)).toMap
+  }
 
   override def receive: Receive = {
-    case Check => checkAll
-
+    case CheckAll => checkAll
     case Store(subsystem, status) => store(subsystem, status)
-
     case GetCurrentStatus => sender ! getCurrentStatus
   }
 
   private def checkAll: Unit = {
-    checkAgora
-    checkLDAP
-    checkCromwell
-    checkDB
-    checkGoogle
+    List(
+      (Agora, checkAgora),
+      (Cromwell, checkCromwell),
+      (Database, checkDB),
+      (GoogleBilling, checkGoogleBilling),
+      (GoogleBuckets, checkGoogleBuckets),
+      (GoogleGenomics, checkGoogleGenomics),
+      (GoogleGroups, checkGoogleGroups),
+      (GooglePubSub, checkGooglePubsub),
+      (LDAP, checkLDAP)
+    ).foreach((processSubsystemResult _).tupled)
   }
 
-  private def checkAgora = {
-    checkSubSystem(Agora, methodRepoDAO.getStatus) { agoraStatus =>
-      SubsystemStatus(agoraStatus.up, agoraStatus.messages)
+  /**
+    * Checks Agora status by calling the /status endpoint.
+    */
+  private def checkAgora: Future[SubsystemStatus] = {
+    methodRepoDAO.getStatus.map(agoraStatus => SubsystemStatus(agoraStatus.up, agoraStatus.messages))
+  }
+
+  /**
+    * Checks Cromwell status.
+    * TODO: Cromwell doesn't have a good status check so this is stubbed out for now.
+    */
+  private def checkCromwell: Future[SubsystemStatus] = {
+    Future.successful(okStatus)
+  }
+
+  /**
+    * Checks database status by running a "select version()" query.
+    */
+  private def checkDB: Future[SubsystemStatus] = {
+    slickDataSource.inTransaction(_.sqlDBStatus).map(_ => okStatus)
+  }
+
+  /**
+    * Checks Google PubSub status by doing a Get call on the notification and group monitor topics
+    * using the pubsub service account.
+    */
+  private def checkGooglePubsub: Future[SubsystemStatus] = {
+    multiCheck(topicsToCheck, "Could not find topic")(googlePubSubDAO.getTopic)
+  }
+
+  /**
+    * Checks Google groups status by doing a Get on the admin and curator groups using the groups
+    * service account.
+    */
+  private def checkGoogleGroups: Future[SubsystemStatus] = {
+    multiCheck(groupsToCheck, "Could not find group")(googleServicesDAO.getGoogleGroup)
+  }
+
+  /**
+    * Checks Google bucket status by doing a Get on the token bucket using the buckets service account.
+    */
+  private def checkGoogleBuckets: Future[SubsystemStatus] = {
+    multiCheck(bucketsToCheck, "Could not find bucket")(googleServicesDAO.getBucket)
+  }
+
+  /**
+    * Checks Google billing status by doing a list() using the billing service count.
+    * Expects at least one account to be returned.
+    */
+  private def checkGoogleBilling: Future[SubsystemStatus] = {
+    googleServicesDAO.listBillingAccountsUsingServiceCredential.map { accts =>
+      if (accts.isEmpty) failedStatus("Could not find any Rawls billing accounts")
+      else okStatus
     }
   }
 
-  private def checkCromwell = {
-    // Note: cromwell doesn't have a health check
-    checkSubSystem(Cromwell, Future.successful(()))(noopMapper)
+  /**
+    * Checks Google genomics status by doing a list() using the genomics service account.
+    * Does not validate the results; only that the API call succeeds.
+    */
+  private def checkGoogleGenomics: Future[SubsystemStatus] = {
+    googleServicesDAO.listGenomicsOperations.map { _ =>
+      okStatus
+    }
   }
 
-  private def checkDB = {
-    // Note: uses the slick thread pool
-    checkSubSystem(Database, slickDataSource.inTransaction(_.sqlDBStatus))(noopMapper)
+  /**
+    * Checks LDAP status by doing a search and validating that we can retrieve at least one user.
+    */
+  private def checkLDAP: Future[SubsystemStatus] = {
+    userDirectoryDAO.getAnyUser.map(_ => okStatus)
   }
 
-
-  private def checkGoogle = {
-    // pubsub
-    val topicChecks = Future.traverse(topicsToCheck.toList) { topic =>
-      googlePubSubDAO.getTopic(topic).map {
-        case Some(_) => SubsystemStatus(true, Seq.empty)
-        case None => SubsystemStatus(false, Seq(s"Could not find topic $topic"))
+  private def multiCheck[A](itemsToCheck: Seq[String], errPrefix: String)(fn: String => Future[Option[A]]): Future[SubsystemStatus] = {
+    val results = itemsToCheck.map { item =>
+      fn(item).map {
+        case Some(_) => okStatus
+        case None => failedStatus(s"$errPrefix: $item")
       }
     }
-    checkSubSystem(GooglePubSub, topicChecks)(_.combineAll)
-
-    // groups
-    val groupChecks = Future.traverse(groupsToCheck.toList) { group =>
-      googleServicesDAO.getGoogleGroup(group).map {
-        case Some(_) => SubsystemStatus(true, Seq.empty)
-        case None => SubsystemStatus(false, Seq(s"Could not find group $group"))
-      }
-    }
-    checkSubSystem(GoogleGroups, groupChecks)(_.combineAll)
-
-    // buckets
-    val bucketChecks = Future.traverse(bucketsToCheck.toList) { bucket =>
-      googleServicesDAO.getBucket(bucket).map {
-        case Some(_) => SubsystemStatus(true, Seq.empty)
-        case None => SubsystemStatus(false, Seq(s"Could not find bucket $bucket"))
-      }
-    }
-    checkSubSystem(GoogleBuckets, bucketChecks)(_.combineAll)
-
-    // billing
-    checkSubSystem(GoogleBilling, googleServicesDAO.listBillingAccountsUsingServiceCredential) { accts =>
-      if (accts.isEmpty) SubsystemStatus(false, Seq("Could not find any Rawls billing accounts"))
-      else SubsystemStatus(true, Seq.empty)
-    }
-
-    // genomics
-    checkSubSystem(GoogleGenomics, googleServicesDAO.listGenomicsOperations) { ops =>
-      // don't validate nonempty
-      SubsystemStatus(true, Seq.empty)
-    }
-
+    Future.fold(results)(Monoid[SubsystemStatus].empty)(Monoid[SubsystemStatus].combine)
   }
 
-  private def checkLDAP = {
-    checkSubSystem(LDAP, userDirectoryDAO.getAnyUser)(noopMapper)
-  }
-
-  private def checkSubSystem[A <: Subsystem, B](subSystem: A, result: => Future[B])(mapper: B => SubsystemStatus): Unit = {
-    result.map(mapper).recover { case NonFatal(ex) =>
-      SubsystemStatus(false, Seq(ex.getMessage))
+  private def processSubsystemResult(subSystem: Subsystem, result: Future[SubsystemStatus]): Unit = {
+    result.withTimeout(FutureTimeout, s"Timed out after ${FutureTimeout.toString} waiting for a response from ${subSystem.toString}")
+    .recover { case NonFatal(ex) =>
+      failedStatus(ex.getMessage)
     } map {
       Store(subSystem, _)
     } pipeTo self
   }
 
-  private def noopMapper = (_: Any) => SubsystemStatus(true, Seq.empty)
-
   private def store(subsystem: Subsystem, status: SubsystemStatus): Unit = {
-    data = data + (subsystem -> status)
+    data = data + (subsystem -> (status, System.currentTimeMillis))
   }
 
   private def getCurrentStatus: StatusCheckResponse = {
-    val overall = data.values.forall(_.ok)
-    StatusCheckResponse(overall, data)
+    val now = System.currentTimeMillis()
+    // Convert any expired statuses to uknown
+    val processed = data.mapValues {
+      case (_, t) if now - t > StaleThreshold.toMillis => unknownStatus
+      case (status, _) => status
+    }
+    // overall status is ok iff all subsystems are ok
+    val overall = processed.forall(_._2.ok)
+    StatusCheckResponse(overall, processed)
   }
-  
+
+  private def okStatus = SubsystemStatus(true, Seq.empty)
+  private def unknownStatus = SubsystemStatus(false, Seq("Unknown"))
+  private def failedStatus(message: String) = SubsystemStatus(false, Seq(message))
+
+  /**
+    * A monoid used for combining SubsystemStatuses.
+    * Zero is an ok status with no messages.
+    * Append uses && on the ok flag, and ++ on the messages.
+    */
   private implicit val SubsystemStatusMonoid = new Monoid[SubsystemStatus] {
     def combine(a: SubsystemStatus, b: SubsystemStatus): SubsystemStatus = {
       SubsystemStatus(a.ok && b.ok, a.messages ++ b.messages)
     }
-    def empty: SubsystemStatus = SubsystemStatus(true, Seq.empty)
+    def empty: SubsystemStatus = okStatus
   }
 
+  /**
+    * Adds non-blocking timeout support to futures.
+    * Example:
+    * {{{
+    *   val future = Future(Thread.sleep(1000*60*60*24*365)) // 1 year
+    *   Await.result(future.withTimeout(5 seconds, "Timed out"), 365 days)
+    *   // returns in 5 seconds
+    * }}}
+    */
+  private implicit class FutureWithTimeout[A](f: Future[A]) {
+    def withTimeout(duration: FiniteDuration, errMsg: String): Future[A] =
+      Future.firstCompletedOf(Seq(f, after(duration, context.system.scheduler)(Future.failed(new TimeoutException(errMsg)))))
+  }
 }
 
