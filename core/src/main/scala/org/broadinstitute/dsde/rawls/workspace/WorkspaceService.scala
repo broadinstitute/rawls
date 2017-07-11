296b401c2560cb457580c8a164c92df006db84ee
ansingh7115 <singh.an@husky.neu.edu>
2017-07-11 17:29:21 -0400

    GAWB-2189: changing name

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -2137,14 +2137,14 @@ class WorkspaceService(protected val userInfo: UserInfo, val dataSource: SlickDa
           withMethodInputs(methodConfig, userInfo) { (wdl, methodInputs) =>
             withSubmissionEntityRecs(submissionRequest, workspaceContext, methodConfig.rootEntityType, dataAccess) { jobEntityRecs =>
               withWorkflowFailureMode(submissionRequest) { workflowFailureMode =>
-                val requiredMethodInputs = methodInputs.filter(input => !input.workflowInput.optional || !input.expression.isEmpty)
+                val nonEmptyMethodInputs = methodInputs.filter(input => !input.workflowInput.optional || !input.expression.isEmpty)
                 //Parse out the entity -> results map to a tuple of (successful, failed) SubmissionValidationEntityInputs
-                MethodConfigResolver.resolveInputsForEntities(workspaceContext, requiredMethodInputs, jobEntityRecs, dataAccess) flatMap { valuesByEntity =>
+                MethodConfigResolver.resolveInputsForEntities(workspaceContext, nonEmptyMethodInputs, jobEntityRecs, dataAccess) flatMap { valuesByEntity =>
                   valuesByEntity
                     .map({ case (entityName, values) => SubmissionValidationEntityInputs(entityName, values) })
                     .partition({ entityInputs => entityInputs.inputResolutions.forall(_.error.isEmpty) }) match {
                     case (succeeded, failed) =>
-                      val methodConfigInputs = requiredMethodInputs.map { methodInput => SubmissionValidationInput(methodInput.workflowInput.fqn, methodInput.expression) }
+                      val methodConfigInputs = nonEmptyMethodInputs.map { methodInput => SubmissionValidationInput(methodInput.workflowInput.fqn, methodInput.expression) }
                       val header = SubmissionValidationHeader(methodConfig.rootEntityType, methodConfigInputs)
                       op(dataAccess, workspaceContext, wdl, header, succeeded.toSeq, failed.toSeq, workflowFailureMode)
                   }
