995d640298024d17c5030db4f683f61eb127febe
Rob Title <rtitle@broadinstitute.org>
2017-07-12 11:48:20 -0400

    Experimental: does using truncate instead of delete improve unit test execution time?

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
@@ -2,6 +2,9 @@ package org.broadinstitute.dsde.rawls.dataaccess.slick
 
 import org.broadinstitute.dsde.rawls.expressions.SlickExpressionParser
 import slick.driver.JdbcProfile
+import slick.jdbc.SQLActionBuilder
+import slick.jdbc.SetParameter.SetUnit
+import slick.lifted.AbstractTable
 
 trait DataAccess
   extends PendingBucketDeletionComponent
@@ -29,38 +32,52 @@ trait DataAccess
     // important to keep the right order for referential integrity !
     // if table X has a Foreign Key to table Y, delete table X first
 
-    TableQuery[GroupSubgroupsTable].delete andThen                // FK to group
-      TableQuery[GroupUsersTable].delete andThen                  // FK to group, users
-      TableQuery[WorkspaceAccessTable].delete andThen             // FK to group, workspace
-      TableQuery[RawlsBillingProjectGroupTable].delete andThen    // FK to group, billingproject
-      TableQuery[EntityAttributeTable].delete andThen             // FK to entity
-      TableQuery[WorkspaceAttributeTable].delete andThen          // FK to entity, workspace
-      TableQuery[SubmissionAttributeTable].delete andThen         // FK to entity, submissionvalidation
-      TableQuery[MethodConfigurationInputTable].delete andThen    // FK to MC
-      TableQuery[MethodConfigurationOutputTable].delete andThen   // FK to MC
-      TableQuery[MethodConfigurationPrereqTable].delete andThen   // FK to MC
-      TableQuery[SubmissionValidationTable].delete andThen        // FK to workflow, workflowfailure
-      TableQuery[WorkflowMessageTable].delete andThen             // FK to workflow
-      TableQuery[WorkflowTable].delete andThen                    // FK to submission, entity
-      TableQuery[SubmissionTable].delete andThen                  // FK to workspace, user, MC, entity
-      TableQuery[MethodConfigurationTable].delete andThen         // FK to workspace
-      TableQuery[EntityTable].delete andThen                      // FK to workspace
-      TableQuery[PendingWorkspaceAccessTable].delete andThen      // FK to workspace, user
-      TableQuery[WorkspaceUserShareTable].delete andThen          // FK to workspace, user
-      TableQuery[WorkspaceGroupShareTable].delete andThen         // FK to workspace, group
-      TableQuery[WorkspaceUserCatalogTable].delete andThen        // FK to workspace, user
-      TableQuery[WorkspaceGroupCatalogTable].delete andThen       // FK to workspace, group
-      TableQuery[WorkspaceTable].delete andThen                   // FK to realm
-      TableQuery[ManagedGroupTable].delete andThen                       // FK to group
-      TableQuery[RawlsBillingProjectTable].delete andThen
-      TableQuery[RawlsGroupTable].delete andThen
-      TableQuery[RawlsUserTable].delete andThen
-      TableQuery[WorkflowAuditStatusTable].delete andThen
-      TableQuery[SubmissionAuditStatusTable].delete andThen
-      TableQuery[PendingBucketDeletionTable].delete andThen
-      TableQuery[EntityAttributeScratchTable].delete andThen
-      TableQuery[WorkspaceAttributeScratchTable].delete andThen
-      TableQuery[ExprEvalScratch].delete
+    TableQuery[GroupSubgroupsTable].truncate andThen                // FK to group
+      TableQuery[GroupUsersTable].truncate andThen                  // FK to group, users
+      TableQuery[WorkspaceAccessTable].truncate andThen             // FK to group, workspace
+      TableQuery[RawlsBillingProjectGroupTable].truncate andThen    // FK to group, billingproject
+      TableQuery[EntityAttributeTable].truncate andThen             // FK to entity
+      TableQuery[WorkspaceAttributeTable].truncate andThen          // FK to entity, workspace
+      TableQuery[SubmissionAttributeTable].truncate andThen         // FK to entity, submissionvalidation
+      TableQuery[MethodConfigurationInputTable].truncate andThen    // FK to MC
+      TableQuery[MethodConfigurationOutputTable].truncate andThen   // FK to MC
+      TableQuery[MethodConfigurationPrereqTable].truncate andThen   // FK to MC
+      TableQuery[SubmissionValidationTable].truncate andThen        // FK to workflow, workflowfailure
+      TableQuery[WorkflowMessageTable].truncate andThen             // FK to workflow
+      TableQuery[WorkflowTable].truncate andThen                    // FK to submission, entity
+      TableQuery[SubmissionTable].truncate andThen                  // FK to workspace, user, MC, entity
+      TableQuery[MethodConfigurationTable].truncate andThen         // FK to workspace
+      TableQuery[EntityTable].truncate andThen                      // FK to workspace
+      TableQuery[PendingWorkspaceAccessTable].truncate andThen      // FK to workspace, user
+      TableQuery[WorkspaceUserShareTable].truncate andThen          // FK to workspace, user
+      TableQuery[WorkspaceGroupShareTable].truncate andThen         // FK to workspace, group
+      TableQuery[WorkspaceUserCatalogTable].truncate andThen        // FK to workspace, user
+      TableQuery[WorkspaceGroupCatalogTable].truncate andThen       // FK to workspace, group
+      TableQuery[WorkspaceTable].truncate andThen                   // FK to realm
+      TableQuery[ManagedGroupTable].truncate andThen                // FK to group
+      TableQuery[RawlsBillingProjectTable].truncate andThen
+      TableQuery[RawlsGroupTable].truncate andThen
+      TableQuery[RawlsUserTable].truncate andThen
+      TableQuery[WorkflowAuditStatusTable].truncate andThen
+      TableQuery[SubmissionAuditStatusTable].truncate andThen
+      TableQuery[PendingBucketDeletionTable].truncate andThen
+      TableQuery[EntityAttributeScratchTable].truncate andThen
+      TableQuery[WorkspaceAttributeScratchTable].truncate andThen
+      TableQuery[ExprEvalScratch].truncate
+  }
+
+  implicit class TruncateTableSupport[A <: AbstractTable[_]](tq: TableQuery[A]) {
+    def truncate: WriteAction[Int] = {
+      val trunc = SQLActionBuilder(List(s"TRUNCATE TABLE `${tq.baseTableRow.tableName}`"), SetUnit).asUpdate
+      // need to temporarily disable foreign key checks for the truncate command
+      DBIO.sequence(
+        Seq(
+          sqlu"""SET FOREIGN_KEY_CHECKS = 0;""",
+          trunc,
+          sqlu"""SET FOREIGN_KEY_CHECKS = 1;"""
+        )
+      ).map(results => results(1)) // return the result of the truncate command
+    }
   }
 
   def sqlDBStatus() = {
