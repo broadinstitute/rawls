5daaad01ad1766786b0afd3b2834e55a5fb6a6ab
Joel Thibault <thibault@broadinstitute.org>
2017-07-24 15:09:18 -0400

    ThresholdOneForOneStrategy tests and updates

--- util/src/main/scala/org/broadinstitute/dsde/rawls/util/ThresholdOneForOneStrategy.scala
+++ util/src/main/scala/org/broadinstitute/dsde/rawls/util/ThresholdOneForOneStrategy.scala
@@ -1,23 +1,35 @@
 package org.broadinstitute.dsde.rawls.util
 
-import akka.actor.{ActorContext, ActorRef, ChildRestartStats, OneForOneStrategy, SupervisorStrategy}
+import akka.actor.{ActorContext, ActorRef, ChildRestartStats, SupervisorStrategy}
 
 import scala.concurrent.duration.Duration
 
-//Retries up to maxNrOfRetries within withinTimeRange.
+//Retries up to maxNrOfRetries regardless of time range.
 //Once the number of retries is above thresholdLimit, call thresholdFunc before restarting child.
-class ThresholdOneForOneStrategy(maxNrOfRetries: Int = -1,
-                                 loggingEnabled: Boolean = true,
-                                 thresholdLimit: Int = -1)
-                                (decider: SupervisorStrategy.Decider)
+class ThresholdOneForOneStrategy(thresholdLimit: Int,
+                                 maxNrOfRetries: Option[Int] = None,
+                                 override val loggingEnabled: Boolean = true)
+                                (override val decider: SupervisorStrategy.Decider)
                                 (thresholdFunc: Int => Unit)
-  extends OneForOneStrategy(maxNrOfRetries, Duration.Inf, loggingEnabled)(decider) {
+  extends SupervisorStrategy {
 
-  //stupid akka stupid makes this stupid private so i redefine it here because screw you, akka
-  protected val retriesWindow: (Option[Int], Option[Int]) = ( if(maxNrOfRetries < 0) None else Some(maxNrOfRetries), None )
+  // this is a stupid hack because
+  // 1. akka stupid makes this stupid private
+  // 2. ChildRestartStats doesn't increment the retry count when maxNrOfRetries is unlimited so we pretend with maxint
+  protected val retriesWindow: (Option[Int], Option[Int]) = {
+    val countWindow = maxNrOfRetries match {
+      // other strategies use a -1 sentinel to mean unlimited so you can do that here too
+      case Some(retries) if retries >= 0 => maxNrOfRetries
+      case _ => Some(Int.MaxValue)
+    }
+    val timeOutWindow = None  // not used here
+    (countWindow, timeOutWindow)
+  }
 
   override def processFailure(context: ActorContext, restart: Boolean, child: ActorRef, cause: Throwable, stats: ChildRestartStats, children: Iterable[ChildRestartStats]): Unit = {
+    // it's necessary to call stats.requestRestartPermission() to increment stats' retry count
     if (restart && stats.requestRestartPermission(retriesWindow) ) {
+      // this is post-increment so thresholdLimit = 0 means this will always run
       if (stats.maxNrOfRetriesCount > thresholdLimit) {
         thresholdFunc(stats.maxNrOfRetriesCount)
       }
@@ -26,4 +38,7 @@ class ThresholdOneForOneStrategy(maxNrOfRetries: Int = -1,
       context.stop(child)
     }
   }
+
+  // the built-in strategies don't do anything here either
+  override def handleChildTerminated(context: ActorContext, child: ActorRef, children: Iterable[ActorRef]): Unit = ()
 }
