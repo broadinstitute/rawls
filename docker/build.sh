8fa111182c566c2bd72b1ba89d887d9d4d053489
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-07-28 11:50:39 -0400

    add stop function to mysql script

--- docker/build.sh
+++ docker/build.sh
@@ -16,13 +16,16 @@ PROJECT=rawls
 function make_jar()
 {
     echo "building jar..."
-    bash ./docker/run-mysql.sh
+    bash ./docker/run-mysql.sh start
 
     # Get the last commit hash of the model directory and set it as an environment variable
     GIT_MODEL_HASH=$(git log -n 1 --pretty=format:%h model)
 
     # make jar.  cache sbt dependencies.
     docker run --rm --link mysql:mysql -e GIT_MODEL_HASH=$GIT_MODEL_HASH -v $PWD:/working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 broadinstitute/scala-baseimage /working/docker/install.sh /working
+
+    # stop mysql db
+    bash ./docker/run-mysql.sh stop
 }
 
 function artifactory_push()
--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/env bash
 
-# check if mysql is running
-CONTAINER=mysql
-RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER || echo "false")
+start() {
+    # check if mysql is running
+    RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER || echo "false")
 
-# mysql set-up
-if ! $RUNNING; then
-    # force remove mysql in case it is stopped
-    echo "attempting to remove old mysql container..."
-    docker rm -f $CONTAINER || echo "docker rm failed. nothing to rm."
+    # mysql set-up
+    if ! $RUNNING; then
+        # force remove mysql in case it is stopped
+        echo "attempting to remove old $CONTAINER container..."
+        docker rm -f $CONTAINER || echo "docker rm failed. nothing to rm."
 
-    # start up mysql
-    echo "starting up mysql container..."
-    docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:5.7.15
+        # start up mysql
+        echo "starting up mysql container..."
+        docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:5.7.15
 
-    # validate mysql
-    echo "running mysql validation..."
-    docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
-    if [ 0 -eq $? ]; then
-        echo "mysql validation succeeded."
-    else
-        echo "mysql validation failed."
-        exit 1
+        # validate mysql
+        echo "running mysql validation..."
+        docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
+        if [ 0 -eq $? ]; then
+            echo "mysql validation succeeded."
+        else
+            echo "mysql validation failed."
+            exit 1
+        fi
     fi
+}
+
+stop() {
+    echo "Stopping docker $CONTAINER container..."
+    docker stop $CONTAINER || echo "mysql stop failed. container already stopped."
+    docker rm -v $CONTAINER || echo "mysql rm -v failed.  container already destroyed."
+}
+
+CONTAINER=mysql
+COMMAND=$1
+if [ $COMMAND = "start" ]; then
+    start
+elif [ $COMMAND = "stop" ]; then
+    stop
+else
+    exit 1
 fi
