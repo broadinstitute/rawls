0230878647530b37120c783825f1ed2b82a418f6
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2017-08-15 09:04:28 -0400

    make config available in correct place (#757)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccessComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccessComponent.scala
@@ -5,5 +5,5 @@ import slick.driver.JdbcDriver
 
 import scala.concurrent.ExecutionContext
 
-class DataAccessComponent(val driver: JdbcDriver, val batchSize: Int, val directoryConfig: DirectoryConfig)(implicit val executionContext: ExecutionContext)
+class DataAccessComponent(val driver: JdbcDriver, val batchSize: Int, override val directoryConfig: DirectoryConfig)(implicit val executionContext: ExecutionContext)
 extends DriverComponent with DataAccess
