bc8c533f0a3ab2c33c4b9e0fffb1745e2f52785b
Rob Title <rtitle@broadinstitute.org>
2017-08-21 16:43:56 -0400

    statsd extension needs to live in the same package

--- metrics/src/main/scala/com/readytalk/metrics/WorkbenchStatsD.scala
+++ metrics/src/main/scala/com/readytalk/metrics/WorkbenchStatsD.scala
@@ -1,12 +1,11 @@
-package org.broadinstitute.dsde.rawls.metrics
+package com.readytalk.metrics
 
-import com.readytalk.metrics.StatsD
 import com.typesafe.scalalogging.LazyLogging
 
 /**
   * Created by rtitle on 8/21/17.
   */
-class RawlsStatsD(host: String, port: Int) extends StatsD(host, port) with LazyLogging {
+class WorkbenchStatsD(host: String, port: Int) extends StatsD(host, port) with LazyLogging {
   // Filter out detailed timer metrics to prevent hitting Hosted Graphite quotas.
   // Keep: mean, p95, stddev
   val MetricSuffixesToFilter = Set("max", "min", "p50", "p75", "p98", "p99", "p999", "samples", "m1_rate", "m5_rate", "m15_rate", "mean_rate")
@@ -20,6 +19,6 @@ class RawlsStatsD(host: String, port: Int) extends StatsD(host, port) with LazyL
   }
 }
 
-object RawlsStatsD {
-  def apply(host: String, port: Int): RawlsStatsD = new RawlsStatsD(host, port)
+object WorkbenchStatsD {
+  def apply(host: String, port: Int): WorkbenchStatsD = new WorkbenchStatsD(host, port)
 }
