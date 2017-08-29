0f9275c1f35355c2816debad5afda59d982d4889
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-08-29 16:25:21 -0400

    put back generic container name

--- docker/sql_validate.sh
+++ docker/sql_validate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SERVICE=$1
+
 # validate mysql
 echo "sleeping for 60 seconds during mysql boot..."
 sleep 60
-mysql -uroot -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
-mysql -urawls-test -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
-mysql -urawls-test -prawls-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" testdb
+mysql -uroot -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
+mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
+mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" testdb
