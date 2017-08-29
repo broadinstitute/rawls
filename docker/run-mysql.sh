027ce53d7b8e5ec88675994ba921a72184004643
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-08-29 16:01:09 -0400

    don't check for running anymore (we are using timestamp tagged containers)

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -3,28 +3,17 @@
 # The CloudSQL console simply states "MySQL 5.6" so we may not match the minor version number
 MYSQL_VERSION=5.6
 start() {
-    # check if mysql is running
-    RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER || echo "false")
+    echo "starting up mysql container..."
+    docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:$MYSQL_VERSION
 
-    # mysql set-up
-    if ! $RUNNING; then
-        # force remove mysql in case it is stopped
-        echo "attempting to remove old $CONTAINER container..."
-        docker rm -f $CONTAINER || echo "docker rm failed. nothing to rm."
-
-        # start up mysql
-        echo "starting up mysql container..."
-        docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:$MYSQL_VERSION
-
-        # validate mysql
-        echo "running mysql validation..."
-        docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
-        if [ 0 -eq $? ]; then
-            echo "mysql validation succeeded."
-        else
-            echo "mysql validation failed."
-            exit 1
-        fi
+    # validate mysql
+    echo "running mysql validation..."
+    docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
+    if [ 0 -eq $? ]; then
+        echo "mysql validation succeeded."
+    else
+        echo "mysql validation failed."
+        exit 1
     fi
 }
 
