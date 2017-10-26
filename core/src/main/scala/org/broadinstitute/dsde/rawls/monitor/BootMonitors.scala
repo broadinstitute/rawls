9a609f11ff78a637b7f9b732c10885054b8d4ece
Matt Bemis <mbemis@broadinstitute.org>
2017-10-26 12:25:42 -0400

    fix compilation error after rebase

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
@@ -14,6 +14,7 @@ import org.broadinstitute.dsde.rawls.dataaccess.{GoogleServicesDAO, SlickDataSou
 import org.broadinstitute.dsde.rawls.model.WorkflowStatuses
 
 import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.util.Try
 
