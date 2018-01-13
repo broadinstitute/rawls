0496841a81ef616f8c73aa007d2f592c47c696c6
Douglas Voet <dvoet@users.noreply.github.com>
2018-01-13 17:48:23 -0500

    fix json escaping

--- core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/WorkflowSubmissionSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/WorkflowSubmissionSpec.scala
@@ -215,13 +215,12 @@ class WorkflowSubmissionSpec(_system: ActorSystem) extends TestKit(_system) with
             testData.wsName.namespace,
             testData.userOwner.userEmail.value,
             "pet-110347448408766049948@broad-dsde-dev.iam.gserviceaccount.com",
-            """{\"client_email\": \"pet-110347448408766049948@broad-dsde-dev.iam.gserviceaccount.com\"}""",
+            """{"client_email": "pet-110347448408766049948@broad-dsde-dev.iam.gserviceaccount.com"}""",
             testData.billingProject.cromwellAuthBucketUrl,
             s"gs://${testData.workspace.bucketName}/${testData.submission1.submissionId}/workflow.logs",
             Some(JsObject(Map("zones" -> JsString("us-central-someother")))),
             false
           ))) {
-        println(mockExecCluster.getDefaultSubmitMember.asInstanceOf[MockExecutionServiceDAO].submitOptions)
         mockExecCluster.getDefaultSubmitMember.asInstanceOf[MockExecutionServiceDAO].submitOptions.map(_.parseJson.convertTo[ExecutionServiceWorkflowOptions])
       }
     }
