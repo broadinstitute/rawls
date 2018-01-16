b238aabd941ca94edd85ec169f4e400f62a0e68e
Douglas Voet <dvoet@users.noreply.github.com>
2018-01-16 09:00:18 -0500

    add skip tests flag

--- docker/install.sh
+++ docker/install.sh
@@ -5,12 +5,19 @@ set -e
 RAWLS_DIR=$1
 cd $RAWLS_DIR
 
+SKIP_TESTS=$2
+
 if [ -e "jenkins_env.sh" ]; then
 	source "jenkins_env.sh"
 fi
 
 # Tests are run in jenkins which has a custom mysql instance just for testing
-#sbt -J-Xms4g -J-Xmx4g -J-XX:MaxMetaspaceSize=1024m test -Dmysql.host=mysql -Dmysql.port=3306 -Ddirectory.url=ldap://opendj:389 -Ddirectory.password=testtesttest
+if [ "$SKIP_TESTS" = "skip-tests" ]; then
+	echo skipping tests
+else
+	sbt -J-Xms4g -J-Xmx4g -J-XX:MaxMetaspaceSize=1024m test -Dmysql.host=mysql -Dmysql.port=3306 -Ddirectory.url=ldap://opendj:389 -Ddirectory.password=testtesttest
+fi
+
 sbt -J-Xms4g -J-Xmx4g -J-XX:MaxMetaspaceSize=1024m assembly
 RAWLS_JAR=$(find target | grep 'rawls.*\.jar')
 mv $RAWLS_JAR .
