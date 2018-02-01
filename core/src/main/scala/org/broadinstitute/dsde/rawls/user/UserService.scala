b4ccad530a2789d1cff71e9f6ef057cab36a00c3
Kaan Yuksel <kyuksel@broadinstitute.org>
2018-02-01 09:49:03 -0500

    revert reformating to reduce merge conflicts with rebasing

--- core/src/main/scala/org/broadinstitute/dsde/rawls/user/UserService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/user/UserService.scala
@@ -935,18 +935,7 @@ class UserService(protected val userInfo: UserInfo, val dataSource: SlickDataSou
                     workspaceCreatorPolicy <- DBIO.from(samDAO.overwritePolicy(SamResourceTypeNames.billingProject, projectName.value, workspaceCreatorPolicyName, SamPolicy(Seq.empty, Seq.empty, Seq(SamProjectRoles.workspaceCreator)), userInfo))
                     canComputeUserPolicy <- DBIO.from(samDAO.overwritePolicy(SamResourceTypeNames.billingProject, projectName.value, canComputeUserPolicyName, SamPolicy(Seq.empty, Seq.empty, Seq(SamProjectRoles.batchComputeUser, SamProjectRoles.notebookUser)), userInfo))
                     groupEmail <- DBIO.from(samDAO.syncPolicyToGoogle(SamResourceTypeNames.billingProject, projectName.value, SamProjectRoles.owner, userInfo)).map(_.keys.headOption.getOrElse(throw new RawlsException("Error getting owner policy email")))
-                    project <- dataAccess.rawlsBillingProjectQuery.create(
-                      RawlsBillingProject(
-                        projectName,
-                        RawlsGroup(
-                          RawlsGroupName(SamProjectRoles.owner),
-                          groupEmail,
-                          Set.empty,
-                          Set.empty),
-                        "gs://" + gcsDAO.getCromwellAuthBucketName(projectName),
-                        CreationStatuses.Creating,
-                        Option(billingAccountName),
-                        None))
+                    project <- dataAccess.rawlsBillingProjectQuery.create(RawlsBillingProject(projectName, RawlsGroup(RawlsGroupName(SamProjectRoles.owner), groupEmail, Set.empty, Set.empty), "gs://" + gcsDAO.getCromwellAuthBucketName(projectName), CreationStatuses.Creating, Option(billingAccountName), None))
                   } yield project
 
                 case Some(_) => throw new RawlsExceptionWithErrorReport(ErrorReport(StatusCodes.Conflict, "project by that name already exists"))
