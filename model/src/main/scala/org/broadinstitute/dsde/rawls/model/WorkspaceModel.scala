c8b4b2bf581c9fd15419922ae628e900fdb3ea18
Adam Nichols <anichols@broadinstitute.org>
2018-02-12 18:42:27 -0500

    Possibly premature, can revert

--- model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
+++ model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
@@ -595,26 +595,19 @@ class WorkspaceJsonSupport extends JsonSupport {
     override def write(method: MethodRepoMethod): JsValue = {
       method match {
         case agora: AgoraMethod =>
-          JsObject(agora.toJson.asJsObject.fields + ("methodUri" -> JsString(agora.methodUri)))
+          JsObject(Map("sourceRepo" -> JsString(agora.repo.scheme)) ++ agora.toJson.asJsObject.fields)
         case dockstore: DockstoreMethod =>
-          JsObject("methodUri" -> JsString(dockstore.methodUri))
+          JsObject(Map("sourceRepo" -> JsString(dockstore.repo.scheme)) ++ dockstore.toJson.asJsObject.fields)
       }
     }
 
     override def read(json: JsValue): MethodRepoMethod = {
-
-      val fromUri = json.asJsObject.fields.get("methodUri") match {
-        case Some(JsString(uri)) => Try(MethodRepoMethod.fromUri(uri)).toOption
-        case _ => None
-      }
-
-      fromUri match {
-        case Some(method) => method
-        case _ =>
-          Try { AgoraMethodFormat.read(json) } match {
-            case Success(method) => method
-            case Failure(t) => throw t
-          }
+      json.asJsObject.fields.get("sourceRepo") match {
+        case Some(JsString(Dockstore.scheme)) => DockstoreMethodFormat.read(json)
+        case Some(JsString(Agora.scheme)) => AgoraMethodFormat.read(json)
+        case None => AgoraMethodFormat.read(json) // If omitted, default to Agora for backwards compatibility
+        case Some(JsString(other)) => throw DeserializationException(s"Illegal method repo \'$other\'")
+        case _ => throw DeserializationException("unexpected json type")
       }
     }
 
