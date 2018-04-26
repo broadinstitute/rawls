e70fa4ef10e5e260f3668c7fda36c438b570a8d1
David An <davidan@genomebridge.org>
2018-04-26 12:49:25 -0400

    pretty print genomics operations output (#906)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/genomics/GenomicsService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/genomics/GenomicsService.scala
@@ -10,7 +10,7 @@ import org.broadinstitute.dsde.rawls.webservice.PerRequest.{PerRequestMessage, R
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import spray.json.JsObject
+import spray.json.{JsObject, PrettyPrinter}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -29,7 +29,9 @@ class GenomicsService(protected val userInfo: UserInfo, val dataSource: SlickDat
 
   def getOperation(jobId: String): Future[PerRequestMessage] = {
     gcsDAO.getGenomicsOperation(jobId).map {
-      case Some(jsobj) => RequestComplete(jsobj)
+      case Some(jsobj) =>
+        implicit val printer = PrettyPrinter
+        RequestComplete(jsobj)
       case None => RequestComplete(StatusCodes.NotFound, s"jobId $jobId not found.")
     }
   }
