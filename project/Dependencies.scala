6f94995641479b62fdc953224155e0b9aa293fdb
Brian Reilly <breilly@broadinstitute.org>
2018-09-11 13:04:08 -0400

    Fix akka-stream dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,12 +16,12 @@ object Dependencies {
   val slickHikariCP: ModuleID = "com.typesafe.slick" %% "slick-hikaricp"  % slickV
 
   val excludeAkkaActor =        ExclusionRule(organization = "com.typesafe.akka", name = "akka-actor_2.12")
-  val excludeAkkaStream =       ExclusionRule(organization = "com.typesafe.akka", name = "akka-stream_2.12")
 
   val akkaActor: ModuleID =         "com.typesafe.akka"   %%  "akka-actor"           % akkaV
   val akkaContrib: ModuleID =       "com.typesafe.akka"   %%  "akka-contrib"         % akkaV
   val akkaSlf4j: ModuleID =         "com.typesafe.akka"   %%  "akka-slf4j"           % akkaV
-  val akkaHttp: ModuleID =          "com.typesafe.akka"   %%  "akka-http"            % akkaHttpV           excludeAll(excludeAkkaActor, excludeAkkaStream)
+  val akkaHttp: ModuleID =          "com.typesafe.akka"   %%  "akka-http"            % akkaHttpV
+  val akkaStream: ModuleID =        "com.typesafe.akka"   %%  "akka-stream"          % akkaV
   val akkaHttpSprayJson: ModuleID = "com.typesafe.akka"   %%  "akka-http-spray-json" % akkaHttpV
   val akkaTestKit: ModuleID =       "com.typesafe.akka"   %%  "akka-testkit"         % akkaV     % "test"
   val akkaHttpTestKit: ModuleID =   "com.typesafe.akka"   %%  "akka-http-testkit"    % akkaHttpV % "test"
@@ -84,6 +84,7 @@ object Dependencies {
     metricsScala,
     metricsStatsd,
     akkaHttp,
+    akkaStream,
     scalatest,
     mockito
   )
@@ -91,6 +92,7 @@ object Dependencies {
   val googleDependencies = metricsDependencies ++ Seq(
     akkaHttpSprayJson,
     akkaHttp,
+    akkaStream,
     akkaActor,
     akkaHttpTestKit,
     scalatest,
@@ -123,6 +125,7 @@ object Dependencies {
     jacksonCore,
     akkaHttpSprayJson,
     akkaHttp,
+    akkaStream,
     jodaTime,
     jodaConvert,
     scalaLogging,
@@ -139,7 +142,7 @@ object Dependencies {
     slick,
     slickHikariCP,
     akkaHttp,
-    akkaHttp,
+    akkaStream,
     swaggerUI,
     commonsJEXL,
     cromwellWdl,
