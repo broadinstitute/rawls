2ba3e99f056fb95551baf8806e212f400f48383a
Brian Reilly <breilly@broadinstitute.org>
2018-09-20 11:37:09 -0400

    Akka to 2.5.16. Akka HTTP to 10.1.5. scala-parser-combinators to 1.1.1.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  val akkaV = "2.4.20" // 2.5.14 works but 2.5.15+ causes an obscure test failure; sticking with 2.4 for now
-  val akkaHttpV = "10.0.14" // can't go to 10.1.x without going to akka 2.5.x
+  val akkaV = "2.5.16"
+  val akkaHttpV = "10.1.5"
   val slickV = "3.2.1"
 
   val googleV = "1.22.0"
@@ -56,7 +56,7 @@ object Dependencies {
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val httpClient: ModuleID =      "org.apache.httpcomponents"     % "httpclient"            % "4.5.3"  // upgrading a transitive dependency to avoid security warnings
   val cats: ModuleID =            "org.typelevel"                 %% "cats"                 % "0.9.0"
-  val parserCombinators =         "org.scala-lang.modules"        %% "scala-parser-combinators" % "1.0.6"
+  val parserCombinators =         "org.scala-lang.modules"        %% "scala-parser-combinators" % "1.1.1"
   val mysqlConnector: ModuleID =  "mysql"                         % "mysql-connector-java"  % "5.1.42"
   val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "3.5.3"
   val logbackClassic: ModuleID =  "ch.qos.logback"                % "logback-classic"       % "1.2.2"
