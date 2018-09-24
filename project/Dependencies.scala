50055d8c3a7172fb62170d3e9464a741ab5259cf
abaumann <abaumann@broadinstitute.org>
2018-09-24 17:13:07 -0400

    Cromwell upgrade for DOS fixes (#986)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val googleV = "1.22.0"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
 
-  val cromwellVersion = "34-c37ded7"
+  val cromwellVersion = "34-cfbe67f"
 
   def excludeGuavaJDK5(m: ModuleID): ModuleID = m.exclude("com.google.guava", "guava-jdk5")
 
