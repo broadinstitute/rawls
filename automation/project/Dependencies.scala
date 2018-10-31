318c3af6969998b185d30d27cbccca740160a7d8
David An <davidan@genomebridge.org>
2018-10-31 13:53:32 -0400

    update serviceTest version to get bugfixes (#1004)

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.12-eb6af64"
+  val serviceTestV = "0.13-dc0998e"
   val workbenchGoogleV = "0.16-eb6af64"
   val workbenchModelV  = "0.10-52d614b"
   val workbenchMetricsV  = "0.3-7ad0aa8"
