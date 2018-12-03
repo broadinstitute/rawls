194f792408e2e4df698d17a94cb6076bc9056996
ansingh7115 <singh.an@husky.neu.edu>
2018-12-03 14:43:29 -0500

    fail tests if gpalloc runs out of billing projects

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.15-c7a8999"
+  val serviceTestV = "0.15-7bc56cf-SNAP"
   val workbenchGoogleV = "0.18-c7a8999"
   val workbenchModelV  = "0.10-52d614b"
   val workbenchMetricsV  = "0.3-7ad0aa8"
