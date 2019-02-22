89f06d7043cf2d623a409743133ee48d6245dd79
Saloni Shah <salonipshah11@gmail.com>
2019-02-22 15:15:31 -0500

    updated cromwell hash

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val googleV = "1.22.0"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
 
-  val cromwellVersion = "37-9f78414"
+  val cromwellVersion = "36-858f647"
 
   def excludeGuavaJDK5(m: ModuleID): ModuleID = m.exclude("com.google.guava", "guava-jdk5")
 
