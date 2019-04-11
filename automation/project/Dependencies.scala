39bbe22938e20e63dcd40f75c661c6751911139d
Marcus Talbott <mtalbott@broadinstitute.org>
2019-04-11 11:51:24 -0400

    update hash

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.16-ed6ab7d-SNAP"
+  val serviceTestV = "0.16-e73039a"
   val workbenchGoogleV = "0.18-c7a8999"
   val workbenchModelV  = "0.10-52d614b"
   val workbenchMetricsV  = "0.3-7ad0aa8"
