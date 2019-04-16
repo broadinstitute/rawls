cb1cde7d190919d20959708bdb4eee9b34f0459c
Marc Talbott <mtalbott@broadinstitute.org>
2019-04-16 13:42:57 -0400

    add eventually blocks to hopefully fix alpha test failures (#1068)

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
@@ -13,11 +13,12 @@ import org.broadinstitute.dsde.workbench.service.test.{CleanUp, RandomUtil}
 import org.broadinstitute.dsde.rawls.model._
 import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport._
 import org.scalatest.{FreeSpecLike, Matchers}
-
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Minutes, Seconds, Span}
 import spray.json._
 import DefaultJsonProtocol._
 
-class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike with Matchers
+class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike with Matchers with Eventually
   with CleanUp with RandomUtil with Retry
   with BillingFixtures with WorkspaceFixtures {
 
@@ -27,6 +28,8 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
 
   val owner: Credentials = UserPool.chooseProjectOwner
   val ownerAuthToken: AuthToken = owner.makeAuthToken()
+
+  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Minutes)), interval = scaled(Span(20, Seconds)))
 
   "Rawls" - {
     "should allow project owners" - {
@@ -58,16 +61,20 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val shareWithStudentA: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Reader, Some(true), Some(false)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, shareWithStudentA)(ownerAuthToken)
 
-            val studentAWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            studentAWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
-            studentAWorkspaceDetails.canShare should be(true)
+            eventually {
+              val studentAWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              studentAWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
+              studentAWorkspaceDetails.canShare should be(true)
+            }
 
             // check that student A can share the workspace with student B
             val shareWithStudentB: Set[AclEntry] = Set(AclEntry(studentB.email, WorkspaceAccessLevel.Reader, Some(false), Some(false)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, shareWithStudentB)(studentAToken)
 
-            val studentBWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentBToken))
-            studentBWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
+            eventually {
+              val studentBWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentBToken))
+              studentBWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
+            }
           }(ownerAuthToken)
         }
       }
@@ -79,9 +86,11 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val shareWithStudentA: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Reader, Some(false), Some(false)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, shareWithStudentA)(ownerAuthToken)
 
-            val studentAWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            studentAWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
-            studentAWorkspaceDetails.canShare should be(false)
+            eventually {
+              val studentAWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              studentAWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
+              studentAWorkspaceDetails.canShare should be(false)
+            }
 
             // check that student A cannot share the workspace with student B
             val shareWithStudentB: Set[AclEntry] = Set(AclEntry(studentB.email, WorkspaceAccessLevel.Reader, Some(false), Some(false)))
@@ -97,16 +106,20 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val writerCanCompute: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Writer, Some(false), Some(true)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, writerCanCompute)(ownerAuthToken)
 
-            val canComputeWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            canComputeWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
-            canComputeWorkspaceDetails.canCompute should be(true)
+            eventually {
+              val canComputeWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              canComputeWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
+              canComputeWorkspaceDetails.canCompute should be(true)
+            }
 
             val revokeCanCompute: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Writer, Some(false), Some(false)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, revokeCanCompute)(ownerAuthToken)
 
-            val noComputeWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            noComputeWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
-            noComputeWorkspaceDetails.canCompute should be(false)
+            eventually {
+              val noComputeWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              noComputeWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
+              noComputeWorkspaceDetails.canCompute should be(false)
+            }
           }(ownerAuthToken)
         }
       }
@@ -117,16 +130,20 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val writerAccess: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Writer))
             Rawls.workspaces.updateAcl(projectName, workspaceName, writerAccess)(ownerAuthToken)
 
-            val writerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            writerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
-            writerWorkspaceDetails.canCompute should be(true)
+            eventually {
+              val writerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              writerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
+              writerWorkspaceDetails.canCompute should be(true)
+            }
 
             val readerAccess: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Reader))
             Rawls.workspaces.updateAcl(projectName, workspaceName, readerAccess)(ownerAuthToken)
 
-            val readerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            readerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
-            readerWorkspaceDetails.canCompute should be(false)
+            eventually {
+              val readerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              readerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Read)
+              readerWorkspaceDetails.canCompute should be(false)
+            }
           }(ownerAuthToken)
         }
       }
@@ -137,8 +154,10 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val writerAccess: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Writer))
             Rawls.workspaces.updateAcl(projectName, workspaceName, writerAccess)(ownerAuthToken)
 
-            val writerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            writerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
+            eventually {
+              val writerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              writerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Write)
+            }
 
             val noAccess: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.NoAccess))
             Rawls.workspaces.updateAcl(projectName, workspaceName, noAccess)(ownerAuthToken)
@@ -154,19 +173,23 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
             val ownerAccess: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Owner))
             Rawls.workspaces.updateAcl(projectName, workspaceName, ownerAccess)(ownerAuthToken)
 
-            val secondOwnerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            secondOwnerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Owner)
-            secondOwnerWorkspaceDetails.canCompute should be(true)
-            secondOwnerWorkspaceDetails.canShare should be(true)
+            eventually {
+              val secondOwnerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              secondOwnerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Owner)
+              secondOwnerWorkspaceDetails.canCompute should be(true)
+              secondOwnerWorkspaceDetails.canShare should be(true)
+            }
 
             // try removing new owner's sharing and compute abilities, it should not work
             val changePermissions: Set[AclEntry] = Set(AclEntry(studentA.email, WorkspaceAccessLevel.Owner, Some(false), Some(false)))
             Rawls.workspaces.updateAcl(projectName, workspaceName, changePermissions)(ownerAuthToken)
 
-            val newOwnerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
-            newOwnerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Owner)
-            newOwnerWorkspaceDetails.canCompute should be(true)
-            newOwnerWorkspaceDetails.canShare should be(true)
+            eventually {
+              val newOwnerWorkspaceDetails = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(studentAToken))
+              newOwnerWorkspaceDetails.accessLevel should equal(WorkspaceAccessLevels.Owner)
+              newOwnerWorkspaceDetails.canCompute should be(true)
+              newOwnerWorkspaceDetails.canShare should be(true)
+            }
           }(ownerAuthToken)
         }
       }
@@ -190,8 +213,10 @@ class WorkspaceApiSpec extends TestKit(ActorSystem("MySpec")) with FreeSpecLike 
   private def workspaceResponse(response: String): WorkspaceResponse = response.parseJson.convertTo[WorkspaceResponse]
 
   private def assertNoAccessToWorkspace(projectName: String, workspaceName: String)(implicit token: AuthToken): Unit = {
-    val exception = intercept[RestException](Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(token))
-    assertExceptionStatusCode(exception, 404)
+    eventually {
+      val exception = intercept[RestException](Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(token))
+      assertExceptionStatusCode(exception, 404)
+    }
   }
 
   private def assertExceptionStatusCode(exception: RestException, statusCode: Int): Unit = {
