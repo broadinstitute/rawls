91d31c9ba8295dc5b07f3621e73e9154afc2f458
dvoet <dvoet@users.noreply.github.com>
2019-06-11 11:02:33 -0400

    fix back rawls (#1098)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BucketDeletionMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BucketDeletionMonitor.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.rawls.monitor
 
 import akka.actor._
+import cats.effect.IO
 import org.broadinstitute.dsde.rawls.dataaccess.GoogleServicesDAO
 import org.broadinstitute.dsde.rawls.monitor.BucketDeletionMonitor.CheckAll
 import org.broadinstitute.dsde.rawls.dataaccess.SlickDataSource
@@ -11,6 +12,7 @@ import org.broadinstitute.dsde.rawls.dataaccess.slick.PendingBucketDeletionRecor
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration._
+import cats.implicits._
 
 object BucketDeletionMonitor {
   def props(datasource: SlickDataSource, gcsDAO: GoogleServicesDAO, initialDelay: FiniteDuration, pollInterval: FiniteDuration)(implicit executionContext: ExecutionContext): Props = {
@@ -35,9 +37,12 @@ class BucketDeletionMonitor(datasource: SlickDataSource, gcsDAO: GoogleServicesD
         dataAccess.pendingBucketDeletionQuery.list()
       }
 
-      _ <- Future.traverse(deleteRecords) { deleteRecord =>
-        deleteBucket(deleteRecord.bucket)
-      }
+      // This used to be Future.traverse but that runs all the futures right now, concurrently. When there are 100s or
+      // 1000s of buckets to delete this causes a problem by overwhelming the thread pool. Switching to use cats
+      // traverse and IO makes this all go serially which is ok in this background process.
+      _ <- deleteRecords.toList.traverse { deleteRecord =>
+        IO.fromFuture(IO(deleteBucket(deleteRecord.bucket)))
+      }.unsafeToFuture()
     } yield ()
 
 
