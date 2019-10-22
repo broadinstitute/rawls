eba23d91876bb049ccee3c3d7adb5154c0d51416
Matt Bemis <mbemis@broadinstitute.org>
2019-10-22 16:45:46 -0400

    more cleanup

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
@@ -51,22 +51,23 @@ object BootMonitors extends LazyLogging {
                    avroUpsertPubSubSubscription: String,
                    avroUpsertBucketName: String)(implicit cs: ContextShift[IO]): Unit = {
     //Reset "Launching" workflows to "Queued"
-//    resetLaunchingWorkflows(slickDataSource)
+    resetLaunchingWorkflows(slickDataSource)
 
     //Boot billing project creation monitor
-//    startCreatingBillingProjectMonitor(system, slickDataSource, gcsDAO, samDAO, projectTemplate, requesterPaysRole)
+    startCreatingBillingProjectMonitor(system, slickDataSource, gcsDAO, samDAO, projectTemplate, requesterPaysRole)
 
     //Boot submission monitor supervisor
     val submissionmonitorConfigRoot = conf.getConfig("submissionmonitor")
     val submissionMonitorConfig = SubmissionMonitorConfig(util.toScalaDuration(submissionmonitorConfigRoot.getDuration("submissionPollInterval")), submissionmonitorConfigRoot.getBoolean("trackDetailedSubmissionMetrics"))
-//    startSubmissionMonitorSupervisor(system, submissionMonitorConfig, slickDataSource, samDAO, gcsDAO, shardedExecutionServiceCluster, metricsPrefix)
+    startSubmissionMonitorSupervisor(system, submissionMonitorConfig, slickDataSource, samDAO, gcsDAO, shardedExecutionServiceCluster, metricsPrefix)
 
     //Boot workflow submission actors
-//    startWorkflowSubmissionActors(system, conf, slickDataSource, gcsDAO, samDAO, methodRepoDAO, dosResolver, shardedExecutionServiceCluster, maxActiveWorkflowsTotal, maxActiveWorkflowsPerUser, metricsPrefix, requesterPaysRole, useWorkflowCollectionField, useWorkflowCollectionLabel, defaultBackend, methodConfigResolver)
+    startWorkflowSubmissionActors(system, conf, slickDataSource, gcsDAO, samDAO, methodRepoDAO, dosResolver, shardedExecutionServiceCluster, maxActiveWorkflowsTotal, maxActiveWorkflowsPerUser, metricsPrefix, requesterPaysRole, useWorkflowCollectionField, useWorkflowCollectionLabel, defaultBackend, methodConfigResolver)
 
     //Boot bucket deletion monitor
-//    startBucketDeletionMonitor(system, slickDataSource, gcsDAO)
+    startBucketDeletionMonitor(system, slickDataSource, gcsDAO)
 
+    //Boot the avro upsert monitor to read and process messages in the specified PubSub topic
     startAvroUpsertMonitor(system, workspaceService, gcsDAO, samDAO, googleStorage, pubSubDAO, avroUpsertPubSubProject, avroUpsertPubSubTopic, avroUpsertPubSubSubscription, avroUpsertBucketName)
   }
 
