f48dd76e85f40364da86d9d97bb1e68304dd3ca0
Matt Bemis <mbemis@broadinstitute.org>
2020-05-19 14:41:01 -0400

    bump WSM client to 0.0.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -93,7 +93,7 @@ object Dependencies {
 
   val accessContextManager = "com.google.apis" % "google-api-services-accesscontextmanager" % "v1beta-rev55-1.25.0"
 
-  val workspaceManager = excludeGuavaJDK5("bio.terra" % "terra-workspace-manager-client" % "0.0.1-SNAPSHOT")
+  val workspaceManager = excludeGuavaJDK5("bio.terra" % "terra-workspace-manager-client" % "0.0.2-SNAPSHOT")
 
   val opencensusScalaCode: ModuleID = "com.github.sebruck" %% "opencensus-scala-core" % "0.7.0-M2"
   val opencensusAkkaHttp: ModuleID = "com.github.sebruck" %% "opencensus-scala-akka-http" % "0.7.0-M2"
