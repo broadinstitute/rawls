0a8df136feb50f99f2667c9223cbdf041649ba6d
Matt Bemis <mbemis@broadinstitute.org>
2020-05-22 15:49:47 -0400

    use case class for TDR snapshot request

--- core/src/main/scala/org/broadinstitute/dsde/rawls/model/DataReferenceModel.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/model/DataReferenceModel.scala
@@ -3,11 +3,14 @@ package org.broadinstitute.dsde.rawls.model
 import bio.terra.workspace.model.DataReferenceDescription
 import bio.terra.workspace.model.DataReferenceList
 import spray.json.DefaultJsonProtocol._
+import spray.json.{JsObject, JsString, JsValue, JsonWriter}
+
 import scala.collection.JavaConverters._
 
 case class DataRepoSnapshot(name: String, snapshotId: String)
 case class DataRepoSnapshotReference(referenceId: String, name: String, workspaceId: String, referenceType: Option[String], reference: Option[String], cloningInstructions: String)
 case class DataRepoSnapshotList(snapshots: List[DataRepoSnapshotReference])
+case class TerraDataRepoSnapshotRequest(instance: String, snapshot: String)
 
 object DataRepoSnapshotReference {
   def apply(ref: DataReferenceDescription): DataRepoSnapshotReference = {
@@ -27,4 +30,14 @@ object DataReferenceModelJsonSupport {
   implicit val DataRepoSnapshotFormat = jsonFormat2(DataRepoSnapshot)
   implicit val DataRepoSnapshotReferenceFormat = jsonFormat6(DataRepoSnapshotReference.apply)
   implicit val DataRepoSnapshotListFormat = jsonFormat1(DataRepoSnapshotList.apply)
+  implicit val TerraDataRepoSnapshotRequestFormat = jsonFormat2(TerraDataRepoSnapshotRequest)
+
+  implicit val terraDataRepoSansphotRequestJsonWriter = new JsonWriter[TerraDataRepoSnapshotRequest] {
+    def write(request: TerraDataRepoSnapshotRequest): JsValue = {
+      JsObject(
+        "instance" -> JsString(request.instance),
+        "snapshot" -> JsString(request.snapshot)
+      )
+    }
+  }
 }
