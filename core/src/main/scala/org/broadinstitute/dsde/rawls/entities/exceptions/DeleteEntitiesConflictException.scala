ec0b02bd6d21c1c912355c0fd343046d4083f5a4
David An <davidan@broadinstitute.org>
2020-05-29 12:53:52 -0400

    remove redundant braces

--- core/src/main/scala/org/broadinstitute/dsde/rawls/entities/exceptions/DeleteEntitiesConflictException.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/entities/exceptions/DeleteEntitiesConflictException.scala
@@ -2,4 +2,4 @@ package org.broadinstitute.dsde.rawls.entities.exceptions
 
 import org.broadinstitute.dsde.rawls.model.AttributeEntityReference
 
-class DeleteEntitiesConflictException(val referringEntities: Set[AttributeEntityReference]) extends DataEntityException {}
+class DeleteEntitiesConflictException(val referringEntities: Set[AttributeEntityReference]) extends DataEntityException
