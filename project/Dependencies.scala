5f140a28fffa032286fc4b26f4e34ee4447566d0
dvoet <dvoet@users.noreply.github.com>
2020-06-11 09:15:10 -0400

    CA-853 updated swagger version (#1228)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   val jodaConvert: ModuleID =     "org.joda"                      % "joda-convert"          % "1.8"
   val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.3.0"
   val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "1.7.30"
-  val swaggerUI: ModuleID =       "org.webjars"                   % "swagger-ui"            % "2.2.5"
+  val swaggerUI: ModuleID =       "org.webjars"                   % "swagger-ui"            % "3.25.0"
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val httpClient: ModuleID =      "org.apache.httpcomponents"     % "httpclient"            % "4.5.3"  // upgrading a transitive dependency to avoid security warnings
   val cats: ModuleID =            "org.typelevel"                 %% "cats"                 % "0.9.0"
