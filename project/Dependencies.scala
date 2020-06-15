6ee96fd017045b243e9ebe3a1228af35561edef5
David An <davidan@broadinstitute.org>
2020-06-15 12:08:35 -0400

    new google2 lib, syntax compatibility fixes; assembly is known broken

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val googleV = "1.22.0"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
-  val workbenchGoogle2V = "0.6-4df42ca"
+  val workbenchGoogle2V = "0.10-bd284e7"
 
   val cromwellVersion = "40-2754783"
 
