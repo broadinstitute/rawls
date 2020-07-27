19d2d69607174965f17d03b132969d822c76e404
David An <davidan@broadinstitute.org>
2020-07-27 13:28:43 -0400

    use final version of google2 lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val googleV = "1.22.0"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
-  val workbenchGoogle2V = "0.11-b9da9fc-SNAP"
+  val workbenchGoogle2V = "0.11-0d87698"
 
   val cromwellVersion = "40-2754783"
 
