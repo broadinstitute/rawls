97f828f84b06a66299a9d48f7277273c4f3561b2
zloery <zloery@gmail.com>
2020-07-28 17:12:12 -0400

    Update rawls to version 0.1.0 of workspace manager client library

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/workspacemanager/WorkspaceManagerDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/workspacemanager/WorkspaceManagerDAO.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.dsde.rawls.dataaccess.workspacemanager
 import java.util.UUID
 
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
-import bio.terra.workspace.model.{CreatedWorkspace, DataReferenceDescription, DataReferenceList, WorkspaceDescription}
+import bio.terra.workspace.model.{CloningInstructionsEnum, CreatedWorkspace, DataReferenceDescription, DataReferenceList, ReferenceTypeEnum, WorkspaceDescription}
 import org.broadinstitute.dsde.rawls.model.DataReferenceName
 import org.broadinstitute.dsde.workbench.model.ErrorReportSource
 
@@ -13,10 +13,10 @@ trait WorkspaceManagerDAO {
   def getWorkspace(workspaceId: UUID, accessToken: OAuth2BearerToken): WorkspaceDescription
   def createWorkspace(workspaceId: UUID, folderManagerAccessToken: OAuth2BearerToken, bodyAccessToken: OAuth2BearerToken): CreatedWorkspace
   def deleteWorkspace(workspaceId: UUID, folderManagerAccessToken: OAuth2BearerToken, bodyAccessToken: OAuth2BearerToken): Unit
-  def createDataReference(workspaceId: UUID, name: DataReferenceName, referenceType: String, reference: String, cloningInstructions: String, accessToken: OAuth2BearerToken): DataReferenceDescription
+  def createDataReference(workspaceId: UUID, name: DataReferenceName, referenceType: ReferenceTypeEnum, reference: String, cloningInstructions: CloningInstructionsEnum, accessToken: OAuth2BearerToken): DataReferenceDescription
   def deleteDataReference(workspaceId: UUID, referenceId: UUID, accessToken: OAuth2BearerToken): Unit
   def getDataReference(workspaceId: UUID, referenceId: UUID, accessToken: OAuth2BearerToken): DataReferenceDescription
-  def getDataReferenceByName(workspaceId: UUID, refType: String, refName: DataReferenceName, accessToken: OAuth2BearerToken): DataReferenceDescription
+  def getDataReferenceByName(workspaceId: UUID, refType: ReferenceTypeEnum, refName: DataReferenceName, accessToken: OAuth2BearerToken): DataReferenceDescription
   def enumerateDataReferences(workspaceId: UUID, offset: Int, limit: Int, accessToken: OAuth2BearerToken): DataReferenceList
 
 }
--- core/src/test/scala/org/broadinstitute/dsde/rawls/entities/datarepo/DataRepoEntityProviderBuilderSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/entities/datarepo/DataRepoEntityProviderBuilderSpec.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.dsde.rawls.entities.datarepo
 import java.util.UUID
 
 import akka.http.scaladsl.model.StatusCodes
-import bio.terra.workspace.model.DataReferenceDescription.ReferenceTypeEnum
+import bio.terra.workspace.model.ReferenceTypeEnum
 import org.broadinstitute.dsde.rawls.TestExecutionContext
 import org.broadinstitute.dsde.rawls.dataaccess.slick.TestDriverComponent
 import org.broadinstitute.dsde.rawls.entities.EntityRequestArguments
@@ -62,7 +62,7 @@ class DataRepoEntityProviderBuilderSpec extends FlatSpec with DataRepoEntityProv
     )
 
     val ex = intercept[DataEntityException] { builder.build(defaultEntityRequestArguments).get }
-    assertResult( s"Reference type value for referenceName is not of type ${ReferenceTypeEnum.DATAREPOSNAPSHOT.getValue}" ) { ex.getMessage }
+    assertResult( s"Reference type value for referenceName is not of type ${ReferenceTypeEnum.DATA_REPO_SNAPSHOT.getValue}" ) { ex.getMessage }
   }
 
   behavior of "DataRepoEntityProviderBuilder.lookupSnapshotForName()"
@@ -87,7 +87,7 @@ class DataRepoEntityProviderBuilderSpec extends FlatSpec with DataRepoEntityProv
     )
 
     val ex = intercept[DataEntityException] { builder.lookupSnapshotForName(DataReferenceName("foo"), defaultEntityRequestArguments) }
-    assertResult( s"Reference type value for foo is not of type ${ReferenceTypeEnum.DATAREPOSNAPSHOT.getValue}" ) { ex.getMessage }
+    assertResult( s"Reference type value for foo is not of type ${ReferenceTypeEnum.DATA_REPO_SNAPSHOT.getValue}" ) { ex.getMessage }
   }
 
   it should "error if workspace manager returns something other than serialized json object in reference payload" in  {
