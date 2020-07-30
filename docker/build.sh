ad11f71b2503f9fd442edb83232fdb1d445fa84a
Zybjana Bedo <zbedo@broadinstitute.org>
2020-07-30 10:34:15 -0400

    [No ticket] Security scan for docker image

--- docker/build.sh
+++ docker/build.sh
@@ -143,9 +143,16 @@ function docker_cmd()
         echo "building ${DOCKERHUB_REGISTRY}:${HASH_TAG}..."
         docker build -t $DOCKERHUB_REGISTRY:${HASH_TAG} .
 
+        echo "scanning ${DOCKERHUB_REGISTRY}:${HASH_TAG}..."
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --exit-code 1 --severity CRITICAL "$DOCKERHUB_REGISTRY":"${HASH_TAG}"
+
         echo "building ${DOCKERHUB_TESTS_REGISTRY}:${HASH_TAG}..."
         cd automation
         docker build -f Dockerfile-tests -t $DOCKERHUB_TESTS_REGISTRY:${HASH_TAG} .
+
+        echo "scanning $DOCKERHUB_TESTS_REGISTRY:${HASH_TAG}..."
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --exit-code 1 --severity CRITICAL "$DOCKERHUB_TESTS_REGISTRY":"${HASH_TAG}"
+
         cd ..
 
         if [ $DOCKER_CMD="push" ]; then
