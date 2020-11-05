92eeaba968ccb6f67115c5ea35da3eb5e99cf698
Douglas Voet <dvoet@users.noreply.github.com>
2020-11-05 09:12:52 -0500

    add billing v2 routes to rawls api

--- core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/RawlsApiService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/RawlsApiService.scala
@@ -56,7 +56,7 @@ object RawlsApiService {
 
 trait RawlsApiService //(val workspaceServiceConstructor: UserInfo => WorkspaceService, val userServiceConstructor: UserInfo => UserService, val genomicsServiceConstructor: UserInfo => GenomicsService, val statisticsServiceConstructor: UserInfo => StatisticsService, val statusServiceConstructor: () => StatusService, val executionServiceCluster: ExecutionServiceCluster, val appVersion: ApplicationVersion, val googleClientId: String, val submissionTimeout: FiniteDuration, override val workbenchMetricBaseName: String, val samDAO: SamDAO, val swaggerConfig: SwaggerConfig)(implicit val executionContext: ExecutionContext, val materializer: Materializer)
   extends WorkspaceApiService with EntityApiService with MethodConfigApiService with SubmissionApiService
-  with AdminApiService with UserApiService with BillingApiService with NotificationsApiService with SnapshotApiService
+  with AdminApiService with UserApiService with BillingApiService with BillingApiServiceV2 with NotificationsApiService with SnapshotApiService
   with StatusApiService with InstrumentationDirectives with SwaggerRoutes with VersionApiService with ServicePerimeterApiService {
 
   val workspaceServiceConstructor: UserInfo => WorkspaceService
@@ -79,7 +79,7 @@ trait RawlsApiService //(val workspaceServiceConstructor: UserInfo => WorkspaceS
 
   // enable/disable snapshot routes based on a config flag
   val dataRepoEnabled = Try(ConfigFactory.load().getBoolean("dataRepo.enabled")).toOption.getOrElse(false)
-  val baseApiRoutes = workspaceRoutes ~ entityRoutes ~ methodConfigRoutes ~ submissionRoutes ~ adminRoutes ~ userRoutes ~ billingRoutes ~ notificationsRoutes ~ servicePerimeterRoutes
+  val baseApiRoutes = workspaceRoutes ~ entityRoutes ~ methodConfigRoutes ~ submissionRoutes ~ adminRoutes ~ userRoutes ~ billingRoutes ~ billingRoutesV2 ~ notificationsRoutes ~ servicePerimeterRoutes
 
   val concatenatedRoutes = if (dataRepoEnabled) {
     baseApiRoutes ~ snapshotRoutes
