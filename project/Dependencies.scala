46b06e64b17c1b20ed883be5273bc25eff94acbf
David An <davidan@broadinstitute.org>
2020-11-24 15:04:46 -0500

    update dependencies to match new workbench-libs google2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,8 +57,8 @@ object Dependencies {
   val googleDeploymentManager: ModuleID = "com.google.apis"   % "google-api-services-deploymentmanager" % ("v2beta-rev20181207-1.28.0")
   val googleGuava: ModuleID =             "com.google.guava"  % "guava" % "19.0"
 
-  val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.30.0"
-  val googleRpcNettyShaded: ModuleID =    "io.grpc" % "grpc-netty-shaded" % "1.30.0"
+  val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.33.1"
+  val googleRpcNettyShaded: ModuleID =    "io.grpc" % "grpc-netty-shaded" % "1.33.1"
   val googleCloudCoreGrpc: ModuleID =     "com.google.cloud" % "google-cloud-core-grpc" % "1.93.6"
 
   val googleAutoValue: ModuleID =         "com.google.auto.value" % "auto-value-annotations" % "1.7.4"
