9bcb191c720b7eb9644be786ee10b9809bbd76c9
Matt Bemis <MatthewBemis@users.noreply.github.com>
2020-12-08 23:32:41 -0500

    AS-535: Remove default bucket IAM after workspace bucket creation (#1327)
    * override bucket IAM rather than adding to it when creating workspace buckets

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val googleV = "1.22.0"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
-  val workbenchGoogle2V = "0.16-42883ed"
+  val workbenchGoogle2V = "0.18-4631ebf"
 
   val cromwellVersion = "40-2754783"
 
