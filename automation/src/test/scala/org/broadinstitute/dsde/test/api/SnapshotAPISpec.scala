c30c0d0b43833b033a1a683237bdffc351b0be8e
David An <davidan@broadinstitute.org>
2020-12-14 09:38:32 -0500

    separate tests for all owners, WIP

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
@@ -36,64 +36,67 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers
   val dataRepoBaseUrl = "https://jade-4.datarepo-integration.broadinstitute.org/"
 
   "TDR Snapshot integration" - {
-    "should allow snapshot references to be added to workspaces" in {
-      val owner: Credentials = UserPool.chooseProjectOwner
-      implicit val ownerAuthToken: AuthToken = owner.makeAuthToken()
 
-      withCleanBillingProject(owner) { projectName =>
-        withWorkspace(projectName, s"${UUID.randomUUID().toString}-snapshot references") { workspaceName =>
+    UserPool.userConfig.Owners.userMap.values.foreach { owner =>
+      s"as ${owner.email}, should allow snapshot references to be added to workspaces" in {
+//        val owner: Credentials = UserPool.chooseProjectOwner
+        implicit val ownerAuthToken: AuthToken = owner.makeAuthToken()
+
+        withCleanBillingProject(owner) { projectName =>
+          withWorkspace(projectName, s"${UUID.randomUUID().toString}-snapshot references") { workspaceName =>
 
 
-          // call data repo to list snapshots
-          // this gets the most recent snapshots in TDR (to which we have read access). This can cause tests to change
-          // over time, if the snapshots keep changing. It's here for convenience - we can always add/remove snapshots from
-          // TDR and this code will always pick up the latest. But if it causes flakiness we could change to sorting by
-          // created_date ASC - that should be more stable.
-          val dataRepoDAO = new TestDataRepoDAO("terra", dataRepoBaseUrl).getRepositoryApi(ownerAuthToken)
+            // call data repo to list snapshots
+            // this gets the most recent snapshots in TDR (to which we have read access). This can cause tests to change
+            // over time, if the snapshots keep changing. It's here for convenience - we can always add/remove snapshots from
+            // TDR and this code will always pick up the latest. But if it causes flakiness we could change to sorting by
+            // created_date ASC - that should be more stable.
+            val dataRepoDAO = new TestDataRepoDAO("terra", dataRepoBaseUrl).getRepositoryApi(ownerAuthToken)
 
-          logger.info(s"!!!!!!!!!!!! calling data repo at $dataRepoBaseUrl as user ${owner.email} ... ")
-          val drSnapshots = Try(dataRepoDAO.enumerateSnapshots(
-            0, 2, "created_date", "desc", "")) match {
-            case Success(s) => s
-            case Failure(ex) =>
-              logger.error(s"!!!!!!!!!!!! data repo call as user ${owner.email} failed: ${ex.getMessage}", ex)
-              throw(ex)
+            logger.info(s"!!!!!!!!!!!! calling data repo at $dataRepoBaseUrl as user ${owner.email} ... ")
+            val drSnapshots = Try(dataRepoDAO.enumerateSnapshots(
+              0, 2, "created_date", "desc", "")) match {
+              case Success(s) => s
+              case Failure(ex) =>
+                logger.error(s"!!!!!!!!!!!! data repo call as user ${owner.email} failed: ${ex.getMessage}", ex)
+                throw(ex)
+            }
+            assume(drSnapshots.getItems.size() == 2,
+              s"TDR at $dataRepoBaseUrl did not have 2 snapshots for this test to use!")
+
+            val dataRepoSnapshotId = drSnapshots.getItems.get(0).getId
+            val anotherDataRepoSnapshotId = drSnapshots.getItems.get(1).getId
+
+            // add snapshot reference to the workspace. Under the covers, this creates the workspace in WSM and adds the ref
+            createSnapshotReference(projectName, workspaceName, dataRepoSnapshotId, "firstSnapshot")
+
+            // validate the snapshot was added correctly: list snapshots in Rawls, should return 1, which we just added.
+            // if we can successfully list snapshot references, it means WSM created its copy of the workspace
+            val firstListResponse = listSnapshotReferences(projectName, workspaceName)
+            val firstResources = Rawls.parseResponseAs[DataReferenceList](firstListResponse).getResources.asScala
+            firstResources.size shouldBe 1
+            firstResources.head.getName shouldBe "firstSnapshot"
+            firstResources.head.getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
+            firstResources.head.getReference.getSnapshot shouldBe dataRepoSnapshotId
+
+            // add a second snapshot reference to the workspace. Under the covers, this recognizes the workspace
+            // already exists in WSM, so it just adds the ref
+            createSnapshotReference(projectName, workspaceName, anotherDataRepoSnapshotId, "secondSnapshot")
+
+            // validate the second snapshot was added correctly: list snapshots in Rawls, should return 2, which we just added
+            val secondListResponse = listSnapshotReferences(projectName, workspaceName)
+            // sort by reference name for easy predictability inside this test: "firstSnapshot" is before "secondSnapshot"
+            val secondResources = Rawls.parseResponseAs[DataReferenceList](secondListResponse)
+              .getResources.asScala.sortBy(_.getName)
+            secondResources.size shouldBe 2
+            secondResources.head.getName shouldBe "firstSnapshot"
+            secondResources.head.getReference.getSnapshot shouldBe dataRepoSnapshotId
+            secondResources.head.getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
+            secondResources(1).getName shouldBe "secondSnapshot"
+            secondResources(1).getReference.getSnapshot shouldBe anotherDataRepoSnapshotId
+            secondResources(1).getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
+
           }
-          assume(drSnapshots.getItems.size() == 2,
-            s"TDR at $dataRepoBaseUrl did not have 2 snapshots for this test to use!")
-
-          val dataRepoSnapshotId = drSnapshots.getItems.get(0).getId
-          val anotherDataRepoSnapshotId = drSnapshots.getItems.get(1).getId
-
-          // add snapshot reference to the workspace. Under the covers, this creates the workspace in WSM and adds the ref
-          createSnapshotReference(projectName, workspaceName, dataRepoSnapshotId, "firstSnapshot")
-
-          // validate the snapshot was added correctly: list snapshots in Rawls, should return 1, which we just added.
-          // if we can successfully list snapshot references, it means WSM created its copy of the workspace
-          val firstListResponse = listSnapshotReferences(projectName, workspaceName)
-          val firstResources = Rawls.parseResponseAs[DataReferenceList](firstListResponse).getResources.asScala
-          firstResources.size shouldBe 1
-          firstResources.head.getName shouldBe "firstSnapshot"
-          firstResources.head.getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
-          firstResources.head.getReference.getSnapshot shouldBe dataRepoSnapshotId
-
-          // add a second snapshot reference to the workspace. Under the covers, this recognizes the workspace
-          // already exists in WSM, so it just adds the ref
-          createSnapshotReference(projectName, workspaceName, anotherDataRepoSnapshotId, "secondSnapshot")
-
-          // validate the second snapshot was added correctly: list snapshots in Rawls, should return 2, which we just added
-          val secondListResponse = listSnapshotReferences(projectName, workspaceName)
-          // sort by reference name for easy predictability inside this test: "firstSnapshot" is before "secondSnapshot"
-          val secondResources = Rawls.parseResponseAs[DataReferenceList](secondListResponse)
-            .getResources.asScala.sortBy(_.getName)
-          secondResources.size shouldBe 2
-          secondResources.head.getName shouldBe "firstSnapshot"
-          secondResources.head.getReference.getSnapshot shouldBe dataRepoSnapshotId
-          secondResources.head.getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
-          secondResources(1).getName shouldBe "secondSnapshot"
-          secondResources(1).getReference.getSnapshot shouldBe anotherDataRepoSnapshotId
-          secondResources(1).getReferenceType shouldBe ReferenceTypeEnum.DATA_REPO_SNAPSHOT
-
         }
       }
     }
