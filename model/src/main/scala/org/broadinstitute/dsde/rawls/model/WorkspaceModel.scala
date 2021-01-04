70cc85ab4b09ca0551342f47c825369e8312eef4
Andrew Jang <andy7i@users.noreply.github.com>
2021-01-04 14:23:51 -0500

    make it clear the companion object is used for unit tests only and that the values for google project ID and Number don't matter

--- model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
+++ model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
@@ -144,19 +144,25 @@ case class Workspace(
   lazy val workspaceIdAsUUID: UUID = UUID.fromString(workspaceId)
 }
 
+/** convenience constructor (for unit tests only!)
+  * defaults workspace version to v2 and google project id and google project number to random strings
+  *
+   */
 object Workspace {
-  /** convenience constructor that defaults workspace version to v2 and google project to namespace */
   def apply(namespace: String,
-    name: String,
-    workspaceId: String,
-    bucketName: String,
-    workflowCollectionName: Option[String],
-    createdDate: DateTime,
-    lastModified: DateTime,
-    createdBy: String,
-    attributes: AttributeMap,
-    isLocked: Boolean = false): Workspace = {
-    new Workspace(namespace, name, workspaceId, bucketName, workflowCollectionName, createdDate, lastModified, createdBy, attributes, isLocked, WorkspaceVersions.V2, GoogleProjectId(namespace), None)
+            name: String,
+            workspaceId: String,
+            bucketName: String,
+            workflowCollectionName: Option[String],
+            createdDate: DateTime,
+            lastModified: DateTime,
+            createdBy: String,
+            attributes: AttributeMap,
+            isLocked: Boolean = false): Workspace = {
+    val randomString = java.util.UUID.randomUUID().toString
+    val googleProjectId = GoogleProjectId(randomString)
+    val googleProjectNumber = GoogleProjectNumber(randomString)
+    new Workspace(namespace, name, workspaceId, bucketName, workflowCollectionName, createdDate, lastModified, createdBy, attributes, isLocked, WorkspaceVersions.V2, googleProjectId, Option(googleProjectNumber))
   }
 }
 
