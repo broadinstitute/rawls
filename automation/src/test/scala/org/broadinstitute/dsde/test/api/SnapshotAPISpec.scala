32e8c0402ddd288861369a4abb72c341b00c5f13
David An <davidan@broadinstitute.org>
2021-01-11 19:52:51 -0500

    AS-550: CUJ test to verify TDR tables [risk: no] (#1344)
    CUJ test to verify TDR tables

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
@@ -5,11 +5,15 @@ import akka.http.scaladsl.model.{StatusCodes, Uri}
 import akka.http.scaladsl.model.Uri.{Path, Query}
 import bio.terra.datarepo.api.RepositoryApi
 import bio.terra.datarepo.client.ApiClient
+import bio.terra.datarepo.model.{EnumerateSnapshotModel, SnapshotModel}
 import bio.terra.workspace.model.{DataReferenceList, ReferenceTypeEnum}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
 import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.rawls.model.EntityTypeMetadata
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.ServiceTestConfig.FireCloud
-import org.broadinstitute.dsde.workbench.config.UserPool
+import org.broadinstitute.dsde.workbench.config.{Credentials, UserPool}
 import org.broadinstitute.dsde.workbench.fixture.{BillingFixtures, WorkspaceFixtures}
 import org.broadinstitute.dsde.workbench.service.Rawls
 import org.broadinstitute.dsde.workbench.service.util.Tags
@@ -29,7 +33,8 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
 
   override protected def beforeAll(): Unit = {
     assert(Try(Uri.parseAbsolute(dataRepoBaseUrl)).isSuccess,
-      s"---> Aborting! Tests in this suite would fail because [$dataRepoBaseUrl] is not a valid url for data repo <---")
+      s"---> Aborting! Tests in this suite would fail because [$dataRepoBaseUrl] is not a valid url for data repo." +
+        s" This is a problem in test config, not in the runtime code. <---")
   }
 
   "TDR Snapshot integration" - {
@@ -55,23 +60,7 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
       withCleanBillingProject(owner) { projectName =>
         withWorkspace(projectName, s"${UUID.randomUUID().toString}-snapshot references") { workspaceName =>
 
-          // call data repo to list snapshots
-          // this gets the most recent snapshots in TDR (to which we have read access). This can cause tests to change
-          // over time, if the snapshots keep changing. It's here for convenience - we can always add/remove snapshots from
-          // TDR and this code will always pick up the latest. But if it causes flakiness we could change to sorting by
-          // created_date ASC - that should be more stable.
-          val dataRepoDAO = new TestDataRepoDAO("terra", dataRepoBaseUrl).getRepositoryApi(ownerAuthToken)
-
-          logger.info(s"calling data repo at $dataRepoBaseUrl as user ${owner.email} ... ")
-          val drSnapshots = Try(dataRepoDAO.enumerateSnapshots(
-            0, 2, "created_date", "desc", "")) match {
-            case Success(s) => s
-            case Failure(ex) =>
-              logger.error(s"data repo call as user ${owner.email} failed: ${ex.getMessage}", ex)
-              throw ex
-          }
-          assume(drSnapshots.getItems.size() == 2,
-            s"TDR at $dataRepoBaseUrl did not have 2 snapshots for this test to use!")
+          val drSnapshots = listDataRepoSnapshots(2, owner)(ownerAuthToken)
 
           val dataRepoSnapshotId = drSnapshots.getItems.get(0).getId
           val anotherDataRepoSnapshotId = drSnapshots.getItems.get(1).getId
@@ -110,8 +99,63 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
       }
     }
 
+    "should report the same tables/columns via metadata API as TDR reports" taggedAs(Tags.AlphaTest, Tags.ExcludeInFiab) in {
+      val numSnapshotsToVerify = 2
+
+      // as of this writing, hermione.owner is the user with access to snapshots
+      val owner = UserPool.userConfig.Owners.getUserCredential("hermione")
+
+      implicit val ownerAuthToken: AuthToken = owner.makeAuthToken()
+
+      // get N snapshots from TDR
+      val drSnapshots = listDataRepoSnapshots(numSnapshotsToVerify, owner)(ownerAuthToken)
+
+      withCleanBillingProject(owner) { projectName =>
+        withWorkspace(projectName, s"${UUID.randomUUID().toString}-snapshot references") { workspaceName =>
+          // loop through each snapshot, and:
+          drSnapshots.getItems.asScala.foreach { snapSummary =>
+            info(s"for snapshot ${snapSummary.getId}: ${snapSummary.getName}")
+
+            // workspace manager enforces validation on reference names:
+            // "Name must be 1 to 63 alphanumeric characters or underscores, and cannot start with an underscore."
+            // so, we replace all dashes (in the uuid) with underscores
+            val referenceName = s"refname_${snapSummary.getId}".replaceAll("-", "_")
+
+            // register the snapshot in Rawls
+            createSnapshotReference(projectName, workspaceName, snapSummary.getId, referenceName)
+
+            // call TDR to describe the snapshot; this includes table metadata
+            val tdrModel = describeDataRepoSnapshot(snapSummary.getId, owner)(ownerAuthToken)
+
+            // call Rawls' entity-type-metadata API for this snapshot reference; this includes table metadata
+            val rawlsModel: Map[String, EntityTypeMetadata] = getEntityTypeMetadata(projectName, workspaceName, referenceName)
+
+            // assert the two versions of metadata have the same tables
+            val tdrTables = tdrModel.getTables.asScala
+            val tdrTableNames = tdrTables.map(_.getName).toSet
+            val rawlsTableNames = rawlsModel.keySet
+
+            withClue(s"Rawls and TDR did not describe the same snapshot tables for reference $referenceName:") {
+              rawlsTableNames should contain theSameElementsAs(tdrTableNames)
+            }
+
+            // for each table, assert the two versions of metadata have the same column names
+            tdrTables.foreach { table =>
+              val tdrColumnNames = table.getColumns.asScala.map(_.getName).toSet
+              val rawlsColumnNames = rawlsModel(table.getName).attributeNames.toSet
+              withClue(s"Rawls and TDR did not describe the same column names for table '${table.getName}' in reference $referenceName:") {
+                rawlsColumnNames should contain theSameElementsAs(tdrColumnNames)
+              }
+            }
+          }
+        }
+      }
+
+    }
+
   }
 
+  // ==================== Rawls helpers ====================
   private def listSnapshotReferences(projectName: String, workspaceName: String, offset: Int = 0, limit: Int = 10)(implicit authToken: AuthToken) = {
     val targetRawlsUrl  = Uri(Rawls.url)
       .withPath(Path(s"/api/workspaces/$projectName/$workspaceName/snapshots"))
@@ -126,6 +170,59 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
       uri = targetRawlsUrl.toString(),
       content = payload)
   }
+
+  private def getEntityTypeMetadata(projectName: String, workspaceName: String, snapRefName: String)(implicit authToken: AuthToken): Map[String, EntityTypeMetadata] = {
+    val targetRawlsUrl  = Uri(Rawls.url)
+      .withPath(Path(s"/api/workspaces/$projectName/$workspaceName/entities"))
+      .withQuery(Query(Map("dataReference" -> snapRefName)))
+    val response = Rawls.getRequest(uri = targetRawlsUrl.toString)
+
+    // workbench-libs' serviceTest code - e.g. Rawls.parseResponseAs - fails to correctly parse Maps from json.
+    // I don't know why, and I am reluctant to change that code just to make this test work. We should fix it
+    // at the source at some point.
+    val respString = Rawls.parseResponse(response)
+    val mapper = new ObjectMapper() with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    val metadataResponseObject = mapper.readValue[Map[String, EntityTypeMetadata]](respString)
+
+    metadataResponseObject
+  }
+
+  // ==================== Data Repo helpers ====================
+  private def listDataRepoSnapshots(numSnapshots: Int, credentials: Credentials)(implicit authToken: AuthToken): EnumerateSnapshotModel = {
+    // call data repo to list snapshots
+    // this gets the most recent snapshots in TDR (to which we have read access). This can cause tests to change
+    // over time, if the snapshots keep changing. It's here for convenience - we can always add/remove snapshots from
+    // TDR and this code will always pick up the latest. But if it causes flakiness we could change to sorting by
+    // created_date ASC - that should be more stable.
+    val dataRepoApi = new TestDataRepoDAO("terra", dataRepoBaseUrl).getRepositoryApi(authToken)
+
+    logger.info(s"calling data repo at $dataRepoBaseUrl as user ${credentials.email} ... ")
+    val drSnapshots = Try(dataRepoApi.enumerateSnapshots(
+      0, numSnapshots, "created_date", "desc", "")) match {
+      case Success(s) => s
+      case Failure(ex) =>
+        logger.error(s"data repo call as user ${credentials.email} failed: ${ex.getMessage}", ex)
+        throw ex
+    }
+    assume(drSnapshots.getItems.size() == numSnapshots,
+      s"---> TDR at $dataRepoBaseUrl did not have $numSnapshots snapshots for this test to use!" +
+        s" This is likely a problem in environment setup, but has a chance of being a problem in runtime code. <---")
+
+    drSnapshots
+  }
+
+  private def describeDataRepoSnapshot(snapshotId: String, credentials: Credentials)(implicit authToken: AuthToken): SnapshotModel = {
+    val dataRepoApi = new TestDataRepoDAO("terra", dataRepoBaseUrl).getRepositoryApi(authToken)
+
+    Try(dataRepoApi.retrieveSnapshot(snapshotId)) match {
+      case Success(s) => s
+      case Failure(ex) =>
+        logger.error(s"data repo call as user ${credentials.email} failed: ${ex.getMessage}", ex)
+        throw ex
+    }
+  }
+
 
   // a bastardized version of the HttpDataRepoDAO in the main rawls codebase
   class TestDataRepoDAO(dataRepoInstanceName: String, dataRepoInstanceBasePath: String) {
