22a1a38da0c442609c024189b8e2827fede75742
Matt Bemis <mbemis@broadinstitute.org>
2021-03-17 21:41:36 -0400

    clean up some imports

--- core/src/main/scala/org/broadinstitute/dsde/rawls/genomics/GenomicsService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/genomics/GenomicsService.scala
@@ -1,12 +1,9 @@
 package org.broadinstitute.dsde.rawls.genomics
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
 import org.broadinstitute.dsde.rawls.dataaccess._
 import org.broadinstitute.dsde.rawls.model._
 import org.broadinstitute.dsde.rawls.util.{FutureSupport, RoleSupport, UserWiths}
-import org.broadinstitute.dsde.rawls.webservice.PerRequest.{PerRequestMessage, RequestComplete}
-import spray.json.{JsObject, PrettyPrinter}
+import spray.json.JsObject
 
 import scala.concurrent.{ExecutionContext, Future}
 
