45c48b49ef250fddf89c5690f31c67d894e31fde
yonghao <yyhao1@gmail.com>
2021-04-12 12:32:31 -0400

    format code

--- core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
@@ -1146,10 +1146,17 @@ class WorkspaceApiServiceSpec extends ApiServiceSpec {
 
 
   it should "clone workspace use different bucket location if bucketLocation is in request" in withApiServicesMockitoGcsDao { services =>
-    val newBucketLocation = Option("us-terra-1");
-    val workspaceCopy = WorkspaceRequest(namespace = testData.workspace.namespace, name = "test_copy", Map.empty, bucketLocation = Option("us-terra1"))
-    when(services.gcsDAO.setupWorkspace(any[UserInfo], ArgumentMatchers.eq(testData.workspace.googleProject),
-      any[Map[WorkspaceAccessLevel, WorkbenchEmail]], ArgumentMatchers.eq(testData.workspace.bucketName), any[Map[String, String]], any[Span], ArgumentMatchers.eq(newBucketLocation))).thenReturn(Future.successful(null))
+    val newBucketLocation = Option("us-terra1");
+    val workspaceCopy = WorkspaceRequest(namespace = testData.workspace.namespace, name = "test_copy", Map.empty, bucketLocation = newBucketLocation)
+    when(services.gcsDAO.setupWorkspace(
+      any[UserInfo],
+      ArgumentMatchers.eq(testData.workspace.googleProject),
+      any[Map[WorkspaceAccessLevel, WorkbenchEmail]],
+      any[String],
+      any[Map[String, String]],
+      any[Span],
+      ArgumentMatchers.eq(newBucketLocation)))
+      .thenReturn(Future.successful(mock[GoogleWorkspaceInfo]))
     Post(s"${testData.workspace.path}/clone", httpJson(workspaceCopy)) ~>
       sealRoute(services.workspaceRoutes) ~>
       check {
@@ -1170,8 +1177,14 @@ class WorkspaceApiServiceSpec extends ApiServiceSpec {
               runAndWait(methodConfigurationQuery.listActive(copiedWorkspaceContext)).toSet
             }
           }
-          verify(services.gcsDAO).setupWorkspace(any[UserInfo], ArgumentMatchers.eq(testData.workspace.googleProject),
-            any[Map[WorkspaceAccessLevel, WorkbenchEmail]], ArgumentMatchers.eq(testData.workspace.bucketName), any[Map[String, String]], any[Span], ArgumentMatchers.eq(newBucketLocation))
+          verify(services.gcsDAO).setupWorkspace(
+            any[UserInfo],
+            ArgumentMatchers.eq(testData.workspace.googleProject),
+            any[Map[WorkspaceAccessLevel, WorkbenchEmail]],
+            any[String],
+            any[Map[String, String]],
+            any[Span],
+            ArgumentMatchers.eq(newBucketLocation))
         }
 
         // TODO: does not test that the path we return is correct.  Update this test in the future if we care about that
