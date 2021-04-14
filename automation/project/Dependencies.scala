032df06bce132f561ecf156db612843c4b1cd0a0
Andrew Jang <andy7i@users.noreply.github.com>
2021-04-14 10:29:45 -0400

    update workbenchGoogleV from 0.21 to 0.21 -- should be non-breaking but is. Needs both the new dependency and exclusion rule to work.

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val jacksonV      = "2.11.3"
 
   val serviceTestV = "0.18-23f5ae7"
-  val workbenchGoogleV = "0.21-24dcd92"
+  val workbenchGoogleV = "0.21-b686a648-SNAP"
   val workbenchGoogle2V = "0.18-4631ebf"
   val workbenchModelV  = "0.14-65bba14"
   val workbenchMetricsV  = "0.5-64a7b29"
@@ -36,6 +36,8 @@ object Dependencies {
     "com.fasterxml.jackson.core" % "jackson-core" % jacksonV,
     "com.fasterxml.jackson.module" % ("jackson-module-scala_" + scalaV) % jacksonV,
     "ch.qos.logback" % "logback-classic" % "1.2.3",
+    "net.logstash.logback" % "logstash-logback-encoder" % "6.6"
+      exclude ("com.fasterxml.jackson.core", "jackson-databind"),
     "com.google.apis" % "google-api-services-oauth2" % "v1-rev112-1.20.0" excludeAll (
       ExclusionRule("com.google.guava", "guava-jdk5"),
       ExclusionRule("org.apache.httpcomponents", "httpclient")
