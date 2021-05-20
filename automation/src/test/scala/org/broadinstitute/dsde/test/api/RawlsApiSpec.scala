c264cc40c6ccf4d9968a7926ff3795abc9d19538
Miguel Covarrubias <mcovarr@users.noreply.github.com>
2021-05-20 16:09:07 -0400

    Increase patience for non-US region tests [QA-1454] (#1434)

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/RawlsApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/RawlsApiSpec.scala
@@ -273,7 +273,7 @@ class RawlsApiSpec extends TestKit(ActorSystem("MySpec")) with AnyFreeSpecLike w
             register cleanUp Rawls.submissions.abortSubmission(projectName, workspaceName, submissionId)
 
             // may need to wait for Cromwell to start processing workflows
-            val submissionPatience = PatienceConfig(timeout = scaled(Span(8, Minutes)), interval = scaled(Span(30, Seconds)))
+            val submissionPatience = PatienceConfig(timeout = scaled(Span(20, Minutes)), interval = scaled(Span(30, Seconds)))
             implicit val patienceConfig: PatienceConfig = submissionPatience
 
             // Get workflow ID from submission details
@@ -372,7 +372,7 @@ class RawlsApiSpec extends TestKit(ActorSystem("MySpec")) with AnyFreeSpecLike w
             register cleanUp Rawls.submissions.abortSubmission(projectName, workspaceName, submissionId)
 
             // may need to wait for Cromwell to start processing workflows
-            val submissionPatience = PatienceConfig(timeout = scaled(Span(8, Minutes)), interval = scaled(Span(30, Seconds)))
+            val submissionPatience = PatienceConfig(timeout = scaled(Span(20, Minutes)), interval = scaled(Span(30, Seconds)))
             implicit val patienceConfig: PatienceConfig = submissionPatience
 
             // Get workflow ID from submission details
