761a7e2ce27d2bafc1d5be1163ee7a0206c691f8
David An <davidan@broadinstitute.org>
2021-05-26 13:59:53 -0400

    validate entity names, add code comments

--- core/src/main/scala/org/broadinstitute/dsde/rawls/deltalayer/DeltaLayerTranslator.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/deltalayer/DeltaLayerTranslator.scala
@@ -2,11 +2,10 @@ package org.broadinstitute.dsde.rawls.deltalayer
 
 import akka.http.scaladsl.model.StatusCodes.{BadRequest, InternalServerError}
 import com.typesafe.scalalogging.LazyLogging
-import org.broadinstitute.dsde.rawls.model.{Attribute, AttributeBoolean, AttributeFormat, AttributeName, AttributeNumber, AttributeString, JsonSupport, PlainArrayAttributeListSerializer}
+import org.broadinstitute.dsde.rawls.model.{AttributeBoolean, AttributeFormat, AttributeName, AttributeNumber, AttributeString, JsonSupport, PlainArrayAttributeListSerializer}
 import org.broadinstitute.dsde.rawls.model.AttributeName.toDelimitedName
 import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{AddUpdateAttribute, AttributeUpdateOperation, EntityUpdateDefinition}
 import org.broadinstitute.dsde.rawls.model.deltalayer.v1.DeltaRow
-import spray.json.{JsValue, enrichAny}
 
 import java.util.UUID
 import scala.util.{Failure, Success, Try}
@@ -18,6 +17,7 @@ object DeltaLayerTranslator extends JsonSupport with LazyLogging {
 
   implicit override val attributeFormat = new AttributeFormat with PlainArrayAttributeListSerializer
 
+  val ERR_INVALID_ENTITYNAME = "This method only accepts entity names that are valid UUIDs, e.g. datarepo_row_id"
   val ERR_INVALID_OPERATION = "This method only accepts AddUpdateAttribute operations."
   val ERR_EMPTY_OPERATIONS = "This method requires at least one AddUpdateAttribute operation."
   val ERR_INVALID_DATATYPE = "This method only accepts string, boolean, and numeric scalar values."
@@ -30,6 +30,12 @@ object DeltaLayerTranslator extends JsonSupport with LazyLogging {
    * @return the original set of updates, if all are valid
    */
   def validateEntityUpdates(entityUpdates: Seq[EntityUpdateDefinition]): Seq[EntityUpdateDefinition] = {
+    // verify that all entity names are legal UUIDs - they should be datarepo_row_ids
+    val uuidAttempts = entityUpdates.map(upd => Try(UUID.fromString(upd.name)))
+    if (uuidAttempts.exists(_.isFailure))
+      throw new DeltaLayerException(ERR_INVALID_ENTITYNAME, code = BadRequest)
+
+    // verify that all operations are AddUpdateAttribute only
     val allOps: Seq[AttributeUpdateOperation] = entityUpdates flatMap { upd => upd.operations }
 
     if (allOps.isEmpty)
@@ -48,7 +54,8 @@ object DeltaLayerTranslator extends JsonSupport with LazyLogging {
       // how did we reach here? This should be impossible
       throw new DeltaLayerException(ERR_EMPTY_OPERATIONS, code = BadRequest)
 
-    // validate value types
+    // validate that all upsert values are string/boolean/number only
+    // TODO: add support for lists of string/boolean/number and nulls
     val allValues = allOps.collect {
       case a:AddUpdateAttribute => a.addUpdateAttribute
     }
@@ -57,7 +64,6 @@ object DeltaLayerTranslator extends JsonSupport with LazyLogging {
       throw new DeltaLayerException("Unexpected error: count of update values did not match count of update operations",
         code = InternalServerError)
 
-    // TODO: should support more types, including lists!
     val (supportedTypes, unsupportedTypes) = allValues.partition {
       case AttributeBoolean(_) => true
       case AttributeNumber(_) => true
@@ -72,6 +78,7 @@ object DeltaLayerTranslator extends JsonSupport with LazyLogging {
       // how did we reach here? This should be impossible
       throw new DeltaLayerException(ERR_INVALID_DATATYPE, code = BadRequest)
 
+    // everything validated; return the original update definitions
     entityUpdates
   }
 
