d481fa2e55d80045ecc4a3ca3d7f327b85e9ff33
David An <davidan@broadinstitute.org>
2021-06-17 17:48:48 -0400

    fixes to DataAccessSpec in light of sharding

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
@@ -39,16 +39,15 @@ trait DataAccess
     // if table X has a Foreign Key to table Y, delete table X first
 
     // TODO: davidan could we instead SET FOREIGN_KEY_CHECKS = 0; truncate tables ...; SET FOREIGN_KEY_CHECKS = 1; ?
-    // do we have access to INFORMATION_SCHEMA.TABLES, and if so can we avoid listing all tables here?
-    val shardDeletes = DBIO.sequence((0 to 15).map(toHexString) flatMap { firstPart =>
+    // do we have access to INFORMATION_SCHEMA.TABLES, and if so can we avoid listing all tables here? Can we use MTables?
+    val shardDeletes = (0 to 15).map(toHexString) flatMap { firstPart =>
       List("07", "8f") map { secondPart =>
         val shardSuffix = firstPart + "_" + secondPart
         new EntityAttributeShardQuery(shardSuffix).delete
       }
-    })
+    }
 
-    // TODO: davidan don't hardcode the shard names here, else this will need to be in sync with liquibase
-    shardDeletes andThen // FK to entity
+    DBIO.sequence(shardDeletes) andThen // FK to entity
       TableQuery[WorkspaceAttributeTable].delete andThen          // FK to entity, workspace
       TableQuery[SubmissionAttributeTable].delete andThen         // FK to entity, submissionvalidation
       TableQuery[MethodConfigurationInputTable].delete andThen    // FK to MC
