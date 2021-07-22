073dbadafa50d5d50554fbc599406ce4cf10b390
UC Ngwudike <63610971+ungwudik@users.noreply.github.com>
2021-07-22 14:02:16 -0400

    Use new snapshot deserialization model (#1486)
    * Use new snapshot deserialization model

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/SnapshotAPISpec.scala
@@ -188,7 +188,7 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
 
           // validate the snapshot was added correctly: list snapshots in Rawls, should return 1, which we just added.
           val listResponse = listSnapshotReferences(projectName, workspaceName)
-          val resources = Rawls.parseResponseAs[ResourceList](listResponse).getResources.asScala
+          val resources = Rawls.parseResponseAs[SnapshotListResponse](listResponse).gcpDataRepoSnapshots
           resources should have size 1
 
           // check that the bq dataset has been created
@@ -221,7 +221,7 @@ class SnapshotAPISpec extends AnyFreeSpecLike with Matchers with BeforeAndAfterA
 
           // validate the snapshot was added correctly: list snapshots in Rawls, should return 1, which we just added.
           val listResponse = listSnapshotReferences(projectName, workspaceName)
-          val resources = Rawls.parseResponseAs[ResourceList](listResponse).getResources.asScala
+          val resources = Rawls.parseResponseAs[SnapshotListResponse](listResponse).gcpDataRepoSnapshots
           resources should have size 1
 
           // check that the bq dataset has been created
