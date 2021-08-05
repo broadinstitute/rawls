e3906fe29f5dc71a381da1911384ea43eabf605b
Andrew Jang <andy7i@users.noreply.github.com>
2021-08-05 10:49:47 -0400

    seems to me like this should only succeed if it encounters the expected error, and not all errors.

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -361,8 +361,8 @@ class WorkspaceService(protected val userInfo: UserInfo, val dataSource: SlickDa
         case e: ApiException => {
           if(e.getCode != StatusCodes.NotFound.intValue) {
             logger.warn(s"Unexpected failure deleting workspace in Workspace Manager. Received ${e.getCode}: [${e.getResponseBody}]")
+            Future.successful()
           }
-          Future.successful()
         }
       }
       // Delete the Delta Layer companion dataset, if it exists
