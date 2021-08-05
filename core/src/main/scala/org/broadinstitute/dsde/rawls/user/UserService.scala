4d3981feb07ba248b32716daec0d60b59f035f16
Andrew Jang <andy7i@users.noreply.github.com>
2021-08-05 17:17:04 -0400

    PR feedback - additional logging

--- core/src/main/scala/org/broadinstitute/dsde/rawls/user/UserService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/user/UserService.scala
@@ -261,7 +261,12 @@ class UserService(protected val userInfo: UserInfo, val dataSource: SlickDataSou
       _ <- dataSource.inTransaction { dataAccess =>
         dataAccess.rawlsBillingProjectQuery.delete(projectName)
       }
-      _ <- samDAO.deleteResource(SamResourceTypeNames.billingProject, projectName.value, ownerUserInfo) // Moving this to the end so that the rawls record is cleared even if there are issues clearing the Sam resource (theoretical workaround for https://broadworkbench.atlassian.net/browse/CA-1206)
+      _ <- samDAO.deleteResource(SamResourceTypeNames.billingProject, projectName.value, ownerUserInfo) recoverWith { // Moving this to the end so that the rawls record is cleared even if there are issues clearing the Sam resource (theoretical workaround for https://broadworkbench.atlassian.net/browse/CA-1206)
+        case t:Throwable => {
+          logger.warn(s"Unexpected failure deleting billing project (while deleting billing project in Sam) for billing project `${projectName.value}`", t)
+          throw t
+        }
+      }
     } yield {
       RequestComplete(StatusCodes.NoContent)
     }
