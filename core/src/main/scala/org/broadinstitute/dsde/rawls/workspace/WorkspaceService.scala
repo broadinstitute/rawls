7a6385a03c169db8b61e17ede4701b8f230c0b1c
Marc Talbott <mtalbott@broadinstitute.org>
2021-08-13 13:17:39 -0400

    [CA-1471] Handle case where Terra billing project doesn't have canCompute policy (#1495)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -954,7 +954,11 @@ class WorkspaceService(protected val userInfo: UserInfo, val dataSource: SlickDa
       case (SamWorkspacePolicyNames.canCompute, email)  => email
     }
     Future.traverse(newWriterEmails) { email =>
-      samDAO.addUserToPolicy(SamResourceTypeNames.billingProject, workspaceName.namespace, SamBillingProjectPolicyNames.canComputeUser, email, userInfo)
+      samDAO.addUserToPolicy(SamResourceTypeNames.billingProject, workspaceName.namespace, SamBillingProjectPolicyNames.canComputeUser, email, userInfo).recoverWith {
+        case regrets: Throwable =>
+          logger.info(s"error adding user to canComputeUser policy of Terra billing project while updating ${workspaceName.toString} likely because it is a v2 billing project which does not have a canComputeUser policy. regrets: ${regrets.getMessage}")
+          Future.successful(())
+      }
     }.map(_ => ())
   }
 
