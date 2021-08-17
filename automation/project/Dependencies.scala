6fada3bcf3d6b2aa45a5bf76fb54e44da4804804
Sky Rubenstein <brubenst@broadinstitute.org>
2021-08-17 13:19:47 -0400

    [CA-1459] Update googleProjectId back to googleProject for backwards compat (#1496)
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
    
    Co-authored-by: Andrew Jang <andy7i@users.noreply.github.com>
    Co-authored-by: Marcus Talbott <mtalbott@broadinstitute.org>
    Co-authored-by: Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val akkaHttpV     = "10.2.0"
   val jacksonV      = "2.12.3"
 
-  val serviceTestV = "0.18-ecf1e1da-SNAP"
+  val serviceTestV = "0.18-8587f213-SNAP"
   val workbenchGoogleV = "0.21-3f3c0e4"
   val workbenchGoogle2V = "0.18-4631ebf"
   val workbenchModelV  = "0.14-65bba14"
@@ -54,7 +54,7 @@ object Dependencies {
     "org.scalatest"       %%  "scalatest"     % "3.2.2"   % "test",
     "org.seleniumhq.selenium" % "selenium-java" % "3.8.1" % "test",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
-    "org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-e55b488f-SNAP"
+    "org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-d3c0583e7-SNAP"
       exclude("com.typesafe.scala-logging", "scala-logging_2.11")
       exclude("com.typesafe.akka", "akka-stream_2.11")
       exclude("bio.terra", "workspace-manager-client"),
