57a933c847967b8a5b2632fac758ade250259a65
Matt Bemis <mbemis@broadinstitute.org>
2021-10-07 16:05:15 -0400

    cleanup from self review

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
@@ -354,20 +354,18 @@ trait EntityComponent {
         sql"""#${baseEntityAndAttributeSql(workspaceContext)} where e.workspace_id = ${workspaceContext.workspaceIdAsUUID}""".as[EntityAndAttributesResult]
       }
 
-      def batchHide(workspaceId: UUID, entities: Seq[AttributeEntityReference]): ReadWriteAction[Seq[Int]] = {
-        workspaceQuery.isWorkspaceSharded(workspaceId).flatMap { sharded =>
-          val shardId = determineShard(workspaceId, sharded)
-          // get unique suffix for renaming
-          val renameSuffix = "_" + getSufficientlyRandomSuffix(1000000000) // 1 billion
-          val deletedDate = new Timestamp(new Date().getTime)
-          // issue bulk rename/hide for all entity attributes, given a set of entities
-          val baseUpdate =
-            sql"""update ENTITY_ATTRIBUTE_#$shardId ea join ENTITY e on ea.owner_id = e.id
-                  set ea.deleted=1, ea.deleted_date=$deletedDate, ea.name=CONCAT(ea.name, $renameSuffix)
-                  where e.workspace_id=$workspaceId and ea.deleted=0 and (e.entity_type, e.name) in ("""
-          val entityTypeNameTuples = reduceSqlActionsWithDelim(entities.map { ref => sql"(${ref.entityType}, ${ref.entityName})" })
-          concatSqlActions(baseUpdate, entityTypeNameTuples, sql")").as[Int]
-        }
+      def batchHide(workspaceContext: Workspace, entities: Seq[AttributeEntityReference]): ReadWriteAction[Seq[Int]] = {
+        val shardId = determineShard(workspaceContext.workspaceIdAsUUID, workspaceContext.sharded)
+        // get unique suffix for renaming
+        val renameSuffix = "_" + getSufficientlyRandomSuffix(1000000000) // 1 billion
+        val deletedDate = new Timestamp(new Date().getTime)
+        // issue bulk rename/hide for all entity attributes, given a set of entities
+        val baseUpdate =
+          sql"""update ENTITY_ATTRIBUTE_#$shardId ea join ENTITY e on ea.owner_id = e.id
+                set ea.deleted=1, ea.deleted_date=$deletedDate, ea.name=CONCAT(ea.name, $renameSuffix)
+                where e.workspace_id=${workspaceContext.workspaceId} and ea.deleted=0 and (e.entity_type, e.name) in ("""
+        val entityTypeNameTuples = reduceSqlActionsWithDelim(entities.map { ref => sql"(${ref.entityType}, ${ref.entityName})" })
+        concatSqlActions(baseUpdate, entityTypeNameTuples, sql")").as[Int]
       }
     }
 
@@ -694,7 +692,7 @@ trait EntityComponent {
       // Thus, we need to keep the "deleted" value for attributes in sync with their parent entity,
       // when hiding that entity.
       workspaceQuery.updateLastModified(workspaceContext.workspaceIdAsUUID) andThen
-        EntityAndAttributesRawSqlQuery.batchHide(workspaceContext.workspaceIdAsUUID, entRefs) andThen
+        EntityAndAttributesRawSqlQuery.batchHide(workspaceContext, entRefs) andThen
         EntityRecordRawSqlQuery.batchHide(workspaceContext.workspaceIdAsUUID, entRefs).map( res => res.sum)
     }
 
