d166d6332544662d6f1a3373755f2a63917570ef
Andrew Jang <andy7i@users.noreply.github.com>
2021-10-14 11:21:46 -0400

    try 2.12.14

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -26,7 +26,7 @@ object Publishing {
   val publishSettings: Seq[Setting[_]] =
     //we only publish to libs-release-local because of a bug in sbt that makes snapshots take
     //priority over the local package cache. see here: https://github.com/sbt/sbt/issues/2687#issuecomment-236586241
-    Seq(publishTo := Option(artifactoryResolver(false)), credentials += artifactoryCredentials, crossScalaVersions := List("2.12.15", "2.13.2"), publishConfiguration := publishConfiguration.value.withOverwrite(true))
+    Seq(publishTo := Option(artifactoryResolver(false)), credentials += artifactoryCredentials, crossScalaVersions := List("2.12.14", "2.13.2"), publishConfiguration := publishConfiguration.value.withOverwrite(true))
 
   val noPublishSettings: Seq[Setting[_]] =
     Seq(
