716e90a9fadb66fee97726d091d3b78c616d9783
David An <davidan@broadinstitute.org>
2021-11-08 09:17:02 -0500

    remove duplicate import

--- core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/EntityApiService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/EntityApiService.scala
@@ -2,7 +2,6 @@ package org.broadinstitute.dsde.rawls.webservice
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
-import CustomDirectives._
 import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.model.StatusCodes.BadRequest
 import akka.http.scaladsl.server
