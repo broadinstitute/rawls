e696b7aa0b50399bb68d3a7249b205acaef05e7c
Edmund Higham <edhigham@gmail.com>
2021-11-16 15:00:52 -0500

    start adding details to the monitor

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
@@ -1,5 +1,41 @@
 package org.broadinstitute.dsde.rawls.monitor
 
-class V1WorkspaceMigrationMonitor {
+import akka.actor.TypedActor.dispatcher
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.rawls.dataaccess.SlickDataSource
+import org.broadinstitute.dsde.rawls.dataaccess.slick.{ReadAction, WriteAction}
+import org.broadinstitute.dsde.rawls.model.Workspace
+import org.broadinstitute.dsde.rawls.monitor.V1WorkspaceMigrationActor.Schedule
+
+object V1WorkspaceMigrationMonitor {
+
+  import slick.jdbc.H2Profile.api._
+
+  private val v1WorkspaceMigrationHistory: String = "V1_WORKSPACE_MIGRATION_HISTORY"
+
+  final def isMigrating(workspace: Workspace): ReadAction[Boolean] =
+    sqlu"""SELECT COUNT(*) FROM ${v1WorkspaceMigrationHistory} h
+           WHERE h.WORKSPACE_ID = '${workspace.workspaceId}'
+             AND h.STARTED IS NOT NULL
+             AND h.FINISHED IS NULL""".map(_ > 0)
+
+  final def schedule(workspace: Workspace): WriteAction[Unit] =
+    sqlu"""INSERT INTO ${v1WorkspaceMigrationHistory} (workspaceId)
+           VALUES (${workspace.workspaceId})""".map(_ => ())
+}
+
+object V1WorkspaceMigrationActor {
+  final case class Schedule(workspace: Workspace)
+}
+
+final class V1WorkspaceMigrationActor(dataSource: SlickDataSource) extends LazyLogging {
+
+  def apply(): Behavior[Schedule] = Behaviors.receive { (_, message) =>
+    dataSource.inTransaction(_ => V1WorkspaceMigrationMonitor.schedule(message.workspace))
+    Behaviors.same
+  }
 
 }
+
--- core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitorSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitorSpec.scala
@@ -1,5 +1,34 @@
 package org.broadinstitute.dsde.rawls.monitor
 
-class V1WorkspaceMigrationMonitorSpec {
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import org.broadinstitute.dsde.rawls.dataaccess.SlickDataSource
+import org.broadinstitute.dsde.rawls.dataaccess.slick.TestDriverComponent
+import org.broadinstitute.dsde.rawls.model.{CreationStatuses, GoogleProjectId, GoogleProjectNumber, RawlsBillingAccountName, RawlsBillingProject, RawlsBillingProjectName, Workspace, WorkspaceShardStates, WorkspaceVersions}
+import org.joda.time.DateTime
+import org.scalatest.concurrent.Eventually
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{BeforeAndAfterAll, OptionValues}
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.util.UUID
+
+class V1WorkspaceMigrationMonitorSpec(_system: ActorSystem) extends TestKit(_system) with MockitoSugar with AnyFlatSpecLike with Matchers with TestDriverComponent with BeforeAndAfterAll with Eventually with OptionValues {
+
+  "V1WorkspaceMigrationMonitor" should "return false when a workspace is not migrating" in {
+    withEmptyTestDatabase { dataSource: SlickDataSource =>
+      val defaultGoogleProjectNumber: GoogleProjectNumber = GoogleProjectNumber("42")
+      val defaultBillingProjectName: RawlsBillingProjectName = RawlsBillingProjectName("test-bp")
+      val defaultBillingAccountName: RawlsBillingAccountName = RawlsBillingAccountName("test-ba")
+      val billingProject = RawlsBillingProject(defaultBillingProjectName, CreationStatuses.Ready, Option(defaultBillingAccountName), None, googleProjectNumber = Option(defaultGoogleProjectNumber))
+      val sourceBucketName = "sourceBucket"
+      val workspace = Workspace(billingProject.projectName.value, "source", UUID.randomUUID().toString, sourceBucketName, None, DateTime.now, DateTime.now, "creator@example.com", Map.empty, false, WorkspaceVersions.V2, GoogleProjectId("some-project"), Option(GoogleProjectNumber("43")), billingProject.billingAccount, None, Option(DateTime.now), WorkspaceShardStates.Sharded)
+
+      eventually {
+        V1WorkspaceMigrationMonitor.isMigrating(workspace)
+      }
+    }
+  }
 
 }
