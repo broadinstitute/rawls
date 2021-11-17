9a24201d776fc514994dc0a6a8eda18359b17fd7
Edmund Higham <edhigham@gmail.com>
2021-11-17 16:41:29 -0500

    I dUn FuKt It

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
@@ -2,32 +2,79 @@ package org.broadinstitute.dsde.rawls.monitor
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import cats.implicits.{catsSyntaxOptionId, toTraverseOps}
 import org.broadinstitute.dsde.rawls.dataaccess.SlickDataSource
-import org.broadinstitute.dsde.rawls.dataaccess.slick.{DriverComponent, ReadAction, WriteAction}
+import org.broadinstitute.dsde.rawls.dataaccess.slick.{ReadAction, WriteAction}
 import org.broadinstitute.dsde.rawls.model.Workspace
+import org.broadinstitute.dsde.rawls.monitor.MigrationOutcome.{Failure, Success}
+import org.conscrypt.ct.SerializationException
 
 import java.time.LocalDateTime
 import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-final case class V1WorkspaceMigrationAttempt(workspaceId: UUID,
+sealed trait MigrationOutcome
+
+object MigrationOutcome {
+  case object Success extends MigrationOutcome
+
+  final case class Failure(message: String) extends MigrationOutcome
+}
+
+final case class V1WorkspaceMigrationAttempt(id: Long,
+                                             workspaceId: UUID,
                                              created: LocalDateTime,
                                              started: Option[LocalDateTime],
                                              finished: Option[LocalDateTime],
-                                             outcome: Option[String],
-                                             message: Option[String])
+                                             outcome: Option[MigrationOutcome])
+
+object V1WorkspaceMigrationAttempt {
+  type RecordType = (Long, UUID, LocalDateTime, Option[LocalDateTime], Option[LocalDateTime], Option[String], Option[String])
+
+  def fromRecord(record: RecordType): Either[String, V1WorkspaceMigrationAttempt] = {
+    type EitherStringT[T] = Either[String, T]
+    record match {
+      case (id, workspaceId, created, started, finished, outcome, message) =>
+        outcome
+          .traverse[EitherStringT, MigrationOutcome] {
+            case "Success" => Right(Success)
+            case "Failure" => Right(Failure(message.getOrElse("")))
+            case other => Left(s"""Failed to read V1WorkspaceMigrationAttempt from record.
+                                   | Unknown migration outcome -- "$other"""".stripMargin)
+          }
+          .map {
+            V1WorkspaceMigrationAttempt(id, workspaceId, created, started, finished, _)
+          }
+    }
+  }
+
+  def unsafeFromRecord(record: RecordType): V1WorkspaceMigrationAttempt =
+    fromRecord(record) match {
+      case Right(r) => r
+      case Left(msg) => throw new SerializationException(msg)
+    }
+
+  def toRecord(attempt: V1WorkspaceMigrationAttempt): RecordType = {
+    val (outcome: Option[String], message: Option[String]) = attempt.outcome match {
+      case Some(Success) => ("Success".some, None)
+      case Some(Failure(message)) => ("Failure".some, message.some)
+      case None => (None, None)
+    }
+    (attempt.id, attempt.workspaceId, attempt.created, attempt.started, attempt.finished, outcome, message)
+  }
+}
 
 trait V1WorkspaceMigrationComponent {
-  this: DriverComponent =>
 
-  import driver.api._
+  import slick.jdbc.H2Profile.api._
 
   private val v1WorkspaceMigrationHistory: String = "V1_WORKSPACE_MIGRATION_HISTORY"
 
   final class V1WorkspaceMigrationHistory(tag: Tag)
     extends Table[V1WorkspaceMigrationAttempt](tag, v1WorkspaceMigrationHistory) {
 
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def workspaceId = column[UUID]("WORKSPACE_ID")
     def created = column[LocalDateTime]("CREATED")
     def started = column[Option[LocalDateTime]]("STARTED")
@@ -36,28 +83,31 @@ trait V1WorkspaceMigrationComponent {
     def message = column[Option[String]]("MESSAGE")
 
     override def * =
-      (workspaceId, created, started, finished, outcome, message) <>
-        (V1WorkspaceMigrationAttempt.tupled, V1WorkspaceMigrationAttempt.unapply)
+      (id, workspaceId, created, started, finished, outcome, message) <>
+        (V1WorkspaceMigrationAttempt.unsafeFromRecord, V1WorkspaceMigrationAttempt.toRecord(_: V1WorkspaceMigrationAttempt).some)
   }
+
+  val migrations = TableQuery[V1WorkspaceMigrationHistory]
 }
 
-object V1WorkspaceMigrationMonitor {
+object V1WorkspaceMigrationMonitor
+  extends V1WorkspaceMigrationComponent {
 
   import slick.jdbc.H2Profile.api._
 
-  private val v1WorkspaceMigrationHistory: String = "V1_WORKSPACE_MIGRATION_HISTORY"
-
   final def isMigrating(workspace: Workspace): ReadAction[Boolean] =
-    sql"""SELECT COUNT(*) FROM #${v1WorkspaceMigrationHistory} h
-          WHERE h.WORKSPACE_ID = ${workspace.workspaceId}
-            AND h.STARTED IS NOT NULL
-            AND h.FINISHED IS NULL""".as[Int]
-      .headOption
-      .map(_.getOrElse(0) > 0)
+    migrations
+      .filter { m =>
+        m.workspaceId === UUID.fromString(workspace.workspaceId) && m.started.isDefined && m.finished.isEmpty
+      }
+      .length
+      .result
+      .map(_ > 0)
 
-  final def schedule(workspace: Workspace): WriteAction[Unit] =
-    sqlu"""INSERT INTO #${v1WorkspaceMigrationHistory} (WORKSPACE_ID)
-           VALUES (${workspace.workspaceId})""".map(_ => ())
+  final def schedule(workspace: Workspace): WriteAction[Unit] = {
+    Console.println((migrations.map(c => c.workspaceId) += UUID.fromString(workspace.workspaceId)).statements)
+    DBIO.seq(migrations.map(c => c.workspaceId) += UUID.fromString(workspace.workspaceId))
+  }
 }
 
 object V1WorkspaceMigrationActor {
