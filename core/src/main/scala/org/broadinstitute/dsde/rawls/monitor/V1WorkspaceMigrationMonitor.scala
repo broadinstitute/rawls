15eef14842b1fa3c79cf26f774f849ba10940b2a
Edmund Higham <edhigham@gmail.com>
2021-12-01 11:32:03 -0500

    add tmp bucket + timestamp to the `MigrationAtempt` record

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitor.scala
@@ -12,10 +12,10 @@ import org.broadinstitute.dsde.rawls.model.{GoogleProjectId, Workspace}
 import org.broadinstitute.dsde.rawls.monitor.MigrationOutcome.{Failure, Success}
 import org.broadinstitute.dsde.workbench.google2.GoogleStorageService
 import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GoogleProject}
+
 import java.sql.Timestamp
 import java.time.LocalDateTime
-import java.util.{Collections, UUID}
-
+import java.util.UUID
 import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -33,15 +33,18 @@ final case class V1WorkspaceMigrationAttempt(id: Long,
                                              created: Timestamp,
                                              started: Option[Timestamp],
                                              finished: Option[Timestamp],
-                                             outcome: Option[MigrationOutcome])
+                                             outcome: Option[MigrationOutcome],
+                                             tmpBucketName: Option[GcsBucketName],
+                                             tmpBucketCreated: Option[Timestamp]
+                                            )
 
 object V1WorkspaceMigrationAttempt {
-  type RecordType = (Long, UUID, Timestamp, Option[Timestamp], Option[Timestamp], Option[String], Option[String])
+  type RecordType = (Long, UUID, Timestamp, Option[Timestamp], Option[Timestamp], Option[String], Option[String], Option[String], Option[Timestamp])
 
   def fromRecord(record: RecordType): Either[String, V1WorkspaceMigrationAttempt] = {
     type EitherStringT[T] = Either[String, T]
     record match {
-      case (id, workspaceId, created, started, finished, outcome, message) =>
+      case (id, workspaceId, created, started, finished, outcome, message, tmpBucketName, tmpBucketCreated) =>
         outcome
           .traverse[EitherStringT, MigrationOutcome] {
             case "Success" => Right(Success)
@@ -49,8 +52,17 @@ object V1WorkspaceMigrationAttempt {
             case other => Left(s"""Failed to read V1WorkspaceMigrationAttempt from record.
                                    | Unknown migration outcome -- "$other"""".stripMargin)
           }
-          .map {
-            V1WorkspaceMigrationAttempt(id, workspaceId, created, started, finished, _)
+          .map { outcome =>
+            V1WorkspaceMigrationAttempt(
+              id,
+              workspaceId,
+              created,
+              started,
+              finished,
+              outcome,
+              tmpBucketName.map(GcsBucketName),
+              tmpBucketCreated
+            )
           }
     }
   }
@@ -67,7 +79,17 @@ object V1WorkspaceMigrationAttempt {
       case Some(Failure(message)) => ("Failure".some, message.some)
       case None => (None, None)
     }
-    (attempt.id, attempt.workspaceId, attempt.created, attempt.started, attempt.finished, outcome, message)
+    (
+      attempt.id,
+      attempt.workspaceId,
+      attempt.created,
+      attempt.started,
+      attempt.finished,
+      outcome,
+      message,
+      attempt.tmpBucketName.map(_.value),
+      attempt.tmpBucketCreated
+    )
   }
 }
 
@@ -91,7 +113,7 @@ trait V1WorkspaceMigrationComponent {
     def tmpBucketCreated = column[Option[Timestamp]]("TMP_BUCKET_CREATED")
 
     override def * =
-      (id, workspaceId, created, started, finished, outcome, message) <>
+      (id, workspaceId, created, started, finished, outcome, message, tmpBucket, tmpBucketCreated) <>
         (V1WorkspaceMigrationAttempt.unsafeFromRecord, V1WorkspaceMigrationAttempt.toRecord(_: V1WorkspaceMigrationAttempt).some)
   }
 
@@ -113,31 +135,59 @@ object V1WorkspaceMigrationMonitor
   final def schedule(workspace: Workspace): WriteAction[Unit] =
     DBIO.seq(migrations.map(_.workspaceId) += workspace.workspaceIdAsUUID)
 
-  final def createBucketInSameRegion(destGoogleProject: GoogleProject, sourceGoogleProject: GoogleProject, sourceBucketName: GcsBucketName, destBucketName: GcsBucketName, googleStorageService: GoogleStorageService[IO]): IO[Unit] = {
+  final def createBucketInSameRegion(sourceBucketName: GcsBucketName,
+                                     destGoogleProject: GoogleProject,
+                                     destBucketName: GcsBucketName,
+                                     googleStorageService: GoogleStorageService[IO]
+                                    ): IO[Unit] = {
     for {
-      sourceBucketOpt <- googleStorageService.getBucket(sourceGoogleProject, sourceBucketName, List(BucketGetOption.userProject(destGoogleProject.value))) // todo: figure out who pays for this
-      sourceBucket = sourceBucketOpt.getOrElse(throw new IllegalStateException(s"Source bucket ${sourceBucketName} could not be found"))
+      // todo: figure out who pays for this
+      sourceBucketOpt <- googleStorageService.getBucket(
+        GoogleProject(null),
+        sourceBucketName,
+        List(BucketGetOption.userProject(destGoogleProject.value))
+      )
+
+      sourceBucket = sourceBucketOpt.getOrElse(
+        throw new IllegalStateException(s"Source bucket ${sourceBucketName} could not be found")
+      )
+
+      // todo: CA-1637 do we need to transfer the storage logs for this workspace? the logs are prefixed
+      // with the ws bucket name, so we COULD do it, but do we HAVE to? it's a csv with the bucket
+      // and the storage_byte_hours in it that is kept for 180 days
       _ <- googleStorageService.insertBucket(
         googleProject = destGoogleProject,
         bucketName = destBucketName,
-        acl = None,
-        labels = Option(sourceBucket.getLabels).getOrElse(Collections.emptyMap()).toMap,
+        labels = Option(sourceBucket.getLabels).map(_.toMap).getOrElse(Map.empty),
         bucketPolicyOnlyEnabled = true,
-        logBucket = Option(GcsBucketName(GoogleServicesDAO.getStorageLogsBucketName(GoogleProjectId(destGoogleProject.value)))), // todo: CA-1637 do we need to transfer the storage logs for this workspace? the logs are prefixed with the ws bucket name, so we COULD do it, but do we HAVE to? it's a csv with the bucket and the storage_byte_hours in it that is kept for 180 days
+        logBucket = Option(GcsBucketName(GoogleServicesDAO.getStorageLogsBucketName(GoogleProjectId(destGoogleProject.value)))),
         location = Option(sourceBucket.getLocation)
       ).compile.drain
+
     } yield ()
   }
 
-  final def createTempBucket(migrationAttempt: V1WorkspaceMigrationAttempt, workspace: Workspace, destGoogleProject: GoogleProject, googleStorageService: GoogleStorageService[IO]): IO[(GcsBucketName, ReadWriteAction[Unit])] = {
-    val bucketName = GcsBucketName(("workspace_migration_" + UUID.randomUUID.toString).replace("-", "").take(63))
+  final def createTempBucket(migrationAttempt: V1WorkspaceMigrationAttempt,
+                             workspace: Workspace,
+                             destGoogleProject: GoogleProject,
+                             googleStorageService: GoogleStorageService[IO]
+                            ): IO[(GcsBucketName, ReadWriteAction[Unit])] = {
+    val tmpBucketName = GcsBucketName(
+      "terra-workspace-migration-" + UUID.randomUUID.toString.replace("-", "")
+    )
+
     for {
-      _ <- createBucketInSameRegion(destGoogleProject, GoogleProject(workspace.googleProjectId.value), GcsBucketName(workspace.bucketName), bucketName, googleStorageService)
-    } yield (bucketName, DBIO.seq(
+      _ <- createBucketInSameRegion(
+        GcsBucketName(workspace.bucketName),
+        destGoogleProject,
+        tmpBucketName,
+        googleStorageService
+      )
+    } yield (tmpBucketName, DBIO.seq(
       migrations
         .filter(_.id === migrationAttempt.id)
         .map(r => (r.tmpBucket, r.tmpBucketCreated))
-        .update((bucketName.value.some, Timestamp.valueOf(LocalDateTime.now).some))
+        .update((tmpBucketName.value.some, Timestamp.valueOf(LocalDateTime.now).some))
     ))
   }
 }
@@ -145,7 +195,7 @@ object V1WorkspaceMigrationMonitor
 object V1WorkspaceMigrationActor {
   final case class Schedule(workspace: Workspace)
 
-  def apply(dataSource: SlickDataSource, gcsDAO: GoogleServicesDAO): Behavior[Schedule] = Behaviors.receive { (_, message) =>
+  def apply(dataSource: SlickDataSource): Behavior[Schedule] = Behaviors.receive { (_, message) =>
     dataSource.inTransaction(_ => V1WorkspaceMigrationMonitor.schedule(message.workspace))
     Behaviors.same
   }
