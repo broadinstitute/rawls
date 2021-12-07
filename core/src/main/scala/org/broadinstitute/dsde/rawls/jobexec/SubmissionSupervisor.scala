e992576d5335f3adeb311c681f8861e8903817c9
Adam Nichols <aednichols@gmail.com>
2021-12-07 15:56:35 -0500

    100 -> 10 per ticket A/C

--- core/src/main/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSupervisor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSupervisor.scala
@@ -194,7 +194,7 @@ class SubmissionSupervisor(executionServiceCluster: ExecutionServiceCluster,
     this.globalWorkflowStatusCounts = workflowStatus
   }
 
-  // restart the actor on failure (e.g. a DB deadlock or failed transaction) up to a maximum of 100 times
+  // restart the actor on failure (e.g. a DB deadlock or failed transaction) up to a maximum of 10 times
   override val supervisorStrategy = {
     val alwaysRestart: SupervisorStrategy.Decider = {
       case _ => Restart
@@ -209,7 +209,7 @@ class SubmissionSupervisor(executionServiceCluster: ExecutionServiceCluster,
         logger.error(s"error monitoring submission after $count times", throwable)
     }
 
-    new ThresholdOneForOneStrategy(thresholdLimit = 0, maxNrOfRetries = Option(100))(alwaysRestart)(thresholdFunc)
+    new ThresholdOneForOneStrategy(thresholdLimit = 0, maxNrOfRetries = Option(10))(alwaysRestart)(thresholdFunc)
   }
 
   private def registerGlobalJobExecGauges(): Unit = {
