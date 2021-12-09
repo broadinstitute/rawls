2600a2e763f62be247f0a78d8d3e639473ac5957
Edmund Higham <edhigham@gmail.com>
2021-12-09 14:16:39 -0500

    [CA-1630] Create Final Workspace Bucket

--- core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitorSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/V1WorkspaceMigrationMonitorSpec.scala
@@ -3,12 +3,13 @@ package org.broadinstitute.dsde.rawls.monitor
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import cats.implicits.catsSyntaxOptionId
 import com.typesafe.config.ConfigFactory
 import org.broadinstitute.dsde.rawls.dataaccess.slick.TestDriverComponent
 import org.broadinstitute.dsde.rawls.model.GoogleProjectId
 import org.broadinstitute.dsde.rawls.workspace.WorkspaceServiceSpec
 import org.broadinstitute.dsde.workbench.google2.GoogleStorageService
-import org.broadinstitute.dsde.workbench.model.google.GoogleProject
+import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GoogleProject}
 import org.broadinstitute.dsde.workbench.util2.{ConsoleLogger, LogLevel}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.flatspec.AnyFlatSpecLike
@@ -18,6 +19,7 @@ import slick.dbio.DBIO
 import slick.jdbc.MySQLProfile.api._
 
 import java.sql.SQLException
+import java.util.UUID
 import scala.language.postfixOps
 
 class V1WorkspaceMigrationMonitorSpec
@@ -45,41 +47,6 @@ class V1WorkspaceMigrationMonitorSpec
       assertThrows[SQLException] {
         runAndWait(V1WorkspaceMigrationMonitor.schedule(minimalTestData.v1Workspace))
       }
-    }
-  }
-
-  // use an existing test project (broad-dsde-dev)
-  "temp Integration Test" should "for realsies create a new bucket in the same region" ignore {
-    val sourceProject = "general-dev-billing-account"
-    val sourceBucket = "az-leotest"
-    val destProject = "terra-dev-7af423b8"
-    val config = ConfigFactory.load()
-    val gcsConfig = config.getConfig("gcs")
-    val serviceProject = GoogleProject(sourceProject)
-    val pathToCredentialJson = "config/rawls-account.json"
-    val v1WorkspaceCopy = minimalTestData.v1Workspace.copy(namespace = sourceProject, googleProjectId = GoogleProjectId(sourceProject), bucketName = sourceBucket)
-
-    withMinimalTestDatabase { _ =>
-      runAndWait {
-        DBIO.seq(
-          workspaceQuery.createOrUpdate(v1WorkspaceCopy),
-          V1WorkspaceMigrationMonitor.schedule(v1WorkspaceCopy)
-        )
-      }
-      val attempt = runAndWait(migrations.filter(_.workspaceId === v1WorkspaceCopy.workspaceIdAsUUID).result).head
-      val writeAction = GoogleStorageService.resource[IO](pathToCredentialJson, None, Option(serviceProject)).use { googleStorageService =>
-        for {
-          res <- V1WorkspaceMigrationMonitor.createTempBucket(attempt, v1WorkspaceCopy, GoogleProject(destProject), googleStorageService)
-          (bucketName, writeAction) = res
-          loadedBucket <- googleStorageService.getBucket(GoogleProject(destProject), bucketName)
-          _ <- googleStorageService.deleteBucket(GoogleProject(destProject), bucketName).compile.drain
-        } yield {
-          loadedBucket shouldBe defined
-          writeAction
-        }
-      }.unsafeRunSync
-
-      runAndWait(writeAction) shouldBe ()
     }
   }
 
@@ -112,24 +79,126 @@ class V1WorkspaceMigrationMonitorSpec
         }
         .unsafeRunSync
 
-      spec.runAndWait(dbOp) shouldBe ()
+      spec.runAndWait(dbOp) shouldBe()
 
       val (projectId, projectNumber, projectConfigured) = IO.fromFuture(IO {
-      services.slickDataSource.database
-        .run {
-          V1WorkspaceMigrationMonitor.migrations
-            .filter(_.workspaceId === spec.testData.v1Workspace.workspaceIdAsUUID)
-            .map(r => (r.newGoogleProjectId, r.newGoogleProjectNumber, r.newGoogleProjectConfigured))
-            .result
-        }
-    })
-      .map(_.head)
-      .unsafeRunSync
+        services.slickDataSource.database
+          .run {
+            V1WorkspaceMigrationMonitor.migrations
+              .filter(_.workspaceId === spec.testData.v1Workspace.workspaceIdAsUUID)
+              .map(r => (r.newGoogleProjectId, r.newGoogleProjectNumber, r.newGoogleProjectConfigured))
+              .result
+          }
+      })
+        .map(_.head)
+        .unsafeRunSync
 
       projectId shouldBe defined
       projectNumber shouldBe defined
       projectConfigured shouldBe defined
     }
   }
+
+  // use an existing test project (broad-dsde-dev)
+  "createTempBucket" should "create a new bucket in the same region as the workspace bucket" ignore {
+    val sourceProject = "general-dev-billing-account"
+    val sourceBucket = "az-leotest"
+    val destProject = "terra-dev-7af423b8"
+    val config = ConfigFactory.load()
+    val gcsConfig = config.getConfig("gcs")
+    val serviceProject = GoogleProject(sourceProject)
+    val pathToCredentialJson = "config/rawls-account.json"
+    val v1WorkspaceCopy = minimalTestData.v1Workspace.copy(namespace = sourceProject, googleProjectId = GoogleProjectId(sourceProject), bucketName = sourceBucket)
+
+    withMinimalTestDatabase { _ =>
+      runAndWait {
+        DBIO.seq(
+          workspaceQuery.createOrUpdate(v1WorkspaceCopy),
+          V1WorkspaceMigrationMonitor.schedule(v1WorkspaceCopy)
+        )
+      }
+
+      // Creating the temp bucket requires that the new google project has been created
+      val attempt = runAndWait(migrations.filter(_.workspaceId === v1WorkspaceCopy.workspaceIdAsUUID).result)
+        .head
+        .copy(newGoogleProjectId = GoogleProjectId(destProject).some)
+
+      val writeAction = GoogleStorageService.resource[IO](pathToCredentialJson, None, Option(serviceProject)).use { googleStorageService =>
+        for {
+          res <- V1WorkspaceMigrationMonitor.createTempBucket(attempt, v1WorkspaceCopy, googleStorageService)
+          (bucketName, writeAction) = res
+          loadedBucket <- googleStorageService.getBucket(GoogleProject(destProject), bucketName)
+          _ <- googleStorageService.deleteBucket(GoogleProject(destProject), bucketName).compile.drain
+        } yield {
+          loadedBucket shouldBe defined
+          writeAction
+        }
+      }.unsafeRunSync
+
+      runAndWait(writeAction) shouldBe ()
+    }
+  }
+
+  "createFinalBucket" should "create a new bucket in the same region as the tmp workspace bucket" ignore {
+    val sourceProject = "general-dev-billing-account"
+    val sourceBucket = "az-leotest"
+    val destProject = "terra-dev-7af423b8"
+    val destBucket = "v1-migration-test-" + UUID.randomUUID.toString.replace("-", "")
+    val serviceProject = GoogleProject(sourceProject)
+    val pathToCredentialJson = "config/rawls-account.json"
+
+    val v1Workspace = minimalTestData.v1Workspace.copy(
+      namespace = sourceProject,
+      googleProjectId = GoogleProjectId(sourceProject),
+      bucketName = destBucket
+    )
+
+    withMinimalTestDatabase { dataSource =>
+      runAndWait {
+        DBIO.seq(
+          workspaceQuery.createOrUpdate(v1Workspace),
+          V1WorkspaceMigrationMonitor.schedule(v1Workspace)
+        )
+      }
+
+      // Creating the bucket requires that the new google project and tmp bucket have been created
+      val attempt = runAndWait(migrations.filter(_.workspaceId === v1Workspace.workspaceIdAsUUID).result)
+        .head
+        .copy(
+          newGoogleProjectId = GoogleProjectId(destProject).some,
+          tmpBucketName = GcsBucketName(sourceBucket).some
+        )
+
+      val writeAction = GoogleStorageService.resource[IO](pathToCredentialJson, None, Option(serviceProject)).use { googleStorageService =>
+        for {
+          res <- V1WorkspaceMigrationMonitor.createFinalBucket(attempt, v1Workspace, googleStorageService)
+          (bucketName, writeAction) = res
+          loadedBucket <- googleStorageService.getBucket(GoogleProject(destProject), bucketName)
+          _ <- googleStorageService.deleteBucket(GoogleProject(destProject), bucketName).compile.drain
+        } yield {
+          loadedBucket shouldBe defined
+          loadedBucket.get.getName shouldBe destBucket
+          writeAction
+        }
+      }.unsafeRunSync
+
+      runAndWait(writeAction) shouldBe ()
+
+      val finalBucketCreated = IO.fromFuture(IO {
+        dataSource.database
+          .run {
+            V1WorkspaceMigrationMonitor.migrations
+              .filter(_.id === attempt.id)
+              .map(_.finalBucketCreated)
+              .result
+          }
+      })
+        .map(_.head)
+        .unsafeRunSync
+
+      finalBucketCreated shouldBe defined
+    }
+  }
+
 }
 
