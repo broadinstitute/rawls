0d8b5294393f3c2b2f1b697ebdeda51bbea932f7
Edmund Higham <edhigham@gmail.com>
2021-12-18 16:31:32 -0500

    extract test mocks to their own files under mock/

--- core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/WorkspaceMigrationMonitorSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/WorkspaceMigrationMonitorSpec.scala
@@ -1,30 +1,23 @@
 package org.broadinstitute.dsde.rawls.monitor
 
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
-import cats.data.NonEmptyList
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import cats.implicits.catsSyntaxOptionId
-import com.google.auth.Credentials
-import com.google.cloud.{Identity, Policy}
-import com.google.cloud.storage.{Acl, Blob, BlobId, Bucket, BucketInfo, Storage}
-import com.google.longrunning.Operation
+import com.google.cloud.storage.Storage
 import com.google.storagetransfer.v1.proto.TransferTypes.TransferJob
 import com.typesafe.config.ConfigFactory
-import fs2.Pipe
 import org.broadinstitute.dsde.rawls.dataaccess.slick.TestDriverComponent
+import org.broadinstitute.dsde.rawls.mock.{MockGoogleStorageService, MockGoogleStorageTransferService}
 import org.broadinstitute.dsde.rawls.model.GoogleProjectId
 import org.broadinstitute.dsde.rawls.monitor.migration.WorkspaceMigrationMonitor
 import org.broadinstitute.dsde.rawls.workspace.WorkspaceServiceSpec
 import org.broadinstitute.dsde.workbench.RetryConfig
+import org.broadinstitute.dsde.workbench.google2.GoogleStorageService
 import org.broadinstitute.dsde.workbench.google2.GoogleStorageTransferService.{JobName, JobTransferSchedule}
-import org.broadinstitute.dsde.workbench.google2.{GcsBlobName, GetMetadataResponse, GoogleStorageService, GoogleStorageTransferService, OperationName, RemoveObjectResult, StorageRole}
 import org.broadinstitute.dsde.workbench.model.TraceId
-import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GcsObjectName, GoogleProject, ServiceAccount}
+import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GoogleProject}
 import org.broadinstitute.dsde.workbench.util2.{ConsoleLogger, LogLevel}
-import org.mockito.Answers.RETURNS_SMART_NULLS
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{mock, when}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
@@ -32,7 +25,6 @@ import org.scalatest.{BeforeAndAfterAll, OptionValues}
 import slick.dbio.DBIO
 import slick.jdbc.MySQLProfile.api._
 
-import java.nio.file.Path
 import java.sql.SQLException
 import java.util.UUID
 import scala.language.postfixOps
@@ -49,61 +41,6 @@ class WorkspaceMigrationMonitorSpec
   implicit val logger = new ConsoleLogger("unit_test", LogLevel(false, false, true, true))
 
   override def afterAll(): Unit = testKit.shutdownTestKit()
-
-  class MockGoogleStorageService extends GoogleStorageService[IO] {
-
-    override def listObjectsWithPrefix(bucketName: GcsBucketName, objectNamePrefix: String, isRecursive: Boolean, maxPageSize: Long, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, GcsObjectName] = ???
-
-    override def listBlobsWithPrefix(bucketName: GcsBucketName, objectNamePrefix: String, isRecursive: Boolean, maxPageSize: Long, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Blob] = ???
-
-    override def createBlob(bucketName: GcsBucketName, objectName: GcsBlobName, objectContents: Array[Byte], objectType: String, metadata: Map[String, String], generation: Option[Long], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Blob] = ???
-
-    override def streamUploadBlob(bucketName: GcsBucketName, objectName: GcsBlobName, metadata: Map[String, String], generation: Option[Long], overwrite: Boolean, traceId: Option[TraceId]): Pipe[IO, Byte, Unit] = ???
-
-    override def setBucketLifecycle(bucketName: GcsBucketName, lifecycleRules: List[BucketInfo.LifecycleRule], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def unsafeGetBlobBody(bucketName: GcsBucketName, blobName: GcsBlobName, traceId: Option[TraceId], retryConfig: RetryConfig): IO[Option[String]] = ???
-
-    override def getBlobBody(bucketName: GcsBucketName, blobName: GcsBlobName, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Byte] = ???
-
-    override def getBlob(bucketName: GcsBucketName, blobName: GcsBlobName, credential: Option[Credentials], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Blob] = ???
-
-    override def downloadObject(blobId: BlobId, path: Path, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def getObjectMetadata(bucketName: GcsBucketName, blobName: GcsBlobName, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, GetMetadataResponse] = ???
-
-    override def setObjectMetadata(bucketName: GcsBucketName, blobName: GcsBlobName, metadata: Map[String, String], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def removeObject(bucketName: GcsBucketName, blobName: GcsBlobName, generation: Option[Long], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, RemoveObjectResult] = ???
-
-    override def getBucket(googleProject: GoogleProject, bucketName: GcsBucketName, bucketGetOptions: List[Storage.BucketGetOption], traceId: Option[TraceId]): IO[Option[Bucket]] = ???
-
-    override def insertBucket(googleProject: GoogleProject, bucketName: GcsBucketName, acl: Option[NonEmptyList[Acl]], labels: Map[String, String], traceId: Option[TraceId], bucketPolicyOnlyEnabled: Boolean, logBucket: Option[GcsBucketName], retryConfig: RetryConfig, location: Option[String]): fs2.Stream[IO, Unit] = ???
-
-    override def deleteBucket(googleProject: GoogleProject, bucketName: GcsBucketName, isRecursive: Boolean, bucketSourceOptions: List[Storage.BucketSourceOption], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Boolean] = ???
-
-    override def setBucketPolicyOnly(bucketName: GcsBucketName, bucketPolicyOnlyEnabled: Boolean, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def setBucketLabels(bucketName: GcsBucketName, labels: Map[String, String], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def setIamPolicy(bucketName: GcsBucketName, roles: Map[StorageRole, NonEmptyList[Identity]], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Unit] = ???
-
-    override def overrideIamPolicy(bucketName: GcsBucketName, roles: Map[StorageRole, NonEmptyList[Identity]], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Policy] = ???
-
-    override def getIamPolicy(bucketName: GcsBucketName, traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Policy] = ???
-  }
-
-  class MockGoogleStorageTransferService extends GoogleStorageTransferService[IO] {
-    override def getStsServiceAccount(project: GoogleProject): IO[ServiceAccount] = ???
-
-    override def createTransferJob(jobName: JobName, jobDescription: String, projectToBill: GoogleProject, originBucket: GcsBucketName, destinationBucket: GcsBucketName, schedule: JobTransferSchedule): IO[TransferJob] = ???
-
-    override def getTransferJob(jobName: JobName, project: GoogleProject): IO[TransferJob] = ???
-
-    override def listTransferOperations(jobName: JobName, project: GoogleProject): IO[Seq[Operation]] = ???
-
-    override def getTransferOperation(operationName: OperationName): IO[Operation] = ???
-  }
 
   "isMigrating" should "return false when a workspace is not being migrated" in {
     withMinimalTestDatabase { _ =>
@@ -250,7 +187,7 @@ class WorkspaceMigrationMonitorSpec
       val writeAction = WorkspaceMigrationMonitor.deleteWorkspaceBucket(
         migration,
         v1Workspace,
-        new MockGoogleStorageService {
+        new MockGoogleStorageService[IO] {
           override def deleteBucket(googleProject: GoogleProject, bucketName: GcsBucketName, isRecursive: Boolean, bucketSourceOptions: List[Storage.BucketSourceOption], traceId: Option[TraceId], retryConfig: RetryConfig): fs2.Stream[IO, Boolean] =
             fs2.Stream.emit(true)
         }
@@ -306,7 +243,7 @@ class WorkspaceMigrationMonitorSpec
         GcsBucketName(v1Workspace.bucketName),
         migration.tmpBucketName.get,
         GoogleProject("to-be-determined"),
-        new MockGoogleStorageTransferService {
+        new MockGoogleStorageTransferService[IO] {
           override def createTransferJob(jobName: JobName, jobDescription: String, projectToBill: GoogleProject, originBucket: GcsBucketName, destinationBucket: GcsBucketName, schedule: JobTransferSchedule): IO[TransferJob] =
             IO.pure {
               TransferJob.newBuilder()
@@ -317,7 +254,7 @@ class WorkspaceMigrationMonitorSpec
             }
         }).unsafeRunSync
 
-      runAndWait(writeAction) shouldBe ()
+      runAndWait(writeAction) shouldBe()
 
       val transferJobs = runAndWait(
         WorkspaceMigrationMonitor.storageTransferJobs
@@ -382,11 +319,11 @@ class WorkspaceMigrationMonitorSpec
       runAndWait(writeAction) shouldBe()
 
       val finalBucketCreated = runAndWait(
-            WorkspaceMigrationMonitor.workspaceMigrations
-              .filter(_.id === migration.id)
-              .map(_.finalBucketCreated)
-              .result
-              .map(_.head)
+        WorkspaceMigrationMonitor.workspaceMigrations
+          .filter(_.id === migration.id)
+          .map(_.finalBucketCreated)
+          .result
+          .map(_.head)
       )
 
       finalBucketCreated shouldBe defined
