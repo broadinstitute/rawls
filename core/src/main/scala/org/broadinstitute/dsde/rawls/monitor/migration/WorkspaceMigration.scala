c74a3669116808632adc2f884a2dabb7ce2dde9a
Edmund Higham <edhigham@gmail.com>
2022-01-13 13:17:47 -0500

    [CA-1702] Add UPDATED, bucket JOB_ISSUED and TRANSFERRED to database RR: https://broadworkbench.atlassian.net/browse/CA-1703 UPDATED is bumped to CURRENT_TIMESTAMP whenever a row is updated. Add new states relating to waiting for submitted sts jobs.

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigration.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigration.scala
@@ -14,6 +14,7 @@ final case class WorkspaceMigration(id: Long,
                                     workspaceId: UUID,
                                     created: Timestamp,
                                     started: Option[Timestamp],
+                                    updated: Timestamp,
                                     finished: Option[Timestamp],
                                     outcome: Option[Outcome],
                                     newGoogleProjectId: Option[GoogleProjectId],
@@ -21,16 +22,24 @@ final case class WorkspaceMigration(id: Long,
                                     newGoogleProjectConfigured: Option[Timestamp],
                                     tmpBucketName: Option[GcsBucketName],
                                     tmpBucketCreated: Option[Timestamp],
+                                    workspaceBucketTransferJobIssued: Option[Timestamp],
+                                    workspaceBucketTransferred: Option[Timestamp],
                                     workspaceBucketDeleted: Option[Timestamp],
                                     finalBucketCreated: Option[Timestamp],
+                                    tmpBucketTransferJobIssued: Option[Timestamp],
+                                    tmpBucketTransferred: Option[Timestamp],
                                     tmpBucketDeleted: Option[Timestamp]
                                    ) {
 
   def getStatus: MigrationStatus = (
     ((finished fmap Finished.curried) <*> outcome).widen[MigrationStatus]
       <+> (tmpBucketDeleted fmap TmpBucketDeleted)
+      <+> (tmpBucketTransferred fmap TmpBucketTransferred)
+      <+> (tmpBucketTransferJobIssued fmap TmpBucketTransferJobIssued)
       <+> (finalBucketCreated fmap FinalWorkspaceBucketCreated)
       <+> (workspaceBucketDeleted fmap WorkspaceBucketDeleted)
+      <+> (workspaceBucketTransferred fmap WorkspaceBucketTransferred)
+      <+> (workspaceBucketTransferJobIssued fmap WorkspaceBucketTransferJobIssued)
       <+> ((tmpBucketCreated fmap TmpBucketCreated.curried) <*> tmpBucketName)
       <+> ((newGoogleProjectConfigured fmap GoogleProjectConfigured.curried) <*> newGoogleProjectId)
       <+> (started fmap Started)
@@ -46,9 +55,11 @@ object MigrationStatus {
   final case class Started(time: Timestamp) extends MigrationStatus
   final case class GoogleProjectConfigured(time: Timestamp, googleProjectId: GoogleProjectId) extends MigrationStatus
   final case class TmpBucketCreated(time: Timestamp, bucketName: GcsBucketName) extends MigrationStatus
+  final case class WorkspaceBucketTransferJobIssued(time: Timestamp) extends MigrationStatus
   final case class WorkspaceBucketTransferred(time: Timestamp) extends MigrationStatus
   final case class WorkspaceBucketDeleted(time: Timestamp) extends MigrationStatus
   final case class FinalWorkspaceBucketCreated(time: Timestamp) extends MigrationStatus
+  final case class TmpBucketTransferJobIssued(time: Timestamp) extends MigrationStatus
   final case class TmpBucketTransferred(time: Timestamp) extends MigrationStatus
   final case class TmpBucketDeleted(time: Timestamp) extends MigrationStatus
   final case class Finished(time: Timestamp, outcome: Outcome) extends MigrationStatus
@@ -59,25 +70,44 @@ private[migration]
 object WorkspaceMigration {
 
   type RecordType = (
-    Long, UUID, Timestamp, Option[Timestamp], Option[Timestamp], Option[String], Option[String],
-      Option[String], Option[String], Option[Timestamp],
-      Option[String], Option[Timestamp],
-      Option[Timestamp], Option[Timestamp], Option[Timestamp]
+    Long,                 // id
+      UUID,               // workspace uuid
+      Timestamp,          // created
+      Option[Timestamp],  // started
+      Timestamp,          // updated
+      Option[Timestamp],  // finished
+      Option[String],     // outcome
+      Option[String],     // message
+      Option[String],     // newGoogleProjectId
+      Option[String],     // newGoogleProjectNumber
+      Option[Timestamp],  // newGoogleProjectConfigured
+      Option[String],     // tmpBucketName
+      Option[Timestamp],  // tmpBucketCreated
+      Option[Timestamp],  // workspaceBucketTransferJobIssued
+      Option[Timestamp],  // workspaceBucketTransferred
+      Option[Timestamp],  // workspaceBucketDeleted
+      Option[Timestamp],  // finalBucketCreated
+      Option[Timestamp],  // tmpBucketTransferJobIssued
+      Option[Timestamp],  // tmpBucketTransferred
+      Option[Timestamp]   // tmpBucketDeleted
     )
 
 
   def fromRecord(record: RecordType): Either[String, WorkspaceMigration] = record match {
-    case (id, workspaceId, created, started, finished, outcome, message,
-    newGoogleProjectId, newGoogleProjectNumber, newGoogleProjectConfigured,
-    tmpBucketName, tmpBucketCreated,
-    workspaceBucketDeleted,
-    finalBucketCreated,
-      tmpBucketDeleted) => Outcome.fromFields(outcome, message).map { outcome =>
+    case (
+      id, workspaceId, created, started, updated, finished, outcome, message,
+      newGoogleProjectId, newGoogleProjectNumber, newGoogleProjectConfigured,
+      tmpBucketName, tmpBucketCreated,
+      workspaceBucketTransferJobIssued, workspaceBucketTransferred, workspaceBucketDeleted,
+      finalBucketCreated,
+      tmpBucketTransferJobIssued, tmpBucketTransferred, tmpBucketDeleted
+      ) => Outcome.fromFields(outcome, message).map { outcome =>
       WorkspaceMigration(
         id,
         workspaceId,
         created,
         started,
+        updated,
         finished,
         outcome,
         newGoogleProjectId.map(GoogleProjectId),
@@ -85,8 +115,12 @@ object WorkspaceMigration {
         newGoogleProjectConfigured,
         tmpBucketName.map(GcsBucketName),
         tmpBucketCreated,
+        workspaceBucketTransferJobIssued,
+        workspaceBucketTransferred,
         workspaceBucketDeleted,
         finalBucketCreated,
+        tmpBucketTransferJobIssued,
+        tmpBucketTransferred,
         tmpBucketDeleted
       )
     }
@@ -100,6 +134,7 @@ object WorkspaceMigration {
       migration.workspaceId,
       migration.created,
       migration.started,
+      migration.updated,
       migration.finished,
       outcome,
       message,
@@ -108,8 +143,12 @@ object WorkspaceMigration {
       migration.newGoogleProjectConfigured,
       migration.tmpBucketName.map(_.value),
       migration.tmpBucketCreated,
+      migration.workspaceBucketTransferJobIssued,
+      migration.workspaceBucketTransferred,
       migration.workspaceBucketDeleted,
       migration.finalBucketCreated,
+      migration.tmpBucketTransferJobIssued,
+      migration.tmpBucketTransferred,
       migration.tmpBucketDeleted
     )
   }
@@ -127,6 +166,7 @@ object WorkspaceMigrationHistory {
     def workspaceId = column[UUID]("WORKSPACE_ID", O.SqlType("BINARY(16)"))
     def created = column[Timestamp]("CREATED")
     def started = column[Option[Timestamp]]("STARTED")
+    def updated = column[Timestamp]("UPDATED")
     def finished = column[Option[Timestamp]]("FINISHED")
     def outcome = column[Option[String]]("OUTCOME")
     def message = column[Option[String]]("MESSAGE")
@@ -135,17 +175,22 @@ object WorkspaceMigrationHistory {
     def newGoogleProjectConfigured = column[Option[Timestamp]]("NEW_GOOGLE_PROJECT_CONFIGURED")
     def tmpBucket = column[Option[String]]("TMP_BUCKET")
     def tmpBucketCreated = column[Option[Timestamp]]("TMP_BUCKET_CREATED")
+    def workspaceBucketTransferJobIssued = column[Option[Timestamp]]("WORKSPACE_BUCKET_TRANSFER_JOB_ISSUED")
+    def workspaceBucketTransferred = column[Option[Timestamp]]("WORKSPACE_BUCKET_TRANSFERRED")
     def workspaceBucketDeleted = column[Option[Timestamp]]("WORKSPACE_BUCKET_DELETED")
     def finalBucketCreated = column[Option[Timestamp]]("FINAL_BUCKET_CREATED")
+    def tmpBucketTransferJobIssued = column[Option[Timestamp]]("TMP_BUCKET_TRANSFER_JOB_ISSUED")
+    def tmpBucketTransferred = column[Option[Timestamp]]("TMP_BUCKET_TRANSFERRED")
     def tmpBucketDeleted = column[Option[Timestamp]]("TMP_BUCKET_DELETED")
 
     override def * =
-      (id, workspaceId, created, started, finished, outcome, message,
+      (
+        id, workspaceId, created, started, updated, finished, outcome, message,
         newGoogleProjectId, newGoogleProjectNumber, newGoogleProjectConfigured,
         tmpBucket, tmpBucketCreated,
-        workspaceBucketDeleted,
+        workspaceBucketTransferJobIssued, workspaceBucketTransferred, workspaceBucketDeleted,
         finalBucketCreated,
-        tmpBucketDeleted
+        tmpBucketTransferJobIssued, tmpBucketTransferred, tmpBucketDeleted
       ) <>
         (MigrationUtils.unsafeFromEither(WorkspaceMigration.fromRecord, _),
           WorkspaceMigration.toRecord(_: WorkspaceMigration).some)
