50e4750997385d4e8b3e9f02d7e0766cbc16bb11
Edmund Higham <edhigham@gmail.com>
2022-02-22 12:39:39 -0500

    remove redundant punctuation

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigrationActor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigrationActor.scala
@@ -473,14 +473,14 @@ object WorkspaceMigrationActor {
 
           // STS requires the following to read from the origin bucket
           _ <- storageService.setIamPolicy(originBucket, Map(
-            (StorageRole.LegacyBucketReader -> serviceAccountList),
-            (StorageRole.ObjectViewer -> serviceAccountList)
+            StorageRole.LegacyBucketReader -> serviceAccountList,
+            StorageRole.ObjectViewer -> serviceAccountList
           )).compile.drain
 
           // STS requires the following to write to the destination bucket
           _ <- storageService.setIamPolicy(destBucket, Map(
-            (StorageRole.LegacyBucketWriter -> serviceAccountList),
-            (StorageRole.ObjectCreator -> serviceAccountList)
+            StorageRole.LegacyBucketWriter -> serviceAccountList,
+            StorageRole.ObjectCreator -> serviceAccountList
           )).compile.drain
 
           jobName <- randomSuffix("transferJobs/terra-workspace-migration-")
@@ -566,13 +566,13 @@ object WorkspaceMigrationActor {
             serviceAccountList = NonEmptyList.one(Identity.serviceAccount(serviceAccount.email.value))
 
             _ <- storageService.removeIamPolicy(transferJob.originBucket, Map(
-              (StorageRole.LegacyBucketReader -> serviceAccountList),
-              (StorageRole.ObjectViewer -> serviceAccountList)
+              StorageRole.LegacyBucketReader -> serviceAccountList,
+              StorageRole.ObjectViewer -> serviceAccountList
             )).compile.drain
 
             _ <- storageService.removeIamPolicy(transferJob.destBucket, Map(
-              (StorageRole.LegacyBucketWriter -> serviceAccountList),
-              (StorageRole.ObjectCreator -> serviceAccountList)
+              StorageRole.LegacyBucketWriter -> serviceAccountList,
+              StorageRole.ObjectCreator -> serviceAccountList
             )).compile.drain
 
           } yield ()
