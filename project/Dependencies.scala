97ce8c21401b827b58e067f71adb273ecc2c5a9a
Jordan Safer <jordansafer2@gmail.com>
2022-03-03 15:13:26 -0500

    [AJ-289] bump google api client v to 1.31.1 for model to match rest of rawls and fix dated oauth client

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaHttpV = "10.2.9"
   val slickV = "3.3.2"
 
-  val googleV = "1.22.0"
+  val googleV = "1.31.1"
   val olderGoogleV = "1.20.0"   // TODO why do we have two google versions?  GAWB-2149
 
   val cromwellVersion = "40-2754783"
