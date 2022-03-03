e9ff9547e3f00bc97540380167aa039cccb4af79
Jordan Safer <jordansafer2@gmail.com>
2022-03-03 14:57:56 -0500

    [AJ-284] bump google 2 to latest image with security fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -104,7 +104,7 @@ object Dependencies {
   val mysqlConnector: ModuleID =  "mysql"                         % "mysql-connector-java"  % "8.0.22"
   val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "3.10.3"
 
-  val workbenchLibsHash = "7ddf186"
+  val workbenchLibsHash = "11a45ad"
 
   val workbenchModelV  = s"0.15-${workbenchLibsHash}"
   val workbenchGoogleV = s"0.21-${workbenchLibsHash}"
