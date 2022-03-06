62484b3e23a42eadc74b67e9f4246c3698bf8888
David An <davidan@broadinstitute.org>
2022-03-06 16:38:58 -0500

    remove akka route definitions

--- core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/SnapshotApiService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/SnapshotApiService.scala
@@ -62,26 +62,6 @@ trait SnapshotApiService extends UserInfoDirectives {
         snapshotServiceConstructor(userInfo).getSnapshotByName(WorkspaceName(workspaceNamespace, workspaceName), referenceName)
       }
     } ~
-    // ---- SNAPSHOT V1 ----
-    path("workspaces" / Segment / Segment / "snapshots") { (workspaceNamespace, workspaceName) =>
-      post {
-        entity(as[NamedDataRepoSnapshot]) { namedDataRepoSnapshot =>
-          complete {
-            snapshotServiceConstructor(userInfo).createSnapshot(WorkspaceName(workspaceNamespace, workspaceName), namedDataRepoSnapshot)
-              .map(dataRepoSnapshotResourceToDataReferenceDescription)
-              .map(StatusCodes.Created -> _)
-          }
-        }
-      } ~
-      get {
-        parameters("offset".as[Int], "limit".as[Int]) { (offset, limit) =>
-          complete {
-            snapshotServiceConstructor(userInfo).enumerateSnapshots(WorkspaceName(workspaceNamespace, workspaceName), offset, limit)
-              .map(snapshotListResponseToDataReferenceList)
-          }
-        }
-      }
-    } ~
     path("workspaces" / Segment / Segment / "snapshots" / "v2" / Segment) { (workspaceNamespace, workspaceName, snapshotId) =>
       get {
         complete {
@@ -100,75 +80,7 @@ trait SnapshotApiService extends UserInfoDirectives {
           snapshotServiceConstructor(userInfo).deleteSnapshot(WorkspaceName(workspaceNamespace, workspaceName), snapshotId).map(_ => StatusCodes.NoContent)
         }
       }
-    } ~
-    // ---- SNAPSHOT V1 ----
-    path("workspaces" / Segment / Segment / "snapshots") { (workspaceNamespace, workspaceName) =>
-      post {
-        entity(as[NamedDataRepoSnapshot]) { namedDataRepoSnapshot =>
-          complete {
-            snapshotServiceConstructor(userInfo).createSnapshot(WorkspaceName(workspaceNamespace, workspaceName), namedDataRepoSnapshot)
-              .map(dataRepoSnapshotResourceToDataReferenceDescription)
-              .map(StatusCodes.Created -> _)
-          }
-        }
-      } ~
-      get {
-        parameters("offset".as[Int], "limit".as[Int]) { (offset, limit) =>
-          complete {
-            snapshotServiceConstructor(userInfo).enumerateSnapshots(WorkspaceName(workspaceNamespace, workspaceName), offset, limit)
-              .map(snapshotListResponseToDataReferenceList)
-          }
-        }
-      }
-    } ~
-    path("workspaces" / Segment / Segment / "snapshots" / Segment) { (workspaceNamespace, workspaceName, snapshotId) =>
-      get {
-        complete {
-          snapshotServiceConstructor(userInfo).getSnapshot(WorkspaceName(workspaceNamespace, workspaceName), snapshotId)
-            .map(dataRepoSnapshotResourceToDataReferenceDescription)
-        }
-      } ~
-      patch {
-        entity(as[UpdateDataReferenceRequestBody]) { updateDataReferenceRequestBody =>
-          complete {
-            snapshotServiceConstructor(userInfo).updateSnapshot(WorkspaceName(workspaceNamespace, workspaceName), snapshotId, updateDataReferenceRequestBody).map(_ => StatusCodes.NoContent)
-          }
-        }
-      } ~
-      delete {
-        complete {
-          snapshotServiceConstructor(userInfo).deleteSnapshot(WorkspaceName(workspaceNamespace, workspaceName), snapshotId).map(_ => StatusCodes.NoContent)
-        }
-      }
     }
-  }
-
-  // -------  REMOVE WHEN GETTING RID OF V1 SNAPSHOT ENDPOINTS -------
-
-  private def dataRepoSnapshotResourceToDataReferenceDescription(resource: DataRepoSnapshotResource): DataReferenceDescription = {
-    new DataReferenceDescription()
-      .name(resource.getMetadata.getName)
-      .description(resource.getMetadata.getDescription)
-      .referenceId(resource.getMetadata.getResourceId)
-      .referenceType(ReferenceTypeEnum.fromValue(resource.getMetadata.getResourceType.getValue))
-      .workspaceId(resource.getMetadata.getWorkspaceId)
-      .reference(new DataRepoSnapshot().instanceName(resource.getAttributes.getInstanceName).snapshot(resource.getAttributes.getSnapshot))
-      .cloningInstructions(resource.getMetadata.getCloningInstructions)
-  }
-
-  private def snapshotListResponseToDataReferenceList(snapshotResponse: SnapshotListResponse): DataReferenceList= {
-    import scala.collection.JavaConverters._
-    val snaps = snapshotResponse.gcpDataRepoSnapshots.map{ res =>
-      new DataReferenceDescription()
-        .name(res.getMetadata.getName)
-        .description(res.getMetadata.getDescription)
-        .referenceId(res.getMetadata.getResourceId)
-        .referenceType(ReferenceTypeEnum.fromValue(res.getMetadata.getResourceType.getValue))
-        .workspaceId(res.getMetadata.getWorkspaceId)
-        .reference(new DataRepoSnapshot().instanceName(res.getAttributes.getInstanceName).snapshot(res.getAttributes.getSnapshot))
-        .cloningInstructions(res.getMetadata.getCloningInstructions)
-    }.asJava
-    new DataReferenceList().resources(snaps)
   }
 
 }
