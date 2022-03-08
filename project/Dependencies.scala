dbfbc5c6cd96a66612e3a93eb7c0d52a0bff8b1b
Edmund Higham <edhigham@gmail.com>
2022-03-08 17:17:27 -0500

    revert Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -103,6 +103,7 @@ object Dependencies {
    */
   val mysqlConnector: ModuleID =  "mysql"                         % "mysql-connector-java"  % "8.0.22"
   val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "3.10.3"
+
   val workbenchLibsHash = "11a45ad"
 
   val workbenchModelV  = s"0.15-${workbenchLibsHash}"
