25f655cff8bd4a42087d213e2808d8e16b08469d
Saloni Shah <salonipshah11@gmail.com>
2022-03-09 12:16:39 -0500

    Filter out "pet service account not found" error before sending events to Sentry [BW-1125] (#1658)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/util/SentryFilteredClientFactory.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/util/SentryFilteredClientFactory.scala
@@ -7,14 +7,19 @@ import io.sentry.{DefaultSentryClientFactory, SentryClient}
 
 import scala.util.{Failure, Success, Try}
 
-/* Filter for Sentry, which will ignore exceptions from https://github.com/slick/slick/issues/1856
-   here at the source, without attempting to send to Sentry.
+/* Filter for Sentry, which will ignore below exceptions from here at the source, without attempting to send to Sentry.
+    - https://github.com/slick/slick/issues/1856 (https://broadworkbench.atlassian.net/browse/AS-217)
+    - "pet service account not found" error: We are ignoring this exception because SubmissionMonitor actor is sending
+      about 150-200 messages per minute to Sentry and this is depleting most of the error capacity in Sentry and
+      putting us over quota (https://broadworkbench.atlassian.net/browse/BW-1125).
  */
 class RawlsSentryEventFilter extends ShouldSendEventCallback {
   override def shouldSend(event: Event): Boolean = {
     // earlier versions of sentry-logback could pass nulls, so we wrap everything in a Try to be defensive.
     // by default this will run in a separate async thread so failures aren't a problem, but still.
-    Try(!event.getMessage.contains("requirement failed: count cannot be decreased")) match {
+    Try(!event.getMessage.contains("requirement failed: count cannot be decreased") &&
+      !event.getMessage.contains("pet service account not found")
+    ) match {
       case Success(send) => send
       case Failure(_) => true
     }
