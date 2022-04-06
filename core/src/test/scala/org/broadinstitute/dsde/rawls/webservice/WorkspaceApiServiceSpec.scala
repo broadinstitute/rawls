264fc43ba2c5ddaf7e8bb4c2fa5c429eafe9538b
Matt Bemis <mbemis@broadinstitute.org>
2022-04-05 20:13:49 -0400

    fix unit test

--- core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
@@ -1910,7 +1910,7 @@ class WorkspaceApiServiceSpec extends ApiServiceSpec {
           assertResult(StatusCodes.OK) {
             status
           }
-          assertResult(BigInt(42), new DateTime(0)) {
+          assertResult(BucketUsageResponse(BigInt(42), new DateTime(0))) {
             responseAs[BucketUsageResponse]
           }
         }
