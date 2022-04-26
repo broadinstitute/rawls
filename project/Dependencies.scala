fbd5f93d339b7954cea331dddd8385b11a380bb5
Scala Steward <me@scala-steward.org>
2022-04-26 21:13:45 +0200

    Update breeze to 1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -84,7 +84,7 @@ object Dependencies {
   val scalatest: ModuleID =       "org.scalatest"                 %% "scalatest"            % "3.2.2" % "test"
   val mockito: ModuleID =         "org.scalatestplus"             %% "mockito-3-4"          % "3.2.10.0" % Test
   val mockserverNetty: ModuleID = "org.mock-server"               % "mockserver-netty"      % "5.11.2" % "test"
-  val breeze: ModuleID =          "org.scalanlp"                  %% "breeze"               % "1.2" % "test"
+  val breeze: ModuleID =          "org.scalanlp"                  %% "breeze"               % "1.3" % "test"
   val ficus: ModuleID =           "com.iheart"                    %% "ficus"                % "1.5.1"
   val apacheCommonsIO: ModuleID = "commons-io"                    % "commons-io"            % "2.6"
   val antlrParser: ModuleID =     "org.antlr"                     % "antlr4-runtime"        % "4.8-1"
