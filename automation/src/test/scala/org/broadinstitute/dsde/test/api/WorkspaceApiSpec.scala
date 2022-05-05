5d5682236f4a5faa9c485c3a0bc779599585db25
cahrens <roberts@broadinstitute.org>
2022-05-05 19:31:22 -0400

    Fix syntax.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
@@ -189,9 +189,9 @@ class WorkspaceApiSpec
         }
       }
 
-      "to get an error message when they try to create a workspace with a bucket region that is invalid" {
+      "to get an error message when they try to create a workspace with a bucket region that is invalid" in {
         implicit val token: AuthToken = ownerAuthToken
-        val invalidRegion = "invalid-regionx"
+        val invalidRegion = "invalid-region"
 
         val exception = withTemporaryBillingProject(billingAccountId) { billingProject =>
           val workspaceName = prependUUID("owner-invalid-region-workspace")
