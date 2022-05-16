baa549b34d72e8570590837eebe74b9192b3a3e2
broadbot <broadbot@broadinstitute.org>
2022-05-16 12:03:11 +0000

    Update grpc-core, grpc-netty-shaded from 1.33.1 to 1.46.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,8 +54,8 @@ object Dependencies {
   val googleDeploymentManager: ModuleID = "com.google.apis"   % "google-api-services-deploymentmanager" % ("v2beta-rev20210311-" + googleV)
   val googleGuava: ModuleID =             "com.google.guava"  % "guava" % "19.0"
 
-  val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.33.1"
-  val googleRpcNettyShaded: ModuleID =    "io.grpc" % "grpc-netty-shaded" % "1.33.1"
+  val googleRpc: ModuleID =               "io.grpc" % "grpc-core" % "1.46.0"
+  val googleRpcNettyShaded: ModuleID =    "io.grpc" % "grpc-netty-shaded" % "1.46.0"
   val googleCloudCoreGrpc: ModuleID =     "com.google.cloud" % "google-cloud-core-grpc" % "1.93.6"
 
   val googleAutoValue: ModuleID =         "com.google.auto.value" % "auto-value-annotations" % "1.7.4"
