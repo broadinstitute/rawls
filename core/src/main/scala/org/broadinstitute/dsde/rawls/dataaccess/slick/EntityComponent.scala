311e53f700255c92bb69d160e8585e37151b346f
Matt Bemis <mbemis@broadinstitute.org>
2022-05-16 12:57:22 -0400

    update lastModified as the last step

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
@@ -836,9 +836,9 @@ trait EntityComponent {
 
     // "deletes" entities of a certain type by hiding and renaming them
     def hideType(workspaceContext: Workspace, entityType: String): ReadWriteAction[Int] = {
-      workspaceQuery.updateLastModified(workspaceContext.workspaceIdAsUUID) andThen
-        EntityAndAttributesRawSqlQuery.batchHideAttributesOfType(workspaceContext, entityType) andThen
-        EntityRecordRawSqlQuery.batchHideType(workspaceContext.workspaceIdAsUUID, entityType).map(res => res.sum)
+      EntityAndAttributesRawSqlQuery.batchHideAttributesOfType(workspaceContext, entityType) andThen
+        EntityRecordRawSqlQuery.batchHideType(workspaceContext.workspaceIdAsUUID, entityType).map(res => res.sum) andThen
+        workspaceQuery.updateLastModified(workspaceContext.workspaceIdAsUUID)
     }
 
     // perform actual deletion (not hiding) of all entities in a workspace
