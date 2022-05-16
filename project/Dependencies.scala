dadaf3a1d7e43a84c4e715e704e56854ff80cbec
broadbot <broadbot@broadinstitute.org>
2022-05-16 12:03:21 +0000

    Update scala-uri from 3.0.0 to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
   val jerseyClient: ModuleID =    "org.glassfish.jersey.core"     % "jersey-client"         % "2.35"   // upgrading a transitive dependency to avoid security warnings
   val cats: ModuleID =            "org.typelevel"                 %% "cats-core"                 % "2.6.1"
   val logbackClassic: ModuleID =  "ch.qos.logback"                % "logback-classic"       % "1.2.11"
-  val scalaUri: ModuleID =        "io.lemonlabs"                  %% "scala-uri"            % "3.0.0"
+  val scalaUri: ModuleID =        "io.lemonlabs"                  %% "scala-uri"            % "3.6.0"
   val scalatest: ModuleID =       "org.scalatest"                 %% "scalatest"            % "3.2.11" % "test"
   val mockito: ModuleID =         "org.scalatestplus"             %% "mockito-4-2"          % "3.2.11.0" % Test
   val mockserverNetty: ModuleID = "org.mock-server"               % "mockserver-netty"      % "5.11.2" % "test"
