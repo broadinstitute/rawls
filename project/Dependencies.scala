414b1d51be8c3eca39d19407bee0407755929b14
broadbot <broadbot@broadinstitute.org>
2022-05-23 12:04:05 +0000

    Update typesafe:config from 1.4.1 to 1.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
   val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.8.10"
   val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.9.9"
   val jodaConvert: ModuleID =     "org.joda"                      % "joda-convert"          % "1.9.2"
-  val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.1"
+  val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.2"
   val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "1.7.30"
   val webjarsLocator: ModuleID =  "org.webjars"                   % "webjars-locator"       % "0.40"
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
