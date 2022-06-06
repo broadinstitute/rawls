0c82be8e4ce5253f973e5275bb4c6c006f96181d
broadbot <broadbot@broadinstitute.org>
2022-06-06 12:04:51 +0000

    Update scala-logging from 3.9.4 to 3.9.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -66,7 +66,7 @@ object Dependencies {
   val metricsScala: ModuleID =       "nl.grons"              %% "metrics4-scala"    % "4.1.9"
   val metricsStatsd: ModuleID =      "com.readytalk"         %  "metrics3-statsd"  % "4.2.0"
 
-  val scalaLogging: ModuleID =    "com.typesafe.scala-logging"    %% "scala-logging"        % "3.9.4"
+  val scalaLogging: ModuleID =    "com.typesafe.scala-logging"    %% "scala-logging"        % "3.9.5"
   val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.8.11"
   val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.9.9"
   val jodaConvert: ModuleID =     "org.joda"                      % "joda-convert"          % "1.9.2"
