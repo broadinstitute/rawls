4d73c89208eeb22668c7a14e51ab486a4a1b5175
Edmund Higham <edhigham@gmail.com>
2022-06-15 14:59:14 -0400

    revert unused changes

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/RawlsBillingProjectComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/RawlsBillingProjectComponent.scala
@@ -61,6 +61,7 @@ object RawlsBillingProjectRecord {
 
     BillingProjectSpendExport(RawlsBillingProjectName(projectRecord.projectName), billingAccount, table)
   }
+
 }
 
 case class RawlsBillingProjectOperationRecord(projectName: String, operationName: GoogleOperationName, operationId: String, done: Boolean, errorMessage: Option[String], api: GoogleApiType)
@@ -393,12 +394,13 @@ trait RawlsBillingProjectComponent {
       )
     }
 
-    def getLastChange(billingProject: RawlsBillingProjectName): ReadAction[Option[BillingAccountChange]] =
+    def getLastChange(billingProject: RawlsBillingProjectName): ReadAction[Option[BillingAccountChange]] = {
       BillingAccountChanges
         .withProjectName(billingProject)
         .sortBy(_.id.desc)
         .result
         .map(_.headOption)
+    }
   }
 
   implicit class BillingAccountChangeExtensions(query: BillingAccountChangeQuery) {
