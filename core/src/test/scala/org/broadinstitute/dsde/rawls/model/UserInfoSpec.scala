3ef54589b9c88abf476e53038d004afdf010bac3
Drew Herbst <aherbst@broadinstitute.org>
2022-06-21 15:03:58 -0400

    [WOR-374] Use the correct endpoint when queyring Sam for user info, do not use the sam user subject ID

--- core/src/test/scala/org/broadinstitute/dsde/rawls/model/UserInfoSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/model/UserInfoSpec.scala
@@ -14,8 +14,5 @@ class UserInfoSpec extends AnyFlatSpec with Matchers {
    it should "return true for B2C users" in {
      val userInfo = UserInfo(RawlsUserEmail("fake@email.com"), OAuth2BearerToken("some-token"), 300, RawlsUserSubjectId("704ef594-9669-45f4-b605-82b499065a49"), None)
      userInfo.isB2C shouldBe true
-
-     val userInfo2 = UserInfo(RawlsUserEmail("fake@email.com"), OAuth2BearerToken("some-token"), 300, RawlsUserSubjectId("another-user"), Some(OAuth2BearerToken("some-google-token")))
-     userInfo2.isB2C shouldBe true
    }
 }
