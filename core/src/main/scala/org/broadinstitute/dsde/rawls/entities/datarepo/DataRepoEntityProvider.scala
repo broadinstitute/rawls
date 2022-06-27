91a7a52393058b9df9cbe067b1cf2e7ee83d3098
Bria Morgan <bmorgan@wm106-f2d.broadinstitute.org>
2022-06-27 15:27:50 -0400

    AJ-433 only add datarepo_row_id if missing; update test

--- core/src/main/scala/org/broadinstitute/dsde/rawls/entities/datarepo/DataRepoEntityProvider.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/entities/datarepo/DataRepoEntityProvider.scala
@@ -56,8 +56,11 @@ class DataRepoEntityProvider(snapshotModel: SnapshotModel,
 
     // reformat TDR's response into the expected response structure - all snapshots should have a datarepo_row_id
     val entityTypesResponse: Map[String, EntityTypeMetadata] = snapshotModel.getTables.asScala.map { table =>
-      val attrs: Seq[String] = table.getColumns.asScala.map(_.getName).toList :+ "datarepo_row_id"
       val primaryKey = pkFromSnapshotTable(table)
+      var attrs: Seq[String] = table.getColumns.asScala.map(_.getName).toList
+      if (primaryKey != datarepoRowIdColumn && !attrs.contains(datarepoRowIdColumn)){
+        attrs = attrs :+ datarepoRowIdColumn
+      }
       (table.getName, EntityTypeMetadata(table.getRowCount, primaryKey, attrs))
     }.toMap
 
