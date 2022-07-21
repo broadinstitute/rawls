6460dd3288986bd3c9da3b8dac504df609507f66
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2022-07-21 10:21:19 -0400

    [WOR-407] Upgrade sentry and emit exceptions in global handler (#1933)
    Context
    
    Since we do not log most stack traces to our logs (due to quota management with logit, I believe) we are losing valuable exception information due to our sole reliance on Sentry logback. It would be helpful to have those stack traces for diagnostic purposes.
    
    This PR
    
    Adds Sentry.captureException calls to the Rawls Akka HTTP global exception handler. This will attach useful stack traces to exceptions that are emitted to Sentry.
    Upgrades sentry to 6.2.1. The DefaultSentryClientFactory class we were relying on for our custom event filtering is gone. The guidance from sentry is to pass in a callback during Sentry initialization, which I've done here.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
   val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.10.14"
   val jodaConvert: ModuleID =     "org.joda"                      % "joda-convert"          % "2.2.2"
   val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.2"
-  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "1.7.30"
+  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "6.3.0"
   val webjarsLocator: ModuleID =  "org.webjars"                   % "webjars-locator"       % "0.40"
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val cats: ModuleID =            "org.typelevel"                 %% "cats-core"                 % "2.6.1"
