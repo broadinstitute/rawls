24a2b87357f59a177b4abbcedc6466dbb1103889
Bria Morgan <calypsomatic@gmail.com>
2022-08-05 16:26:12 -0400

    AJ-513 add datarepo_row_id to snapshot model (#1970)
    * aj-513 undo aj-433 and add datarepo_row_id when snapshot model first created
    
    * aj-513 add unit test
    
    Co-authored-by: Bria Morgan <bmorgan@wm106-f2d.broadinstitute.org>

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/datarepo/HttpDataRepoDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/datarepo/HttpDataRepoDAO.scala
@@ -3,11 +3,15 @@ package org.broadinstitute.dsde.rawls.dataaccess.datarepo
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import bio.terra.datarepo.api.RepositoryApi
 import bio.terra.datarepo.client.ApiClient
-import bio.terra.datarepo.model.SnapshotModel
+import bio.terra.datarepo.model.{ColumnModel, SnapshotModel, TableDataType}
+import org.broadinstitute.dsde.rawls.entities.datarepo.DataRepoBigQuerySupport
 
+import java.util.stream.Collectors
 import java.util.UUID
 
 class HttpDataRepoDAO(dataRepoInstanceName: String, dataRepoInstanceBasePath: String) extends DataRepoDAO {
+
+  private val datareporow_id = new ColumnModel().name(DataRepoBigQuerySupport.datarepoRowIdColumn).datatype(TableDataType.STRING)
 
   private def getApiClient(accessToken: String): ApiClient = {
     val client: ApiClient = new ApiClient()
@@ -25,6 +29,14 @@ class HttpDataRepoDAO(dataRepoInstanceName: String, dataRepoInstanceBasePath: St
 
   override def getSnapshot(snapshotId: UUID, accessToken: OAuth2BearerToken): SnapshotModel = {
     // future enhancement: allow callers to specify the list of SnapshotRetrieveIncludeModel to retrieve
-    getRepositoryApi(accessToken).retrieveSnapshot(snapshotId, java.util.Collections.emptyList())
+    addDataRepoRowId(getRepositoryApi(accessToken).retrieveSnapshot(snapshotId, java.util.Collections.emptyList()))
+  }
+
+  //Snapshots-by-reference always have a datarepo_row_id, but that is not included in the model and should be
+  private def addDataRepoRowId(snapshot: SnapshotModel): SnapshotModel = {
+    Option(snapshot.getTables) foreach { tables =>
+      snapshot.tables(tables.stream().map(t => t.addColumnsItem(datareporow_id)).collect(Collectors.toList()))
+    }
+    snapshot
   }
 }
