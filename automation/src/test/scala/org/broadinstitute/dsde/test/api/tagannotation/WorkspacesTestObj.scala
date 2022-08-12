c9aa8150045bd28d30716783ca81a9c05bf587bf
Ivan <ichang@broadinstitute.org>
2022-08-12 11:46:10 -0400

    Test envvar

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
@@ -2,5 +2,4 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
-// For taggedAs
 object WorkspacesTestObj extends Tag("org.broadinstitute.dsde.test.api.tagannotation.WorkspacesTest")
