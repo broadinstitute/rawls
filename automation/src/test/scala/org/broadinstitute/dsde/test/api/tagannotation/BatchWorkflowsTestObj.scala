bda5120448b2a292e5b9eced7a1acfc2c6dd7afe
Ivan <ichang@broadinstitute.org>
2022-08-12 18:24:09 -0400

    Test global param

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/BatchWorkflowsTestObj.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/BatchWorkflowsTestObj.scala
@@ -2,4 +2,5 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
+// taggedAs
 object BatchWorkflowsTestObj extends Tag("org.broadinstitute.dsde.test.api.tagannotation.BatchWorkflowsTest")
