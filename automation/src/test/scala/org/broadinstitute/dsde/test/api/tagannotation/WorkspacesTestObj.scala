eba3603b3ad141486a37d671e8d3090eb86bc969
Ivan <ichang@broadinstitute.org>
2022-08-15 13:02:53 -0400

    Test global param

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
@@ -2,4 +2,5 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
+// taggedAs
 object WorkspacesTestObj extends Tag("org.broadinstitute.dsde.test.api.tagannotation.WorkspacesTest")
