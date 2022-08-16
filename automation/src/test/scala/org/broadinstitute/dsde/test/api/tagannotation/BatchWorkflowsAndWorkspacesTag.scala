cda55db3bf9dda41d5b1e730877f136805fb7972
Ivan <ichang@broadinstitute.org>
2022-08-16 14:31:50 -0400

    Test refactored pipeline and concurrent jenkins build

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/BatchWorkflowsAndWorkspacesTag.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/BatchWorkflowsAndWorkspacesTag.scala
@@ -2,4 +2,5 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
+//taggedAs
 object BatchWorkflowsAndWorkspacesTag extends Tag("org.broadinstitute.dsde.test.api.tagannotation.OwnedByBatchWorkflowsAndWorkspaces")
