6ac47ad796555d99ab0f44488e49c619353737f4
Ivan <ichang@broadinstitute.org>
2022-08-18 11:06:01 -0400

    Test tags by names closer to API functionality.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj.scala
@@ -2,5 +2,4 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
-// taggesAs
 object WorkspacesTestObj extends Tag("org.broadinstitute.dsde.test.api.tagannotation.WorkspacesTest")
