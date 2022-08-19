9cc52d81d5c6440e85ff6806438ea67b5747df17
Ivan <ichang@broadinstitute.org>
2022-08-19 15:55:58 -0400

    Refactored tag interfaces.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTest1.java
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTest1.java
@@ -3,4 +3,4 @@ package org.broadinstitute.dsde.test.api.tagannotation;
 import org.broadinstitute.dsde.test.api.tagannotation.rawls.RawlsTestAnnotationBase;
 
 @RawlsTestAnnotationBase
-public @interface WorkspacesTestObj1 {}
+public @interface WorkspacesTest1 {}
--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj1.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTestObj1.scala
@@ -2,4 +2,4 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
-object WorkspacesTestObj1 extends Tag("org.broadinstitute.dsde.test.api.tagannotation.WorkspacesTestObj1")
+object WorkspacesTestObj1 extends Tag("org.broadinstitute.dsde.test.api.tagannotation.WorkspacesTest1")
