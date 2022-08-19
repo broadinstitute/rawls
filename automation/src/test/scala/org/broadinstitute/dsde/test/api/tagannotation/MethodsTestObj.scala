524f2a2d0bc02f2c7ecc7b6f258110b57853555d
Ivan <ichang@broadinstitute.org>
2022-08-19 08:44:06 -0400

    PR test based on latest develop.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/MethodsTestObj.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/MethodsTestObj.scala
@@ -2,4 +2,5 @@ package org.broadinstitute.dsde.test.api.tagannotation
 
 import org.scalatest.Tag
 
+//taggedAs
 object MethodsTestObj extends Tag("org.broadinstitute.dsde.test.api.tagannotation.MethodsTest")
