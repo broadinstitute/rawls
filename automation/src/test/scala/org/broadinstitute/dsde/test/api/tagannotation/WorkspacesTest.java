289f895ad2e868615e6a57fce49aade5f54c699a
Ivan <ichang@broadinstitute.org>
2022-08-18 20:25:19 -0400

    PR test on latest develop.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTest.java
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/WorkspacesTest.java
@@ -3,6 +3,7 @@ package org.broadinstitute.dsde.test.api.tagannotation;
 import java.lang.annotation.*;
 import org.scalatest.TagAnnotation;
 
+//taggedAs
 @TagAnnotation
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
