e207c3b5bb2551aae3f1da796ef1086e05ac7633
Ivan <ichang@broadinstitute.org>
2022-08-22 13:10:48 -0400

    Run refactored tag interfaces.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/rawls/RalwsTestGroupTags.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/tagannotation/rawls/RalwsTestGroupTags.scala
@@ -2,7 +2,8 @@ package org.broadinstitute.dsde.test.api.tagannotation.rawls
 
 import org.scalatest.Tag
 
-// used in taggedAs
+// used in taggedAs for individual test cases
+
 object AuthDomainsTest extends Tag("org.broadinstitute.dsde.test.api.AuthDomainsTest")
 object BillingsTest extends Tag("org.broadinstitute.dsde.test.api.BillingsTest")
 object DataRepoSnapshotsTest extends Tag("org.broadinstitute.dsde.test.api.DataRepoSnapshotsTest")
