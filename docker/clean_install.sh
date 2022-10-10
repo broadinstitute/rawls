c90883c781e258acbe3426f359a421a9b79da5ed
Ivan <ichang@broadinstitute.org>
2022-10-10 11:18:26 -0400

    Fixed typo.

--- docker/clean_install.sh
+++ docker/clean_install.sh
@@ -12,6 +12,6 @@ export SBT_OPTS="-Xms5g -Xmx5g -XX:MaxMetaspaceSize=5g"
 echo "starting sbt clean assembly ..."
 sbt 'set assembly / test := {}' clean assembly
 echo "... clean assembly complete, finding and moving jar ..."
-RAWLS_JAR=$(find target | grep 'sam.*\.jar')
+RAWLS_JAR=$(find target | grep 'rawls.*\.jar')
 mv $RAWLS_JAR .
 echo "... jar moved."
