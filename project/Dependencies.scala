2df05939a7446cd0a10a5a65c77a0bea53328f0a
broadbot <broadbot@broadinstitute.org>
2022-11-07 12:01:22 +0000

    Update liquibase-core from 3.10.3 to 4.17.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -92,7 +92,7 @@ object Dependencies {
      any issues elsewhere in Rawls before upgrading.
    */
   val mysqlConnector: ModuleID =  "mysql"                         % "mysql-connector-java"  % "8.0.22"
-  val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "3.10.3"
+  val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "4.17.2"
 
   val workbenchLibsHash = "f7103bc"
 
