699b2aefd49651f902a98e9e67d9f43ef8698daf
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2022-12-01 14:10:56 -0500

    Ignore the right test this time

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/RawlsApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/RawlsApiSpec.scala
@@ -247,7 +247,7 @@ class RawlsApiSpec
       }(owner.makeAuthToken(billingScopes))
     }
 
-    "should be able to create workspace and run sub-workflow tasks in non-US regions" taggedAs(MethodsTest) in {
+    "should be able to create workspace and run sub-workflow tasks in non-US regions" ignore taggedAs(MethodsTest) in {
       implicit val token: AuthToken = studentBToken
 
       // this will create a method with a workflow containing 3 sub-workflows
@@ -475,7 +475,7 @@ class RawlsApiSpec
 
 //    Disabling this test until we decide what to do with it. See AP-177
 
-    "should retrieve metadata with widely scattered sub-workflows in a short time" taggedAs(MethodsTest) ignore {
+    "should retrieve metadata with widely scattered sub-workflows in a short time" taggedAs(MethodsTest) {
       implicit val token: AuthToken = studentAToken
 
       val scatterWidth = 500
