34930a5dc485b5c1916960511d31811feacf02ce
Blake Geno <bgeno@broadinstitute.org>
2023-01-10 13:16:16 -0500

    add database migration to rename field

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigrationActor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/migration/WorkspaceMigrationActor.scala
@@ -326,7 +326,7 @@ object WorkspaceMigrationActor {
         )
 
       for {
-        _ <- workspace.billingAccountErrorMessage.traverse_ { message =>
+        _ <- workspace.errorMessage.traverse_ { message =>
           raiseError {
             makeError("a billing account error exists on workspace", Map("billingAccountErrorMessage" -> message))
           }
