1f3b751389eaefe2447bf2fcb06553e84cca583c
David An <davidan@broadinstitute.org>
2023-03-20 15:46:06 -0400

    AJ-864: add tracing attributes for filter-by-name and filter-by-column (#2274)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/EntityComponent.scala
@@ -473,8 +473,15 @@ trait EntityComponent {
 
         val filterByColumn =
           entityQuery.columnFilter match {
-            case None => sql""
+            case None =>
+              parentContext.tracingSpan.map { span =>
+                span.putAttribute("isFilterByColumn", OpenCensusAttributeValue.booleanAttributeValue(false))
+              }
+              sql""
             case Some(columnFilter) =>
+              parentContext.tracingSpan.map { span =>
+                span.putAttribute("isFilterByColumn", OpenCensusAttributeValue.booleanAttributeValue(true))
+              }
               val shardId = determineShard(workspaceContext.workspaceIdAsUUID)
 
               sql""" and e.id in (
@@ -950,8 +957,14 @@ trait EntityComponent {
       // if filtering by name, retrieve that entity directly, else do the full query:
       nameFilter match {
         case Some(entityName) =>
+          parentContext.tracingSpan.map { span =>
+            span.putAttribute("isFilterByName", OpenCensusAttributeValue.booleanAttributeValue(true))
+          }
           loadSingleEntityForPage(workspaceContext, entityType, entityName, entityQuery)
         case _ =>
+          parentContext.tracingSpan.map { span =>
+            span.putAttribute("isFilterByName", OpenCensusAttributeValue.booleanAttributeValue(false))
+          }
           EntityAndAttributesRawSqlQuery.activeActionForPagination(workspaceContext,
                                                                    entityType,
                                                                    entityQuery,
