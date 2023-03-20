08621b72845c062d84d97c0668f69e7c76d22bae
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-03-20 09:37:30 -0700

    Add category mapping.

--- core/src/test/scala/org/broadinstitute/dsde/rawls/model/SpendReportingModelSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/model/SpendReportingModelSpec.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.rawls.model
 
 import bio.terra.profile.model.SpendReport
+import bio.terra.profile.model.SpendReportingForDateRange.CategoryEnum
 import bio.terra.profile.model.{SpendReportingForDateRange => SpendReportingForDateRangeBPM}
 import bio.terra.profile.model.{SpendReportingAggregation => SpendReportingAggregationBPM}
 import org.joda.time.DateTime
@@ -13,6 +14,9 @@ import scala.jdk.CollectionConverters._
 
 class SpendReportingModelSpec extends AnyFlatSpecLike {
   object TestData {
+    val defaultCurrency = "USD"
+    val defaultCredits = "0" // always 0 for Azure
+
     def buildBPMEmptyReport: SpendReport = {
       // some period
       val from = DateTime.now().minusMonths(2)
@@ -37,15 +41,24 @@ class SpendReportingModelSpec extends AnyFlatSpecLike {
         )
     }
 
-    def someBPMNonEmptyReport(from: DateTime, to: DateTime, costs: Seq[BigDecimal]): SpendReport = {
-      val currency = "USD"
+    def someBPMNonEmptyReport(from: DateTime, to: DateTime, costs: Map[CategoryEnum, BigDecimal]): SpendReport = {
       val categoriesCosts = costs
-        .map(cost => buildSpendReportingForDateRange(cost.toString(), currency, null, null, null))
-        .asJava
+        .map(costPerCategoryKvp =>
+          buildSpendReportingForDateRange(costPerCategoryKvp._2.toString,
+                                          defaultCurrency,
+                                          costPerCategoryKvp._1,
+                                          null,
+                                          null
+          )
+        )
+        .asJavaCollection
+        .stream()
+        .toList
+
       new SpendReport()
         .spendSummary(
-          buildSpendReportingForDateRange(costs.sum.toString(),
-                                          currency,
+          buildSpendReportingForDateRange(costs.values.sum.toString(),
+                                          defaultCurrency,
                                           null,
                                           from.toString(ISODateTimeFormat.date()),
                                           to.toString(ISODateTimeFormat.date())
@@ -68,7 +81,7 @@ class SpendReportingModelSpec extends AnyFlatSpecLike {
     ): SpendReportingForDateRangeBPM =
       new SpendReportingForDateRangeBPM()
         .cost(cost)
-        .credits("0")
+        .credits(defaultCredits)
         .currency(currency)
         .category(category)
         .startTime(from)
@@ -90,16 +103,34 @@ class SpendReportingModelSpec extends AnyFlatSpecLike {
     val to = DateTime.now().plusMonths(1)
     val computeCost: BigDecimal = 100.20
     val storageCost: BigDecimal = 54.32
-    val categoriesCosts = Seq(computeCost, storageCost)
+    val categoriesCosts = Map(CategoryEnum.COMPUTE -> computeCost, CategoryEnum.STORAGE -> storageCost)
     val someReport = TestData.someBPMNonEmptyReport(from, to, categoriesCosts)
 
     val result = SpendReportingResultsConvertor(someReport)
 
     result shouldNot equal(null)
     result.spendSummary shouldNot equal(null)
-    result.spendSummary.cost shouldBe categoriesCosts.sum.toString()
+    result.spendSummary.cost shouldBe categoriesCosts.values.sum.toString()
     result.spendDetails shouldNot equal(null)
     result.spendDetails(0) shouldNot equal(null)
     result.spendDetails(0).spendData should have size categoriesCosts.size
+
+    val computeDetails = result
+      .spendDetails(0)
+      .spendData
+      .find(v => v.category.nonEmpty && v.category.get.equals(TerraSpendCategories.Compute))
+    computeDetails.get shouldNot equal(null)
+    computeDetails.get.cost shouldBe computeCost.toString()
+    computeDetails.get.credits shouldBe TestData.defaultCredits
+    computeDetails.get.currency shouldBe TestData.defaultCurrency
+
+    val storageDetails = result
+      .spendDetails(0)
+      .spendData
+      .find(v => v.category.nonEmpty && v.category.get.equals(TerraSpendCategories.Storage))
+    storageDetails.get shouldNot equal(null)
+    storageDetails.get.cost shouldBe storageCost.toString()
+    storageDetails.get.credits shouldBe TestData.defaultCredits
+    computeDetails.get.currency shouldBe TestData.defaultCurrency
   }
 }
