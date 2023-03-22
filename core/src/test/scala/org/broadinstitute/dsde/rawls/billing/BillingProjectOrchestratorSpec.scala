d2ed4e2a6fa1ad11c91e9e2d71c70ab0656e3d82
Blake Geno <bgeno@broadinstitute.org>
2023-03-22 12:17:17 -0400

    WOR-837 - Make Terra resilient if a landing zone fails to create (#2277)
    * proceed with deletion immediately if there is no landing zone to delete
    
    * second pass at scalafmt
    
    * add test to explicitly hit case of BPM lifecycle initiateDelete returning None for jobId

--- core/src/test/scala/org/broadinstitute/dsde/rawls/billing/BillingProjectOrchestratorSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/billing/BillingProjectOrchestratorSpec.scala
@@ -323,6 +323,7 @@ class BillingProjectOrchestratorSpec extends AnyFlatSpec {
 
   def initiateDeleteLifecycle(returnValue: Future[Option[UUID]]): BillingProjectLifecycle = {
     val billingProjectLifecycle = mock[BillingProjectLifecycle]
+    when(billingProjectLifecycle.deleteJobType).thenReturn(BpmBillingProjectDelete)
     when(billingProjectLifecycle.initiateDelete(ArgumentMatchers.any(), ArgumentMatchers.any())(ArgumentMatchers.any()))
       .thenReturn(returnValue)
     billingProjectLifecycle
@@ -472,6 +473,28 @@ class BillingProjectOrchestratorSpec extends AnyFlatSpec {
     verify(billingProjectLifecycle).finalizeDelete(billingProjectName, testContext)
   }
 
+  it should "call initiateDelete and finializeDelete when the BPM lifecyle returns a jobId of None" in {
+    val billingProjectName = RawlsBillingProjectName("fake_billing_account_name")
+    val billingProjectLifecycle = mock[BillingProjectLifecycle]
+    when(billingProjectLifecycle.initiateDelete(billingProjectName, testContext)).thenReturn(Future.successful(None))
+    when(billingProjectLifecycle.finalizeDelete(billingProjectName, testContext)).thenReturn(Future.successful())
+    val bpo = new BillingProjectOrchestrator(
+      testContext,
+      alwaysGiveAccessSamDao,
+      mock[NotificationDAO],
+      happyBillingRepository(Some(UUID.randomUUID().toString)),
+      mock[BillingProjectLifecycle], // google
+      billingProjectLifecycle, // bpm
+      mock[MultiCloudWorkspaceConfig],
+      mock[WorkspaceManagerResourceMonitorRecordDao] // nothing mocked - will fail if called
+    )
+
+    Await.result(bpo.deleteBillingProjectV2(billingProjectName), Duration.Inf)
+
+    verify(billingProjectLifecycle).initiateDelete(billingProjectName, testContext)
+    verify(billingProjectLifecycle).finalizeDelete(billingProjectName, testContext)
+  }
+
   it should "call the BPM lifecycle to initiate delete of an Azure project" in {
     val billingProjectName = RawlsBillingProjectName("fake_billing_account_name")
     val jobId = UUID.fromString("c1024c05-40a6-4a12-b12e-028e445aec3b")
@@ -498,7 +521,7 @@ class BillingProjectOrchestratorSpec extends AnyFlatSpec {
 
   it should "create a job to delete the Azure project after calling initiateDelete" in {
     val billingProjectName = RawlsBillingProjectName("fake_billing_account_name")
-    val jobId = UUID.fromString("c1024c05-40a6-4a12-b12e-028e445aec3b")
+    val jobId = UUID.randomUUID()
 
     def matchedExpectedEvent(e: WorkspaceManagerResourceMonitorRecord) =
       e.jobControlId.toString == jobId.toString &&
@@ -507,7 +530,6 @@ class BillingProjectOrchestratorSpec extends AnyFlatSpec {
         e.jobType == BpmBillingProjectDelete
     val monitorRecordDao = mock[WorkspaceManagerResourceMonitorRecordDao](RETURNS_SMART_NULLS)
     when(monitorRecordDao.create(ArgumentMatchers.argThat(matchedExpectedEvent))).thenReturn(Future.successful())
-
     val bpo = new BillingProjectOrchestrator(
       testContext,
       alwaysGiveAccessSamDao,
