b11f9203b6e517ff82e5f34d7d9ef2194587271b
tlangs <tlangs@broadinstitute.org>
2023-03-23 11:03:06 -0400

    Update wb-libs version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
   val mysqlConnector: ModuleID =  "mysql"                         % "mysql-connector-java"  % "8.0.30"
   val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "4.17.2"
 
-  val workbenchLibsHash = "867ff8b"
+  val workbenchLibsHash = "f0ee838"
 
   val workbenchModelV  = s"0.15-${workbenchLibsHash}"
   val workbenchGoogleV = s"0.24-${workbenchLibsHash}"
