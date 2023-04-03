8f85456ea78a8a7939d8abf8e078bf15f1e1e857
Tristan Garwood <tristan.garwood@gmail.com>
2023-04-03 14:45:09 -0400

    Fix tests and add comments

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/FastPassMonitor.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/FastPassMonitor.scala
@@ -1,26 +1,28 @@
 package org.broadinstitute.dsde.rawls.monitor
 import akka.actor.{Actor, Props}
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.rawls.dataaccess.SlickDataSource
 import org.broadinstitute.dsde.rawls.dataaccess.slick.{DataAccess, ReadWriteAction}
 import org.broadinstitute.dsde.rawls.model.{FastPassGrant, Workspace}
 import org.broadinstitute.dsde.rawls.monitor.FastPassMonitor.DeleteExpiredGrants
-import org.broadinstitute.dsde.rawls.util.TracingUtils.traceDBIOWithParent
 import org.broadinstitute.dsde.workbench.google.{GoogleIamDAO, GoogleStorageDAO}
+import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 import org.broadinstitute.dsde.workbench.model.google.iam.IamResourceTypes
 import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GoogleProject}
-import org.broadinstitute.dsde.workbench.model.{GoogleSubjectId, WorkbenchEmail}
-import slick.dbio.{DBIO, DBIOAction, Effect, NoStream}
+import org.broadinstitute.dsde.workbench.openTelemetry.OpenTelemetryMetrics
+import slick.dbio.DBIO
 
-import java.util.UUID
-import scala.collection.immutable
 import scala.concurrent.Future
 import scala.language.postfixOps
 
 object FastPassMonitor {
   sealed trait FastPassMonitorMessage
   case object DeleteExpiredGrants extends FastPassMonitorMessage
-  def props(dataSource: SlickDataSource, googleIamDao: GoogleIamDAO, googleStorageDao: GoogleStorageDAO): Props = Props(
+  def props(dataSource: SlickDataSource, googleIamDao: GoogleIamDAO, googleStorageDao: GoogleStorageDAO)(implicit
+    openTelemetry: OpenTelemetryMetrics[IO]
+  ): Props = Props(
     new FastPassMonitor(dataSource, googleIamDao, googleStorageDao)
   )
 }
@@ -28,10 +30,14 @@ object FastPassMonitor {
 class FastPassMonitor private (dataSource: SlickDataSource,
                                googleIamDao: GoogleIamDAO,
                                googleStorageDao: GoogleStorageDAO
-) extends Actor
+)(implicit openTelemetry: OpenTelemetryMetrics[IO])
+    extends Actor
     with LazyLogging {
   import context.dispatcher
+
+  private val openTelemetryTags: Map[String, String] = Map("service" -> "FastPassMonitor")
   override def receive: Receive = { case DeleteExpiredGrants =>
+    openTelemetry.incrementCounter("fastpass-monitor-receive-msg", tags = openTelemetryTags).unsafeToFuture()
     deleteExpiredGrants()
   }
 
@@ -41,10 +47,13 @@ class FastPassMonitor private (dataSource: SlickDataSource,
         .findExpiredFastPassGrants()
         .flatMap { expiredGrants =>
           logger.info(s"Found ${expiredGrants.size} total expired grants")
+          // Convert to stream and group by workspaceId
           DBIO.sequence(expiredGrants.to(LazyList).groupBy(_.workspaceId).map { case (workspaceId, workspaceGrants) =>
             logger.info(s"Found ${workspaceGrants.size} expired grants for workspace $workspaceId")
+            // Query workspace info then group by accountEmail
             dataAccess.workspaceQuery.findByIdOrFail(workspaceId).flatMap { workspace =>
               DBIO.sequence(workspaceGrants.groupBy(_.accountEmail).map { case (_, accountEmailGrants) =>
+                // Remove grants for a given workspace and accountEmail
                 removeGrantsForAccountEmailInWorkspace(dataAccess, workspace, accountEmailGrants)
               })
             }
@@ -57,10 +66,12 @@ class FastPassMonitor private (dataSource: SlickDataSource,
                                                      workspace: Workspace,
                                                      grants: Iterable[FastPassGrant]
   ): ReadWriteAction[Unit] = {
+    // Projects and buckets need different api calls, so group by resourceType
     grants.groupBy(_.resourceType).map { case (resourceType, resourceTypeGrants) =>
       val organizationRoles = resourceTypeGrants.map(_.organizationRole).toSet
       // The grouped resourceTypeGrants are the same except for roles, so we can just take the first one
       val resourceTypeGrant = resourceTypeGrants.head
+      // For a given resource type, remove the roles for the accountEmail
       resourceType match {
         case IamResourceTypes.Project =>
           googleIamDao.removeRoles(GoogleProject(workspace.googleProjectId.value),
@@ -78,7 +89,12 @@ class FastPassMonitor private (dataSource: SlickDataSource,
           )
         case _ => throw new RuntimeException(s"Unsupported resource type ${resourceTypeGrant.resourceType}")
       }
+      openTelemetry.incrementCounter("fastpass-monitor-google-call", tags = openTelemetryTags).unsafeToFuture()
     }
+    // Once all the api calls have been made remove all the corresponding grants from the db
+    DBIO.from(
+      openTelemetry.incrementCounter("fastpass-monitor-grant-delete", tags = openTelemetryTags).unsafeToFuture()
+    )
     DBIO.sequence(grants.map(grant => dataAccess.fastPassGrantQuery.delete(grant.id)))
   }.map(_ => DBIO.successful(()))
 }
