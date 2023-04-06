c635596f986096e6d8ab103bd6bba3a342068d55
Marcus Talbott <mtalbott@broadinstitute.org>
2023-04-06 15:30:20 -0400

    add tests

--- core/src/test/scala/org/broadinstitute/dsde/rawls/spendreporting/SpendReportingServiceSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/spendreporting/SpendReportingServiceSpec.scala
@@ -781,6 +781,104 @@ class SpendReportingServiceSpec extends AnyFlatSpecLike with Matchers with Mocki
     endDateCapture.getValue shouldBe to.toDate
   }
 
+  it should "not get the spend report from BPM for Google billing projects with a billing profile" in {
+    val from = DateTime.now().minusMonths(2)
+    val to = from.plusMonths(1)
+    val billingProfileId = UUID.randomUUID()
+    val bpmDAO = mock[BillingProfileManagerDAO](RETURNS_SMART_NULLS)
+    when(bpmDAO.getBillingProfile(mockitoEq(billingProfileId), any()))
+      .thenReturn(Option(new ProfileModel().id(billingProfileId).cloudPlatform(BpmCloudPlatform.GCP)))
+
+    val billingRepository = mock[BillingRepository](RETURNS_SMART_NULLS)
+    when(billingRepository.getBillingProject(mockitoEq(billingProject.projectName)))
+      .thenReturn(Future.successful(Option(billingProject.copy(billingProfileId = Option(billingProfileId.toString)))))
+
+    val samDAO = mock[SamDAO](RETURNS_SMART_NULLS)
+    when(samDAO.userHasAction(any(), any(), any(), any())).thenReturn(Future.successful(true))
+    val bigQueryService = mockBigQuery(TestData.Workspace.table)
+    val service = spy(
+      new SpendReportingService(
+        testContext,
+        mock[SlickDataSource],
+        bigQueryService,
+        billingRepository,
+        bpmDAO,
+        samDAO,
+        spendReportingServiceConfig
+      )
+    )
+    val billingProjectSpendExport =
+      BillingProjectSpendExport(RawlsBillingProjectName(""), RawlsBillingAccountName(""), None)
+    doReturn(Future.successful(billingProjectSpendExport)).when(service).getSpendExportConfiguration(any())
+    doReturn(Future.successful(TestData.googleProjectsToWorkspaceNames)).when(service).getWorkspaceGoogleProjects(any())
+
+    Await.result(
+      service.getSpendForBillingProject(
+        billingProject.projectName,
+        from,
+        to,
+        Set(SpendReportingAggregationKeyWithSub(SpendReportingAggregationKeys.Workspace))
+      ),
+      Duration.Inf
+    )
+
+    verify(bpmDAO, Mockito.times(0)).getAzureSpendReport(any(), any(), any(), any())
+    verify(service, Mockito.times(1)).getSpendForGCPBillingProject(
+      mockitoEq(billingProject.projectName),
+      mockitoEq(from),
+      mockitoEq(to),
+      mockitoEq(Set(SpendReportingAggregationKeyWithSub(SpendReportingAggregationKeys.Workspace)))
+    )
+  }
+
+  it should "not get the spend report from BPM for Google billing projects without a billing profile" in {
+    val from = DateTime.now().minusMonths(2)
+    val to = from.plusMonths(1)
+    val bpmDAO = mock[BillingProfileManagerDAO](RETURNS_SMART_NULLS)
+
+    val billingRepository = mock[BillingRepository](RETURNS_SMART_NULLS)
+    when(billingRepository.getBillingProject(mockitoEq(billingProject.projectName)))
+      .thenReturn(Future.successful(Option(billingProject.copy(billingProfileId = None))))
+
+    val samDAO = mock[SamDAO](RETURNS_SMART_NULLS)
+    when(samDAO.userHasAction(any(), any(), any(), any())).thenReturn(Future.successful(true))
+    val bigQueryService = mockBigQuery(TestData.Workspace.table)
+    val service = spy(
+      new SpendReportingService(
+        testContext,
+        mock[SlickDataSource],
+        bigQueryService,
+        billingRepository,
+        bpmDAO,
+        samDAO,
+        spendReportingServiceConfig
+      )
+    )
+    val billingProjectSpendExport =
+      BillingProjectSpendExport(RawlsBillingProjectName(""), RawlsBillingAccountName(""), None)
+    doReturn(Future.successful(billingProjectSpendExport)).when(service).getSpendExportConfiguration(any())
+    doReturn(Future.successful(TestData.googleProjectsToWorkspaceNames)).when(service).getWorkspaceGoogleProjects(any())
+
+    Await.result(
+      service.getSpendForBillingProject(
+        billingProject.projectName,
+        from,
+        to,
+        Set(SpendReportingAggregationKeyWithSub(SpendReportingAggregationKeys.Workspace))
+      ),
+      Duration.Inf
+    )
+
+    verify(bpmDAO, Mockito.times(0)).getBillingProfile(any(), any())
+    verify(bpmDAO, Mockito.times(0)).getAzureSpendReport(any(), any(), any(), any())
+    verify(service, Mockito.times(1)).getSpendForGCPBillingProject(
+      mockitoEq(billingProject.projectName),
+      mockitoEq(from),
+      mockitoEq(to),
+      mockitoEq(Set(SpendReportingAggregationKeyWithSub(SpendReportingAggregationKeys.Workspace)))
+    )
+  }
+
   it should "handle/rethrow ApiException from BPM client" in {
     val from = DateTime.now().minusMonths(2)
     val to = from.plusMonths(1)
