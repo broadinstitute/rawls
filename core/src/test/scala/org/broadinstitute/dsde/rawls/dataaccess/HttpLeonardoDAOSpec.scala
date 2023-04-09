d24a047660d0164f6f0377aff3ed6dbd2beff2bb
David An <davidan@broadinstitute.org>
2023-04-09 16:17:28 -0400

    clean up unit test

--- core/src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpLeonardoDAOSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpLeonardoDAOSpec.scala
@@ -4,17 +4,20 @@ import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import com.typesafe.config.{Config, ConfigFactory}
 import org.broadinstitute.dsde.rawls.config.LeonardoConfig
-import org.broadinstitute.dsde.workbench.client.leonardo.ApiClient
 import org.mockito.{ArgumentMatchers, Mockito}
 import org.scalatest.flatspec.AnyFlatSpecLike
 
 import java.util.UUID
-import scala.language.postfixOps
+import scala.util.Try
+
+import scala.jdk.CollectionConverters._
 
 class HttpLeonardoDAOSpec extends TestKit(ActorSystem("HttpLeonardoDAOSpec")) with AnyFlatSpecLike {
 
   val testConf: Config =
-    ConfigFactory.parseMap(java.util.Map.of("wdsType", "test-wds-type", "server", "test-leo-server"))
+    ConfigFactory
+      .parseMap(Map("wdsType" -> "CROMWELL", "server" -> "http://localhost").asJava)
+      .resolve()
   val leonardoConfig: LeonardoConfig = LeonardoConfig.apply(testConf)
 
   val token: String = "my-token"
@@ -27,14 +30,19 @@ class HttpLeonardoDAOSpec extends TestKit(ActorSystem("HttpLeonardoDAOSpec")) wi
     val leonardoDAO = Mockito.spy(
       new HttpLeonardoDAO(leonardoConfig)
     )
-    leonardoDAO.createWDSInstance(token, workspaceId)
+
+    // this call will fail in createApp() because it's looking for a server running on localhost.
+    // but we can still verify the arguments it passes to createApp()
+    // alternative is to spin up a mockserver we can hit for a 2xx response.
+    Try(leonardoDAO.createWDSInstance(token, workspaceId))
+
     Mockito
       .verify(leonardoDAO)
       .createApp(
         ArgumentMatchers.eq(token),
         ArgumentMatchers.eq(workspaceId),
         ArgumentMatchers.eq(s"wds-$workspaceId"),
-        ArgumentMatchers.eq("test-wds-type")
+        ArgumentMatchers.eq("CROMWELL")
       )
   }
 
