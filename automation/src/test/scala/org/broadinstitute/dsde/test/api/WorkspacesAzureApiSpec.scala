dcb9be2455e57520ad9bff6ec6f6259b61d2043c
cahrens <roberts@broadinstitute.org>
2023-05-04 13:44:39 -0400

    Attempt an actual test run.

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspacesAzureApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspacesAzureApiSpec.scala
@@ -25,36 +25,34 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
     owner.makeAuthToken()
   }
 
-//  private val azureManagedAppCoordinates = AzureManagedAppCoordinates(
-//    UUID.fromString("fad90753-2022-4456-9b0a-c7e5b934e408"),
-//    UUID.fromString("f557c728-871d-408c-a28b-eb6b2141a087"),
-//    "staticTestingMrg",
-//    Some(UUID.fromString("f41c1a97-179b-4a18-9615-5214d79ba600"))
-//  )
+  private val azureManagedAppCoordinates = AzureManagedAppCoordinates(
+    UUID.fromString("fad90753-2022-4456-9b0a-c7e5b934e408"),
+    UUID.fromString("f557c728-871d-408c-a28b-eb6b2141a087"),
+    "staticTestingMrg",
+    Some(UUID.fromString("f41c1a97-179b-4a18-9615-5214d79ba600"))
+  )
 
   implicit val token: AuthToken = ownerAuthToken
 
   "Rawls" should "allow creation and deletion of azure workspaces" in {
-    assert(true)
-//    withTemporaryAzureBillingProject(azureManagedAppCoordinates) { projectName =>
-//      val workspaceName = generateWorkspaceName()
-//      Rawls.workspaces.create(
-//        projectName,
-//        workspaceName,
-//        Set.empty,
-//        Map(AttributeName.withDefaultNS("disableAutomaticAppCreation") -> AttributeBoolean(true))
-//      )
-//
-//      val response = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName))
-//      response.workspace.name should be(workspaceName)
-//      response.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
-//      response.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
-//
-//      Rawls.workspaces.delete(projectName, workspaceName)
-//      assertNoAccessToWorkspace(projectName, workspaceName)
-//    }
-  }
+    withTemporaryAzureBillingProject(azureManagedAppCoordinates) { projectName =>
+      val workspaceName = generateWorkspaceName()
+      Rawls.workspaces.create(
+        projectName,
+        workspaceName,
+        Set.empty,
+        Map(AttributeName.withDefaultNS("disableAutomaticAppCreation") -> AttributeBoolean(true))
+      )
 
+      val response = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName))
+      response.workspace.name should be(workspaceName)
+      response.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
+      response.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
+
+      Rawls.workspaces.delete(projectName, workspaceName)
+      assertNoAccessToWorkspace(projectName, workspaceName)
+    }
+  }
 
   private def generateWorkspaceName(): String = {
     s"${UUID.randomUUID().toString()}-azure-test-workspace"
@@ -64,7 +62,6 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
     exception.message.parseJson.asJsObject.fields("statusCode").convertTo[Int] should be(statusCode)
   }
 
-
   private def assertNoAccessToWorkspace(projectName: String, workspaceName: String)(implicit token: AuthToken): Unit = {
     eventually {
       val exception = intercept[RestException](Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName)(token))
@@ -73,5 +70,4 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
   }
 
   private def workspaceResponse(response: String): WorkspaceResponse = response.parseJson.convertTo[WorkspaceResponse]
-
 }
