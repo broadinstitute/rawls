f362d497f7e4433a5e24b7bc2914a7cf45cba2a7
cahrens <roberts@broadinstitute.org>
2023-05-23 13:07:07 -0400

    Add clone test (WOR-922).

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspacesAzureApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspacesAzureApiSpec.scala
@@ -1,20 +1,29 @@
 package org.broadinstitute.dsde.test.api
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model._
+import akka.stream.scaladsl.Sink
+import akka.testkit.TestKit.awaitCond
+import akka.util.ByteString
+import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport._
 import org.broadinstitute.dsde.rawls.model.{AzureManagedAppCoordinates, WorkspaceCloudPlatform, WorkspaceResponse, WorkspaceType}
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{Credentials, UserPool}
 import org.broadinstitute.dsde.workbench.fixture.BillingFixtures.withTemporaryAzureBillingProject
+import org.broadinstitute.dsde.workbench.service.test.CleanUp
 import org.broadinstitute.dsde.workbench.service.{Rawls, RestException}
-import org.broadinstitute.dsde.workbench.service.test.{CleanUp, RandomUtil}
-
 import org.scalatest.concurrent.Eventually.eventually
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 import spray.json.DefaultJsonProtocol._
 import spray.json._
-import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport._
 
 import java.util.UUID
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
 
 
 @WorkspacesAzureTest
@@ -32,7 +41,10 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
     Some(UUID.fromString("f41c1a97-179b-4a18-9615-5214d79ba600"))
   )
 
+  private val wsmUrl = Rawls.url.replace("rawls", "workspace")
+
   implicit val token: AuthToken = ownerAuthToken
+  implicit val system = ActorSystem()
 
   "Rawls" should "allow creation and deletion of azure workspaces" in {
     withTemporaryAzureBillingProject(azureManagedAppCoordinates) { projectName =>
@@ -43,14 +55,23 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
         Set.empty,
         Map("disableAutomaticAppCreation" -> "true")
       )
+      try {
+        val response = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName))
+        response.workspace.name should be(workspaceName)
+        response.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
+        response.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
+      } finally {
+        Rawls.workspaces.delete(projectName, workspaceName)
+        assertNoAccessToWorkspace(projectName, workspaceName)
+      }
+    }
+  }
 
-      val response = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceName))
-      response.workspace.name should be(workspaceName)
-      response.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
-      response.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
+  it should "allow access to WorkspaceManager API" in {
+    val statusRequest = Rawls.getRequest(wsmUrl + "status")
 
-      Rawls.workspaces.delete(projectName, workspaceName)
-      assertNoAccessToWorkspace(projectName, workspaceName)
+    withClue(s"WSM status API returned ${statusRequest.status.intValue()} ${statusRequest.status.reason()}!") {
+      statusRequest.status shouldBe StatusCodes.OK
     }
   }
 
@@ -58,32 +79,76 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
     withTemporaryAzureBillingProject(azureManagedAppCoordinates) { projectName =>
       val workspaceName = generateWorkspaceName()
       val workspaceCloneName = generateWorkspaceName()
+
+      val analysesDir = "analyses"
+      val analysesFilename = analysesDir + "/testFile.txt"
+      val analysesContents = "hello world"
+
+      val nonAnalysesFilename = "willNotClone.txt"
+      val nonAnalysesContents = "user upload content"
+
       Rawls.workspaces.create(
         projectName,
         workspaceName,
         Set.empty,
         Map("disableAutomaticAppCreation" -> "true")
       )
+      try {
+        val sasUrl = getSasUrl(projectName, workspaceName)
 
-      Rawls.workspaces.clone(
-        projectName,
-        workspaceName,
-        projectName,
-        workspaceCloneName,
-        Set.empty,
-        None,
-        Map("disableAutomaticAppCreation" -> "true")
-      )
-      val clonedResponse = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceCloneName))
-      clonedResponse.workspace.name should equal(workspaceCloneName)
-      clonedResponse.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
-      clonedResponse.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
+        // Upload the blob that will be cloned
+        uploadBlob(sasUrl, analysesFilename, analysesContents)
+        val downloadContents = downloadBlob(sasUrl, analysesFilename)
+        withClue(s"testing uploaded blob ${analysesFilename}") {
+          downloadContents shouldBe analysesContents
+        }
 
-      Rawls.workspaces.delete(projectName, workspaceName)
-      assertNoAccessToWorkspace(projectName, workspaceName)
+        // Upload the blob that should not be cloned
+        uploadBlob(sasUrl, nonAnalysesFilename, nonAnalysesContents)
+        val downloadNonAnalysesContents = downloadBlob(sasUrl, nonAnalysesFilename)
+        withClue(s"testing uploaded blob ${nonAnalysesFilename}") {
+          downloadNonAnalysesContents shouldBe nonAnalysesContents
+        }
 
-      Rawls.workspaces.delete(projectName, workspaceCloneName)
-      assertNoAccessToWorkspace(projectName, workspaceCloneName)
+        Rawls.workspaces.clone(
+          projectName,
+          workspaceName,
+          projectName,
+          workspaceCloneName,
+          Set.empty,
+          Some(analysesDir),
+          Map("disableAutomaticAppCreation" -> "true")
+        )
+        try {
+          val clonedResponse = workspaceResponse(Rawls.workspaces.getWorkspaceDetails(projectName, workspaceCloneName))
+          clonedResponse.workspace.name should equal(workspaceCloneName)
+          clonedResponse.workspace.cloudPlatform should be(Some(WorkspaceCloudPlatform.Azure))
+          clonedResponse.workspace.workspaceType should be(Some(WorkspaceType.McWorkspace))
+
+          withClue(s"Verifying container cloning has completed") {
+            awaitCond(
+              isCloneCompleted(projectName, workspaceCloneName),
+              60 seconds,
+              2 seconds
+            )
+          }
+
+          val cloneSasUrl = getSasUrl(projectName, workspaceCloneName)
+          val downloadCloneContents = downloadBlob(cloneSasUrl, analysesFilename)
+          withClue(s"testing blob ${analysesFilename} cloned") {
+            downloadCloneContents shouldBe analysesContents
+          }
+          withClue(s"testing blob ${nonAnalysesFilename} did not clone") {
+            verifyBlobNotCloned(cloneSasUrl, nonAnalysesFilename)
+          }
+        } finally {
+          Rawls.workspaces.delete(projectName, workspaceCloneName)
+          assertNoAccessToWorkspace(projectName, workspaceCloneName)
+        }
+      } finally {
+        Rawls.workspaces.delete(projectName, workspaceName)
+        assertNoAccessToWorkspace(projectName, workspaceName)
+      }
     }
   }
 
@@ -103,4 +168,82 @@ class AzureWorkspacesSpec extends AnyFlatSpec with Matchers with CleanUp {
   }
 
   private def workspaceResponse(response: String): WorkspaceResponse = response.parseJson.convertTo[WorkspaceResponse]
+
+  private def getWorkspaceId(projectName: String, workspaceName: String)(implicit token: AuthToken): String = {
+    Rawls.workspaces
+      .getWorkspaceDetails(projectName, workspaceName)
+      .parseJson
+      .asJsObject
+      .getFields("workspace")
+      .flatMap { workspace =>
+        workspace.asJsObject.getFields("workspaceId")
+      }
+      .head
+      .convertTo[String]
+  }
+
+  private def isCloneCompleted(projectName: String, workspaceName: String)(implicit token: AuthToken): Boolean = {
+    val cloneTransferComplete = Rawls.workspaces
+      .getWorkspaceDetails(projectName, workspaceName)
+      .parseJson
+      .asJsObject
+      .getFields("workspace")
+      .flatMap { workspace =>
+        workspace.asJsObject.getFields("completedCloneWorkspaceFileTransfer")
+      }
+    cloneTransferComplete.headOption.isDefined
+  }
+
+  private def uploadBlob(containerUrl: String, blobName: String, contents: String): Unit = {
+    val urlParts = containerUrl.split("\\?")
+    val fullBlobUrl = urlParts.head + s"/${blobName}?" + urlParts.tail.head
+    val headers = List(RawHeader("x-ms-blob-type", "BlockBlob"))
+
+    val uploadRequest = HttpRequest(HttpMethods.PUT, fullBlobUrl, headers, HttpEntity(ContentTypes.`text/plain(UTF-8)`, contents))
+    val uploadResponse = Await.result(Http().singleRequest(uploadRequest), 2.minutes)
+
+    withClue(s"Upload blob ${blobName}") {
+      uploadResponse.status shouldBe StatusCodes.Created
+    }
+  }
+
+  private def downloadBlob(containerUrl: String, blobName: String): String = {
+    val downloadResponse = getBlobResponse(containerUrl, blobName)
+    withClue(s"Download blob ${blobName}") {
+      downloadResponse.status shouldBe StatusCodes.OK
+    }
+    val byteStringSink: Sink[ByteString, Future[ByteString]] = Sink.fold(ByteString("")) { (z, i) =>
+      z.concat(i)
+    }
+    val entityFuture = downloadResponse.entity.dataBytes.runWith(byteStringSink)
+    Await.result(entityFuture, 1.second).decodeString("UTF-8")
+  }
+
+  private def verifyBlobNotCloned(containerUrl: String, blobName: String): Unit = {
+    val downloadResponse = getBlobResponse(containerUrl, blobName)
+    withClue(s"Check blob ${blobName} does not exist") {
+      downloadResponse.status shouldBe StatusCodes.NotFound
+    }
+  }
+
+  private def getBlobResponse(containerUrl: String, blobName: String): HttpResponse = {
+    val urlParts = containerUrl.split("\\?")
+    val fullBlobUrl = urlParts.head + s"/${blobName}?" + urlParts.tail.head
+    val downloadRequest = HttpRequest(HttpMethods.GET, fullBlobUrl)
+    Await.result(Http().singleRequest(downloadRequest), 2.minutes)
+  }
+
+  private def getSasUrl(projectName: String, workspaceName: String): String = {
+    val workspaceId = getWorkspaceId(projectName, workspaceName)
+    val resourceResponse = Rawls.parseResponse(Rawls.getRequest(wsmUrl + s"api/workspaces/v1/${workspaceId}/resources?stewardship=CONTROLLED&limit=1000"))
+    val containerId = resourceResponse.parseJson
+      .asJsObject
+      .getFields("resources").head
+      .asInstanceOf[JsArray].elements.head.asJsObject
+      .getFields("metadata").head.asJsObject
+      .getFields("resourceId").head.convertTo[String]
+
+    val sasResponse = Rawls.postRequest(wsmUrl + s"api/workspaces/v1/${workspaceId}/resources/controlled/azure/storageContainer/${containerId}/getSasToken")
+    sasResponse.parseJson.asJsObject.getFields("url").head.convertTo[String]
+  }
 }
