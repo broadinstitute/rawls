c9750ddf4dff92ffc5e383cb94550ff494262101
Adam Nichols <aednichols@gmail.com>
2023-06-30 16:58:03 -0400

    PROD-841 fix submission `get()` function (#2423)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/SubmissionComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/SubmissionComponent.scala
@@ -139,7 +139,11 @@ trait SubmissionComponent {
 
     /* gets a submission */
     def get(workspaceContext: Workspace, submissionId: String): ReadAction[Option[Submission]] =
-      loadSubmission(UUID.fromString(submissionId))
+      findByWorkspaceAndId(workspaceContext.workspaceIdAsUUID, UUID.fromString(submissionId)).result.flatMap(recs =>
+        DBIO.sequence(recs map { rec: SubmissionRecord =>
+          loadSubmission(rec.id) map (sub => sub.get) 
+        })
+      ).map(_.headOption)
 
     /* lists all submissions in a workspace */
     def list(workspaceContext: Workspace): ReadAction[Seq[Submission]] =
