8b9c0f45e7fddd5ef86446b64df59a994a3ee04c
Bria Morgan <calypsomatic@gmail.com>
2023-07-05 16:42:24 -0400

    AJ-1112 update rawls model (#2430)
    * update rawls model
    
    * scalafmt

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
     "org.scalatest"       %%  "scalatest"     % "3.2.2"   % Test,
     "org.seleniumhq.selenium" % "selenium-java" % "3.8.1" % Test,
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
-    "org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-dbe3ca9c"
+    "org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-53fb1d87e"
       exclude("com.typesafe.scala-logging", "scala-logging_2.13")
       exclude("com.typesafe.akka", "akka-stream_2.13")
       exclude("bio.terra", "workspace-manager-client"),
