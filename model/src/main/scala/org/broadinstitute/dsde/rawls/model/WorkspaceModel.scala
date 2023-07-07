56fd49c8aaa7e6a5c973d06945d3ecb4ba72af68
Blake Geno <bgeno@broadinstitute.org>
2023-07-07 09:41:22 -0400

    add default argument to avoid breaking external use of model

--- model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
+++ model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceModel.scala
@@ -722,7 +722,7 @@ case class WorkspaceResponse(accessLevel: Option[WorkspaceAccessLevel],
                              bucketOptions: Option[WorkspaceBucketOptions],
                              owners: Option[Set[String]],
                              azureContext: Option[AzureManagedAppCoordinates],
-                             policies: Option[List[WorkspacePolicy]]
+                             policies: Option[List[WorkspacePolicy]] = None
 )
 
 case class WorkspaceDetails(
