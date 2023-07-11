9d10d4a95807f835eca7efb4bf63baeeef174954
aherbst-broad <aherbst@broadinstitute.org>
2023-07-11 10:02:48 -0400

    Formatting

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -468,15 +468,17 @@ class WorkspaceService(protected val ctx: RawlsRequestContext,
                                            azureContext.getResourceGroupId
                 ),
                 Option(wsmInfo.getPolicies)
-                  .map(policies => policies.asScala.toList.map(input =>
-                    WorkspacePolicy(
-                      input.getName,
-                      input.getNamespace,
-                      Option(input.getAdditionalData)
-                        .map(data => data.asScala.map(p => p.getKey -> p.getValue).toMap)
-                        .getOrElse(Map.empty)
+                  .map(policies =>
+                    policies.asScala.toList.map(input =>
+                      WorkspacePolicy(
+                        input.getName,
+                        input.getNamespace,
+                        Option(input.getAdditionalData)
+                          .map(data => data.asScala.map(p => p.getKey -> p.getValue).toMap)
+                          .getOrElse(Map.empty)
+                      )
                     )
-                  ))
+                  )
                   .getOrElse(List.empty)
               )
             case None =>
