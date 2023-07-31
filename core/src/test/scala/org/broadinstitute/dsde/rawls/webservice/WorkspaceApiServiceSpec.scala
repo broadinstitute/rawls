b80ddecad5097183965a1aed7b8ba67a76a95408
aherbst-broad <aherbst@broadinstitute.org>
2023-07-31 14:42:00 -0400

    Update test

--- core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceSpec.scala
@@ -7,14 +7,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route.{seal => sealRoute}
 import bio.terra.profile.model.ProfileModel
 import bio.terra.workspace.model.JobReport.StatusEnum
-import bio.terra.workspace.model.{
-  AzureContext,
-  ErrorReport => _,
-  JobReport,
-  JobResult,
-  ResourceList,
-  WorkspaceDescription
-}
+import bio.terra.workspace.model.{AzureContext, ErrorReport => _, JobReport, JobResult, WorkspaceDescription}
 import com.google.api.services.cloudbilling.model.ProjectBillingInfo
 import com.google.api.services.cloudresourcemanager.model.Project
 import io.opencensus.trace.Span
@@ -36,7 +29,7 @@ import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
 import org.broadinstitute.dsde.workbench.model.google.GcsBucketName
 import org.joda.time.DateTime
 import org.mockito.ArgumentMatchers
-import org.mockito.ArgumentMatchers.{any, anyInt}
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import spray.json.DefaultJsonProtocol._
 import spray.json.{enrichAny, JsObject}
@@ -1223,6 +1216,12 @@ class WorkspaceApiServiceSpec extends ApiServiceSpec {
 
         when(services.workspaceManagerDAO.getWorkspace(any[UUID], any[RawlsRequestContext]))
           .thenReturn(new WorkspaceDescription().id(UUID.randomUUID()).azureContext(new AzureContext()))
+        when(services.workspaceManagerDAO.deleteWorkspaceV2(any[UUID], any[RawlsRequestContext]))
+          .thenReturn(new JobResult().jobReport(new JobReport().id(UUID.randomUUID.toString)))
+        when(services.workspaceManagerDAO.getDeleteWorkspaceV2Result(any[UUID], any[String], any[RawlsRequestContext]))
+          .thenReturn(
+            new JobResult().jobReport(new JobReport().id(UUID.randomUUID.toString).status(StatusEnum.SUCCEEDED))
+          )
 
         Delete(azureWorkspace.path) ~>
           sealRoute(services.workspaceRoutes) ~>
@@ -1235,8 +1234,8 @@ class WorkspaceApiServiceSpec extends ApiServiceSpec {
         assertResult(None) {
           runAndWait(workspaceQuery.findByName(azureWorkspace.toWorkspaceName))
         }
-        verify(services.workspaceManagerDAO).deleteWorkspace(ArgumentMatchers.eq(azureWorkspace.workspaceIdAsUUID),
-                                                             any[RawlsRequestContext]
+        verify(services.workspaceManagerDAO).deleteWorkspaceV2(ArgumentMatchers.eq(azureWorkspace.workspaceIdAsUUID),
+                                                               any[RawlsRequestContext]
         )
       }
     }
