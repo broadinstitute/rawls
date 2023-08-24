e8365f05a8f901ec64107c08ac3ffc6ad77aeb95
cahrens <roberts@broadinstitute.org>
2023-08-23 21:09:45 -0400

    Try updating workbenchLibs versions (WOR-1212).

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,12 +7,12 @@ object Dependencies {
   val akkaHttpV     = "10.2.0"
   val jacksonV      = "2.14.1"
 
-  val workbenchLibsHash = "85a080a"
-  val serviceTestV = s"3.1-${workbenchLibsHash}"
-  val workbenchGoogleV = s"0.27-${workbenchLibsHash}"
-  val workbenchGoogle2V = s"0.30-${workbenchLibsHash}"
-  val workbenchModelV  = s"0.18-${workbenchLibsHash}"
-  val workbenchMetricsV  = s"0.7-${workbenchLibsHash}"
+  val workbenchLibsHash = "e42c23c"
+  val serviceTestV = s"4.0-${workbenchLibsHash}"
+  val workbenchGoogleV = s"0.29-${workbenchLibsHash}"
+  val workbenchGoogle2V = s"0.32-${workbenchLibsHash}"
+  val workbenchModelV  = s"0.19-${workbenchLibsHash}"
+  val workbenchMetricsV  = s"0.8-${workbenchLibsHash}"
 
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
   val workbenchMetrics: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-metrics" % workbenchMetricsV
