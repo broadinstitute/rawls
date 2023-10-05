67ea2ba6fa6061b614eedf7742d89399f16a9f79
Josh Ladieu <111856+jladieu@users.noreply.github.com>
2023-10-05 16:43:12 -0400

    Revert "AJ-1309: Disable cross-platform snapshots-by-reference" (#2569)
    * Revert "AJ-1309: Disable cross-platform snapshots-by-reference (#2556)"
    
    This reverts commit 48ad1c54965434ab3309548ab089213fce5da24f.

--- core/src/test/scala/org/broadinstitute/dsde/rawls/mock/MockWorkspaceManagerDAO.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/mock/MockWorkspaceManagerDAO.scala
@@ -28,8 +28,7 @@ class MockWorkspaceManagerDAO(
 
   val references: TrieMap[(UUID, UUID), DataRepoSnapshotResource] = TrieMap()
 
-  def mockGetWorkspaceResponse(workspaceId: UUID) =
-    new WorkspaceDescription().id(workspaceId).gcpContext(new GcpContext().projectId("some-project-id"))
+  def mockGetWorkspaceResponse(workspaceId: UUID) = new WorkspaceDescription().id(workspaceId)
   def mockCreateWorkspaceResponse(workspaceId: UUID) = new CreatedWorkspace().id(workspaceId)
   def mockReferenceResponse(workspaceId: UUID, referenceId: UUID) = references.getOrElse(
     (workspaceId, referenceId),
