2b0c703fd791a1b868838326d43303b582399c09
broadbot <broadbot@broadinstitute.org>
2023-10-09 12:01:40 +0000

    Update liquibase-core from 4.17.2 to 4.24.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
   val apacheCommonsIO: ModuleID = "commons-io"                    % "commons-io"            % "2.13.0"
   val antlrParser: ModuleID =     "org.antlr"                     % "antlr4-runtime"        % "4.13.0"
   val mysqlConnector: ModuleID =  "com.mysql"                         % "mysql-connector-j"  % "8.1.0"
-  val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "4.17.2"
+  val liquibaseCore: ModuleID =   "org.liquibase"                 % "liquibase-core"        % "4.24.0"
 
   val workbenchLibsHash = "e42c23c"
 
