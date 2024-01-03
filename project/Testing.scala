14a0eb235b7b57d73cb177b9e5ea0a1acfd91b5f
David An <davidan@broadinstitute.org>
2024-01-03 10:27:50 -0500

    tweak logging for unit tests

--- project/Testing.scala
+++ project/Testing.scala
@@ -24,32 +24,8 @@ object Testing {
 
   val testSettingsWithoutDb: Seq[Setting[_]] = List(
     Test / testOptions += Tests.Setup(() => sys.props += "mockserver.logLevel" -> "WARN"),
-    // SLF4J initializes itself upon the first logging call.  Because sbt
-    // runs tests in parallel it is likely that a second thread will
-    // invoke a second logging call before SLF4J has completed
-    // initialization from the first thread's logging call, leading to
-    // these messages:
-    //   SLF4J: The following loggers will not work because they were created
-    //   SLF4J: during the default configuration phase of the underlying logging system.
-    //   SLF4J: See also http://www.slf4j.org/codes.html#substituteLogger
-    //   SLF4J: com.imageworks.common.concurrent.SingleThreadInfiniteLoopRunner
-    //
-    // As a workaround, load SLF4J's root logger before starting the unit
-    // tests
-
-    // Source: https://github.com/typesafehub/scalalogging/issues/23#issuecomment-17359537
-    // References:
-    //   http://stackoverflow.com/a/12095245
-    //   http://jira.qos.ch/browse/SLF4J-167
-    //   http://jira.qos.ch/browse/SLF4J-97
-    Test / testOptions += Tests.Setup(classLoader =>
-      classLoader
-        .loadClass("org.slf4j.LoggerFactory")
-        .getMethod("getLogger", classLoader.loadClass("java.lang.String"))
-        .invoke(null, "ROOT")
-    ),
     Test / testOptions ++= Seq(Tests.Filter(s => !isIntegrationTest(s))),
-    Test / testOptions += Tests.Argument("-oDG"), // D = individual test durations, G = stack trace reminders at end
+    Test / testOptions += Tests.Argument("-oD"), // D = individual test durations
     IntegrationTest / testOptions := Seq(Tests.Filter(s => isIntegrationTest(s))),
     Test / parallelExecution := false
   )
