7b0747519887b9462eb74030763164b215df0777
aherbst-broad <74209955+aherbst-broad@users.noreply.github.com>
2024-02-28 08:44:48 -0500

    [WOR-1452] Call Leo during Google workspace deletion to cleanup dangling resources (#2745)
    Context
    
    We want to notify Leonardo at the time of Google workspace deletion to cleanup any remaining internal Leo DB records and their linked Sam resources. The premise is that Rawls will delete the workspace google cloud project as part of workspace deletion, cleaning up the actual cloud resources, so Leo does not have anything further to do in GCP and only needs to reconcile it's state.
    
    This PR
    
    Adds the needed API to the Leo DAO
    Refactors the current LeonardoDeletionAction class to a LeonardoService class. The intention is to keep the Leo DAO "dumb" and let the service class contain the requisite retry and error handling logic. Rawls' current pattern for DAOs is inconsistent. For example, HttpGoogleServicesDAO does all of the above, while the WSM DAO is much lighter weight and pushes the error handling and retries to the callsites.
    I'd like to centralize on a pattern of "bare, dumb DAO" + "smarter service that knows how to retry, handle errors and propagate a sane exception hierarchy to upper layers". This is a baby step in that direction.
    Removes support for deleting MC azure workspaces via the current WorkspaceService deleteWorkspace codepath. Nothing calls this codepath for MC workspaces anymore and so we shouldn't need the added complexity of the isAzureMcWorkspace checks that are peppered throughout that code.

--- core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
@@ -15,6 +15,7 @@ import org.broadinstitute.dsde.rawls.config._
 import org.broadinstitute.dsde.rawls.coordination.UncoordinatedDataSourceAccess
 import org.broadinstitute.dsde.rawls.dataaccess._
 import org.broadinstitute.dsde.rawls.dataaccess.datarepo.DataRepoDAO
+import org.broadinstitute.dsde.rawls.dataaccess.leonardo.LeonardoService
 import org.broadinstitute.dsde.rawls.dataaccess.resourcebuffer.ResourceBufferDAO
 import org.broadinstitute.dsde.rawls.dataaccess.slick.{TestData, TestDriverComponent}
 import org.broadinstitute.dsde.rawls.dataaccess.workspacemanager.WorkspaceManagerDAO
@@ -30,11 +31,7 @@ import org.broadinstitute.dsde.rawls.resourcebuffer.ResourceBufferService
 import org.broadinstitute.dsde.rawls.serviceperimeter.ServicePerimeterService
 import org.broadinstitute.dsde.rawls.user.UserService
 import org.broadinstitute.dsde.rawls.util.MockitoTestUtils
-import org.broadinstitute.dsde.rawls.workspace.{
-  MultiCloudWorkspaceAclManager,
-  RawlsWorkspaceAclManager,
-  WorkspaceService
-}
+import org.broadinstitute.dsde.rawls.workspace.{MultiCloudWorkspaceAclManager, RawlsWorkspaceAclManager, WorkspaceService}
 import org.broadinstitute.dsde.rawls.{RawlsException, RawlsExceptionWithErrorReport, RawlsTestUtils}
 import org.broadinstitute.dsde.workbench.dataaccess.{NotificationDAO, PubSubNotificationDAO}
 import org.broadinstitute.dsde.workbench.google.mock.{MockGoogleBigQueryDAO, MockGoogleIamDAO, MockGoogleStorageDAO}
@@ -490,6 +487,7 @@ class SubmissionSpec(_system: ActorSystem)
         new RequesterPaysSetupService(slickDataSource, gcsDAO, bondApiDAO, requesterPaysRole = "requesterPaysRole")
 
       val workspaceManagerDAO = new MockWorkspaceManagerDAO
+      val leonardoService = mock[LeonardoService](RETURNS_SMART_NULLS)
       val entityManager = EntityManager.defaultEntityManager(
         dataSource,
         workspaceManagerDAO,
@@ -532,6 +530,7 @@ class SubmissionSpec(_system: ActorSystem)
         execServiceCluster,
         execServiceBatchSize,
         workspaceManagerDAO,
+        leonardoService,
         methodConfigResolver,
         gcsDAO,
         samDAO,
