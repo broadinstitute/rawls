d2da351dc32a745e772a130376e6672509d0a8af
Bria Morgan <bmorgan@broadinstitute.org>
2024-04-05 09:20:35 -0400

    try diff branch for testing

--- pact4s/src/test/scala/org/broadinstitute/dsde/rawls/provider/RawlsProviderSpec.scala
+++ pact4s/src/test/scala/org/broadinstitute/dsde/rawls/provider/RawlsProviderSpec.scala
@@ -247,7 +247,10 @@ class RawlsProviderSpec extends AnyFlatSpec with BeforeAndAfterAll with PactVeri
   // 2. For normal Sam PR, verify all consumer pacts in Pact Broker labelled with a deployed environment (alpha, dev, prod, staging).
   consumerBranch match {
     case Some(s) if !s.isBlank => consumerVersionSelectors = consumerVersionSelectors.branch(s, consumerName)
-    case _                     => consumerVersionSelectors = consumerVersionSelectors.deployedOrReleased.mainBranch
+//    case _                     => consumerVersionSelectors = consumerVersionSelectors.deployedOrReleased.mainBranch
+    case _ =>
+      consumerVersionSelectors =
+        consumerVersionSelectors.deployedOrReleased.mainBranch.branch("aj-1697-rawls-contract", Some("wds"))
   }
 
   val provider: ProviderInfoBuilder =
