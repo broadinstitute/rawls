86ce581c4ca4d1df1abff6031c2e6ac8fcdd2056
Adina Shanholtz <adinas@microsoft.com>
2024-04-10 15:11:46 -0700

    get rid of optimize func and try to fetch wsm

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -311,7 +311,19 @@ class WorkspaceService(protected val ctx: RawlsRequestContext,
       getV2WorkspaceContextAndPermissions(workspaceName, SamWorkspaceActions.read, Option(attrSpecs)) flatMap {
         workspaceContext =>
           dataSource.inTransaction { dataAccess =>
-            val wsmContext = wsmService.optimizedFetchAggregatedWorkspace(workspaceContext, ctx)
+            // some GCP workspaces, like those with linked snapshots, have a stub WSM workspace
+            val wsmContext =
+              try
+                wsmService.fetchAggregatedWorkspace(workspaceContext, ctx)
+              catch {
+                case _: Exception =>
+                  // return workspace with no WSM information
+                  AggregatedWorkspace(workspaceContext,
+                                      Some(workspaceContext.googleProjectId),
+                                      azureCloudContext = None,
+                                      policies = List.empty
+                  )
+              }
 
             // maximum access level is required to calculate canCompute and canShare. Therefore, if any of
             // accessLevel, canCompute, canShare is specified, we have to get it.
