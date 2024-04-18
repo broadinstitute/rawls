141767482e10831d81b2c03d957a4983544d0161
Blake Geno <bgeno@broadinstitute.org>
2024-04-18 10:10:41 -0400

    extract abstract base class for clone steps

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/clone/CloneWorkspaceInitStep.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/clone/CloneWorkspaceInitStep.scala
@@ -1,38 +1,38 @@
 package org.broadinstitute.dsde.rawls.monitor.workspace.runners.clone
 
-import bio.terra.workspace.model.CloneWorkspaceResult
 import bio.terra.workspace.model.JobReport.StatusEnum
-import com.typesafe.scalalogging.LazyLogging
-import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord.{Complete, Incomplete, JobStatus}
+import org.broadinstitute.dsde.rawls.dataaccess.WorkspaceManagerResourceMonitorRecordDao
+import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord
+import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord.JobType.JobType
+import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord.{Complete, Incomplete, JobStatus, JobType}
 import org.broadinstitute.dsde.rawls.dataaccess.workspacemanager.WorkspaceManagerDAO
 import org.broadinstitute.dsde.rawls.model.RawlsRequestContext
-import org.broadinstitute.dsde.rawls.model.WorkspaceState.CreateFailed
 import org.broadinstitute.dsde.rawls.workspace.{WorkspaceManagerPollingOperationException, WorkspaceRepository}
 
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
 class CloneWorkspaceInitStep(
-                              val workspaceManagerDAO: WorkspaceManagerDAO,
-                              val workspaceRepository: WorkspaceRepository,
-                              val workspaceId: UUID,
-                              val jobId: UUID
+                               workspaceManagerDAO: WorkspaceManagerDAO,
+                               workspaceRepository: WorkspaceRepository,
+                               monitorRecordDao: WorkspaceManagerResourceMonitorRecordDao,
+                               workspaceId: UUID,
+                               job: WorkspaceManagerResourceMonitorRecord
                             )
-                            (implicit executionContext: ExecutionContext) extends WorkspaceCloneStatusUpdate {
+                            (implicit executionContext: ExecutionContext)
+  extends WorkspaceCloningStep(workspaceManagerDAO, workspaceRepository, monitorRecordDao, workspaceId, job) {
+
+  override val jobType: JobType = JobType.CloneWorkspaceInit
 
   def runStep(userCtx: RawlsRequestContext): Future[JobStatus] = {
-    val result = workspaceManagerDAO.getCloneWorkspaceResult(workspaceId, jobId.toString, userCtx)
+    val result = workspaceManagerDAO.getCloneWorkspaceResult(workspaceId, job.jobControlId.toString, userCtx)
     result.getJobReport.getStatus match {
-      case StatusEnum.SUCCEEDED =>
-        // no status update needed (this is all under the 'Cloning Resources' state)
-        // the next job will be scheduled by the runner
-        Future(Complete)
+      case StatusEnum.SUCCEEDED => scheduleNextJob(UUID.randomUUID()).map(_ => Complete)
       case StatusEnum.RUNNING => Future(Incomplete)
       case StatusEnum.FAILED =>
           fail("Initial Workspace Creation", result).map(_ => Complete)
     }
   }
-
 
 }
 
---
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/clone/WorkspaceCloningStep.scala
@@ -0,0 +1,69 @@
+package org.broadinstitute.dsde.rawls.monitor.workspace.runners.clone
+
+import bio.terra.workspace.model.CloneWorkspaceResult
+import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.rawls.dataaccess.WorkspaceManagerResourceMonitorRecordDao
+import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord
+import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord.{JobStatus, JobType}
+import org.broadinstitute.dsde.rawls.dataaccess.workspacemanager.WorkspaceManagerDAO
+import org.broadinstitute.dsde.rawls.model.{RawlsRequestContext, RawlsUserEmail}
+import org.broadinstitute.dsde.rawls.model.WorkspaceState.CreateFailed
+import org.broadinstitute.dsde.rawls.workspace.WorkspaceRepository
+
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+
+abstract class WorkspaceCloningStep(
+                                     val workspaceManagerDAO: WorkspaceManagerDAO,
+                                     val workspaceRepository: WorkspaceRepository,
+                                     val monitorRecordDao: WorkspaceManagerResourceMonitorRecordDao,
+                                     val workspaceId: UUID,
+                                     val job: WorkspaceManagerResourceMonitorRecord
+                                   ) extends LazyLogging {
+
+  val jobType: JobType.JobType
+
+
+  def scheduleNextJob(nextJobId: UUID)(
+    implicit executionContext: ExecutionContext): Future[Unit] = {
+    val nextJobType = job.jobType match {
+      case JobType.CloneWorkspaceInit =>
+        Some(JobType.CreateWdsAppInClonedWorkspace)
+      case JobType.CreateWdsAppInClonedWorkspace => Some(JobType.CloneWorkspaceContainerInit)
+      case JobType.CloneWorkspaceContainerInit => Some(JobType.CloneWorkspaceContainerResult)
+      case JobType.CloneWorkspaceContainerResult => None
+      // This should be caught earlier, but better to be explicit and fail fast
+      case _ => throw new IllegalArgumentException(s"Invalid job type for clone job: ${job.jobType}")
+    }
+
+    nextJobType.map(jobType =>
+      monitorRecordDao.create(
+        WorkspaceManagerResourceMonitorRecord.forCloneWorkspace(
+          nextJobId,
+          job.workspaceId.get,
+          job.userEmail.map(RawlsUserEmail).get,
+          job.args,
+          jobType
+        )
+      )
+    ).getOrElse(Future())
+  }
+
+
+  def fail(operationName: String, result: CloneWorkspaceResult)(implicit ex: ExecutionContext): Future[Int] = {
+    val errorMessage = Option(result.getErrorReport)
+      .map(report => report.getMessage)
+      .getOrElse("Error not specified in job")
+    val jobId = result.getJobReport.getId
+    val message = s"Workspace Clone Operation $operationName failed for jobId $jobId: $errorMessage"
+    // todo: log as well?
+    workspaceRepository.setFailedState(workspaceId, CreateFailed, message)
+  }
+
+  def fail(operationName: String, errorMessage: String)(implicit executionContext: ExecutionContext): Future[Int] = {
+    val message = s"Workspace Clone Operation $operationName failed: $errorMessage"
+    // todo: log?
+    workspaceRepository.setFailedState(workspaceId, CreateFailed, message)
+  }
+
+}
