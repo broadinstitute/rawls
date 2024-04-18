b053a971cf99c132c017b9f01615a34f79f70508
Marcus Talbott <mtalbott@broadinstitute.org>
2024-04-18 14:56:49 -0400

    imports

--- core/src/main/scala/org/broadinstitute/dsde/rawls/billing/BillingProfileManagerClientProvider.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/billing/BillingProfileManagerClientProvider.scala
@@ -1,18 +1,11 @@
 package org.broadinstitute.dsde.rawls.billing
 
-import bio.terra.common.tracing.JakartaTracingFilter
 import bio.terra.profile.api.{AzureApi, ProfileApi, SpendReportingApi, UnauthenticatedApi}
-import bio.terra.profile.client.{ApiClient, RFC3339DateFormat}
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import io.opencensus.trace.Tracing
-import io.opentelemetry.api.GlobalOpenTelemetry
+import bio.terra.profile.client.ApiClient
 import jakarta.ws.rs.client.ClientBuilder
 import org.broadinstitute.dsde.rawls.model.RawlsRequestContext
-import org.broadinstitute.dsde.rawls.util.{TracingUtils, WithOtelContextFilter}
+import org.broadinstitute.dsde.rawls.util.TracingUtils
 import org.glassfish.jersey.client.ClientConfig
-import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider
 import org.glassfish.jersey.jnh.connector.JavaNetHttpConnectorProvider
 
 /**
