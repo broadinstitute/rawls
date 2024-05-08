950a41fccc7a35ec84e9327cce432387c0d0aaf1
bennettn4 <nibennett@microsoft.com>
2024-05-08 09:46:40 -0400

    initial commit
    create new disabled service which silently fails calls instead of explicitly throwing an error

--- core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/DisabledServiceFactory.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/DisabledServiceFactory.scala
@@ -1,7 +1,10 @@
 package org.broadinstitute.dsde.rawls.serviceFactory
 
-import java.lang.reflect.Proxy
-import scala.reflect.{classTag, ClassTag}
+import cats.effect.IO
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+import java.lang.reflect.{Method, Proxy}
+import scala.reflect.{ClassTag, classTag}
 
 object DisabledServiceFactory {
 
@@ -19,5 +22,27 @@ object DisabledServiceFactory {
         (_, method, _) =>
           throw new UnsupportedOperationException(s"${method.toString} is not supported in Azure control plane.")
       )
+      .asInstanceOf[T]
+
+  /**
+   * Create a new instance of a service that throws UnsupportedOperationException for all non-Unit methods.
+   * For Unit methods, an error is not explicitly thrown but instead logged and does not block dependent processes.
+   * Implemented using a dynamic proxy.
+   * @tparam T the type of the service, must be a trait
+   * @return a new instance of the service that throws UnsupportedOperationException for all non-Unit methods
+   */
+  def newSilentDisabledService[T: ClassTag]: T =
+    Proxy
+      .newProxyInstance(
+        classTag[T].runtimeClass.getClassLoader,
+        Array(classTag[T].runtimeClass),
+         (_, method, _) =>
+          if (method.getReturnType().equals(Unit.getClass())) {
+            implicit val log4CatsLogger = Slf4jLogger.getLogger[IO]
+            log4CatsLogger.error(s"${method.toString} is not supported in Azure control plane. Service has been identified to be non-blocking while inoperable and will silently fail.")
+          } else {
+            throw new UnsupportedOperationException(s"${method.toString} is not supported in Azure control plane.")
+          }
+  )
       .asInstanceOf[T]
 }
