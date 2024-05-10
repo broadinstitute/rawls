9452d13027d83a2e02f4ad76715086d4d9a8016d
bennettn4 <117685842+bennettn4@users.noreply.github.com>
2024-05-10 14:42:04 -0400

    Update to disabled services for Rawls on Azure without google dependencies  (#2865)
    * initial commit
    
    create new disabled service which silently fails calls instead of explicitly throwing an error
    
    * Update DisabledServiceFactory
    
    * Update DisabledServiceFactory logging
    
    * formatting

--- core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/DisabledServiceFactory.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/DisabledServiceFactory.scala
@@ -1,9 +1,11 @@
 package org.broadinstitute.dsde.rawls.serviceFactory
 
-import java.lang.reflect.Proxy
+import com.typesafe.scalalogging.LazyLogging
+
+import java.lang.reflect.{Method, Proxy}
 import scala.reflect.{classTag, ClassTag}
 
-object DisabledServiceFactory {
+object DisabledServiceFactory extends LazyLogging {
 
   /**
    * Create a new instance of a service that throws UnsupportedOperationException for all methods.
@@ -18,6 +20,30 @@ object DisabledServiceFactory {
         Array(classTag[T].runtimeClass),
         (_, method, _) =>
           throw new UnsupportedOperationException(s"${method.toString} is not supported in Azure control plane.")
+      )
+      .asInstanceOf[T]
+
+  /**
+   * Create a new instance of a service that throws UnsupportedOperationException for all non-Unit methods.
+   * For Unit methods, an error is not explicitly thrown but instead logged and does not block dependent processes.
+   * Implemented using a dynamic proxy.
+   * @tparam T the type of the service, must be a trait
+   * @return a new instance of the service that throws UnsupportedOperationException for all non-Unit methods
+   */
+  def newSilentDisabledService[T: ClassTag]: T =
+    Proxy
+      .newProxyInstance(
+        classTag[T].runtimeClass.getClassLoader,
+        Array(classTag[T].runtimeClass),
+        (_, method, _) =>
+          if (method.getReturnType().equals(().getClass())) {
+            logger.error(
+              s"${method.toString} is not supported in Azure control plane. Service has been identified to be non-blocking while inoperable and will silently fail."
+            )
+            None
+          } else {
+            throw new UnsupportedOperationException(s"${method.toString} is not supported in Azure control plane.")
+          }
       )
       .asInstanceOf[T]
 }
