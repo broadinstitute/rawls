8192643e1ad755b6c1f02cf0bc51d3a04fbb8f38
Josh Ladieu <111856+jladieu@users.noreply.github.com>
2024-05-24 12:32:34 -0400

    AJ-1797: Eliminate import-service integration from Rawls (#2866)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/BootMonitors.scala
@@ -72,8 +72,7 @@ object BootMonitors extends LazyLogging {
                    samDAO: SamDAO,
                    notificationDAO: NotificationDAO,
                    pubSubDAO: GooglePubSubDAO,
-                   importServicePubSubDAO: GooglePubSubDAO,
-                   importServiceDAO: ImportServiceDAO,
+                   cwdsDAO: CwdsDAO,
                    workspaceManagerDAO: WorkspaceManagerDAO,
                    billingProfileManagerDAO: BillingProfileManagerDAO,
                    leonardoDAO: LeonardoDAO,
@@ -182,7 +181,6 @@ object BootMonitors extends LazyLogging {
         util.toScalaDuration(appConfigManager.conf.getDuration("avroUpsertMonitor.pollJitter")),
         appConfigManager.conf.getString("avroUpsertMonitor.importRequestPubSubTopic"),
         appConfigManager.conf.getString("avroUpsertMonitor.importRequestPubSubSubscription"),
-        appConfigManager.conf.getString("avroUpsertMonitor.updateImportStatusPubSubTopic"),
         appConfigManager.conf.getString("avroUpsertMonitor.updateCwdsPubSubTopic"),
         appConfigManager.conf.getInt("avroUpsertMonitor.ackDeadlineSeconds"),
         appConfigManager.conf.getInt("avroUpsertMonitor.batchSize"),
@@ -196,8 +194,7 @@ object BootMonitors extends LazyLogging {
                              samDAO,
                              googleStorage,
                              pubSubDAO,
-                             importServicePubSubDAO,
-                             importServiceDAO,
+                             cwdsDAO,
                              avroUpsertMonitorConfig,
                              slickDataSource
       )
@@ -427,8 +424,7 @@ object BootMonitors extends LazyLogging {
                                      samDAO: SamDAO,
                                      googleStorage: GoogleStorageService[IO],
                                      googlePubSubDAO: GooglePubSubDAO,
-                                     importServicePubSubDAO: GooglePubSubDAO,
-                                     importServiceDAO: ImportServiceDAO,
+                                     cwdsDAO: CwdsDAO,
                                      avroUpsertMonitorConfig: AvroUpsertMonitorConfig,
                                      dataSource: SlickDataSource
   ) =
@@ -439,8 +435,7 @@ object BootMonitors extends LazyLogging {
         samDAO,
         googleStorage,
         googlePubSubDAO,
-        importServicePubSubDAO,
-        importServiceDAO,
+        cwdsDAO,
         avroUpsertMonitorConfig,
         dataSource
       )
