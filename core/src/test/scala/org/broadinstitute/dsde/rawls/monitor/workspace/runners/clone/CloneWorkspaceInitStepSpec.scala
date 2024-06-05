41b5bbc52c668a03e9f63059c32a5f240435af09
Christina Ahrens Roberts <roberts@broadinstitute.org>
2024-06-05 13:40:38 -0400

    [WOR-1695] Mark Rawls workspace state as CloningFailed if Azure workspace resource failed to clone (#2905)

--- core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/clone/CloneWorkspaceInitStepSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/clone/CloneWorkspaceInitStepSpec.scala
@@ -1,6 +1,13 @@
 package org.broadinstitute.dsde.rawls.monitor.workspace.runners.clone
 
-import bio.terra.workspace.model.{CloneWorkspaceResult, JobReport}
+import bio.terra.workspace.model.{
+  CloneResourceResult,
+  CloneWorkspaceResult,
+  ClonedWorkspace,
+  JobReport,
+  ResourceCloneDetails,
+  ResourceType
+}
 import org.broadinstitute.dsde.rawls.TestExecutionContext
 import org.broadinstitute.dsde.rawls.dataaccess.WorkspaceManagerResourceMonitorRecordDao
 import org.broadinstitute.dsde.rawls.dataaccess.slick.WorkspaceManagerResourceMonitorRecord
@@ -22,6 +29,7 @@ import org.scalatestplus.mockito.MockitoSugar
 
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
 
 class CloneWorkspaceInitStepSpec extends AnyFlatSpecLike with MockitoSugar with Matchers with ScalaFutures {
 
@@ -96,6 +104,75 @@ class CloneWorkspaceInitStepSpec extends AnyFlatSpecLike with MockitoSugar with 
     )
   }
 
+  it should "complete and update the workspace as CloningFailed when resources failed to clone" in {
+    val jobId = UUID.randomUUID()
+    val cloneJobId = "not-a-real-uuid"
+    val monitorRecord = WorkspaceManagerResourceMonitorRecord.forCloneWorkspace(
+      jobId,
+      workspaceId,
+      RawlsUserEmail(userEmail),
+      Some(Map(WorkspaceCloningRunner.WORKSPACE_INITIAL_CLONE_JOBID_KEY -> cloneJobId)),
+      JobType.CloneWorkspaceInit
+    )
+    val ctx = mock[RawlsRequestContext]
+    val workspaceManagerDAO = mock[WorkspaceManagerDAO]
+    when(workspaceManagerDAO.getCloneWorkspaceResult(workspaceId, cloneJobId, ctx))
+      .thenReturn(
+        new CloneWorkspaceResult()
+          .workspace(
+            new ClonedWorkspace().resources(
+              Set(
+                new ResourceCloneDetails()
+                  .name("failedResource1")
+                  .resourceType(ResourceType.AZURE_VM)
+                  .result(CloneResourceResult.FAILED)
+                  .errorMessage("error1"),
+                new ResourceCloneDetails()
+                  .name("succeededResource")
+                  .resourceType(ResourceType.AZURE_VM)
+                  .result(CloneResourceResult.SUCCEEDED),
+                new ResourceCloneDetails()
+                  .name("skippedResource")
+                  .resourceType(ResourceType.AZURE_VM)
+                  .result(CloneResourceResult.SKIPPED),
+                new ResourceCloneDetails()
+                  .name("failedResource2")
+                  .resourceType(ResourceType.AZURE_DATABASE)
+                  .result(CloneResourceResult.FAILED)
+                  .errorMessage("error2")
+              ).toList.asJava
+            )
+          )
+          .jobReport(new JobReport().status(JobReport.StatusEnum.SUCCEEDED))
+      )
+    val workspaceRepository = mock[WorkspaceRepository]
+    when(
+      workspaceRepository.setFailedState(
+        ArgumentMatchers.eq(workspaceId),
+        ArgumentMatchers.eq(CloningFailed),
+        ArgumentMatchers.anyString()
+      )
+    ).thenReturn(Future(1))
+    val step = new CloneWorkspaceInitStep(
+      workspaceManagerDAO,
+      workspaceRepository,
+      mock[WorkspaceManagerResourceMonitorRecordDao],
+      workspaceId,
+      monitorRecord
+    )
+
+    whenReady(step.runStep(ctx)) {
+      _ shouldBe Complete
+    }
+    verify(workspaceRepository).setFailedState(
+      ArgumentMatchers.eq(workspaceId),
+      ArgumentMatchers.eq(CloningFailed),
+      ArgumentMatchers.eq(
+        s"Workspace Clone Operation [Initial Workspace Clone], source workspace: [Unknown], dest workspace [$workspaceId] failed for jobId [$jobId]: resource (failedResource1, AZURE_VM) failed to clone with error \"error1\", resource (failedResource2, AZURE_DATABASE) failed to clone with error \"error2\""
+      )
+    )
+  }
+
   it should "complete and schedule the next job when the job report status is SUCCEEDED" in {
     val jobId = UUID.randomUUID()
     val cloneJobId = "not-a-real-uuid"
@@ -109,7 +186,24 @@ class CloneWorkspaceInitStepSpec extends AnyFlatSpecLike with MockitoSugar with 
     val ctx = mock[RawlsRequestContext]
     val workspaceManagerDAO = mock[WorkspaceManagerDAO]
     when(workspaceManagerDAO.getCloneWorkspaceResult(workspaceId, cloneJobId, ctx))
-      .thenReturn(new CloneWorkspaceResult().jobReport(new JobReport().status(JobReport.StatusEnum.SUCCEEDED)))
+      .thenReturn(
+        new CloneWorkspaceResult()
+          .workspace(
+            new ClonedWorkspace().resources(
+              Set(
+                new ResourceCloneDetails()
+                  .name("succeededResource")
+                  .resourceType(ResourceType.AZURE_VM)
+                  .result(CloneResourceResult.SUCCEEDED),
+                new ResourceCloneDetails()
+                  .name("skippedResource")
+                  .resourceType(ResourceType.AZURE_VM)
+                  .result(CloneResourceResult.SKIPPED)
+              ).toList.asJava
+            )
+          )
+          .jobReport(new JobReport().status(JobReport.StatusEnum.SUCCEEDED))
+      )
     val workspaceRepository = mock[WorkspaceRepository]
     val recordDao = mock[WorkspaceManagerResourceMonitorRecordDao]
     doAnswer { a =>
