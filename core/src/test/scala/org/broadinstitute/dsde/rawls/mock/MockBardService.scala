1e7bb65229f8e848ec6b612a8356b1911a5f75f1
tlangs <tlangs@broadinstitute.org>
2024-06-21 10:41:19 -0400

    non-zero mock http connections

--- core/src/test/scala/org/broadinstitute/dsde/rawls/mock/MockBardService.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/mock/MockBardService.scala
@@ -2,4 +2,4 @@ package org.broadinstitute.dsde.rawls.mock
 
 import org.broadinstitute.dsde.rawls.metrics.BardService
 
-class MockBardService extends BardService(false, "", 0) {}
+class MockBardService extends BardService(false, "", 1) {}
