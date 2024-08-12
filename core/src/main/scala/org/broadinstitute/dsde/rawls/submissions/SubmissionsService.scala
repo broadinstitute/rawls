4572c58fcd4caa6ed55ba00a798a16ef20ddeffb
Saloni Shah <salonipshah11@gmail.com>
2024-08-12 16:37:50 -0400

    remove commented code

--- core/src/main/scala/org/broadinstitute/dsde/rawls/submissions/SubmissionsService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/submissions/SubmissionsService.scala
@@ -804,32 +804,6 @@ class SubmissionsService(
       EntityRequestArguments(workspaceContext, ctx, methodConfiguration.dataReferenceName, None)
     )
 
-//  private def gatherMethodConfigInputs(
-//    methodConfig: MethodConfiguration
-//  ): Future[MethodConfigResolver.GatherInputsResult] =
-//    toFutureTry(methodRepoDAO.getMethod(methodConfig.methodRepoMethod, ctx.userInfo)).map {
-//      case Success(None) =>
-//        throw new RawlsExceptionWithErrorReport(
-//          errorReport = ErrorReport(StatusCodes.NotFound,
-//                                    s"Cannot get ${methodConfig.methodRepoMethod.methodUri} from method repo."
-//          )
-//        )
-//      case Success(Some(wdl)) =>
-//        methodConfigResolver
-//          .gatherInputs(ctx.userInfo, methodConfig, wdl)
-//          .recoverWith { case regrets =>
-//            Failure(new RawlsExceptionWithErrorReport(errorReport = ErrorReport(StatusCodes.BadRequest, regrets)))
-//          }
-//          .get
-//      case Failure(throwable) =>
-//        throw new RawlsExceptionWithErrorReport(
-//          errorReport = ErrorReport(StatusCodes.BadGateway,
-//                                    s"Unable to query the method repo.",
-//                                    methodRepoDAO.toErrorReport(throwable)
-//          )
-//        )
-//    }
-
   private def saveSubmission(workspaceContext: Workspace,
                              submissionId: UUID,
                              submissionRequest: SubmissionRequest,
