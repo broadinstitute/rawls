59a762b8c1cb6b958e68f529871c9ee759bf3c6c
Blake Geno <bgeno@broadinstitute.org>
2024-09-24 17:16:01 -0400

    WOR-1823: use original comparisons to resolve canShare (#3045)
    * use original comparisons to resolve canShare
    
    * formatting

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -283,12 +283,11 @@ class WorkspaceService(
       }
       owners = ownersPolicy.map(policy => policy.memberEmails.map(_.value))
       canShare <- options.anyPresentFuture("canShare") {
-        accessLevel match {
-          case WorkspaceAccessLevels.Read  => Future.successful(false)
-          case WorkspaceAccessLevels.Owner => Future.successful(true)
-          case _ =>
-            val sharePolicy = SamWorkspaceActions.sharePolicy(accessLevel.toString.toLowerCase())
-            samDAO.userHasAction(SamResourceTypeNames.workspace, workspaceId, sharePolicy, ctx)
+        if (accessLevel < WorkspaceAccessLevels.Read) Future.successful(false)
+        else if (accessLevel >= WorkspaceAccessLevels.Owner) Future.successful(true)
+        else {
+          val sharePolicy = SamWorkspaceActions.sharePolicy(accessLevel.toString.toLowerCase())
+          samDAO.userHasAction(SamResourceTypeNames.workspace, workspaceId, sharePolicy, ctx)
         }
       }
       wsmService = new AggregatedWorkspaceService(workspaceManagerDAO)
@@ -2230,7 +2229,6 @@ class WorkspaceService(
         )
     }
 
-
   def failIfBucketRegionInvalid(bucketRegion: Option[String]): Future[Unit] =
     bucketRegion.traverse_ { region =>
       // if the user specifies a region for the workspace bucket, it must be in the proper format
@@ -2264,8 +2262,6 @@ class WorkspaceService(
         gcsDAO.getRegionForRegionalBucket(sourceBucketName, Option(googleProjectId))
       case (None, None) => Future(Some(config.defaultLocation))
     }
-
-
 
 }
 
