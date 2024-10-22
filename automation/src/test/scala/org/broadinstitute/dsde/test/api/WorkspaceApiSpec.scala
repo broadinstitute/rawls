5c41d7f513ce8b6b9d4ae7a8921181ecf6c2193e
Justin H Yang <juyang@broadinstitute.org>
2024-10-22 10:51:22 -0700

    typo

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/WorkspaceApiSpec.scala
@@ -138,8 +138,8 @@ class WorkspaceApiSpec
       labels should contain allElementsOf bufferLabels
       labels should contain allElementsOf rawlsLabels
 
-      Rawls.workspaces.delete(billingProjectName, workspaceName)(token: ownerAtuhToken)
-      Rawls.billingV2.deleteBillingProject(billingProjectName)(token: ownerAtuhToken)
+      Rawls.workspaces.delete(billingProjectName, workspaceName)(token: ownerAuthToken)
+      Rawls.billingV2.deleteBillingProject(billingProjectName)(token: ownerAuthToken)
     }
 
     "should grant the proper IAM roles on the underlying google project when creating a workspace" in {
