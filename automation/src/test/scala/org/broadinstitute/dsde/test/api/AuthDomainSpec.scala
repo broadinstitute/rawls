74a9979b3bf6be6a3511c883288392ea1f36313c
Justin H Yang <juyang@broadinstitute.org>
2024-10-22 13:39:09 -0700

    typo?

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/AuthDomainSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/AuthDomainSpec.scala
@@ -35,7 +35,7 @@ class AuthDomainSpec extends AnyFlatSpec with Matchers with WorkspaceFixtures wi
   lazy val projectOwnerToken = bee.Owners.getUserCredential("hermione").map(_.makeAuthToken).get
   lazy val (projectUserToken, groupOwnerToken) = {
     val users = bee.chooseStudents(2)
-    (users(0), users(1))
+    (users[0], users[1])
   }
 
   val billingAccountId: String = ServiceTestConfig.Projects.billingAccountId
