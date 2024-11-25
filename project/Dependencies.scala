291cef50945b140f2b8644695249c73af4a8294a
broadbot <broadbot@broadinstitute.org>
2024-11-25 12:21:47 +0000

    CORE-69: Update sentry-logback from 7.16.0 to 7.18.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.18.1"
   val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.13.0"
   val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.3"
-  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "7.16.0"
+  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "7.18.0"
   val webjarsLocator: ModuleID =  "org.webjars"                   % "webjars-locator"       % "0.52"
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val cats: ModuleID =            "org.typelevel"                 %% "cats-core"                 % "2.12.0"
