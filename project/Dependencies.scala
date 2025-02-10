d412ca9c225e61e2bb83900ab6df68e91edce03d
broadbot <broadbot@broadinstitute.org>
2025-02-10 12:11:12 +0000

    CORE-69: Update joda-time from 2.13.0 to 2.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
 
   val scalaLogging: ModuleID =    "com.typesafe.scala-logging"    %% "scala-logging"        % "3.9.5"
   val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.18.2"
-  val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.13.0"
+  val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.13.1"
   val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.3"
   val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "7.21.0"
   val webjarsLocator: ModuleID =  "org.webjars"                   % "webjars-locator"       % "0.52"
