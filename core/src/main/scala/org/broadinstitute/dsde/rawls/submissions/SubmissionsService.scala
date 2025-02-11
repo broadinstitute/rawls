19f2bcbb1a97862275f79e64aeec5f0d44f5de38
Kevin Palis <palis@broadinstitute.org>
2025-02-10 19:05:41 -0500

    Adding cost cap data

--- core/src/main/scala/org/broadinstitute/dsde/rawls/submissions/SubmissionsService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/submissions/SubmissionsService.scala
@@ -461,7 +461,11 @@ class SubmissionsService(
                   case None       => workflow
                 }
               }
-              val costedSubmission = submission.copy(cost = Some(costMap.values.sum), workflows = costedWorkflows)
+              val costedSubmission = submission.copy(
+                cost = Some(costMap.values.sum), 
+                workflows = costedWorkflows,
+                perWorkflowCostCap = submission.perWorkflowCostCap // Include the cost cap data
+              )
               costedSubmission
           }
         }
