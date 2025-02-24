bd3cd9f1f92da9525e4e262a0778a4b893fb73a2
Kevin Marete <kevinmmarete@gmail.com>
2025-02-24 18:58:19 -0500

    Show billing project spend report to workspace owners

--- core/src/main/scala/org/broadinstitute/dsde/rawls/spendreporting/SpendReportingService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/spendreporting/SpendReportingService.scala
@@ -1,10 +1,9 @@
 package org.broadinstitute.dsde.rawls.spendreporting
 
-import java.util.{Currency, UUID}
 import akka.http.scaladsl.model.StatusCodes
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import com.google.cloud.bigquery.{Field, FieldValue, FieldValueList, JobStatistics, Option => _, _}
+import com.google.cloud.bigquery.{FieldValueList, JobStatistics, Option => _, _}
 import com.typesafe.scalalogging.LazyLogging
 import nl.grons.metrics4.scala.{Counter, Histogram}
 import org.broadinstitute.dsde.rawls.billing.{
@@ -17,18 +16,18 @@ import org.broadinstitute.dsde.rawls.dataaccess.{SamDAO, SlickDataSource}
 import org.broadinstitute.dsde.rawls.metrics.{GoogleInstrumented, HitRatioGauge, RawlsInstrumented}
 import org.broadinstitute.dsde.rawls.model.{SpendReportingAggregationKeyWithSub, _}
 import org.broadinstitute.dsde.rawls.spendreporting.SpendReportingService._
+import org.broadinstitute.dsde.rawls.util.TracingUtils.traceFutureWithParent
 import org.broadinstitute.dsde.rawls.workspace.WorkspaceService
-import org.broadinstitute.dsde.workbench.google2.GoogleBigQueryService
 import org.broadinstitute.dsde.rawls.{RawlsException, RawlsExceptionWithErrorReport}
+import org.broadinstitute.dsde.workbench.google2.GoogleBigQueryService
 import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 import org.joda.time.format.ISODateTimeFormat
 import org.joda.time.{DateTime, Days}
 
+import java.util.{Currency, UUID}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters._
 import scala.math.BigDecimal.RoundingMode
-import org.broadinstitute.dsde.rawls.util.TracingUtils.traceFutureWithParent
-
 import scala.util.Try
 
 object SpendReportingService {
@@ -470,34 +469,68 @@ class SpendReportingService(
     bytesProcessedCounter += stats.getEstimatedBytesProcessed
   }
 
+  def getOwnedWorkspaceGoogleProjects(
+    childContext: RawlsRequestContext
+  ): Future[Map[RawlsBillingProjectName, Seq[Workspace]]] =
+    samDAO
+      .listResourcesWithActions(
+        SamResourceTypeNames.workspace,
+        SamWorkspaceActions.readSpendReport,
+        childContext
+      )
+      .flatMap {
+        case ownerWorkspaces if ownerWorkspaces.isEmpty =>
+          Future.successful(Map.empty[RawlsBillingProjectName, Seq[Workspace]])
+        case ownerWorkspaces =>
+          val validWorkspaceIds = ownerWorkspaces
+            .map(_.getResourceId)
+            .filter(resourceId => Try(UUID.fromString(resourceId)).isSuccess) // filter out non-UUIDs
+            .toList
+          workspaceServiceConstructor(childContext).getGCPWorkspacesByBillingProjects(validWorkspaceIds)
+      }
+
+  def getOwnedWorkspaceGoogleProjectsInProject(
+    projectName: RawlsBillingProjectName,
+    childContext: RawlsRequestContext
+  ): Future[Map[GoogleProjectId, WorkspaceName]] =
+    requireProjectAction(projectName, SamBillingProjectActions.createWorkspace) {
+      for {
+        // Get user owned workspaces in the project
+        projectNames <- getOwnedWorkspaceGoogleProjects(childContext).map {
+          _.flatMap { case (_, value) =>
+            value.collect {
+              case workspace if workspace.namespace == projectName.value =>
+                workspace.googleProjectId -> workspace.toWorkspaceName
+            }
+          }.toMap
+        }
+      } yield projectNames
+    }
+
   def getSpendForGCPBillingProject(
     project: RawlsBillingProjectName,
     start: DateTime,
     end: DateTime,
     aggregations: Set[SpendReportingAggregationKeyWithSub]
-  ): Future[SpendReportingResults] = {
+  ): Future[SpendReportingResults] = traceFutureWithParent("getSpendForGCPBillingProject", ctx) { childContext =>
     validateReportParameters(start, end)
-    requireProjectAction(project,
-                         SamBillingProjectActions.createWorkspace
-    ) { // read_spend_report is a subset of create_workspace and allows workspace owners to see spend data
-      for {
-        spendExportConf <- getSpendExportConfiguration(project)
-        projectNames <- getWorkspaceGoogleProjects(project)
+    for {
+      spendExportConf <- getSpendExportConfiguration(project)
+      projectNames <- getOwnedWorkspaceGoogleProjectsInProject(project, childContext)
 
-        query = getQuery(aggregations, spendExportConf)
-        queryJob = setUpQuery(query, spendExportConf, start, end, projectNames)
+      query = getQuery(aggregations, spendExportConf)
+      queryJob = setUpQuery(query, spendExportConf, start, end, projectNames)
 
-        job: Job <- bigQueryService.use(_.runJob(queryJob)).unsafeToFuture().map(_.waitFor())
-        _ = logSpendQueryStats(job.getStatistics[JobStatistics.QueryStatistics])
-        result = job.getQueryResults()
-      } yield result.getValues.asScala.toList match {
-        case Nil =>
-          throw RawlsExceptionWithErrorReport(
-            StatusCodes.NotFound,
-            s"no spend data found for billing project ${project.value} between dates ${toISODateString(start)} and ${toISODateString(end)}"
-          )
-        case rows => extractSpendReportingResults(rows, start, end, projectNames, aggregations)
-      }
+      job: Job <- bigQueryService.use(_.runJob(queryJob)).unsafeToFuture().map(_.waitFor())
+      _ = logSpendQueryStats(job.getStatistics[JobStatistics.QueryStatistics])
+      result = job.getQueryResults()
+    } yield result.getValues.asScala.toList match {
+      case Nil =>
+        throw RawlsExceptionWithErrorReport(
+          StatusCodes.NotFound,
+          s"no spend data found for billing project ${project.value} between dates ${toISODateString(start)} and ${toISODateString(end)}"
+        )
+      case rows => extractSpendReportingResults(rows, start, end, projectNames, aggregations)
     }
   }
 
@@ -608,23 +641,7 @@ class SpendReportingService(
   ): Future[Map[String, Seq[(GoogleProjectId, WorkspaceName)]]] =
     traceFutureWithParent("getBillingWithSpendPermission", parentContext) { childContext =>
       for {
-        ownerWorkspaces <- samDAO.listResourcesWithActions(
-          SamResourceTypeNames.workspace,
-          SamWorkspaceActions.readSpendReport,
-          childContext
-        )
-        groupedWorkspaces <-
-          if (ownerWorkspaces.isEmpty) {
-            Future.successful(Map.empty[RawlsBillingProjectName, Seq[Workspace]])
-          } else {
-            // Ignore non-UUID workspaceIds; these shouldn't happen but if they do, we don't want them
-            workspaceServiceConstructor(childContext).getGCPWorkspacesByBillingProjects(
-              ownerWorkspaces
-                .map(_.getResourceId)
-                .filter(resourceId => Try(UUID.fromString(resourceId)).isSuccess)
-                .toList
-            )
-          }
+        groupedWorkspaces <- getOwnedWorkspaceGoogleProjects(childContext)
         // Only use the BPs we know exist in the DB and are GCP
         spendConfigs <-
           if (groupedWorkspaces.isEmpty) {
