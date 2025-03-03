52ed3337d51eba2a3db8453dee1100b144af5cdf
David An <davidan@broadinstitute.org>
2025-03-03 13:11:59 -0500

    CORE-69: Minor and patch updates - jackson-core to 2.18.3 - scalafmt-core to 3.9.2 - logback-classic to 1.5.17 - workspace-manager-client to 0.254.1177-SNAPSHOT - sentry-logback to 8.3.0 - google-cloud-nio to 0.127.32 (#3209)
    * CORE-69: Update workspace-manager-client from 0.254.1175-SNAPSHOT to 0.254.1177-SNAPSHOT
    
    * CORE-69: Update logback-classic from 1.5.16 to 1.5.17
    
    * CORE-69: Update jackson-core from 2.18.2 to 2.18.3
    
    * CORE-69: Update google-cloud-nio from 0.127.31 to 0.127.32
    
    * CORE-69: Update sentry-logback from 8.2.0 to 8.3.0
    
    * CORE-69: Update sbt from 1.10.7 to 1.10.8
    
    * CORE-69: Update sbt-dependency-tree from 1.10.7 to 1.10.8
    
    * CORE-69: Update scalafmt-core from 3.9.1 to 3.9.2
    
    * revert to sbt 1.10.7
    
    * Scala steward ignores sbt 1.10.8
    
    ---------
    
    Co-authored-by: broadbot <broadbot@broadinstitute.org>
    Co-authored-by: Bria Morgan <calypsomatic@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,14 +63,14 @@ object Dependencies {
   val metricsStatsd: ModuleID =      "com.readytalk"         %  "metrics3-statsd"  % "4.2.0"
 
   val scalaLogging: ModuleID =    "com.typesafe.scala-logging"    %% "scala-logging"        % "3.9.5"
-  val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.18.2"
+  val jacksonCore: ModuleID =     "com.fasterxml.jackson.core"    % "jackson-core"          % "2.18.3"
   val jodaTime: ModuleID =        "joda-time"                     % "joda-time"             % "2.13.1"
   val typesafeConfig: ModuleID =  "com.typesafe"                  % "config"                % "1.4.3"
-  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "8.2.0"
+  val sentryLogback: ModuleID =   "io.sentry"                     % "sentry-logback"        % "8.3.0"
   val webjarsLocator: ModuleID =  "org.webjars"                   % "webjars-locator"       % "0.52"
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val cats: ModuleID =            "org.typelevel"                 %% "cats-core"                 % "2.13.0"
-  val logbackClassic: ModuleID =  "ch.qos.logback"                % "logback-classic"       % "1.5.16"
+  val logbackClassic: ModuleID =  "ch.qos.logback"                % "logback-classic"       % "1.5.17"
   val scalaUri: ModuleID =        "io.lemonlabs"                  %% "scala-uri"            % "4.0.3"
   val scalatest: ModuleID =       "org.scalatest"                 %% "scalatest"            % "3.2.19" % "test"
   val mockito: ModuleID =         "org.scalatestplus"             %% "mockito-4-2"          % "3.2.11.0" % Test
@@ -106,7 +106,7 @@ object Dependencies {
   val workbenchOauth2: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-oauth2" % workbenchOauth2V
   val workbenchOauth2Tests: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-oauth2" % workbenchOauth2V % "test" classifier "tests"
 
-  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.127.31" % "test"
+  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.127.32" % "test"
 
   val workbenchUtil: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-util" % s"0.10-${workbenchLibsHash}"
 
@@ -129,7 +129,7 @@ object Dependencies {
   // "Terra Common Lib" Exclusions:
   def tclExclusions(m: ModuleID): ModuleID = m.excludeAll(excludeSpringBoot, excludeSpringAop, excludeSpringData, excludeSpringFramework, excludeOpenCensus, excludeGoogleFindBugs, excludeBroadWorkbench, excludePostgresql, excludeSnakeyaml, excludeSlf4j)
 
-  val workspaceManager = clientLibExclusions("bio.terra" % "workspace-manager-client" % "0.254.1175-SNAPSHOT")
+  val workspaceManager = clientLibExclusions("bio.terra" % "workspace-manager-client" % "0.254.1177-SNAPSHOT")
   val dataRepo = clientLibExclusions("bio.terra" % "datarepo-jakarta-client" % "1.593.0-SNAPSHOT")
   val resourceBufferService = clientLibExclusions("bio.terra" % "terra-resource-buffer-client" % "0.198.42-SNAPSHOT")
   val billingProfileManager = clientLibExclusions("bio.terra" % "billing-profile-manager-client" % "0.1.605-SNAPSHOT")
