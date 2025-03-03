bca55f3212a6117b874fd655b35189386aa26d5e
Saman Ehsan <sehsan@broadinstitute.org>
2025-03-03 12:48:39 -0500

    Add credit and currency fields

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/WorkspaceSpendReportComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/WorkspaceSpendReportComponent.scala
@@ -1,5 +1,8 @@
 package org.broadinstitute.dsde.rawls.dataaccess.slick
 
+import akka.http.scaladsl.model.StatusCodes
+import org.broadinstitute.dsde.rawls.RawlsExceptionWithErrorReport
+
 import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import org.broadinstitute.dsde.rawls.model._
 import org.broadinstitute.dsde.workbench.model.google.GoogleProject
@@ -14,10 +17,14 @@ case class WorkspaceSpendReportRecord(
   googleProjectId: String,
   reportStartDate: LocalDateTime,
   reportEndDate: LocalDateTime,
+  currency: String,
+  isDataAvailable: Boolean,
   totalCompute: Option[Float],
   totalStorage: Option[Float],
   otherSpend: Option[Float],
-  isDataAvailable: Boolean
+  computeCredits: Option[Float],
+  storageCredits: Option[Float],
+  otherCredits: Option[Float]
 )
 
 object WorkspaceSpendReportRecord {
@@ -28,10 +35,14 @@ object WorkspaceSpendReportRecord {
       workspaceSpendReport.googleProjectId,
       workspaceSpendReport.reportStartDate,
       workspaceSpendReport.reportEndDate,
+      workspaceSpendReport.currency,
+      workspaceSpendReport.isDataAvailable,
       workspaceSpendReport.totalCompute,
       workspaceSpendReport.totalStorage,
       workspaceSpendReport.otherSpend,
-      workspaceSpendReport.isDataAvailable
+      workspaceSpendReport.computeCredits,
+      workspaceSpendReport.storageCredits,
+      workspaceSpendReport.otherCredits
     )
 
   def toWorkspaceSpendReport(record: WorkspaceSpendReportRecord): WorkspaceSpendReport =
@@ -40,10 +51,14 @@ object WorkspaceSpendReportRecord {
       record.googleProjectId,
       record.reportStartDate,
       record.reportEndDate,
+      record.currency,
+      record.isDataAvailable,
       record.totalCompute,
       record.totalStorage,
       record.otherSpend,
-      record.isDataAvailable
+      record.computeCredits,
+      record.storageCredits,
+      record.otherCredits
     )
 
   def convertJodaToJava(dateTimeOpt: Option[DateTime]): LocalDateTime =
@@ -73,26 +88,38 @@ object WorkspaceSpendReportRecord {
         var totalStorage: Option[Float] = None
         var totalCompute: Option[Float] = None
         var otherSpend: Option[Float] = None
+        var storageCredits: Option[Float] = None
+        var computeCredits: Option[Float] = None
+        var otherCredits: Option[Float] = None
         spendData =>
           spendData.subAggregation.get.spendData.foreach { categorySpend =>
             val categoryCost = Some(categorySpend.cost.toFloat)
+            val categoryCredits = Some(categorySpend.credits.toFloat)
             categorySpend.category match {
-              case Some(TerraSpendCategories.Storage) => totalStorage = categoryCost
-              case Some(TerraSpendCategories.Compute) => totalCompute = categoryCost
-              case Some(TerraSpendCategories.Other)   => otherSpend = categoryCost
-              case None => throw new IllegalArgumentException("Spend data has no category") // Does this ever happen?
+              case Some(TerraSpendCategories.Storage) =>
+                totalStorage = categoryCost
+                storageCredits = categoryCredits
+              case Some(TerraSpendCategories.Compute) =>
+                totalCompute = categoryCost
+                computeCredits = categoryCredits
+              case Some(TerraSpendCategories.Other) =>
+                otherSpend = categoryCost
+                otherCredits = categoryCredits
             }
           }
-          // TODO: Store credits and currency (what about aggregation key)?
           WorkspaceSpendReport.newWorkspaceSpendReport(
             spendData.googleProjectId.get.value,
             convertJodaToJava(summary.startTime),
             convertJodaToJava(summary.endTime),
+            summary.currency,
+            isDataAvailable = true,
             totalCompute,
             totalStorage,
             otherSpend,
-            isDataAvailable = true
-          ) // TODO: Handle N/A case
+            computeCredits,
+            storageCredits,
+            otherCredits
+          )
       }
     }
   }
@@ -102,33 +129,42 @@ object WorkspaceSpendReportRecord {
   ): SpendReportingResults = {
     var start: Option[DateTime] = None
     var end: Option[DateTime] = None
-    var total = BigDecimal(0.0)
+    var total_spend = BigDecimal(0.0)
     var total_credits = BigDecimal(0.0)
+    val currency = records.map(_.currency).distinct match {
+      case head :: List() => Currency.getInstance(head)
+      case head :: tail =>
+        throw RawlsExceptionWithErrorReport(
+          StatusCodes.InternalServerError,
+          s"Inconsistent currencies found while aggregating spend data: $head and ${tail.head} cannot be combined"
+        )
+      case List() => throw RawlsExceptionWithErrorReport(StatusCodes.NotFound, "No currencies found for spend data")
+    }
+
     val all = records.map { record =>
-      val currencyString = "$"
+      val currencyString = record.currency
       val currencyCode = Currency.getInstance(currencyString)
       val projectId = record.googleProjectId
 
       def toBigDecimal(cost: Option[Float]): BigDecimal =
         BigDecimal(cost.getOrElse(0.0f)).setScale(currencyCode.getDefaultFractionDigits, RoundingMode.HALF_EVEN)
 
-      val credits = BigDecimal(0.0).toString() // TODO: Store credits per category
       val subAggregation = List(
         SpendReportingForDateRange(
           toBigDecimal(record.otherSpend).toString(),
-          credits,
+          toBigDecimal(record.otherCredits).toString(),
           currencyCode.toString,
           category = Option(TerraSpendCategories.Other)
         ),
         SpendReportingForDateRange(
           toBigDecimal(record.totalStorage).toString(),
-          credits,
+          toBigDecimal(record.storageCredits).toString(),
           currencyCode.toString,
           category = Option(TerraSpendCategories.Storage)
         ),
         SpendReportingForDateRange(
           toBigDecimal(record.totalCompute).toString(),
-          credits,
+          toBigDecimal(record.computeCredits).toString(),
           currencyCode.toString,
           category = Option(TerraSpendCategories.Compute)
         )
@@ -137,24 +173,29 @@ object WorkspaceSpendReportRecord {
       val totalCompute: Option[Float] = record.totalCompute
       val totalStorage: Option[Float] = record.totalStorage
       val otherSpend: Option[Float] = record.otherSpend
-      val total_cost: Option[Float] = for {
-        c1 <- totalCompute
-        c2 <- totalStorage
-        c3 <- otherSpend
-      } yield c1 + c2 + c3
+      val cost: Option[Float] = for {
+        cost1 <- totalCompute
+        cost2 <- totalStorage
+        cost3 <- otherSpend
+      } yield cost1 + cost2 + cost3
 
-//      val credits =
-//        getRoundedNumericValue("other_credits") + getRoundedNumericValue("storage_credits") + getRoundedNumericValue(
-//          "compute_credits"
-//        )
-      total = total + toBigDecimal(total_cost)
-//      total_credits = total_credits + credits
+      val computeCredits: Option[Float] = record.computeCredits
+      val storageCredits: Option[Float] = record.storageCredits
+      val otherCredits: Option[Float] = record.otherCredits
+      val credits: Option[Float] = for {
+        credit1 <- computeCredits
+        credit2 <- storageCredits
+        credit3 <- otherCredits
+      } yield credit1 + credit2 + credit3
+
+      total_spend = total_spend + toBigDecimal(cost)
+      total_credits = total_credits + toBigDecimal(credits)
       start = convertLocalDateTimeToJodaDateTime(record.reportStartDate)
       end = convertLocalDateTimeToJodaDateTime(record.reportEndDate)
       val workspaceTotal = SpendReportingForDateRange(
-        total_cost.toString,
-        credits,
-        currencyCode.toString,
+        total_spend.toString,
+        total_credits.toString,
+        currency.toString,
         start,
         end,
         workspace = projectNames.get(GoogleProjectId(projectId)),
@@ -165,9 +206,9 @@ object WorkspaceSpendReportRecord {
     }
 
     val summary = SpendReportingForDateRange(
-      total.toString,
+      total_spend.toString,
       total_credits.toString,
-      "USD",
+      currency.toString,
       start,
       end
     )
@@ -191,22 +232,34 @@ trait WorkspaceSpendReportComponent {
 
     def reportEndDate = column[LocalDateTime]("REPORT_END_DATE", O.SqlType("DATETIME"))
 
+    def currency = column[String]("CURRENCY", O.Length(100))
+
+    def isDataAvailable = column[Boolean]("IS_DATA_AVAILABLE")
+
     def totalCompute = column[Option[Float]]("TOTAL_COMPUTE", O.SqlType("NUMBER(10,2)"))
 
     def totalStorage = column[Option[Float]]("TOTAL_STORAGE", O.SqlType("NUMBER(10,2)"))
 
     def otherSpend = column[Option[Float]]("OTHER_SPEND", O.SqlType("NUMBER(10,2)"))
 
-    def isDataAvailable = column[Boolean]("IS_DATA_AVAILABLE")
+    def computeCredits = column[Option[Float]]("COMPUTE_CREDITS", O.SqlType("NUMBER(10,2)"))
+
+    def storageCredits = column[Option[Float]]("STORAGE_CREDITS", O.SqlType("NUMBER(10,2)"))
+
+    def otherCredits = column[Option[Float]]("OTHER_CREDITS", O.SqlType("NUMBER(10,2)"))
 
     def * = (id,
              googleProjectId,
              reportStartDate,
              reportEndDate,
+             currency,
+             isDataAvailable,
              totalCompute,
              totalStorage,
              otherSpend,
-             isDataAvailable
+             computeCredits,
+             storageCredits,
+             otherCredits
     ) <> ((WorkspaceSpendReportRecord.apply _).tupled, WorkspaceSpendReportRecord.unapply)
 
   }
--- model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceSpendReportModel.scala
+++ model/src/main/scala/org/broadinstitute/dsde/rawls/model/WorkspaceSpendReportModel.scala
@@ -6,18 +6,26 @@ object WorkspaceSpendReport {
   def newWorkspaceSpendReport(googleProjectId: String,
                               reportStartDate: LocalDateTime,
                               reportEndDate: LocalDateTime,
+                              currency: String,
+                              isDataAvailable: Boolean,
                               totalCompute: Option[Float],
                               totalStorage: Option[Float],
                               otherSpend: Option[Float],
-                              isDataAvailable: Boolean
+                              computeCredits: Option[Float],
+                              storageCredits: Option[Float],
+                              otherCredits: Option[Float],
   ) = WorkspaceSpendReport(-1L,
     googleProjectId,
     reportStartDate,
     reportEndDate,
+    currency,
+    isDataAvailable,
     totalCompute,
     totalStorage,
     otherSpend,
-    isDataAvailable
+    computeCredits,
+    storageCredits,
+    otherCredits
   )
 }
 
@@ -25,8 +33,12 @@ case class WorkspaceSpendReport(id: Long,
                                 googleProjectId: String,
                                 reportStartDate: LocalDateTime,
                                 reportEndDate: LocalDateTime,
+                                currency: String,
+                                isDataAvailable: Boolean,
                                 totalCompute: Option[Float],
                                 totalStorage: Option[Float],
                                 otherSpend: Option[Float],
-                                isDataAvailable: Boolean)
+                                computeCredits: Option[Float],
+                                storageCredits: Option[Float],
+                                otherCredits: Option[Float])
 
