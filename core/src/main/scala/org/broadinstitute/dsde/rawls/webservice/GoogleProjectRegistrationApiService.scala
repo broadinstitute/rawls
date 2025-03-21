2b5e1e47a7c3bf34ffd37ebbb19c79ad026d01a8
Kevin Marete <kevinmmarete@gmail.com>
2025-03-21 00:39:38 -0400

    Updated api service

--- core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/GoogleProjectRegistrationApiService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/GoogleProjectRegistrationApiService.scala
@@ -7,7 +7,12 @@ import akka.http.scaladsl.server.Directives._
 import io.opentelemetry.context.Context
 import org.broadinstitute.dsde.rawls.googleProject.GoogleProjectRegistrationService
 import org.broadinstitute.dsde.rawls.model.GoogleProjectRegistrationJsonSupport$._
-import org.broadinstitute.dsde.rawls.model.{GoogleProjectRegistration, RawlsRequestContext}
+import org.broadinstitute.dsde.rawls.model.{
+  GoogleProjectId,
+  GoogleProjectRegistration,
+  RawlsBillingProjectName,
+  RawlsRequestContext
+}
 import org.broadinstitute.dsde.rawls.openam.UserInfoDirectives
 
 import scala.concurrent.ExecutionContext
@@ -20,22 +25,48 @@ trait GoogleProjectRegistrationApiService extends UserInfoDirectives {
   def googleProjectRegistrationRoutes(otelContext: Context = Context.root()): server.Route =
     requireUserInfo(Option(otelContext)) { userInfo =>
       val ctx = RawlsRequestContext(userInfo, Option(otelContext))
-      path("googleProjects") {
-        put {
-          entity(as[GoogleProjectRegistration]) { entity =>
-            complete {
-              googleProjectRegServiceConstructor(ctx)
-                .registerGoogleProject(
-                  entity
-                )
-                .map {
-                  case None          => StatusCodes.OK -> None
-                  case Some(project) => StatusCodes.Created -> Some(project)
-                }
+      pathPrefix("googleProjects") {
+        pathEndOrSingleSlash {
+          get {
+            parameters(
+              "billingProjectId".as[String]
+            ) { billingProjectId =>
+              complete {
+                googleProjectRegServiceConstructor(ctx)
+                  .getGoogleProjects(Option(RawlsBillingProjectName(billingProjectId)))
+                  .map {
+                    case Some(googleProjectsResponse) => StatusCodes.OK -> Option(googleProjectsResponse)
+                    case None                         => StatusCodes.NotFound -> None
+                  }
+              }
             }
           }
-        }
+          put {
+            entity(as[GoogleProjectRegistration]) { entity =>
+              complete {
+                googleProjectRegServiceConstructor(ctx)
+                  .registerGoogleProject(
+                    entity
+                  )
+                  .map {
+                    case None          => StatusCodes.OK -> None
+                    case Some(project) => StatusCodes.Created -> Some(project)
+                  }
+              }
+            }
+          }
+        } ~
+          path(Segment) { googleProjectId =>
+            get {
+              complete {
+                googleProjectRegServiceConstructor(ctx).getBillingProjectById(GoogleProjectId(googleProjectId)).map {
+                  case Some(googleProjectResponse) => StatusCodes.OK -> Option(googleProjectResponse)
+                  case None                        => StatusCodes.NotFound -> None
+                }
+              }
+            }
+
+          }
       }
     }
-
 }
