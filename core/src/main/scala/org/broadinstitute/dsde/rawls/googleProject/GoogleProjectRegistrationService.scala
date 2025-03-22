360b1d1f8f7691dc6b608d0164593eb7a2abac2c
Kevin Marete <kevinmmarete@gmail.com>
2025-03-22 00:05:26 -0400

    Updated Service

--- core/src/main/scala/org/broadinstitute/dsde/rawls/googleProject/GoogleProjectRegistrationService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/googleProject/GoogleProjectRegistrationService.scala
@@ -6,7 +6,9 @@ import org.broadinstitute.dsde.rawls.billing.BillingRepository
 import org.broadinstitute.dsde.rawls.dataaccess.{GoogleServicesDAO, SamDAO}
 import org.broadinstitute.dsde.rawls.model.{
   ErrorReport,
+  GoogleProjectId,
   GoogleProjectRegistration,
+  RawlsBillingProjectName,
   RawlsRequestContext,
   SamBillingProjectActions,
   SamGoogleProjectActions,
@@ -109,4 +111,69 @@ class GoogleProjectRegistrationService(protected val ctx: RawlsRequestContext,
         case None => Future.successful(None)
       }
     } yield finalResult
+
+  def getGoogleProjects(billingProjectName: Option[RawlsBillingProjectName]): Future[Seq[GoogleProjectRegistration]] = {
+    // Retrieve accessible Google projects for the user
+    val accessibleGoogleProjectsFuture = samDAO
+      .listUserResources(SamResourceTypeNames.googleProject, ctx)
+      .map(_.map(resource => GoogleProjectId(resource.resourceId)).toSet)
+
+    accessibleGoogleProjectsFuture.flatMap { accessibleGoogleProjects =>
+      // Fetch registrations for accessible projects
+      googleProjectRegRepo.getGoogleProjectRegistrations(accessibleGoogleProjects).map { registrations =>
+        // Filter registrations by billing project name if provided
+        val filteredRegistrations = billingProjectName
+          .map(name => registrations.filter(_.billingProjectId == name))
+          .getOrElse(registrations)
+        // If billing project name is specified and no registrations match, return empty sequence
+        if (billingProjectName.isDefined && filteredRegistrations.isEmpty) {
+          Seq.empty
+        } else {
+          // Combine registered and unregistered projects
+          val registeredProjectIds = filteredRegistrations.map(_.googleProjectId).toSet
+          val unregisteredProjects = accessibleGoogleProjects.diff(registeredProjectIds).map { projectId =>
+            GoogleProjectRegistration(
+              googleProjectId = projectId,
+              billingAccount = None,
+              message = None,
+              billingProjectId = RawlsBillingProjectName("")
+            )
+          }
+          // Return the combined result
+          filteredRegistrations ++ unregisteredProjects
+        }
+      }
+    }
+  }
+
+  def getGoogleProjectById(googleProjectId: GoogleProjectId): Future[Option[GoogleProjectRegistration]] =
+    // Check if the user has the readPolicies action on the specified Google project
+    samDAO
+      .userHasAction(
+        SamResourceTypeNames.googleProject,
+        googleProjectId.value,
+        SamGoogleProjectActions.readPolicies,
+        ctx
+      )
+      .flatMap { hasAccess =>
+        if (hasAccess) {
+          // If the user has access, fetch the Google project registration
+          googleProjectRegRepo.getGoogleProjectRegistration(googleProjectId).map {
+            case Some(project) => Some(project)
+            case None          =>
+              // If no registration is found, create a default GoogleProjectRegistration
+              Some(
+                GoogleProjectRegistration(
+                  googleProjectId = googleProjectId,
+                  billingAccount = None,
+                  message = None,
+                  billingProjectId = RawlsBillingProjectName("")
+                )
+              )
+          }
+        } else {
+          // If the user does not have access, return None
+          Future.successful(None)
+        }
+      }
 }
