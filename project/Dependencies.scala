2b668a972ab6ec7b0ed4be34a255d241f0d3e674
broadbot <broadbot@broadinstitute.org>
2025-03-24 12:21:23 +0000

    CORE-69: Update scala-uri from 4.0.3 to 4.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   val commonsJEXL: ModuleID =     "org.apache.commons"            % "commons-jexl"          % "2.1.1"
   val cats: ModuleID =            "org.typelevel"                 %% "cats-core"                 % "2.13.0"
   val logbackClassic: ModuleID =  "ch.qos.logback"                % "logback-classic"       % "1.5.18"
-  val scalaUri: ModuleID =        "io.lemonlabs"                  %% "scala-uri"            % "4.0.3"
+  val scalaUri: ModuleID =        "com.indoorvivants"                  %% "scala-uri"            % "4.2.0"
   val scalatest: ModuleID =       "org.scalatest"                 %% "scalatest"            % "3.2.19" % "test"
   val mockito: ModuleID =         "org.scalatestplus"             %% "mockito-4-2"          % "3.2.11.0" % Test
   val mockserverNetty: ModuleID = "org.mock-server"               % "mockserver-netty"      % "5.15.0" % "test"
