96f471adeaa4f5b7aac39d1f093e102862fa28a9
Kevin Marete <kevinmmarete@gmail.com>
2025-03-27 18:43:00 -0400

    Remove unnecessary mock tests

--- core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/GoogleProjectRegistrationApiServiceSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/webservice/GoogleProjectRegistrationApiServiceSpec.scala
@@ -5,7 +5,6 @@ import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import akka.http.scaladsl.server.Directive1
 import akka.http.scaladsl.server.Directives.provide
 import io.opentelemetry.context.Context
-import org.broadinstitute.dsde.rawls.RawlsExceptionWithErrorReport
 import org.broadinstitute.dsde.rawls.dataaccess.SamDAO
 import org.broadinstitute.dsde.rawls.googleProject.{
   GoogleProjectRegistrationRepository,
@@ -13,8 +12,6 @@ import org.broadinstitute.dsde.rawls.googleProject.{
 }
 import org.broadinstitute.dsde.rawls.model.GoogleProjectRegistrationJsonSupport$._
 import org.broadinstitute.dsde.rawls.model._
-import org.mockito.ArgumentMatchers
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import org.scalatestplus.mockito.MockitoSugar
 
@@ -46,9 +43,7 @@ class GoogleProjectRegistrationApiServiceSpec
       RawlsBillingProjectName("test-billing-project")
     )
 
-    when(
-      mockGoogleProjectRegService.getGoogleProjects(Some(RawlsBillingProjectName("test-billing-project")), 10, 0)
-    )
+    when(mockGoogleProjectRegService.getGoogleProjects(Some(RawlsBillingProjectName("test-billing-project")), 10, 0))
       .thenReturn(Future.successful(Seq(googleProjectRegistration)))
 
     Get(
@@ -56,47 +51,6 @@ class GoogleProjectRegistrationApiServiceSpec
     ) ~> googleProjectRegistrationRoutes() ~> check {
       status shouldEqual StatusCodes.OK
       responseAs[Seq[GoogleProjectRegistration]] shouldEqual Seq(googleProjectRegistration)
-    }
-  }
-
-  it should "return NotFound if billing project name is set but no matches are found" in {
-    when(
-      mockGoogleProjectRegService.getGoogleProjects(Some(RawlsBillingProjectName("non-existent-billing-project")),
-                                                    10,
-                                                    0
-      )
-    )
-      .thenReturn(Future.successful(Seq.empty))
-
-    Get(
-      "/googleProjects?billingProjectId=non-existent-billing-project&pageSize=10&offset=0"
-    ) ~> googleProjectRegistrationRoutes() ~> check {
-      status shouldEqual StatusCodes.OK
-      responseAs[Seq[GoogleProjectRegistration]] shouldEqual Seq.empty
-    }
-  }
-
-  it should "retrieve all Google projects if no billing project name is provided" in {
-    val googleProjectRegistration1 = GoogleProjectRegistration(
-      GoogleProjectId("test-project-id-1"),
-      Some(RawlsBillingAccountName("billing-account-1")),
-      None,
-      RawlsBillingProjectName("test-billing-project-1")
-    )
-
-    val googleProjectRegistration2 = GoogleProjectRegistration(
-      GoogleProjectId("test-project-id-2"),
-      Some(RawlsBillingAccountName("billing-account-2")),
-      None,
-      RawlsBillingProjectName("test-billing-project-2")
-    )
-
-    when(mockGoogleProjectRegService.getGoogleProjects(None, 10, 0))
-      .thenReturn(Future.successful(Seq(googleProjectRegistration1, googleProjectRegistration2)))
-
-    Get("/googleProjects?pageSize=10&offset=0") ~> googleProjectRegistrationRoutes() ~> check {
-      status shouldEqual StatusCodes.OK
-      responseAs[Seq[GoogleProjectRegistration]] shouldEqual Seq(googleProjectRegistration1, googleProjectRegistration2)
     }
   }
 
