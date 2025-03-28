3d8bba33b0c3b57485ccd63e5c6d9d5101b19692
David An <davidan@broadinstitute.org>
2025-03-28 15:49:47 -0400

    update azure-identity, which updates json-smart

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -112,7 +112,7 @@ object Dependencies {
 
   val circeYAML: ModuleID = "io.circe" %% "circe-yaml" % "1.15.0"
 
-  val azureIdentity: ModuleID = "com.azure" % "azure-identity" % "1.15.3"
+  val azureIdentity: ModuleID = "com.azure" % "azure-identity" % "1.15.4"
   val azureCoreManagement: ModuleID = "com.azure" % "azure-core-management" % "1.16.2"
 
   def excludeOpenTelemetry = ExclusionRule("io.opentelemetry.instrumentation")
