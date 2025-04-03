80dbb7034a5f318ea010e8327fda078801c5c972
LizBaldo <baldo@broadinstitute.org>
2025-04-03 15:07:41 -0400

    always update error message + cleanup array type

--- core/src/main/scala/org/broadinstitute/dsde/rawls/expressions/parser/antlr/ErrorThrowingListener.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/expressions/parser/antlr/ErrorThrowingListener.scala
@@ -29,11 +29,8 @@ class ErrorThrowingListener extends BaseErrorListener {
                            msg: String,
                            e: RecognitionException
   ): Unit = {
-    val errorMsg = if (msg.startsWith("mismatched input")) {
-      s"Error while parsing the expression. The value you entered is not in the correct format for this data type. For a string, format input as \"value\". For an array, format input as {\"value1\", \"value2\"}. For a file, format input as \"gs://\". For a boolean, format input as `true` or `false`."
-    } else {
-      s"Error while parsing the expression. Offending symbol is on line $line at position $charPositionInLine. Error: $msg"
-    }
+    val errorMsg =
+      s"Error while parsing the expression. The value you entered is not in the correct format for this data type. For a string, format input as \"value\". For an array, format input as [value1, value2]. For a file, format input as \"gs://\". For a boolean, format input as `true` or `false`. Offending symbol is on line $line at position $charPositionInLine. Error: $msg"
     throw new RawlsException(errorMsg, new ParseCancellationException(errorMsg, e))
   }
   /*_*/
