5d2c739d3d78a8113cb1b6fbce894f08659c5241
David An <davidan@broadinstitute.org>
2025-04-30 12:25:11 -0400

    CORE-364: quicksilver migration for engineers (#3283)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/Boot.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/Boot.scala
@@ -479,14 +479,6 @@ object Boot extends IOApp with LazyLogging {
         new WorkspaceSettingRepository(slickDataSource)
       )
 
-      val entityServiceConstructor: RawlsRequestContext => EntityService = EntityService.constructor(
-        slickDataSource,
-        samDAO,
-        workbenchMetricBaseName = metricsPrefix,
-        entityManager,
-        appConfigManager.conf.getInt("entities.pageSizeLimit")
-      )
-
       val snapshotServiceConstructor: RawlsRequestContext => SnapshotService = SnapshotService.constructor(
         slickDataSource,
         samDAO,
@@ -556,6 +548,15 @@ object Boot extends IOApp with LazyLogging {
                                     samDAO,
                                     appDependencies.googleStorageService
         )(implicitly, IORuntime.global)
+
+      val entityServiceConstructor: RawlsRequestContext => EntityService = EntityService.constructor(
+        slickDataSource,
+        samDAO,
+        workbenchMetricBaseName = metricsPrefix,
+        entityManager,
+        appConfigManager.conf.getInt("entities.pageSizeLimit"),
+        Option(workspaceSettingServiceConstructor)
+      )
 
       val googleProjectRegistrationServiceConstructor: RawlsRequestContext => GoogleProjectRegistrationService =
         new GoogleProjectRegistrationService(_, samDAO, googleProjectRegRepo, billingRepository, gcsDAO)
