460fd09c511e0fb1e0f6c71695117ec59b21e8d1
Marc Talbott <mtalbott@broadinstitute.org>
2025-05-01 15:08:06 -0400

    [CORE-440] v3 Snapshot API (#3285)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/model/TpsModel.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/model/TpsModel.scala
@@ -19,5 +19,11 @@ object TpsModel {
       override val additionalDataKey: String =
         "" // protected-data policies do not pass additional data and therefore do not need keys
     }
+
+    // Rawls doesn't set up this policy, but it can cause conflicts when combining PAOs. This is here so we can check for its existence and try to avoid conflicts.
+    case object RegionConstraint extends TpsPolicy {
+      override val name: String = "region-constraint"
+      override val additionalDataKey: String = "region-name"
+    }
   }
 }
