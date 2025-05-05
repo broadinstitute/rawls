3de6bc8392ca882be39c779bda7b1fa16ea25ee7
Saman Ehsan <sehsan@broadinstitute.org>
2025-05-05 14:04:51 -0400

    Add update workspace billing endpoint

--- core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceV2.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/webservice/WorkspaceApiServiceV2.scala
@@ -127,6 +127,19 @@ trait WorkspaceApiServiceV2 extends UserInfoDirectives {
                   }
                 }
               }
+            } ~
+            pathPrefix("billingProject") {
+              pathEndOrSingleSlash {
+                patch {
+                  entity(as[WorkspaceRequestUpdateBilling]) { updateRequest =>
+                    complete {
+                      workspaceServiceConstructor(ctx)
+                        .updateWorkspaceBillingProject(workspaceName, updateRequest.newBillingProjectName)
+                        .map(_ => StatusCodes.NoContent)
+                    }
+                  }
+                }
+              }
             }
         } ~
           pathPrefix("bucketMigration") {
