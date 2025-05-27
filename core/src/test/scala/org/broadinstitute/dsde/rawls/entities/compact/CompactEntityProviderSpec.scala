ea475211ca5014e19f0e025f17073e947b969b84
Saman Ehsan <sehsan@broadinstitute.org>
2025-05-27 18:11:43 -0400

    Add copyEntity test with hard conflict

--- core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderSpec.scala
@@ -11,7 +11,7 @@ import org.broadinstitute.dsde.rawls.entities.compact.entityQuery.CountAndSource
 import org.broadinstitute.dsde.rawls.entities.exceptions._
 import org.broadinstitute.dsde.rawls.model.AgoraEntityType.EntityType
 import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{AddUpdateAttribute, AttributeUpdateOperation, EntityUpdateDefinition}
-import org.broadinstitute.dsde.rawls.model.{Attributable, AttributeEntityReference, AttributeEntityReferenceList, AttributeName, AttributeNumber, AttributeRename, AttributeString, Entity, EntityPointer, EntityQuery, EntityQueryResultMetadata, EntityTypeMetadata, EntityTypeRename, RawlsRequestContext, RawlsUserEmail, RawlsUserSubjectId, SortDirections, UserInfo, Workspace}
+import org.broadinstitute.dsde.rawls.model.{Attributable, AttributeEntityReference, AttributeEntityReferenceList, AttributeName, AttributeNumber, AttributeRename, AttributeString, Entity, EntityHardConflict, EntityPointer, EntityQuery, EntityQueryResultMetadata, EntityTypeMetadata, EntityTypeRename, RawlsRequestContext, RawlsUserEmail, RawlsUserSubjectId, SortDirections, UserInfo, Workspace}
 import org.broadinstitute.dsde.rawls.util.{AttributeSupport, MockitoTestUtils}
 import org.joda.time.DateTime
 import org.mockito.ArgumentMatchers.{any, anyString, eq => mockitoEq}
@@ -1475,6 +1475,33 @@ class CompactEntityProviderSpec
     verify(mockQuery, times(1)).copyEntitiesToNewWorkspace(sourceWorkspace.workspaceIdAsUUID, destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
   }
 
+  it should "not copy entities when a hard conflict is present" in {
+    val mockQuery = mock[slickDataSource.dataAccess.compactEntityQuery.type]
+    val mockRepository = mock[CompactEntityRepository]
+    when(mockRepository.queries).thenReturn(mockQuery)
+    when(mockRepository.dataSource).thenReturn(slickDataSource)
+    when[ReadWriteAction[Int]](mockRepository.updateLastModified(any[UUID]()))
+      .thenReturn(DBIO.successful(1))
+
+    val sourceWorkspace = Workspace("namespace", "sourceWorkspace", UUID.randomUUID.toString, "source-bucket", None, new DateTime(), new DateTime(), "creator", Map.empty)
+    val destWorkspace = Workspace("namespace", "destWorkspace", UUID.randomUUID.toString, "dest-bucket", None, new DateTime(), new DateTime(), "creator", Map.empty)
+
+    val entityType = "sampleType"
+    val entityNames = Seq("entity1")
+
+    val mockEntityRefs = entityNames.map(name => EntityPointer(entityType, name))
+    when(mockQuery.getEntityRefs(any[UUID], any[Set[EntityPointer]])).thenReturn(DBIO.successful(Seq(CompactEntityRefRecord(1, "entity1", entityType))))
+    val provider = providerWithMocks(mockRepository, EntityRequestArguments(sourceWorkspace, defaultRequestContext))
+
+    val result = Await.result(provider.copyEntities(sourceWorkspace, destWorkspace, entityType, entityNames, linkExistingEntities = false, defaultRequestContext), atMost)
+    result.entitiesCopied shouldBe empty
+    result.hardConflicts shouldBe Seq(EntityHardConflict(entityType, "entity1"))
+    result.softConflicts shouldBe empty
+
+    verify(mockQuery, times(1)).getEntityRefs(destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
+    verify(mockQuery, times(0)).copyEntitiesToNewWorkspace(sourceWorkspace.workspaceIdAsUUID, destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
+
+  }
 
   // ====================================================================================================
   //  helper methods
