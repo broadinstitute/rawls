7f5d761ccd68f394f5b17be804c9756e3b026525
Saman Ehsan <sehsan@broadinstitute.org>
2025-05-27 18:29:50 -0400

    Add copyEntity test for soft conflict and linkExistingEntities = false

--- core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderSpec.scala
@@ -11,7 +11,7 @@ import org.broadinstitute.dsde.rawls.entities.compact.entityQuery.CountAndSource
 import org.broadinstitute.dsde.rawls.entities.exceptions._
 import org.broadinstitute.dsde.rawls.model.AgoraEntityType.EntityType
 import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{AddUpdateAttribute, AttributeUpdateOperation, EntityUpdateDefinition}
-import org.broadinstitute.dsde.rawls.model.{Attributable, AttributeEntityReference, AttributeEntityReferenceList, AttributeName, AttributeNumber, AttributeRename, AttributeString, Entity, EntityHardConflict, EntityPointer, EntityQuery, EntityQueryResultMetadata, EntityTypeMetadata, EntityTypeRename, RawlsRequestContext, RawlsUserEmail, RawlsUserSubjectId, SortDirections, UserInfo, Workspace}
+import org.broadinstitute.dsde.rawls.model.{Attributable, AttributeEntityReference, AttributeEntityReferenceList, AttributeName, AttributeNumber, AttributeRename, AttributeString, Entity, EntityHardConflict, EntityPointer, EntityQuery, EntityQueryResultMetadata, EntitySoftConflict, EntityTypeMetadata, EntityTypeRename, RawlsRequestContext, RawlsUserEmail, RawlsUserSubjectId, SortDirections, UserInfo, Workspace}
 import org.broadinstitute.dsde.rawls.util.{AttributeSupport, MockitoTestUtils}
 import org.joda.time.DateTime
 import org.mockito.ArgumentMatchers.{any, anyString, eq => mockitoEq}
@@ -1501,6 +1501,37 @@ class CompactEntityProviderSpec
     verify(mockQuery, times(1)).getEntityRefs(destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
     verify(mockQuery, times(0)).copyEntitiesToNewWorkspace(sourceWorkspace.workspaceIdAsUUID, destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
 
+  }
+
+  it should "not copy entities when soft conflicts are present and linkExistingEntities is false" in {
+    val mockQuery = mock[slickDataSource.dataAccess.compactEntityQuery.type]
+    val mockRepository = mock[CompactEntityRepository]
+    when(mockRepository.queries).thenReturn(mockQuery)
+    when(mockRepository.dataSource).thenReturn(slickDataSource)
+    when[ReadWriteAction[Int]](mockRepository.updateLastModified(any[UUID]()))
+      .thenReturn(DBIO.successful(1))
+
+    val sourceWorkspace = Workspace("namespace", "sourceWorkspace", UUID.randomUUID.toString, "source-bucket", None, new DateTime(), new DateTime(), "creator", Map.empty)
+    val destWorkspace = Workspace("namespace", "destWorkspace", UUID.randomUUID.toString, "dest-bucket", None, new DateTime(), new DateTime(), "creator", Map.empty)
+
+    val entityType = "sampleType"
+    val entityNames = Seq("entity1")
+
+    val mockEntityRefs = entityNames.map(name => EntityPointer(entityType, name))
+    when(mockQuery.getEntityRefs(destWorkspace.workspaceIdAsUUID, Set(EntityPointer(entityType, "entity1")))).thenReturn(DBIO.successful(Seq()))
+    when(mockQuery.getEntityRefs(destWorkspace.workspaceIdAsUUID, Set(EntityPointer(entityType, "entity2")))).thenReturn(DBIO.successful(Seq(CompactEntityRefRecord(2, "entity2", entityType))))
+    when(mockQuery.getEntitySubtrees(sourceWorkspace.workspaceIdAsUUID, entityType, entityNames.toSet))
+      .thenReturn(DBIO.successful(Map.from(mockEntityRefs.map(ref => ref -> Set(EntityPointer(entityType, "entity2"))))))
+
+    val provider = providerWithMocks(mockRepository, EntityRequestArguments(sourceWorkspace, defaultRequestContext))
+    val result = Await.result(provider.copyEntities(sourceWorkspace, destWorkspace, entityType, entityNames, linkExistingEntities = false, defaultRequestContext), atMost)
+
+    result.entitiesCopied shouldBe empty
+    result.hardConflicts shouldBe empty
+    result.softConflicts shouldBe Seq(EntitySoftConflict(entityType, "entity1", Seq(EntitySoftConflict(entityType, "entity2", Seq.empty))))
+
+    verify(mockQuery, times(1)).getEntityRefs(destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
+    verify(mockQuery, times(0)).copyEntitiesToNewWorkspace(sourceWorkspace.workspaceIdAsUUID, destWorkspace.workspaceIdAsUUID, mockEntityRefs.toSet)
   }
 
   // ====================================================================================================
