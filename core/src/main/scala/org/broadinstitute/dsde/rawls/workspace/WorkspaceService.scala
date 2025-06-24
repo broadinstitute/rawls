6482c702bf5f83fb446f25b71119ff4a10e06dab
David An <davidan@broadinstitute.org>
2025-06-24 13:54:42 -0400

    centralize logic

--- core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/workspace/WorkspaceService.scala
@@ -1020,9 +1020,7 @@ class WorkspaceService(
             newAttrs = sourceWorkspaceContext.attributes ++ destWorkspaceRequest.attributes
             destWorkspaceContext <- traceDBIOWithParent("createNewWorkspaceContext (cloneWorkspace)", ctx) { s =>
               val forceEnhancedBucketMonitoring =
-                destWorkspaceRequest.enhancedBucketLogging.exists(identity) || sourceWorkspace.bucketName.startsWith(
-                  s"${config.workspaceBucketNamePrefix}-secure"
-                )
+                destWorkspaceRequest.enhancedBucketLogging.exists(identity) || isBucketSecure(sourceWorkspace)
               createNewWorkspaceContext(
                 destWorkspaceRequest.copy(authorizationDomain = Option(newAuthDomain),
                                           attributes = newAttrs,
@@ -2519,6 +2517,9 @@ class WorkspaceService(
                                                      Identity.group(workspaceProjectOwnerEmail.value)
       )
     } yield ()
+
+  def isBucketSecure(workspace: Workspace): Boolean =
+    workspace.bucketName.startsWith(s"${config.workspaceBucketNamePrefix}-secure")
 }
 
 class InvalidWorkspaceAclUpdateException(errorReport: ErrorReport) extends RawlsExceptionWithErrorReport(errorReport)
