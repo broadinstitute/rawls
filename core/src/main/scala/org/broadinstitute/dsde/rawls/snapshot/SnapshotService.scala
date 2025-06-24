9ed1ac9628b66f12f61b1007ac54f27e4993dfde
David An <davidan@broadinstitute.org>
2025-06-24 13:54:42 -0400

    centralize logic

--- core/src/main/scala/org/broadinstitute/dsde/rawls/snapshot/SnapshotService.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/snapshot/SnapshotService.scala
@@ -224,8 +224,9 @@ class SnapshotService(protected val ctx: RawlsRequestContext,
 
       // if any snapshots contain protected data, the workspace must be protected
       _ = if (
-        snapshotPaos.exists(PolicyUtilities.containsPolicy(_, TpsPolicies.ProtectedData)) && !rawlsWorkspace.bucketName
-          .startsWith("fc-secure")
+        snapshotPaos.exists(
+          PolicyUtilities.containsPolicy(_, TpsPolicies.ProtectedData)
+        ) && !workspaceServiceConstructor(ctx).isBucketSecure(rawlsWorkspace)
       ) {
         throw new ProtectedDataException("Unable to add protected snapshot to unprotected workspace.")
       }
