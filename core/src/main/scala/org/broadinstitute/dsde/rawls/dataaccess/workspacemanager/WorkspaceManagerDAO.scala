172db5cc30a6485f55331b0f240e0cad630bd1d5
David An <davidan@broadinstitute.org>
2025-06-26 16:25:41 -0400

    remove listWorkspace WSM implementations

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/workspacemanager/WorkspaceManagerDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/workspacemanager/WorkspaceManagerDAO.scala
@@ -13,7 +13,6 @@ trait WorkspaceManagerDAO {
   val errorReportSource = ErrorReportSource("WorkspaceManager")
 
   def getWorkspace(workspaceId: UUID, ctx: RawlsRequestContext): WorkspaceDescription
-  def listWorkspaces(ctx: RawlsRequestContext, batchSize: Int = 100): List[WorkspaceDescription]
   def createWorkspace(workspaceId: UUID,
                       workspaceType: WorkspaceType,
                       policyInputs: Option[WsmPolicyInputs],
