a2d06f0cab757b671eba7f4169b97a3d0593c376
David An <davidan@broadinstitute.org>
2025-07-01 13:37:41 -0400

    optimize calls to Sam in WorkspaceSupport

--- core/src/main/scala/org/broadinstitute/dsde/rawls/util/WorkspaceSupport.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/util/WorkspaceSupport.scala
@@ -17,6 +17,7 @@ import org.broadinstitute.dsde.rawls.model.{
   WorkspaceName
 }
 import org.broadinstitute.dsde.rawls.workspace.WorkspaceRepository
+import org.broadinstitute.dsde.workbench.client.sam.ApiException
 
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
@@ -50,6 +51,11 @@ trait WorkspaceSupport {
             else Future.failed(NoSuchWorkspaceException(workspace.toWorkspaceName))
           }
         }
+    } recoverWith {
+      case apiException: ApiException
+          if apiException.getCode == StatusCodes.Unauthorized.intValue
+            && apiException.getMessage.equals("User is disabled.") =>
+        Future.failed(new UserDisabledException(StatusCodes.Unauthorized, "Unauthorized"))
     }
 
   def accessCheck(workspaceId: String, requiredAction: SamResourceAction): Future[Unit] =
@@ -125,7 +131,7 @@ trait WorkspaceSupport {
     ignoreLock: Boolean = false
   ): Future[Workspace] =
     for {
-      workspace <- getV2WorkspaceContext(workspaceName, attributeSpecs)
+      workspace <- getV2WorkspaceOrThrow(workspaceName, attributeSpecs)
       _ <- accessCheck(workspace, requiredAction)
       _ <- if (ignoreLock) Future.successful() else checkLock(workspace, requiredAction)
     } yield workspace
@@ -136,7 +142,7 @@ trait WorkspaceSupport {
     attributeSpecs: Option[WorkspaceAttributeSpecs] = None
   ): Future[Workspace] =
     for {
-      workspace <- getV2WorkspaceContextByWorkspaceId(workspaceId, attributeSpecs)
+      workspace <- getV2WorkspaceByWorkspaceIdOrThrow(workspaceId, attributeSpecs)
       _ <- accessCheck(workspaceId, requiredAction)
       _ <- checkLock(workspace, requiredAction)
     } yield workspace
@@ -145,25 +151,41 @@ trait WorkspaceSupport {
                                          attributeSpecs: Option[WorkspaceAttributeSpecs] = None
   ): Future[Workspace] = for {
     _ <- userEnabledCheck
-    workspaceUuid = Try(UUID.fromString(workspaceId)) match {
-      case Success(uid) => uid
-      case Failure(_) =>
-        throw new RawlsExceptionWithErrorReport(errorReport = ErrorReport(StatusCodes.BadRequest, "invalid UUID"))
-    }
-    workspaceContext <- workspaceRepository.getWorkspace(workspaceUuid, attributeSpecs)
-  } yield workspaceContext match {
-    case Some(workspace) => workspace
-    case None            => throw NoSuchWorkspaceException(workspaceId)
-  }
+    workspaceContext <- getV2WorkspaceByWorkspaceIdOrThrow(workspaceId, attributeSpecs)
+  } yield workspaceContext
 
   def getV2WorkspaceContext(workspaceName: WorkspaceName,
                             attributeSpecs: Option[WorkspaceAttributeSpecs] = None
   ): Future[Workspace] = for {
     _ <- userEnabledCheck
-    workspaceContext <- workspaceRepository.getWorkspace(workspaceName, attributeSpecs)
-  } yield workspaceContext match {
-    case Some(workspace) => workspace
-    case None            => throw NoSuchWorkspaceException(workspaceName)
-  }
+    workspaceContext <- getV2WorkspaceOrThrow(workspaceName, attributeSpecs)
+  } yield workspaceContext
+
+  private def getV2WorkspaceOrThrow(workspaceName: WorkspaceName,
+                                    attributeSpecs: Option[WorkspaceAttributeSpecs] = None
+  ): Future[Workspace] =
+    for {
+      workspaceContext <- workspaceRepository.getWorkspace(workspaceName, attributeSpecs)
+    } yield workspaceContext match {
+      case Some(workspace) => workspace
+      case None            => throw NoSuchWorkspaceException(workspaceName)
+    }
+
+  private def getV2WorkspaceByWorkspaceIdOrThrow(workspaceId: String,
+                                                 attributeSpecs: Option[WorkspaceAttributeSpecs] = None
+  ): Future[Workspace] =
+    Try(UUID.fromString(workspaceId)) match {
+      case Success(workspaceUuid) =>
+        for {
+          workspaceContext <- workspaceRepository.getWorkspace(workspaceUuid, attributeSpecs)
+        } yield workspaceContext match {
+          case Some(workspace) => workspace
+          case None            => throw NoSuchWorkspaceException(workspaceId)
+        }
+      case Failure(_) =>
+        Future.failed(
+          new RawlsExceptionWithErrorReport(errorReport = ErrorReport(StatusCodes.BadRequest, "invalid UUID"))
+        )
+    }
 
 }
