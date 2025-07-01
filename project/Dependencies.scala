7a737f44d6df6c49d4ec5e9d93a14bd7a1507a4b
broadbot <broadbot@broadinstitute.org>
2025-07-01 12:26:57 +0000

    CORE-69: Update azure-identity from 1.16.1 to 1.16.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -114,7 +114,7 @@ object Dependencies {
 
   val circeYAML: ModuleID = "io.circe" %% "circe-yaml" % "1.15.0"
 
-  val azureIdentity: ModuleID = "com.azure" % "azure-identity" % "1.16.1"
+  val azureIdentity: ModuleID = "com.azure" % "azure-identity" % "1.16.2"
   val azureCoreManagement: ModuleID = "com.azure" % "azure-core-management" % "1.18.1"
 
   def excludeOpenTelemetry = ExclusionRule("io.opentelemetry.instrumentation")
