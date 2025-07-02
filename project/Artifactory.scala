ce569e90d77f48e032fd2053b1f0bca43c7d4032
Uchechukwu Ngwudike <ungwudik@broadinstitute.org>
2025-07-02 15:07:17 -0500

    test

--- project/Artifactory.scala
+++ project/Artifactory.scala
@@ -2,4 +2,5 @@ object Artifactory {
   val artifactoryHost = "broadinstitute.jfrog.io"
   val artifactory = s"https://$artifactoryHost/broadinstitute/"
   val googleArtifactoryRegistry = "https://us-central1-maven.pkg.dev/dsp-artifact-registry/"
+  // test
 }
