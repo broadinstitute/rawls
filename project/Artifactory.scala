263ba45fa92f112b40642737fe0f910460ac8427
Uchechukwu Ngwudike <ungwudik@broadinstitute.org>
2025-07-02 15:15:53 -0500

    undo test

--- project/Artifactory.scala
+++ project/Artifactory.scala
@@ -2,5 +2,4 @@ object Artifactory {
   val artifactoryHost = "broadinstitute.jfrog.io"
   val artifactory = s"https://$artifactoryHost/broadinstitute/"
   val googleArtifactoryRegistry = "https://us-central1-maven.pkg.dev/dsp-artifact-registry/"
-  // test
 }
