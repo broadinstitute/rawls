2f814939816381be38ed4a45f577039bc42c808f
David An <davidan@broadinstitute.org>
2025-07-03 09:10:24 -0400

    delete WorkspaceResourceMonitor and its runners

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/workspace/runners/deletion/actions/WorkspaceDeletionActionFailureException.scala
+++
@@ -1,5 +0,0 @@
-package org.broadinstitute.dsde.rawls.monitor.workspace.runners.deletion.actions
-
-import org.broadinstitute.dsde.rawls.RawlsException
-
-case class WorkspaceDeletionActionFailureException(message: String) extends RawlsException(message) {}
