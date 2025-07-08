80cbe3e79b7ffe0635ed9c1d0d39a9b5740cd2e8
David An <davidan@broadinstitute.org>
2025-07-08 13:22:36 -0400

    remove CloudPlatform.apply(WSMCloudPlatform)

--- core/src/main/scala/org/broadinstitute/dsde/rawls/model/CloudPlatform.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/model/CloudPlatform.scala
@@ -1,7 +1,6 @@
 package org.broadinstitute.dsde.rawls.model
 
 import bio.terra.profile.model.{CloudPlatform => BPMCloudPlatform, ProfileModel}
-import bio.terra.workspace.model.{CloudPlatform => WSMCloudPlatform}
 import bio.terra.datarepo.model.{CloudPlatform => DRCloudPlatform}
 
 object CloudPlatform extends Enumeration {
@@ -13,12 +12,6 @@ object CloudPlatform extends Enumeration {
   def apply(platform: BPMCloudPlatform): CloudPlatform = platform match {
     case BPMCloudPlatform.GCP   => GCP
     case BPMCloudPlatform.AZURE => AZURE
-  }
-
-  def apply(platform: WSMCloudPlatform): CloudPlatform = platform match {
-    case WSMCloudPlatform.GCP   => GCP
-    case WSMCloudPlatform.AZURE => AZURE
-    case WSMCloudPlatform.AWS   => UNKNOWN // AWS exists only in WorkspaceManager at the moment
   }
 
   def apply(platform: DRCloudPlatform): CloudPlatform = platform match {
