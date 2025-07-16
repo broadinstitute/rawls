79d7604a8bfffedd4a4f5f1bdc4b6685c870c172
David An <davidan@broadinstitute.org>
2025-07-16 12:21:55 -0400

    revamp metrics names

--- core/src/main/scala/org/broadinstitute/dsde/rawls/entities/base/EntityProviderMetrics.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/entities/base/EntityProviderMetrics.scala
@@ -3,18 +3,17 @@ package org.broadinstitute.dsde.rawls.entities.base
 import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.common.{AttributeKey, Attributes}
 import io.opentelemetry.api.metrics.{DoubleHistogram, LongCounter}
-import io.opentelemetry.instrumentation.api.semconv.http.HttpClientMetrics
 import org.broadinstitute.dsde.rawls.metrics.RawlsInstrumented
 
 import scala.jdk.CollectionConverters._
 
 trait EntityProviderMetrics extends RawlsInstrumented {
 
-  protected val workbenchMetricBaseName: String
+  private val PREFIX = "rawls_entityprovider"
 
   private val FunctionKey = AttributeKey.stringKey("function")
-  private val ProviderNameKey = AttributeKey.stringKey("providerName")
-  private val ErrorClassKey = AttributeKey.stringKey("errorType")
+  private val ProviderNameKey = AttributeKey.stringKey("providername")
+  private val ErrorClassKey = AttributeKey.stringKey("errortype")
 
   private val BucketBoundaries =
     List[java.lang.Double](0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0).asJava
@@ -23,7 +22,7 @@ trait EntityProviderMetrics extends RawlsInstrumented {
 
   private def entityProviderFunctionLatency: DoubleHistogram =
     meter
-      .histogramBuilder(s"${workbenchMetricBaseName}_provider_function_latency")
+      .histogramBuilder(s"${PREFIX}_function_latency")
       .setDescription("Latency of entity provider function calls")
       .setUnit("ms")
       .setExplicitBucketBoundariesAdvice(BucketBoundaries)
@@ -31,7 +30,7 @@ trait EntityProviderMetrics extends RawlsInstrumented {
 
   private def entityProviderErrorCount: LongCounter =
     meter
-      .counterBuilder(s"${workbenchMetricBaseName}_provider_error_count")
+      .counterBuilder(s"${PREFIX}_error_count")
       .setDescription("Count of errors in entity provider functions")
       .setUnit("error")
       .build()
