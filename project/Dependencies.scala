a870bd8b22479e29940aa3b1b124cb27379808e4
David An <davidan@broadinstitute.org>
2025-07-28 09:19:23 -0400

    Revert "CORE-69: upgrade akka and akka-http (#3414)"
    This reverts commit 4c998531a29d7cb80172cad532b63eef5ec1be10.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  val akkaV = "2.10.5"
-  val akkaHttpV = "10.7.1"
+  val akkaV = "2.6.20"
+  val akkaHttpV = "10.2.10"
   val slickV = "3.6.1"
 
   val googleV = "2.0.0" // service-specific client libraries
