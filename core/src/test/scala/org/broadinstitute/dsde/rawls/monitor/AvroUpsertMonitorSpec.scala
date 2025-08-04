8098e6c90a9c2d43b1662e1921acb8ebcc1523c6
Bria Morgan <bmorgan@broadinstitute.org>
2025-08-04 10:41:27 -0400

    convert avroupsertmonitorspec

--- core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/AvroUpsertMonitorSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/monitor/AvroUpsertMonitorSpec.scala
@@ -4,10 +4,9 @@ import akka.actor.ActorSystem
 import akka.stream.scaladsl.Source
 import akka.testkit.TestKit
 import cats.effect.unsafe.implicits.global
-import org.broadinstitute.dsde.rawls.RawlsException
 import org.broadinstitute.dsde.rawls.dataaccess._
 import org.broadinstitute.dsde.rawls.dataaccess.slick.TestDriverComponent
-import org.broadinstitute.dsde.rawls.entities.EntityService
+import org.broadinstitute.dsde.rawls.entities.{EntityManager, EntityService}
 import org.broadinstitute.dsde.rawls.google.GooglePubSubDAO.MessageRequest
 import org.broadinstitute.dsde.rawls.google.MockGooglePubSubDAO
 import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{
@@ -15,28 +14,30 @@ import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{
   AttributeUpdateOperation,
   EntityUpdateDefinition
 }
+import org.broadinstitute.dsde.rawls.model.WorkspaceSettingConfig.CompactDataTablesConfig
 import org.broadinstitute.dsde.rawls.model.{
   AttributeEntityReference,
   AttributeFormat,
   AttributeName,
   AttributeString,
-  DataReferenceName,
+  CompactDataTablesSetting,
   Entity,
-  GoogleProjectId,
   ImportStatuses,
   RawlsRequestContext,
   TypedAttributeListSerializer,
   UserInfo,
-  WorkspaceName
+  WorkspaceName,
+  WorkspaceSettingTypes
 }
 import org.broadinstitute.dsde.rawls.openam.MockUserInfoDirectives
 import org.broadinstitute.dsde.rawls.webservice.ApiServiceSpec
+import org.broadinstitute.dsde.rawls.workspace.WorkspaceSettingRepository
 import org.broadinstitute.dsde.workbench.google2.GcsBlobName
 import org.broadinstitute.dsde.workbench.google2.mock.FakeGoogleStorageInterpreter
 import org.broadinstitute.dsde.workbench.model.google.GcsBucketName
 import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{times, verify, when}
+import org.mockito.Mockito.{doReturn, spy, times, verify, when}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.Eventually
 import org.scalatest.concurrent.PatienceConfiguration.{Interval, Timeout}
@@ -64,6 +65,7 @@ class AvroUpsertMonitorSpec(_system: ActorSystem)
   ) extends ApiServices
       with MockUserInfoDirectives {
     override val submissionMonitorsEnabled: Boolean = false
+
   }
 
   def withApiServices[T](dataSource: SlickDataSource)(testCode: TestApiService => T): T = {
@@ -140,12 +142,37 @@ class AvroUpsertMonitorSpec(_system: ActorSystem)
   def setUp(services: TestApiService) = {
     setUpPubSub(services)
 
+    val workspaceSettingRepository = new WorkspaceSettingRepository(slickDataSource)
+    val spyWorkspaceSettingRepository = spy(workspaceSettingRepository)
+
+    doReturn(Future.successful(Some(CompactDataTablesSetting(CompactDataTablesConfig(enabled = true)))))
+      .when(spyWorkspaceSettingRepository)
+      .getWorkspaceSettingOfType(
+        ArgumentMatchers.any[UUID](),
+        ArgumentMatchers.eq(WorkspaceSettingTypes.CompactDataTables)
+      )
+    val mockEntityManager = EntityManager.defaultEntityManager(
+      slickDataSource,
+      spyWorkspaceSettingRepository,
+      services.testConf.getBoolean("entityStatisticsCache.enabled"),
+      services.testConf.getDuration("entities.queryTimeout"),
+      workbenchMetricBaseName
+    )
+
+    val mockEntityServiceConstructor: RawlsRequestContext => EntityService = EntityService.constructor(
+      slickDataSource,
+      services.samDAO,
+      workbenchMetricBaseName,
+      mockEntityManager,
+      1000
+    )
+
     val mockCwdsDAO = new MockCwdsDAO()
 
     // Start the monitor
     system.actorOf(
       AvroUpsertMonitorSupervisor.props(
-        services.entityServiceConstructor,
+        mockEntityServiceConstructor,
         services.gcsDAO,
         services.samDAO,
         googleStorage,
@@ -299,7 +326,8 @@ class AvroUpsertMonitorSpec(_system: ActorSystem)
       // Check in db if entities are there
       withWorkspaceContext(testData.workspace) { context =>
         eventually(Timeout(scaled(timeout)), Interval(scaled(interval))) {
-          val entitiesOfType = runAndWait(entityQuery.UnitTestHelpers.listActiveEntitiesOfType(context, entityType))
+          val entitiesOfType =
+            runAndWait(compactEntityRepository.queries.listActiveEntitiesOfType(context.workspaceIdAsUUID, entityType))
           assertResult(upsertQuantity)(entitiesOfType.size)
           upsertRange(upsertQuantity) foreach { idx =>
             val name = s"avro-entity-$idx"
