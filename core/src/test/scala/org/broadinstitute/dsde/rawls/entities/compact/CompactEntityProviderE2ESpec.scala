6732767f057118ec177cabd600e9037e59db52f4
David An <davidan@broadinstitute.org>
2025-08-21 15:34:17 -0400

    handle  as a root expression in Quicksilver

--- core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderE2ESpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/entities/compact/CompactEntityProviderE2ESpec.scala
@@ -1,6 +1,7 @@
 package org.broadinstitute.dsde.rawls.entities.compact
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import akka.stream.scaladsl.{Sink, Source}
 import cromwell.client.model.{ToolInputParameter, ValueType}
@@ -1233,6 +1234,50 @@ class CompactEntityProviderE2ESpec extends TestDriverComponentWithFlatSpecAndMat
     entityInputs.inputResolutions.head.error shouldBe empty
     entityInputs.inputResolutions.head.inputName shouldBe "my-input-name"
     entityInputs.inputResolutions.head.value should contain(AttributeString(s"$typeUnderTest-002"))
+  }
+
+  it should "validate entity type even with `this` as a root expression" in withMinimalTestDatabase { _ =>
+    // save exemplar data
+    val typeUnderTest = "cat"
+    val fooAttribute: AttributeName = AttributeName.withDefaultNS("foo")
+    val exemplarDataWithCommonNames: Seq[Entity] =
+      Seq(
+        Entity(s"001", typeUnderTest, Map(fooAttribute -> AttributeString(s"$typeUnderTest-001"))),
+        Entity(s"002", typeUnderTest, Map(fooAttribute -> AttributeString(s"$typeUnderTest-002"))),
+        Entity(s"003", typeUnderTest, Map(fooAttribute -> AttributeString(s"$typeUnderTest-003")))
+      )
+    runAndWait(
+      compactEntityQuery.batchWriteEntities(minimalTestData.workspace.workspaceIdAsUUID,
+                                            exemplarDataWithCommonNames,
+                                            insertOnly = false
+      )
+    )
+
+    // get provider
+    val provider = defaultProvider()
+
+    // set up arguments for expression evaluation
+    val expression = Option("this") // <-- this is the root expression, important for this test
+    val entityType = Option(s"$typeUnderTest-doesnotmatch") // <-- this is the entity type, important for this test
+    val expressionEvaluationContext =
+      ExpressionEvaluationContext(entityType, Option("002"), expression, Option(typeUnderTest))
+
+    val toolInputParameter = new ToolInputParameter()
+      .name("my-input-name")
+      .valueType(new ValueType().typeName(ValueType.TypeNameEnum.STRING))
+    val processableInputs = Set(MethodInput(toolInputParameter, "this.foo"))
+    val gatherInputsResult = GatherInputsResult(processableInputs, Set(), Set(), Set())
+
+    val actualException =
+      intercept[RawlsExceptionWithErrorReport] {
+        Await
+          .result(provider.evaluateExpressions(expressionEvaluationContext, gatherInputsResult, Map()), atMost)
+          .toList
+      }
+
+    actualException.errorReport.statusCode should contain(StatusCodes.BadRequest)
+    actualException.errorReport.message should include("expects an entity of type")
+
   }
 
   // ====================================================================================================
