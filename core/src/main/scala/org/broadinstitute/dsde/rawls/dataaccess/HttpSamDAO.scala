98f66b109145fcb77db9d5d2654d32137226d77e
David An <davidan@broadinstitute.org>
2025-08-26 14:23:08 -0400

    use rawls admin permissions to delete pets from a v1 billing project

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpSamDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpSamDAO.scala
@@ -743,6 +743,18 @@ class HttpSamDAO(baseSamServiceURL: String,
         callback.future.map(_ => ())
       }
 
+    override def deletePetPerProject(userId: String,
+                                     googleProject: GoogleProjectId,
+                                     ctx: RawlsRequestContext
+    ): Future[Unit] =
+      retry(when401or5xx) { () =>
+        val callback = new SamApiCallback[Void]("deletePetPerProject")
+
+        adminApi(ctx).deletePetPerProjectAsync(userId, googleProject.value, callback)
+
+        callback.future.map(_ => ())
+      }
+
     override def userHasResourceTypeAdminPermission(resourceTypeName: SamResourceTypeName,
                                                     action: SamResourceAction,
                                                     ctx: RawlsRequestContext
@@ -753,6 +765,7 @@ class HttpSamDAO(baseSamServiceURL: String,
 
       callback.future.map(_.booleanValue())
     }
+
   }
 
   override def getStatus(): Future[SubsystemStatus] = {
