cb211b9a57a9bb41ac2106604e6fdf310bb3e1fd
broadbot <broadbot@broadinstitute.org>
2025-09-01 12:33:40 +0000

    CORE-69: Update azure-core-management from 1.18.1 to 1.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -113,7 +113,7 @@ object Dependencies {
   val circeYAML: ModuleID = "io.circe" %% "circe-yaml" % "1.15.0"
 
   val azureIdentity: ModuleID = "com.azure" % "azure-identity" % "1.16.3"
-  val azureCoreManagement: ModuleID = "com.azure" % "azure-core-management" % "1.18.1"
+  val azureCoreManagement: ModuleID = "com.azure" % "azure-core-management" % "1.19.0"
 
   def excludeOpenTelemetry = ExclusionRule("io.opentelemetry.instrumentation")
   def clientLibExclusions(m: ModuleID): ModuleID = m.excludeAll(excludeOpenTelemetry)
