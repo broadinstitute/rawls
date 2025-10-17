9797fc45b652eaf50dfeb0f2ffe5fcbdbdebcdd5
David An <davidan@broadinstitute.org>
2025-10-17 09:09:04 -0400

    scalafmt

--- core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/jobexec/SubmissionSpec.scala
@@ -938,7 +938,10 @@ class SubmissionSpec(_system: ActorSystem)
       val submissionData = checkSubmissionStatus(submissionsService, newSubmissionReport.submissionId)
       assert(submissionData.workflows.size == 1)
 
-      val subList = Await.result(submissionsService.listSubmissions(testData.wsName, testContext, Option.empty, Option.empty), Duration.Inf)
+      val subList =
+        Await.result(submissionsService.listSubmissions(testData.wsName, testContext, Option.empty, Option.empty),
+                     Duration.Inf
+        )
 
       val oneSub = subList.filter(s => s.submissionId == newSubmissionReport.submissionId)
       assert(oneSub.nonEmpty)
@@ -1289,7 +1292,10 @@ class SubmissionSpec(_system: ActorSystem)
     val submissionData = checkSubmissionStatus(submissionsService, newSubmissionReport.submissionId)
     assert(submissionData.workflows.size == 1)
 
-    val subList = Await.result(submissionsService.listSubmissions(testData.wsName, testContext, Option.empty, Option.empty), Duration.Inf)
+    val subList =
+      Await.result(submissionsService.listSubmissions(testData.wsName, testContext, Option.empty, Option.empty),
+                   Duration.Inf
+      )
 
     val oneSub = subList.filter(s => s.submissionId == newSubmissionReport.submissionId)
     assert(oneSub.nonEmpty)
