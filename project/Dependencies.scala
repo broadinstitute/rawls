e4a9cdb2afc44309ca9dfd88e2293e4cc1c515af
broadbot <broadbot@broadinstitute.org>
2025-11-01 12:19:16 +0000

    CORE-69: Update google-cloud-nio from 0.128.5 to 0.128.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -107,7 +107,7 @@ object Dependencies {
   val workbenchOauth2: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-oauth2" % workbenchOauth2V
   val workbenchOauth2Tests: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-oauth2" % workbenchOauth2V % "test" classifier "tests"
 
-  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.128.5" % "test"
+  val googleStorageLocal: ModuleID = "com.google.cloud" % "google-cloud-nio" % "0.128.7" % "test"
 
   val workbenchUtil: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-util" % s"0.10-${workbenchLibsHash}"
 
