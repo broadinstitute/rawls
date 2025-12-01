625a2f16407b6d926488d5085d3bf149f427b50a
broadbot <broadbot@broadinstitute.org>
2025-12-01 12:22:12 +0000

    CORE-69: Update terra-policy-client from 1.0.39-SNAPSHOT to 1.0.41-SNAPSHOT

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -135,7 +135,7 @@ object Dependencies {
   val terraCommonLib = tclExclusions(clientLibExclusions("bio.terra" % "terra-common-lib" % "1.1.65-SNAPSHOT" classifier "plain"))
   val sam: ModuleID = clientLibExclusions("org.broadinstitute.dsde.workbench" %% "sam-client" % "v0.0.423")
   val leonardo: ModuleID = "org.broadinstitute.dsde.workbench" % "leonardo-client_2.13" % "1.3.6-82c1f7d"
-  val policyService = clientLibExclusions("bio.terra" % "terra-policy-client" % "1.0.39-SNAPSHOT")
+  val policyService = clientLibExclusions("bio.terra" % "terra-policy-client" % "1.0.41-SNAPSHOT")
 
   // OpenTelemetry
   val openTelemetryInstrumentationVersion = "2.20.0"
