c2962e4d5a969b42c82736922074571f201566b9
David An <davidan@broadinstitute.org>
2026-01-27 11:33:32 -0500

    maintain set order

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/CompactEntityComponent.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/CompactEntityComponent.scala
@@ -712,41 +712,40 @@ class CompactEntityQuery(driverComponent: DriverComponent)
       DBIO.successful(Map.empty[String, Seq[CompactEntityRecord]])
     } else {
       val initialPointer = EntityPointer(startingEntityType, startingEntityName)
-      val initialGrouped: Map[String, Set[EntityPointer]] =
+      val initialGrouped: Map[String, Seq[EntityPointer]] =
         if (startingEntityType == rootEntityType)
-          Map(startingEntityName -> Set(initialPointer))
+          Map(startingEntityName -> Seq(initialPointer))
         else
-          Map("" -> Set(initialPointer))
+          Map("" -> Seq(initialPointer))
 
       def traverseGroups(
-        groupedEntities: Map[String, Set[EntityPointer]],
+        groupedEntities: Map[String, Seq[EntityPointer]],
         currentEntityType: String,
         remainingChain: Seq[String],
         grouped: Boolean
-      ): ReadAction[Map[String, Set[EntityPointer]]] =
+      ): ReadAction[Map[String, Seq[EntityPointer]]] =
         if (remainingChain.isEmpty) DBIO.successful(groupedEntities)
         else {
           val relation = remainingChain.head
           // For each group, get all referenced entities for this relation
           val nextGroupsF = DBIO
             .sequence(groupedEntities.map { case (groupKey, pointers) =>
-              getEntities(workspaceId, pointers).map { entities =>
+              getEntities(workspaceId, pointers.toSet).map { entities =>
                 val nextPointers = entities.flatMap { entityRecord =>
                   entityRecord.toEntity.attributes.get(AttributeName.fromDelimitedName(relation)) match {
-                    case Some(rel: AttributeEntityReference) => Seq(rel.toPointer)
-                    // TODO CTM-330: here, set entities are in the correct order.
+                    case Some(rel: AttributeEntityReference)      => Seq(rel.toPointer)
                     case Some(rels: AttributeEntityReferenceList) => rels.list.map(_.toPointer)
                     case _                                        => Seq.empty[EntityPointer]
                   }
-                }.toSet // TODO CTM-330: here, the set entities are changed from a Seq to a Set, and lose their order.
+                }
                 groupKey -> nextPointers
               }
             }.toSeq)
             .map(_.toMap)
 
           nextGroupsF.flatMap { nextGroups =>
-            val allNextPointers = nextGroups.values.flatten.toSet
-            if (allNextPointers.map(_.entityType).size > 1) {
+            val allNextPointers = nextGroups.values.flatten.toSeq
+            if (allNextPointers.map(_.entityType).distinct.size > 1) {
               DBIO.failed(
                 new RawlsExceptionWithErrorReport(
                   ErrorReport(
