0a7f369012cd0551b2e0a6b680dc38a552776383
lmcnatt <lucymcnatt@gmail.com>
2026-01-28 12:04:13 -0500

    added comment

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpGoogleServicesDAO.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/HttpGoogleServicesDAO.scala
@@ -469,6 +469,8 @@ class HttpGoogleServicesDAO(val clientSecrets: GoogleClientSecrets,
 
       // `getZones()` returns the zones as resource urls of form `https://www.googleapis.com/compute/v1/projects/project_id/zones/us-central1-b",
       // Hence split it by `/` and get last element of array to get the zone
+      // the filter is due to new AI zones in these regions are not accepted by GCP Batch
+      // so we exlude them explicitly, see CTM-343
       zonesAsResourceUrls.map(_.split("/").last).filterNot(zone => zone.matches(""".*-(ai\w*)$"""))
     } { case e =>
       throw new RawlsException(s"Something went wrong while retrieving zones for region `$region` under Google " +
