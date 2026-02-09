ccc263e963320e9518aa9afdc085cb60996c8225
David An <davidan@broadinstitute.org>
2026-02-09 12:48:35 -0500

    code comments

--- core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/QuicksilverMigrationMonitorSupport.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/monitor/QuicksilverMigrationMonitorSupport.scala
@@ -134,10 +134,15 @@ trait QuicksilverMigrationMonitorSupport extends LazyLogging {
   ): EntityCorrectionStatusType = {
     val attrStatuses = attributeStatuses.values.toSet
 
+    // when no attributes exist, use Correct; there is nothing that could be corrected
     if (attrStatuses.isEmpty) {
       EntityCorrectionStatus.Correct
+
+      // if any attribute was corrected, mark the entity as Corrected
     } else if (attrStatuses.contains(AttributeCorrectionStatus.Corrected)) {
       EntityCorrectionStatus.Corrected
+
+      // handle cases where all attributes have the same status
     } else if (attrStatuses == Set(AttributeCorrectionStatus.Correct)) {
       EntityCorrectionStatus.Correct
     } else if (attrStatuses == Set(AttributeCorrectionStatus.Reordered)) {
@@ -154,8 +159,18 @@ trait QuicksilverMigrationMonitorSupport extends LazyLogging {
       EntityCorrectionStatus.NotAList
     } else if (attrStatuses == Set(AttributeCorrectionStatus.TypeDifferent)) {
       EntityCorrectionStatus.TypeDifferent
+
+      // single-status sets were handled above. Now handle cases where attributes
+      // have multiple cases.
+      //
+      // if any attribute is correctable (e.g. "Reordered"), mark the entity as Mixed.
+      // Mixed entities will get processed by the correction monitor.
     } else if (attrStatuses.contains(AttributeCorrectionStatus.Reordered)) {
       EntityCorrectionStatus.Mixed
+
+      // We have multiple attribute statuses, but none of them is "Reordered".
+      // Mark the entity as MixedButNotCorrectable, which will not be processed by
+      // the correction monitor.
     } else {
       EntityCorrectionStatus.MixedButNotCorrectable
     }
