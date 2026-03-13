f599c7895ce1f67c55199e5adc31312c346161d9
Liz Baldo <lizbaldo@Liz-Baldo-H5267Y2XDX.local>
2026-03-13 15:25:53 -0400

    always send the workspace google project to the DRSHub request body

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/drs/DrsHubModel.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/drs/DrsHubModel.scala
@@ -2,7 +2,7 @@ package org.broadinstitute.dsde.rawls.dataaccess.drs
 
 import spray.json.RootJsonFormat
 
-case class DrsHubRequest(url: String, fields: Array[String])
+case class DrsHubRequest(url: String, fields: Array[String], userProject: Option[String])
 
 case class DrsHubAccessUrl(url: Option[String], headers: Option[Map[String, String]])
 case class DrsHubMinimalResponse(accessUrl: Option[DrsHubAccessUrl])
@@ -10,7 +10,7 @@ case class DrsHubMinimalResponse(accessUrl: Option[DrsHubAccessUrl])
 object DrsHubJsonSupport {
   import spray.json.DefaultJsonProtocol._
 
-  implicit val DrsHubRequestFormat: RootJsonFormat[DrsHubRequest] = jsonFormat2(DrsHubRequest)
+  implicit val DrsHubRequestFormat: RootJsonFormat[DrsHubRequest] = jsonFormat3(DrsHubRequest)
   implicit val DrsHubAccessUrlFormat: RootJsonFormat[DrsHubAccessUrl] = jsonFormat2(DrsHubAccessUrl)
   implicit val DrsHubV2ResponseFormat: RootJsonFormat[DrsHubMinimalResponse] = jsonFormat1(DrsHubMinimalResponse)
 }
