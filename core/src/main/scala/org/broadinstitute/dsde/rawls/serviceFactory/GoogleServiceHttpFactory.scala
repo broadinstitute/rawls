828ca5675efb9edfb83bd3326c241b23eb39df9b
David An <davidan@broadinstitute.org>
2026-07-27 15:50:16 -0400

    compatibility fix: http4s uses ember, not blaze

--- core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/GoogleServiceHttpFactory.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/serviceFactory/GoogleServiceHttpFactory.scala
@@ -5,23 +5,23 @@ import org.broadinstitute.dsde.rawls.config.RawlsConfigManager
 import org.broadinstitute.dsde.rawls.serviceFactory.DisabledServiceFactory.newDisabledService
 import org.broadinstitute.dsde.workbench.google2.{GoogleServiceHttp, NotificationCreaterConfig}
 import org.http4s.Uri
-import org.http4s.blaze.client.BlazeClientBuilder
-import org.typelevel.log4cats.StructuredLogger
+import org.http4s.ember.client.EmberClientBuilder
+import org.typelevel.log4cats.{LoggerFactory, StructuredLogger}
 
 import scala.concurrent.ExecutionContext
 
 object GoogleServiceHttpFactory {
-  def createGoogleServiceHttp[F[_]: Async](appConfigManager: RawlsConfigManager, executionContext: ExecutionContext)(
-    implicit
+  def createGoogleServiceHttp[F[_]: Async](appConfigManager: RawlsConfigManager)(implicit
     F: Sync[F] with Temporal[F],
-    logger: StructuredLogger[F]
+    logger: StructuredLogger[F],
+    loggerFactory: LoggerFactory[F]
   ): Resource[F, GoogleServiceHttp[F]] =
     appConfigManager.gcsConfig match {
       case Some(gcsConfig) =>
         val pathToCredentialJson = gcsConfig.getString("pathToCredentialJson")
         val googleApiUri = Uri.unsafeFromString(gcsConfig.getString("google-api-uri"))
         val metadataNotificationConfig = NotificationCreaterConfig(pathToCredentialJson, googleApiUri)
-        BlazeClientBuilder[F].withExecutionContext(executionContext).resource.flatMap { httpClient =>
+        EmberClientBuilder.default[F].build.flatMap { httpClient =>
           GoogleServiceHttp.withRetryAndLogging(httpClient, metadataNotificationConfig)
         }
       case None =>
